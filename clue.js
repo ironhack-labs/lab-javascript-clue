1af6ffd8873adeba0a9448c039d92e4691439830
Gon <gonzalo.orio@gmail.com>
2017-01-19 09:32:33 +0100

    modificicaon final

--- clue.js
+++ clue.js
@@ -65,46 +65,55 @@ var person = [];
     console.log(person);
 
 var rope = {
+    weapon: "rope",
     weight:"",
     colour:"",
     count: "",
 }
 var knife = {
+    weapon: "knife",
     weight:"",
     colour:"",
     count: "",
 }
 var candlestick= {
+    weapon: "candlestick",
     weight:"",
     colour:"",
     count: "",
 }
 var dumbbell = {
+    weapon: "dumbbell",
     weight:"",
     colour:"",
     count: "",
 }
 var poison = {
+    weapon: "poison",
     weight:"",
     colour:"",
     count: "",
 }
 var axe = {
+    weapon: "axe",
     weight:"",
     colour:"",
     count: "",
 }
 var bat = {
+    weapon: "bat",
     weight:"",
     colour:"",
     count: "",
 }
 var trophy = {
+    weapon: "trophy",
     weight:"",
     colour:"",
     count: "",
 }
 var pistol = {
+    weapon: "pistol",
     weight:"",
     colour:"",
     count: "",
@@ -116,63 +125,78 @@ var weapons = [];
 
 
 var dinningRoom = {
+    place:"dinningRoom",
     size:"",
     light:"",
 }
 
 var conservatory = {
+    place:"conservatory",
     size:"",
     light:"",
 }
 var kitchen = {
+    place:"kitchen",
     size:"",
     light:"",
 }
 var study = {
+    place:"study",
     size:"",
     light:"",
 }
 var library = {
+    place:"library",
     size:"",
     light:"",
 }
 var billiarRoom = {
+    place:"billiarRoom",
     size:"",
     light:"",
 }
 var lounge = {
+    place:"lounge",
     size:"",
     light:"",
 }
 var ballroom = {
+    place:"ballroom",
     size:"",
     light:"",
 }
 var hall = {
+    place:"hall",
     size:"",
     light:"",
 }
 var aSpa = {
+    place:"aSpa",
     size:"",
     light:"",
 }
 var livingRoom = {
+    place:"livingRoom",
     size:"",
     light:"",
 }
 var observatory = {
+    place:"observatory",
     size:"",
     light:"",
 }
 var theater = {
+    place:"theater",
     size:"",
     light:"",
 }
 var guestHouse = {
+    place:"guestHouse",
     size:"",
     light:"",
 }
 var aPatio = {
+    place:"aPatio",
     size:"",
     light:"",
 }
@@ -198,3 +222,5 @@ var aleatRoom = randomCharacter(rooms);
 console.log(aleatCharacter.firstName);
 console.log(aleatWeapon);
 console.log(aleatRoom);
+
+console.log("In this case, the de killer is "+aleatCharacter.firstName+" "+aleatCharacter.lastName+" with a "+aleatWeapon.weapon+" in the "+aleatRoom.place);
