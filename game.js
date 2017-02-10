d0aa724e1d204e06873e952c9fd6c40a560515ee
Alejandro <ajimenez1724@gmail.com>
2017-02-09 21:36:11 -0500

    almost there

---
+++ game.js
@@ -0,0 +1,131 @@
+
+// PEOPLE DATA STRUCTURE  IMPORTANT! connections = popularity
+
+var mrGreen = {
+  color: "green",
+  connections: "yes",
+  helps: "yes",
+  price: "yes",
+  money: "yes",
+  darkPast: "no"
+};
+
+var drOrchid = {
+  color: "white",
+  adopted: "by Mr. Boddy",
+  educatedPLace: "Switzeland",
+  expolsion: "because incident posion with daffodils",
+  dadProffesion: "biologist",
+  price: "yes",
+  helps: "no",
+  money: "no",
+  darkPast: "yes"
+};
+
+var mrPlum = {
+  color: "purple",
+  connections: "yes",
+  helps: "yes",
+  price: "no",
+  money: "yes",
+  darkPast: "no",
+};
+
+var msScarlet = {
+  color: "red",
+  connections: "yes",
+  helps: "yes",
+  price: "yes",
+  money: "yes",
+  darkPast: "yes"
+};
+
+var msPeacock = {
+  color: "blue",
+  connections: "yes",
+  helps: "yes",
+  price: "yes",
+  money: "yes",
+  darkPast: "no"
+};
+
+var msPeacock = {
+  color: "yellow",
+  connections: "yes",
+  helps: "no",
+  price: "yes",
+  money: "no",
+  darkPast: "no"
+};
+
+
+// WEAPONS DATA STRUCTURE
+
+var rope = {
+  weight: "6 pounds",
+  color: "blue",
+  power: "6",
+  suspects: ""
+};
+
+var knife = {
+  weight: "2 pounds",
+  color: "red",
+  power: "4",
+  suspects: ""
+};
+
+var candlestick = {
+  weight: "30 pounds",
+  color: "yellow",
+  power: "5",
+  suspects: ["msPeacock","msScarlet","mrPlump" ]
+};
+var dumbbell = {
+  weight: "80 pounds",
+  color: "grey",
+  power: "7",
+  suspects: "mrMustard"
+};
+
+var poison = {
+  weight: "1 pound",
+  color: "green",
+  power: "10",
+  suspects: "msOrchid"
+};
+
+var axe = {
+  weight: "50 pounds",
+  color: "silver",
+  power: "",
+  suspects: "8"
+};
+var bat = {
+  weight: "10 pounds",
+  color: "brown",
+  power: "3",
+  suspects: ""
+};
+
+var trophy = {
+  weight: "20 pounds",
+  color: "gold",
+  power: "2",
+  suspects: ["mrMustad","msScarlet"]
+};
+
+var pistol = {
+  weight: "25 pounds",
+  color: "black",
+  power: "9",
+  suspects: ""
+};
+
+
+  // Properties
+
+
+
+
+// ROOMS DATA STRUCTURE
