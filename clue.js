5127bef18ff23495fe294c5b7f95fead5728486b
mz_lovelyj <liljp064@gmail.com>
2017-02-10 18:05:37 -0500

    in progess

--- clue.js
+++ clue.js
@@ -1,55 +1,66 @@
-var mrGreen = {
-  [first name: "Jacob", Last name: "Green", color: "green"]
-};
+var rooms = [
+'Dining room','Conservatory','Kitchen','Study','Library','Billiard room','Lounge','Ballroom','Hall',
+'A spa','Living room','Observatory','Theater','Guest house','A patio']
 
-Var doctorOrchid ={
-  [first name: "Doctor", last name:"Orchid", color: "white"]
-}
-var mrPlum = {
-  First name: "victor", last name: "plum", color: "purple"]
-}
-var Kasandra = {
-  first name: "Kasandra", last name: "Scarlet", color: "red"]
-}
-var Eleanor = {
-  [first name: "Eleanor", last name: "Peacock", color:"blue"]
-}
-var jack = {
-  [first name: "jack", last name: "Mustard", color: "yellow"]
-}
+var weapons = [
+  Rope = { weight: '2 lbs.', color: 'brown', painLevel: 'Ouch'},
+  Knife = { weight: '1 lbs.', color: 'silver', painLevel: 'Really ouch'},
+  Candlestick = { weight: '0.5 lbs.', color: 'white', painLevel: 'Not really ouch'},
+  Dumbbell = { weight: '10 lbs.', color: 'silver', painLevel: 'You\'re Dead.'},
+  Poison = { weight: '3 lbs.', color: 'green', painLevel: 'You\'re also dead.'},
+  Axe = { weight: '0.8 lbs.', color: 'wood', painLevel: 'You lost an arm.'},
+  Bat = { weight: '1.2 lbs.', color: 'Light brown', painLevel: 'Very ouch'},
+  Trophy = { weight: '5 lbs.', color: 'Gold', painLevel: 'Kinda Ouch'},
+  Pistol = { weight: '1.3 lbs.', color: 'Black', painLevel: 'You\'re bleeding to death.'}
+]
 
-var weapons = {"Rope",
-"Knife",
-"Candlestick",
-"Dumbbell",
-"Poison",
-"Axe",
-"Bat",
-"Trophy",
-"Pistol"}
+var suspects = [
+  green = {firstName: 'Jacob',
+  lastName: 'Green',
+  color: 'Green',
+  descriptions: 'He has a lot of connections and is always willing to help people out -- for a price.'
+  },
 
-var rooms =[
-"Dining room",
-"Conservatory",
-"Kitchen",
-"Study",
-"Library",
-"Billiard room",
-"Lounge",
-"Ballroom",
-"Hall",
-"A spa",
-"Living room",
-"Observatory",
-"Theater",
-"Guest house",
-"A patio"]
+  orchid =  {firstName: 'Doctor',
+  lastName: 'Orchid',
+  color: 'White',
+  description: 'She is the adopted daugther of Mr. Boddy She was privately educated in Switzerland until her expulsion after an incident involving daffodils resulted in a near-fatal poisoning',
+  occupation: 'Biologist with a PhD in plant toxicology'
+  },
 
-var randomnumber = Math.random();
-randomnumber = randomnumber * rooms.length;
-randomnumber = Math.floor(randomnumber);
+  plum = {firstName: 'Victor',
+  lastName: 'Plum',
+  color: 'Purple',
+  description: 'A Billionaire who is embracing his new popularity.',
+  occupation: 'Video Game Designer' },
 
-console.log(rooms[randomnumber]);
+  scarlet = {firstName: 'Kassandra',
+  lastName: 'Scarlet',
+  color: 'Red',
+  description: 'Her past haunts her.',
+  occupation: 'A list Movie Start' },
+
+  peacock = {firstName: 'Eleanor',
+  lastName: 'Peacock',
+  color: 'Blue',
+  description: 'From a wealthy family and uses her status and money to win popularity',
+  occupation: 'Lazy' },
+
+  mustard = {firstName: 'Jack',
+  lastName: 'Mustard',
+  color: 'Yellow',
+  description: 'Tries to get by on his former glory.',
+  occupation: 'Former Football Player' },
+]
 
 
-return rooms [randomnumber];
+//var randomSuspect = Math.floor(Math.random() * suspects.length);
+//var randomRoom = Math.floor(Math.random() * rooms.length);
+//var randomWeapons = Math.floor(Math.random() * weapons.length);
+
+//function pickfromcards() {
+  //console.log(suspects[randomSuspect]);
+  //console.log(rooms[randomRoom]);
+  //console.log(weapons[randomWeapons]);}
+
+//pickfromcards();
