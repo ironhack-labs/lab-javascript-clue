51d5c1781d3fa48b683345d61efb7de2c1ba4183
Yana Ustsinovich <yustinovich@icloud.com>
2017-02-09 21:41:11 -0500

    Initial structure Clue

---
+++ Clue.js
@@ -0,0 +1,21 @@
+
+var suspects = [
+  {firstName: "Jacob", lastName: "Green", occupation: "unknown", age: 30, color: "green",
+  description: 'He has a lot of connections and is always willing to help people out -- for a price.'},
+  {firstName: "Doctor", lastName: "Orchid", occupation: "scientist", age: 40, color: "white",
+  description: 'he was privately educated in Switzerland until her expulsion after an incident involving daffodils resulted in a near-fatal poisoning.'}
+];
+
+var weapons = [
+  {weaponName: "Rope", weight: 2, color: "beige", damage: 5},
+  {weaponName: "Knife", weight: 1, color: "silver", damage: 30},
+  {weaponName: "Pistol", weight: 4, color: "black", damage: 100}
+];
+
+var rooms = [
+  {roomName: "Dining room", size: 400, color: "white", secretEntrance: true},
+  {roomName: "Library", size: 500, color: "mahogany", secretEntrance: true},
+  {roomName: "A spa", size: 200, color: "blue", secretEntrance: false}
+];
+
+var cards = [suspects, weapons, rooms];
