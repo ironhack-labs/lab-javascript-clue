c6a2439940877a8b753244725b782f4f4312232a
Alice Guan <aliguan726@gmail.com>
2017-02-11 07:53:13 -0500

    redid assignment. done

--- clue.js
+++ clue.js
@@ -1,14 +1,16 @@
 var suspects = [
-  { firstName: 'Jacob',
+  {firstName: 'Jacob',
   lastName: 'Green',
   color: 'Green',
-  descriptions: 'He has a lot of connections and is always willing to help people out -- for a price.' },
+  descriptions: 'He has a lot of connections and is always willing to help people out -- for a price.'
+  },
 
   {firstName: 'Doctor',
   lastName: 'Orchid',
   color: 'White',
   description: 'She is the adopted daugther of Mr. Boddy She was privately educated in Switzerland until her expulsion after an incident involving daffodils resulted in a near-fatal poisoning',
-  occupation: 'Biologist with a PhD in plant toxicology' },
+  occupation: 'Biologist with a PhD in plant toxicology'
+  },
 
   {firstName: 'Victor',
   lastName: 'Plum',
@@ -35,7 +37,7 @@ var suspects = [
   occupation: 'Former Football Player' },
 ]
 
-var rooms = [ 'Dining room', 'Conservatory', 'Kitchen', 'Study', 'Library', 'Billiard room', 'Lounge', 'Ballroom', 'Hall', 'A spa', 'Living room', 'Observatory', 'Theater', 'Guest house', 'A patio.' ];
+var rooms = [ 'Dining room', 'Conservatory', 'Kitchen', 'Study', 'Library', 'Billiard room', 'Lounge', 'Ballroom', 'Hall', 'A spa', 'Living room', 'Observatory', 'Theater', 'Guest house', 'patio' ];
 
 var weapons = [
   { name: 'Rope', weight: '2 lbs.', color: 'brown', painLevel: 'Ouch'},
@@ -50,14 +52,35 @@ var weapons = [
 ]
 
 
+var cardStack = [suspects, rooms, weapons];
+var selectedElements = [];
+var dead = [];
 
-var randomSuspect = Math.floor(Math.random() * suspects.length);
-var randomRoom = Math.floor(Math.random() * rooms.length);
-var randomWeapons = Math.floor(Math.random() * weapons.length);
+//Select element from card stack
+function pickElements() {
+  for(i = 0; i < cardStack.length; i++) {
+    var element = Math.floor(Math.random() * cardStack[i].length);
+    selectedElements.push(cardStack[i][element]);
+  }
 
-
-function reveal() {
-  console.log('OH NO! ' + suspects[randomSuspect].firstName + ' ' + suspects[randomSuspect].lastName + ' was killed in the ' + rooms[randomRoom] + ' with a ' + weapons[randomWeapons].name + '!');
 }
 
+pickElements();
+
+function reveal() {
+var ask = prompt('Would you like to reveal the killer? Yes or no?');
+  //Selecting a victim and making sure victim and killer is not the same.
+  var victim = Math.floor(Math.random() * suspects.length);
+  if (suspects[victim] === selectedElements[0] )  {
+    console.log('There was a suicide. ' + suspects[victim].firstName + ' ' + suspects[victim].lastName + ' killed theirself. Please reinvestigate case.');
+  } else {
+    dead.push(suspects[victim].firstName, suspects[victim].lastName);
+  }
+  //Making sure user wants to reveal killer and will only display if killer & victim are not the same.
+  if (ask.toLowerCase() === 'yes' && suspects[victim] != selectedElements[0] ) {
+  console.log('OH NO! ' + selectedElements[0].firstName + ' ' + selectedElements[0].lastName + ' killed ' + dead[0] + ' ' + dead[1] + ' in the ' + selectedElements[1] + ' with a ' + selectedElements[2].name + '!');
+  } else {
+    console.log('Hmmmm....maybe you\'re the killer!');
+  }
+}
 reveal();
