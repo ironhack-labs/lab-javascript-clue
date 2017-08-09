41a64b9e4c0af96bdbacaaca4bff1959ede345c7
allanac <acole006@fiu.edu>
2017-08-09 16:44:37 -0400

    done

--- starter-code/clue.js
+++ starter-code/clue.js
@@ -1,92 +1,101 @@
-/*
 
----- Characters ----
+var game = {
 
-  Mr Green
-  first name: Jacob
-  last name:  Green
-  color: green
-  description: He has a lot of connections
-  age: 45
-  image: https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg
-  occupation: Entrepreneur
+  characters: [
+              {alias: "Mr Green",
+              firstName:"Jacob",
+              lastName:  "Green",
+              color: "green",
+              description: "He has a lot of connections",
+              age: 45,
+              image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+              occupation: "Entrepreneur"},
 
-  Dr Orchid
-  first name: Doctor
-  last name: Orchid
-  color: white
-  description: PhD in plant toxicology. Adopted daughter of Mr. Boddy
-  age: 26
-  image: http://www.radiotimes.com/uploads/images/Original/111967.jpg
-  occupation: Scientist
+              {alias: "Dr Orchid",
+              firstName: "Doctor",
+              lastName: "Orchid",
+              color: "white",
+              description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+              age: 26,
+              image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+              occupation: "Scientist"},
 
-  Prof Plum
-  first name: Victor
-  last name: Plum
-  color: purple
-  description: Billionare video game designer
-  age: 22
-  image: https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg
-  occupation: Designer
+              {alias: "Prof Plum",
+              firstName: "Victor",
+              lastName: "Plum",
+              color: "purple",
+              description: "Billionare video game designer",
+              age: 22,
+              image: "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
+              occupation: "Designer"},
 
-  Miss Scarlet
-  first name: Kasandra
-  last name: Scarlet
-  color: red
-  description: She is an A-list movie star with a dark past,
-  age: 31,
-  image: https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg,
-  occupation: Actor
+              {alias: "Miss Scarlet",
+              firstName: "Kasandra",
+              lastName: "Scarlet",
+              color: "red",
+              description: "She is an A-list movie star with a dark past",
+              age: 31,
+              image: "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",
+              occupation: "Actor"},
 
- Mrs Peacock
-  first name: Eleanor
-  last name: Peacock
-  color: blue
-  description: She is from a wealthy family and uses her status and money to earn popularity
-  age: 36
-  image: https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg
-  occupation: Socialité
+              {alias: "Mrs Peacock",
+              firstName: "Eleanor",
+              lastName: "Peacock",
+              color: "blue",
+              description: "She is from a wealthy family and uses her status and money to earn popularity",
+              age: 36,
+              image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+              occupation: "Socialité"},
 
- Mr Mustard
-  first name: Jack
-  last name: Mustard
-  color: yellow
-  description: He is a former football player who tries to get by on his former glory
-  age: 62
-  image: https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg
-  occupation: Retired Football player
+              {alias: "Mr Mustard",
+              firstName: "Jack",
+              lastName: "Mustard",
+              color: "yellow",
+              description: "He is a former football player who tries to get by on his former glory",
+              age: 62,
+              image: "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",
+              occupation: "Retired Football player"}
+],
 
----- Weapons ----
+weapons: [
 
-Rope - Weight: 10
-Knife - Weight: 8
-Candlestick - Weight: 2
-Dumbbell - Weight: 30
-Poison - Weight: 2
-Axe - Weight: 15
-Bat - Weight: 13
-Trophy - Weight: 25
-Pistol - Weight: 20
+{   rope: {weight: 10},
+    knife: {weight: 8},
+    candlestick: {weight: 2},
+    dumbbell:  {weight: 30},
+    poison:  {weight: 2},
+    axe : {weight: 15},
+    bat:  {weight: 13},
+    trophy:  {weight: 25},
+    pistol:  {weight: 20}
+  }
+],
 
+Rooms: [
+        "Dinning Room",
+        "Conservatory",
+        "Kitchen",
+        "Study",
+        "Library",
+        "Billiard Room",
+        "Lounge",
+        "Ballroom",
+        "Hall",
+        "Spa",
+        "Living Room",
+        "Observatory",
+        "Theater",
+        "Guest House",
+        "Patio"
+      ]
+}
 
----- Rooms ----
-Dinning Room
-Conservatory
-Kitchen
-Study
-Library
-Billiard Room
-Lounge
-Ballroom
-Hall
-Spa
-Living Room
-Observatory
-Theater
-Guest House
-Patio
+// Generate a random number
 
-*/
+var randomNumberBetween0and2 = Math.floor(Math.random() * 3);
+
+//function randomWholeNumber ()
+
 
 function random_selector(array) {
 
@@ -102,3 +111,5 @@ function reveal_mistery(mistery) {
 
 var mistery_envelope = pick_mistery();
 reveal_mistery(mistery_envelope);
+
+console.log(game);
