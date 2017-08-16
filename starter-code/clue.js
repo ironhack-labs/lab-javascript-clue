9c2f45cd68dab4955c0073bfd03afc154f3b2712
ritorrehuelamo <torrehuelamo@gmail.com>
2017-08-16 17:09:49 +0200

    pick_mistery()

--- starter-code/clue.js
+++ starter-code/clue.js
@@ -89,98 +89,111 @@ Patio
 */
 
 // Objetos Personas
-//
-var characters = [{
-  mrGreen: [{
-    firstName: "Jacob",
-    lastName: "Green",
-    color: "green",
-    description: "He has a lot of connections",
-    age: 45,
-    image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
-    occupation: "Entrepreneur"
-  }],
-  drOrchid: [{
-    firstName: "Doctor",
-    lastName: "Orchid",
-    color: "white",
-    description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
-    age: 26,
-    image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
-    occupation: "Scientist"
-  }],
-  profPlum: [{
-    firstName: "Victor",
-    lastName: "Plum",
-    color: "purple",
-    description: "Billionare video game designer",
-    age: 22,
-    image: "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
-    occupation: "Designer"
-  }],
-  msScarlett: [{
-    firstName: "Kasandra",
-    lastName: "Scarlet",
-    color: "red",
-    description: "She is an A-list movie star with a dark past",
-    age: 31,
-    image: "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",
-    occupation: "Actor"
-  }],
-  mrsPeacock: [{
-    firstName: "Eleanor",
-    lastName: "Peacock",
-    color: "blue",
-    description: "She is from a wealthy family and uses her status and money to earn popularity",
-    age: 36,
-    image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
-    occupation: "Socialité"
-  }],
-  mrMustard: [{
-    firstName: "Jack",
-    lastName: "Mustard",
-    color: "yellow",
-    description: "He is a former football player who tries to get by on his former glory",
-    age: 62,
-    image: "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",
-    occupation: "Retired Football player"
 
-  }]
-}];
+var characters = {
+  character: [{
+      name: "mrGreen",
+      firstName: "Jacob",
+      lastName: "Green",
+      color: "green",
+      description: "He has a lot of connections",
+      age: 45,
+      image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+      occupation: "Entrepreneur"
+    },
+    {
+      name: "drOrchid",
+      firstName: "Doctor",
+      lastName: "Orchid",
+      color: "white",
+      description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+      age: 26,
+      image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+      occupation: "Scientist"
+    },
+    {
+      name: "profPlum",
+      firstName: "Victor",
+      lastName: "Plum",
+      color: "purple",
+      description: "Billionare video game designer",
+      age: 22,
+      image: "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
+      occupation: "Designer"
+    },
+    {
+      name: "msScarlett",
+      firstName: "Kasandra",
+      lastName: "Scarlet",
+      color: "red",
+      description: "She is an A-list movie star with a dark past",
+      age: 31,
+      image: "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",
+      occupation: "Actor"
+    },
+    {
+      name: "mrsPeacock",
+      firstName: "Eleanor",
+      lastName: "Peacock",
+      color: "blue",
+      description: "She is from a wealthy family and uses her status and money to earn popularity",
+      age: 36,
+      image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+      occupation: "Socialité"
+    },
+    {
+      name: "mrMustard",
+      firstName: "Jack",
+      lastName: "Mustard",
+      color: "yellow",
+      description: "He is a former football player who tries to get by on his former glory",
+      age: 62,
+      image: "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",
+      occupation: "Retired Football player"
+    }
+  ]
+}
 
 // Objetos Armas
 
-
-
-var weapons = {
-  rope: [{
-    weight: 10
-  }],
-  knife: [{
+var weapons = {weapon: [
+  {
+    name: "rope",
     weight: 8
-  }],
-  candlestick: [{
+  },
+  {
+    name: "knife",
+    weight: 8
+  },
+  {
+    name: "candlestick",
     weight: 2
-  }],
-  dumbbell: [{
+  },
+  {
+    name: "dumbbell",
     weight: 30
-  }],
-  poison: [{
+  },
+  {
+    name: "poison",
     weight: 2
-  }],
-  axe: [{
+  },
+  {
+    name: "axe",
     weight: 15
-  }],
-  bat: [{
+  },
+  {
+    name: "bat",
     weight: 13
-  }],
-  trophy: [{
+  },
+  {
+    name: "trophy",
     weight: 25
-  }],
-  pistol: [{
+  },
+  {
+    name: "pistol",
     weight: 20
-  }]
-};
+  }
+]}
 
 // Objetos Habitaciones
 
@@ -211,13 +224,18 @@ function random_selector(array) {
 }
 
 function pick_mistery() {
-  var mistery =[];
-random_selector(characters)
+  var mistery = [];
+  mistery.push(random_selector(characters.character));
+  mistery.push(random_selector(weapons.weapon));
+  mistery.push(random_selector(rooms));
+  return mistery;
 }
 
 function reveal_mistery(mistery) {
 
 }
+
+// console.log(pick_mistery())
 
 var mistery_envelope = pick_mistery();
 reveal_mistery(mistery_envelope);
