f2d5dd0ae52e1182b7e225d77a86fae4fe5e36d2
ritorrehuelamo <torrehuelamo@gmail.com>
2017-08-16 13:06:02 +0200

    PP Activityç

--- starter-code/clue.js
+++ starter-code/clue.js
@@ -88,12 +88,131 @@ Patio
 
 */
 
-function random_selector(array) {
+// Objetos Personas
+//
+var characters = [{
+  mrGreen: [{
+    firstName: "Jacob",
+    lastName: "Green",
+    color: "green",
+    description: "He has a lot of connections",
+    age: 45,
+    image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+    occupation: "Entrepreneur"
+  }],
+  drOrchid: [{
+    firstName: "Doctor",
+    lastName: "Orchid",
+    color: "white",
+    description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+    age: 26,
+    image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+    occupation: "Scientist"
+  }],
+  profPlum: [{
+    firstName: "Victor",
+    lastName: "Plum",
+    color: "purple",
+    description: "Billionare video game designer",
+    age: 22,
+    image: "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
+    occupation: "Designer"
+  }],
+  msScarlett: [{
+    firstName: "Kasandra",
+    lastName: "Scarlet",
+    color: "red",
+    description: "She is an A-list movie star with a dark past",
+    age: 31,
+    image: "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",
+    occupation: "Actor"
+  }],
+  mrsPeacock: [{
+    firstName: "Eleanor",
+    lastName: "Peacock",
+    color: "blue",
+    description: "She is from a wealthy family and uses her status and money to earn popularity",
+    age: 36,
+    image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+    occupation: "Socialité"
+  }],
+  mrMustard: [{
+    firstName: "Jack",
+    lastName: "Mustard",
+    color: "yellow",
+    description: "He is a former football player who tries to get by on his former glory",
+    age: 62,
+    image: "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",
+    occupation: "Retired Football player"
 
+  }]
+}];
+
+// Objetos Armas
+
+
+
+var weapons = {
+  rope: [{
+    weight: 10
+  }],
+  knife: [{
+    weight: 8
+  }],
+  candlestick: [{
+    weight: 2
+  }],
+  dumbbell: [{
+    weight: 30
+  }],
+  poison: [{
+    weight: 2
+  }],
+  axe: [{
+    weight: 15
+  }],
+  bat: [{
+    weight: 13
+  }],
+  trophy: [{
+    weight: 25
+  }],
+  pistol: [{
+    weight: 20
+  }]
+};
+
+// Objetos Habitaciones
+
+var rooms = [
+  "Dinning Room",
+  "Conservatory",
+  "Kitchen",
+  "Study",
+  "Library",
+  "Billiard Room",
+  "Lounge",
+  "Ballroom",
+  "Hall",
+  "Spa",
+  "Living Room",
+  "Observatory",
+  "Theater",
+  "Guest House",
+  "Patio"
+];
+
+
+
+function random_selector(array) {
+  var randomNumber = parseInt(Math.random() * array.length);
+  var card = array[randomNumber];
+  return card;
 }
 
 function pick_mistery() {
-
+  var mistery =[];
+random_selector(characters)
 }
 
 function reveal_mistery(mistery) {
