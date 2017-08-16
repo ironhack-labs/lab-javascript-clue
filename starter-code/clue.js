9103c427b4a5a72f8a56601ffb338ec6d21f7d02
Manuel del Pino Lucena <manueldelpinolucena@gmail.com>
2017-08-16 13:06:48 +0200

    otro

--- starter-code/clue.js
+++ starter-code/clue.js
@@ -1,104 +1,147 @@
-/*
+// Sospechosos
 
----- Characters ----
+    var MrGreen ={
+      firstName: "Jacob",
+      lastName: "Green",
+      color: "green",
+      description: "He has a lot of connections",
+      age: 45,
+      image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+      occupation: "Entrepreneur"
+  }
 
-  Mr Green
-  first name: Jacob
-  last name:  Green
-  color: green
-  description: He has a lot of connections
-  age: 45
-  image: https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg
-  occupation: Entrepreneur
+    var DrOrchid={
+      firstName: "Doctor",
+      lastName: "Orchid",
+      color: "white",
+      description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+      age: 26,
+      image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+      occupation: "Scientist"
+    }
 
-  Dr Orchid
-  first name: Doctor
-  last name: Orchid
-  color: white
-  description: PhD in plant toxicology. Adopted daughter of Mr. Boddy
-  age: 26
-  image: http://www.radiotimes.com/uploads/images/Original/111967.jpg
-  occupation: Scientist
+    var ProfPlum={
+      firstName: "Victor",
+      lastName: "Plum",
+      color: "purple",
+      description: "Billionare video game designer",
+      age: 22,
+      image: "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
+      occupation: "Designer"
+    }
 
-  Prof Plum
-  first name: Victor
-  last name: Plum
-  color: purple
-  description: Billionare video game designer
-  age: 22
-  image: https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg
-  occupation: Designer
+    var MissScarlet={
+         firstName: "Kasandra",
+         lastName: "Scarlet",
+         color: "red",
+         description: "She is an A-list movie star with a dark past",
+         age: 31,
+         image: "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",
+        occupation: "Actor"
+     }
 
-  Miss Scarlet
-  first name: Kasandra
-  last name: Scarlet
-  color: red
-  description: She is an A-list movie star with a dark past,
-  age: 31,
-  image: https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg,
-  occupation: Actor
+    var MrsPeacock={
+          firstName: "Eleanor",
+          lastName: "Peacock",
+          color: "blue",
+          description: "She is from a wealthy family and uses her status and money to earn popularity",
+          age: 36,
+          image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+          occupation: "Socialité",
+      }
 
- Mrs Peacock
-  first name: Eleanor
-  last name: Peacock
-  color: blue
-  description: She is from a wealthy family and uses her status and money to earn popularity
-  age: 36
-  image: https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg
-  occupation: Socialité
+    var MrMustard={
+           firstName: "Jack",
+           lastName: "Mustard",
+           color: "yellow",
+           description: "He is a former football player who tries to get by on his former glory",
+           age: 62,
+           image: "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",
+           occupation: "Retired Football player"
+       }
 
- Mr Mustard
-  first name: Jack
-  last name: Mustard
-  color: yellow
-  description: He is a former football player who tries to get by on his former glory
-  age: 62
-  image: https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg
-  occupation: Retired Football player
+  var suspects=[MrGreen, DrOrchid, ProfPlum, MissScarlet, MrsPeacock, MrMustard]
 
----- Weapons ----
+//Armas
 
-Rope - Weight: 10
-Knife - Weight: 8
-Candlestick - Weight: 2
-Dumbbell - Weight: 30
-Poison - Weight: 2
-Axe - Weight: 15
-Bat - Weight: 13
-Trophy - Weight: 25
-Pistol - Weight: 20
+var Rope={
+    name: "Rope",
+    weight: 10
+}
+var Knife={
+    name: "Knife",
+    weight: 8
+}
+var Candlestick={
+    name: "Candlestick",
+    weight: 2
+}
+var Dumbbell={
+    name: "Dumbbell",
+    weight: 30
+}
+var Poison={
+    name: "Poison",
+    weight: 2
+}
+var Axe={
+    name: "Axe",
+    weight: 15
+}
+var Bat={
+    name: "Bat",
+    weight: 13
+}
+var Trophy={
+    name: "Trophy",
+    weight: 25
+}
+var Pistol={
+    name: "Pistol",
+    weight: 20
+}
+
+var weapons = [Rope, Knife, Candlestick, Dumbbell, Poison, Axe, Bat, Trophy, Pistol]
 
 
----- Rooms ----
-Dinning Room
-Conservatory
-Kitchen
-Study
-Library
-Billiard Room
-Lounge
-Ballroom
-Hall
-Spa
-Living Room
-Observatory
-Theater
-Guest House
-Patio
+//Habitaciones
 
-*/
+var habitaciones=[
+    "Dinning Room",
+    "Conservatory",
+    "Kitchen",
+    "Study",
+    "Library",
+    "Billiard Room",
+    "Lounge",
+    "Ballroom",
+    "Hall",
+    "Spa",
+    "Living Room",
+    "Observatory",
+    "Theater",
+    "Guest House",
+    "Patio"]
+
+//Funciones
 
 function random_selector(array) {
-
+      var numAleatorio= Math.floor(Math.random()*(array.length));
+      if(numAleatorio == array.length){
+        numAleatorio = array.length-1;
+      }
+      return numAleatorio
 }
 
-function pick_mistery() {
+var mistery = []
 
+function pick_mistery(array) {
+  mistery[0] = random_selector(suspects)
+  mistery[1] = random_selector(weapons)
+  mistery[2] = random_selector(habitaciones)
 }
+console.log("El asesino es: " + habitaciones[random_selector(habitaciones)]);
 
-function reveal_mistery(mistery) {
+pick_mistery(mistery)
 
-}
-
-var mistery_envelope = pick_mistery();
-reveal_mistery(mistery_envelope);
+console.log(mistery)
