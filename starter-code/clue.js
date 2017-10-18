534a9721afd7af419f2f3ae6a045ae8511782bf8
sdediego <sergio.dediego@outlook.com>
2017-10-18 13:18:46 +0200

    Iteracion 2

--- starter-code/clue.js
+++ starter-code/clue.js
@@ -88,12 +88,120 @@ Patio
 
 */
 
-function random_selector(array) {
+// Suspects
+var mrGreen = {
+    firstName: "Jacob",
+    lastName:  "Green",
+    color: "green",
+    description: "He has a lot of connections",
+    age: 45,
+    image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+    occupation: "Entrepreneur",
+};
 
+var drOrchid = {
+    firstName: "Doctor",
+    lastName: "Orchid",
+    color: "white",
+    description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+    age: 26,
+    image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+    occupation: "Scientist",
+};
+
+var profPlum = {
+    firstName: "Victor",
+    lastName: "Plum",
+    color: "purple",
+    description: "Billionare video game designer",
+    age: 22,
+    image: "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
+    occupation: "Designer",
+};
+
+var missScarlet = {
+    firstName: "Kasandra",
+    lastName: "Scarlet",
+    color: "red",
+    description: "She is an A-list movie star with a dark past",
+    age: 31,
+    image: "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",
+    occupation: "Actor",
+};
+
+var mrsPeacock = {
+ firstName: "Eleanor",
+ lastName: "Peacock",
+ color: "blue",
+ description: "She is from a wealthy family and uses her status and money to earn popularity",
+ age: 36,
+ image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+ occupation: "Socialité",
+};
+
+var mrMustard = {
+ firstName: "Jack",
+ lastName: "Mustard",
+ color: "yellow",
+ description: "He is a former football player who tries to get by on his former glory",
+ age: 62,
+ image: "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",
+ occupation: "Retired Football player",
+};
+
+var suspects = [
+        mrGreen,
+        drOrchid,
+        profPlum,
+        missScarlet,
+        mrsPeacock,
+        mrMustard,
+];
+
+// Rooms
+var rooms = [
+    "Dinning Room",
+    "Conservatory",
+    "Kitchen",
+    "Study",
+    "Library",
+    "Billiard Room",
+    "Lounge",
+    "Ballroom",
+    "Hall",
+    "Spa",
+    "Living Room",
+    "Observatory",
+    "Theater",
+    "Guest House",
+    "Patio",
+];
+
+// Weapons
+var weapons = [
+    {name: "rope", weight: 10},
+    {name: "knife", weight: 8},
+    {name: "candlestick", weight: 2},
+    {name: "dumbbell", weight: 30},
+    {name: "namepoison", weight: 2},
+    {name: "axe", weight: 15},
+    {name: "bat", weight: 13},
+    {name: "trophy", weight: 25},
+    {name: "pistol", weight: 20},
+];
+
+function random_selector(array) {
+    var randomIndex = Math.floor(Math.random() * (array.length - 1));
+    return array[randomIndex];
 }
 
 function pick_mistery() {
-
+    var stacks = [suspects, rooms, weapons];
+    for (var i = 0; i < stacks.length; i++) {
+        var cards = [];
+        var card = random_selector(stacks[i]);
+        cards.push(card);
+    }
 }
 
 function reveal_mistery(mistery) {
