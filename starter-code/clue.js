d0b7eab10817c709a1da5da9989a1c1bc2efe2a9
Alberto Rivera <alberto.rivera.merida@gmail.com>
2017-10-18 16:12:09 +0200

    Cluedo - Done

--- starter-code/clue.js
+++ starter-code/clue.js
@@ -1,10 +1,8 @@
-
-
 //---- Characters ----
 
 var mrGreen = {
   first_name: "Jacob",
-  last_name:  "Green",
+  last_name: "Green",
   color: "green",
   description: "He has a lot of connections",
   age: "45",
@@ -12,120 +10,171 @@ var mrGreen = {
   occupation: "Entrepreneur"
 };
 
-  var drOrchid ={
-    first_name: "Doctor",
-    last_name:"Orchid",
-    color: "white",
-    description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
-    age: 26,
-    image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
-    occupation: "Scientist"};
+var drOrchid = {
+  first_name: "Doctor",
+  last_name: "Orchid",
+  color: "white",
+  description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+  age: 26,
+  image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+  occupation: "Scientist"
+};
 
-  var profPlum ={
+var profPlum = {
   first_name: "Victor",
   last_name: "Plum",
   color: "purple",
   description: "Billionare video game designer",
   age: 22,
   image: "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
-  occupation: "Designer"};
+  occupation: "Designer"
+};
 
-  var missScarlet={
+var missScarlet = {
   first_name: "Kasandra",
   last_name: "Scarlet",
   color: "red",
   description: "She is an A-list movie star with a dark past",
   age: 31,
   image: "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",
-  occupation: "Actor"};
+  occupation: "Actor"
+};
 
- var mrsPeacock={
+var mrsPeacock = {
   first_name: "Eleanor",
   last_name: "Peacock",
   color: "blue",
   description: "She is from a wealthy family and uses her status and money to earn popularity",
   age: 36,
   image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
-  occupation: "Socialité"};
+  occupation: "Socialité"
+};
 
- var mrMustard={
+var mrMustard = {
   first_name: "Jack",
   last_name: "Mustard",
   color: "yellow",
   description: "He is a former football player who tries to get by on his former glory",
   age: 62,
   image: "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",
-  occupation: "Retired Football player"};
+  occupation: "Retired Football player"
+};
 
-var characters=[];
-characters.push(mrGreen,drOrchid,profPlum,missScarlet,mrsPeacock,mrMustard);
+var charactersArray = [];
+charactersArray.push(mrGreen, drOrchid, profPlum, missScarlet, mrsPeacock, mrMustard);
+
 //---- Weapons ----
-var rope={
+
+var rope = {
+  name: "rope",
   weight: 10
 };
-var knife={
+var knife = {
+  name: "knife",
   weight: 8
 };
-var candlestick={
+var candlestick = {
+  name: "candlestick",
   weight: 2
 };
-var dumbbell={
+var dumbbell = {
+  name: "dumbbell",
   weight: 30
 };
-var poison={
+var poison = {
+  name: "poison",
   weight: 2
 };
-var axe ={
-  weight: 15};
-
-var bat={
+var axe = {
+  name: "axe",
+  weight: 15
+};
+var bat = {
+  name: "bat",
   weight: 13
 };
-var trophy={
+var trophy = {
+  name: "trophy",
   weight: 25
 };
-var pistol={
-  weight:20
+var pistol = {
+  name: "pistol",
+  weight: 20
 };
-var weapons=[];
-weapons.push(rope,knife,candlestick,dumbbell,poison,axe,bat,trophy,pistol);
-//---- Rooms ----
-var dinningRoom={};
-var conservatory={};
-var kitchen={};
-var study={};
-var library={};
-var billiardRoom={};
-var lounge={};
-var ballroom={};
-var hall={};
-var spa={};
-var livingRoom={};
-var observatory={};
-var theater={};
-var guestHouse={};
-var patio={};
 
-var rooms=[];
-rooms.push(dinningRoom,conservatory,kitchen,study,library,billiardRoom,lounge,ballroom,hall,spa,livingRoom,observatory,theater,guestHouse,patio);
+var weaponsArray = [];
+weaponsArray.push(rope, knife, candlestick, dumbbell, poison, axe, bat, trophy, pistol);
+
+//---- Rooms ----
+
+var diningRoom = {
+  name: "Dinning Room"
+};
+var conservatory = {
+  name: "Conservatory"
+};
+var kitchen = {
+  name: "Kitchen"
+};
+var study = {
+  name: "Study"
+};
+var library = {
+  name: "Library"
+};
+var billiardRoom = {
+  name: "Billiard Room"
+};
+var lounge = {
+  name: "Lounge"
+};
+var ballroom = {
+  name: "Ballroom"
+};
+var hall = {
+  name: "Hall"
+};
+var spa = {
+  name: "Spa"
+};
+var livingRoom = {
+  name: "Living Room"
+};
+var observatory = {
+  name: "Observatory"
+};
+var theater = {
+  name: "Theater"
+};
+var guestHouse = {
+  name: "Guest House"
+};
+var patio = {
+  name: "Patio"
+};
+
+var roomsArray = [];
+roomsArray.push(diningRoom, conservatory, kitchen, study, library, billiardRoom, lounge, ballroom, hall, spa, livingRoom, observatory, theater, guestHouse, patio);
 
 function random_selector(array) {
-  var x = Math.floor((Math.random() * array.length));
+  var x = Math.floor(Math.random() * array.length);
   return array[x];
 }
 
-function pick_mistery(array) {
-  var mistery =
-  {personajes: random_selector(characters),
-  armas: random_selector(weapons),
-  habitaciones: random_selector(rooms)};
+function pick_mistery() {
+  var mistery = {
+    character: random_selector(charactersArray),
+    weapon: random_selector(weaponsArray),
+    room: random_selector(roomsArray)
+  };
   return mistery;
 }
 
-function reveal_mistery() {
-  characters.forEach(function(mistery){
-    console.log(mistery.first_name);
-  });
+function reveal_mistery(mistery) {
+  var asesino = mistery.character.first_name;
+  var arma = mistery.weapon.name;
+  var habitacion = mistery.room.name;
+  console.log(asesino, arma, habitacion);
 }
 
 
