3f75b91242de6e3ce516f8f1d6b267bd788b89be
AlberPedraza <alberpedrazaes@gmail.com>
2017-10-18 11:15:19 +0200

    iteration 2

--- starter-code/clue.js
+++ starter-code/clue.js
@@ -88,9 +88,45 @@ Patio
 
 */
 
-function random_selector(array) {
 
+var personajes = [
+  {
+  firstName: "Jacob", lastName:  "Green", color: "green", description: "He has a lot of connections",age: 45,image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",occupation: "Entrepreneur"
+  },
+   {    
+  firstName: "Doctor",  lastName: "Orchid",  color: "white",  description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+  age: 26,  image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",  occupation: "Scientist"
+  }, 
+  {  
+  firstName: "Victor",  lastName: "Plum",  color: "purple",  description: "Billionare video game designer",  age: 22,  image: "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",  occupation: "Designer"
+  },      
+  {    
+  firstName: "Kasandra",  lastName: "Scarlet",  color: "red" ,description: "She is an A-list movie star with a dark past",  age: 31,
+  image: "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",  occupation: "Actor"
+  }, 
+  {    
+  firstName: "Eleanor",  lastName: "Peacock",  color: "blue",  description: "She is from a wealthy family and uses her status and money to earn popularity",  age: 36,  image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",  occupation: "Socialité"
+   },   
+  {    
+  firstName: "Jack",  lastName: "Mustard",  color: "yellow",  description: "He is a former football player who tries to get by on his former glory",  age: 62,  image: "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",  occupation: "Retired Football player"
+  }
+ ]
+
+
+
+var weapons = {
+Rope :{Weight: 10},Knife : {Weight: 8}, Candlestick : {Weight: 2},Dumbbell : {Weight: 30}, Poison : {Weight: 2}, Axe : {Weight: 15},Bat : {Weight: 13}, Trophy : {Weight: 25}, Pistol : {Weight: 20}
 }
+
+var rooms =["Dinning Room","Conservatory","Kitchen","Study","Library","Billiard Room","Lounge","Ballroom","Hall","Spa Living Room","Observatory","Theater","Guest House","Patio"]
+
+
+function random_selector(x) {
+
+var asesino = Math.floor(Math.random()*6);
+console.log(x[asesino]);
+}
+random_selector(personajes);
 
 function pick_mistery() {
 
@@ -102,3 +138,4 @@ function reveal_mistery(mistery) {
 
 var mistery_envelope = pick_mistery();
 reveal_mistery(mistery_envelope);
+
