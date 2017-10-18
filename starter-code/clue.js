92f1330cb870860b013e41e09b3b65cd5fc096f5
Yurema Rebollo <yuremarc@gmail.com>
2017-10-18 16:35:20 +0200

    Primera Iteration-Yurema, María José

--- starter-code/clue.js
+++ starter-code/clue.js
@@ -88,17 +88,71 @@ Patio
 
 */
 
-function random_selector(array) {
+var suspects = [
+    {
+      firstName: "Jacob",
+      lastName: "Green",
+      color: "Green",
+      descripcion: "He has a lot o conections",
+      age: 45,
+      image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+      ocupation: "entrepreneur"},
+      {
+      firstName: "Doctor",
+      lastName: "Orchid",
+      color: "White",
+      descripcion: "PhD in plant toxicology. Adopted daughter of Mr.Boddy",
+      age: 26,
+      image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+      occupation: "Scientist"},
+      {
+      firstName: "Victor",
+      lastName: "Plum",
+      color: "purple",
+      description: "Billionare video game designer",
+      age: 22,
+      image: "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
+      occupation: "Designer"},
+      {
+      firstName: "Kasandra",
+      lastName: "Scarlet",
+      color: "red",
+      description: "She is an A-list movie star with a dark past",
+      age: 31,
+      image: "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",
+      occupation: "Actor"},
+    {
+      firstName: "Eleanor",
+      lastName: "Peacock",
+      color: "blue",
+      description: "She is from a wealthy family and uses her status and money to earn popularity",
+      age: 36,
+      image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+      occupation: "Socialité"},
 
-}
+    {
+      firstName: "Jack",
+      lastName: "Mustard",
+      color: "yellow",
+      description: "He is a former football player who tries to get by on his former glory",
+      age: 62,
+      image: "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",
+      occupation: "Retired Football player"},
+      ]
 
-function pick_mistery() {
 
-}
+  var rooms= ["Dinning Room","Conservatory","Kitchen","Study","Library","Billiard Room","Lounge","Ballroom","Hall","Spa","Living Room","Observatory","Theater","Guest House","Patio"];
+  var weapons= [
+    {nameWeapon:"Rope", Weight: 10},
+    {nameWeapon:"Knife", Weight: 8},
+    {nameWeapon:"Candlestick", Weight: 2},
+    {nameWeapon:"Dumbbell", Weight: 30},
+    {nameWeapon:"Poison", Weight: 2},
+    {nameWeapon:"Axe", Weight: 15},
+    {nameWeapon:"Bat", Weight: 13},
+    {nameWeapon: "Trophy", Weight: 25},
+    {nameWeapon:"Pistol", Weight: 20},
+    ];
 
-function reveal_mistery(mistery) {
-
-}
-
-var mistery_envelope = pick_mistery();
-reveal_mistery(mistery_envelope);
+    console.log("Rooms:"+rooms);
+    console.log for
