8e2f4d5a1826575199887791e39a8f310cb78bd4
Nikolaas Verlee <nikotomad@gmail.com>
2017-10-18 13:16:21 +0200

    todas las iteraciones hechas.Hemos usado propiedades distintas por hacerlo en repl.it.

--- starter-code/clue.js
+++ starter-code/clue.js
@@ -1,104 +1,187 @@
-/*
+var mrGreen = {
+    firstName: "Jacob",
+    lastName: "Green",
+    occupation: "Scientist",
+    age: 42,
+    description: "He has a lot of connections and is always willing to help people out -- for a price",
+    color: "green"
+  }
 
----- Characters ----
+  var msOrchid = {
+    firstName: "Doctor",
+    lastName: "Orchid",
+    occupation: "Biologist",
+    age: 52,
+    description: "She is the adopted daugther of Mr. Boddy, a biologist with a PhD in plant toxicology. She was privately educated in Switzerland until her expulsion after an incident involving daffodils resulted in a near-fatal poisoning",
+    color: "white"
+  }
+  var mrPlum = {
+    firstName: "Victor",
+    lastName: "Plum",
+    occupation: "Videogame Designer",
+    age: 32,
+    description: "He is a billionaire video game designer who is embracing his new popularity",
+    color: "purple"
+  }
+  var mrScarlet = {
+    firstName: "Kasandra",
+    lastName: "Scarlet",
+    occupation: "Movie star",
+    age: 22,
+    description: "She is an A-list movie star whose past haunts her.",
+    color: "red"
+  }
+  var msPeacock = {
+    firstName: "Eleanor",
+    lastName: "Peacock",
+    occupation: "Famous Wealthy Person",
+    age: 33,
+    description: "She is from a wealthy family and uses her status and money to earn popularity",
+    color: "blue"
+  }
+  var mrMustard = {
+    firstName: "Jack",
+    lastName: "Mustard",
+    occupation: "Former Football Player",
+    age: 45,
+    description: "He is a former football player who tries to get by on his former glory.",
+    color: "yellow"
+  }
 
-  Mr Green
-  first name: Jacob
-  last name:  Green
-  color: green
-  description: He has a lot of connections
-  age: 45
-  image: https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg
-  occupation: Entrepreneur
+  //weapons
 
-  Dr Orchid
-  first name: Doctor
-  last name: Orchid
-  color: white
-  description: PhD in plant toxicology. Adopted daughter of Mr. Boddy
-  age: 26
-  image: http://www.radiotimes.com/uploads/images/Original/111967.jpg
-  occupation: Scientist
+  var rope = {
+    name : "rope",
+    weight: 0,
+    damage: 3
+  }
+  var knife = {
+    name : "knife",
+    weight: 2,
+    damage: 3
+  }
+  var candlestick = {
+    name : "candlestick",
+    weight: 20,
+    damage: 2
+  }
+  var dumbbell = {
+    name : "dumbbell",
+    weight: 10,
+    damage: 3
+  }
+  var poison = {
+    name : "poison",
+    weight: 3,
+    damage: 1
+  }
+  var axe = {
+    name : "axe",
+    weight: 2,
+    damage: 15
+  }
+  var bat = {
+    name : "bat",
+    weight: 11,
+    damage: 8
+  }
+  var trophy = {
+    name : "trophy",
+    weight: 9,
+    damage: 6
+  }
+  var pistol = {
+    name : "pistol",
+    weight: 1,
+    damage: 4
+  }
 
-  Prof Plum
-  first name: Victor
-  last name: Plum
-  color: purple
-  description: Billionare video game designer
-  age: 22
-  image: https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg
-  occupation: Designer
+  //rooms
 
-  Miss Scarlet
-  first name: Kasandra
-  last name: Scarlet
-  color: red
-  description: She is an A-list movie star with a dark past,
-  age: 31,
-  image: https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg,
-  occupation: Actor
+  var diningroom = {
+    size: 6
+  }
+  var conservatory = {
+    size: 12
+  }
+  var kitchen = {
+    size: 61
+  }
+  var study = {
+    size: 65
+  }
+  var library = {
+    size: 66
+  }
+  var billard = {
+    size: 6
+  }
+  var lounge = {
+    size: 61
+  }
+  var ballroom = {
+    size: 11
+  }
+  var hall = {
+    size: 2
+  }
+  var spa = {
+    size: 4
+  }
+  var livinroom = {
+    size: 55
+  }
+  var observatory = {
+    size: 65
+  }
+  var theatre = {
+    size: 78
+  }
+  var guesthouse = {
+    size: 12
+  }
+  var patio = {
+    size: 25
+  }
 
- Mrs Peacock
-  first name: Eleanor
-  last name: Peacock
-  color: blue
-  description: She is from a wealthy family and uses her status and money to earn popularity
-  age: 36
-  image: https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg
-  occupation: Socialité
+var suspects = [];
+suspects.push(mrGreen, msOrchid, mrPlum, mrScarlet, msPeacock, mrMustard);
 
- Mr Mustard
-  first name: Jack
-  last name: Mustard
-  color: yellow
-  description: He is a former football player who tries to get by on his former glory
-  age: 62
-  image: https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg
-  occupation: Retired Football player
+var weapons = []
+weapons.push(rope, knife,candlestick, dumbbell, poison, axe, bat, trophy, pistol);
 
----- Weapons ----
-
-Rope - Weight: 10
-Knife - Weight: 8
-Candlestick - Weight: 2
-Dumbbell - Weight: 30
-Poison - Weight: 2
-Axe - Weight: 15
-Bat - Weight: 13
-Trophy - Weight: 25
-Pistol - Weight: 20
+var rooms = []
+rooms.push(diningroom, conservatory, kitchen, study, library, billard, lounge, ballroom, hall, spa, livinroom, observatory, theatre, guesthouse, patio);
 
 
----- Rooms ----
-Dinning Room
-Conservatory
-Kitchen
-Study
-Library
-Billiard Room
-Lounge
-Ballroom
-Hall
-Spa
-Living Room
-Observatory
-Theater
-Guest House
-Patio
-
-*/
-
-function random_selector(array) {
-
+//Iteration 2 - 1
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
-function pick_mistery() {
 
+
+// Get one random element from card stack
+function extract(deck){
+  var r = getRandom (0,deck.length)
+  return deck[r];
 }
 
-function reveal_mistery(mistery) {
+extract(suspects);
 
+//Iteration 2 - 2
+var caseFile = [];
+
+function differentCards (){
+ var a = extract(suspects);
+  var b =extract(weapons);
+  var c = extract(rooms);
+  caseFile.push(a, b , c);
 }
 
-var mistery_envelope = pick_mistery();
-reveal_mistery(mistery_envelope);
+function revealMistery(){
+  return caseFile;
+}
+
+differentCards ();
+revealMistery();
