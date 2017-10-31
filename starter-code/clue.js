226c8223f37e0783aa95fda70eab060a313eb546
juliabellone <tucacu@gmail.com>
2017-10-31 21:39:11 +0100

    Update clue.js

--- starter-code/clue.js
+++ starter-code/clue.js
@@ -1,104 +1,150 @@
-/*
+ var characters = [
+   {firstName: "Jacob", 
+   lastName: "Green", 
+   color: "Green", 
+   occupation: "Entrepreneur", 
+   age: "45", 
+   description: "He has a lot of connections", 
+  image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg"
 
----- Characters ----
+    },
+   {firstName: "Doctor", 
+   lastName: "Orchid", 
+   color: "White", 
+   occupation: "Scientist", 
+   age: "26", 
+   description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy", 
+   image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg"
+    },
+    {firstName: "Victor", 
+   lastName: "Plum", 
+   color: "Purple", 
+   occupation: "Game designer", 
+   age: "22", 
+   description: "Billionare video game designer", 
+   image: "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg"
+    },{firstName: "Kasandra", 
+   lastName: "Scarlet" , 
+   color: "Red", 
+   occupation: "Movie Star", 
+   age: "31", 
+   description: "She is an A-list movie star with a dark past",
+    image: "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg"
+   
+    },{firstName: "Eleanor" , 
+   lastName: "Peacock" , 
+   color: "Blue" , 
+   occupation: "Socialité", 
+   age: " 31", 
+   description: "She is from a wealthy family and uses her status and money to earn popularity", 
+    image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg"
+    },
+    {firstName: "Jack" , 
+   lastName: "Mustard" , 
+   color: "Yellow", 
+   occupation: "Football Player", 
+   age: "62", 
+   description: "He is a former football player who tries to get by on his former glory", 
+   image: "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg"
+    },
+  ]
+  
+  var weapons = [
+   {
+     weapon: "Rope",
+     weight: 10,
+     color: " ",
+     hurtIndex: 0
+   },
+   {
+     weapon: "Knife",
+     weight: 8,
+     color: " ",
+     hurtIndex: 0
+   },
+   {
+     weapon: "Candlestick",
+     weight: 2,
+     color: " ",
+     hurtIndex: 0
+   },
+   {
+     weapon: "Dumbbell",
+     weight: 30,
+     color: " ",
+     hurtIndex: 0
+   },
+   {
+     weapon: "Poison",
+     weight: 2,
+     color: " ",
+     hurtIndex: 0
+   },
+   {
+     weapon: "Axe",
+     weight: 15,
+     color: " ",
+     hurtIndex: 0
+   },
+   {
+     weapon: "Bat",
+     weight: 13,
+     color: " ",
+     hurtIndex: 0
+   },
+   {
+     weapon: "Trophy",
+     weight: 25,
+     color: " ",
+     hurtIndex: 0
+   },
+   {
+     weapon: "Pistol",
+     weight: 20,
+     color: " ",
+     hurtIndex: 0
+   },
+  ]
+  
+  
+rooms = [
+"Dinning Room",
+"Conservatory",
+"Kitchen",
+"Study",
+"Library",
+"Billiard Room",
+"Lounge",
+"Ballroom",
+"Hall",
+"Spa",
+"Living Room",
+"Observatory",
+"Theater",
+"Guest House",
+"Patio"
+]
 
-  Mr Green
-  first name: Jacob
-  last name:  Green
-  color: green
-  description: He has a lot of connections
-  age: 45
-  image: https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg
-  occupation: Entrepreneur
-
-  Dr Orchid
-  first name: Doctor
-  last name: Orchid
-  color: white
-  description: PhD in plant toxicology. Adopted daughter of Mr. Boddy
-  age: 26
-  image: http://www.radiotimes.com/uploads/images/Original/111967.jpg
-  occupation: Scientist
-
-  Prof Plum
-  first name: Victor
-  last name: Plum
-  color: purple
-  description: Billionare video game designer
-  age: 22
-  image: https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg
-  occupation: Designer
-
-  Miss Scarlet
-  first name: Kasandra
-  last name: Scarlet
-  color: red
-  description: She is an A-list movie star with a dark past,
-  age: 31,
-  image: https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg,
-  occupation: Actor
-
- Mrs Peacock
-  first name: Eleanor
-  last name: Peacock
-  color: blue
-  description: She is from a wealthy family and uses her status and money to earn popularity
-  age: 36
-  image: https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg
-  occupation: Socialité
-
- Mr Mustard
-  first name: Jack
-  last name: Mustard
-  color: yellow
-  description: He is a former football player who tries to get by on his former glory
-  age: 62
-  image: https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg
-  occupation: Retired Football player
-
----- Weapons ----
-
-Rope - Weight: 10
-Knife - Weight: 8
-Candlestick - Weight: 2
-Dumbbell - Weight: 30
-Poison - Weight: 2
-Axe - Weight: 15
-Bat - Weight: 13
-Trophy - Weight: 25
-Pistol - Weight: 20
-
-
----- Rooms ----
-Dinning Room
-Conservatory
-Kitchen
-Study
-Library
-Billiard Room
-Lounge
-Ballroom
-Hall
-Spa
-Living Room
-Observatory
-Theater
-Guest House
-Patio
-
-*/
 
 function random_selector(array) {
-
+  var index = Math.floor(Math.random()*array.length);
+  return array[index]; 
 }
 
 function pick_mistery() {
-
+  var myMistery = [];
+  myMistery.push(random_selector(characters), random_selector(weapons), random_selector(rooms));
+  return (myMistery);
 }
 
 function reveal_mistery(mistery) {
-
+  mistery.forEach(function(card) {
+  console.log(card)  
+  })
+  
 }
 
 var mistery_envelope = pick_mistery();
 reveal_mistery(mistery_envelope);
+
+  
