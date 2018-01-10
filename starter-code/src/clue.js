c42c18fd90451acefa03940a0697f2fa0e52ae5f
Giorgio Grassini <giorgiograssini@gmail.com>
2018-01-10 16:18:49 +0100

    done

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -85,10 +85,121 @@ name: Patio
 */
 
 // Characters Collection
-var charactersArray = [];
+var charactersArray = [
+    {
+        first_name:   'Jacob',
+        last_name:    'Green',
+        color:        'green',
+        description:  'He has a lot of connections',
+        age:          45,
+        image:        'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+        occupation:   'Entrepreneur',
+    },
+
+    {
+        first_name:   'Doctor',
+        lst_name:    'Orchid',
+        color:        'white',
+        description:  'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
+        age:          26,
+        image:        'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
+        ocupation:   'Scientist',
+    },
+
+    {
+            first_name:   'Victor',
+            last_name:    'Plum',
+            color:        'purple',
+            description:  'Billionare video game designer',
+            age:          22,
+            image:        'https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg (185 kB) ',
+            occupation:   'Designer',
+    },
+
+     {
+            first_name:   'Kasandra',
+            last_name:    'Scarlet',
+            color:        'red',
+            description:  'She is an A-list movie star with a dark past',
+            age:          31,
+            image:        'https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg (745 kB) ',
+            occupation:   'Actor',
+        },
+    
+     {
+            first_name:   'Eleanor',
+            last_name:    'Peacock',
+            color:        'blue',
+            description:  'She is from a wealthy family and uses her status and money to earn popularity',
+            age:          36,
+            image:        'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg (289 kB) ',
+            occupation:   'Socialité',
+            },
+
+     {
+                first_name:   'Jack',
+                last_name:    'Mustard',
+                color:        'yellow',
+                description:  'He is a former football player who tries to get by on his former glory',
+                age:          62,
+                image:        'https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg (381 kB) ',
+                occupation:   'Retired Football player',
+                },
+
+];
 
 // Rooms' Collection
-var roomsArray = [];
+var roomsArray = [
+    {name:'Dinning Room'},
+    {name:'Conservatory'},
+    {name:'kitchen'},
+    {name:'Study'},
+    {name:'Library'},
+    {name:'Billiard Room'},
+    {name: 'Lounge'},
+    {name: 'Ballroom'},
+    {name: 'Hall'},
+    {name: 'Spa'},
+    {name: 'Living Room'},
+    {name: 'Observatory'},
+    {name: 'Theater'},
+    {name: 'Guest House'},
+    {name: 'Patio'},
+];
 
 // Weapons Collection
-var weaponsArray = [];
+var weaponsArray = [
+    {name: 'rope', weight: 10},
+    {name: 'knife', weight: 8},
+    {name: 'candlestick', weight: 2},
+    {name: 'dumbbell', weight: 30},
+    {name: 'poison', weight: 2},
+    {name: 'axe'  , weight: 15},
+    {name: 'bat'  , weight: 13},
+    {name: 'trophy', weight: 25},
+    {name: 'pistol', weight: 20},
+];
+
+function randomSelector(arr){
+
+    if(arr.length===0){
+        return undefined;
+    }
+
+    return arr[Math.floor(Math.random()*arr.length)];
+}
+
+function pickMistery(){
+
+var misteryEnvelope = []
+
+    misteryEnvelope.push(randomSelector(charactersArray));
+    misteryEnvelope.push(randomSelector(weaponsArray));
+    misteryEnvelope.push(randomSelector(roomsArray));
+
+return misteryEnvelope;
+}
+
+function revealMistery(arr){
+    return ((arr[0].first_name) + " " +(arr[0].last_name) +  ' killed Mr.Boddy using the ' + arr[1].name + ' in the ' + arr[2].name + "!!!!");
+}
