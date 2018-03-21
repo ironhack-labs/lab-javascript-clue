ef56c6e3981867f9a252db97354efc92ff214a1a
Tanis Pan <e.pan.gar@gmail.com>
2018-03-21 13:39:41 +0100

    Iteración 3

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -150,8 +150,21 @@ occupation:   "Retired Football player"
 
 // Rooms' Collection
 var roomsArray = [
-
-  
+{name: "Dinning Room"},
+{name: "Conservatory"},
+{name: "Kitchen"},
+{name: "Study"},
+{name: "Library"},
+{name: "Billiard Room"},
+{name: "Lounge"},
+{name: "Ballroom"},
+{name: "Hall"},
+{name: "Spa"},
+{name: "Living Room"},
+{name: "Observatory"},
+{name: "Theater"},
+{name: "Guest House"},
+{name: "Patio"}
 ];
 
 // Weapons Collection
