d36b7e3842752eee80bf0b847187318a549ffd27
Tanis Pan <e.pan.gar@gmail.com>
2018-03-21 13:34:26 +0100

    Iteración 2

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -147,8 +147,59 @@ occupation:   "Retired Football player"
   }
 ];
 
+
 // Rooms' Collection
-var roomsArray = [];
+var roomsArray = [
+
+  
+];
 
 // Weapons Collection
-var weaponsArray = [];
+var weaponsArray = [
+  {
+    name: "rope",
+    weight: 10,
+  },
+  
+  {
+    name: "knife",
+    weight: 8,
+  },
+  
+  {
+    name: "candlestick",
+    weight: 2,
+  },
+  
+  {
+    name: "dumbbell",
+    weight: 30.
+  },
+  
+  {
+  name: "poison",
+  weight: 2,
+  },
+  
+  {
+    name: "axe",
+    weight: 15,
+  },
+  
+  {
+    name: "bat",
+    weight: 13,
+  },
+  
+  {
+    name: "trophy",
+    weight: 25,
+  
+  },
+  
+  {
+    name: "pistol",
+    weight: 20,
+  }
+  ];
+  
