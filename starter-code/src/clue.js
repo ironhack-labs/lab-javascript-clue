567819496d9a47334c1b5ea64564226a06d0ae37
Raul.dll <rdl.llave@gmail.com>
2018-03-21 15:02:02 +0100

    mitad del ejercicio concluido

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -85,10 +85,171 @@ name: Patio
 */
 
 // Characters Collection
-var charactersArray = [];
+
+var mrGreen = {
+  first_name: "Jacob",
+  last_name: "Green",
+  color: "green",
+  description: "He has a lot of connections",
+  age: 45,
+  occupation: "Entrepreneur"
+};
+var drOrchid = {
+  frst_name: "Doctor",
+  lst_name: "Orchid",
+  color: "white",
+  description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+  age: 26,
+  ocupation: "Scientist"
+};
+
+var profPlum = {
+  first_name: "Victor",
+  last_name: "Plum",
+  color: "purple",
+  description: "Billionare video game designer",
+  age: 22,
+  image: "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
+  occupation: "Designer"
+};
+
+var missScarlet = {
+  first_name: "Kasandra",
+  last_name: "Scarlet",
+  color: "red",
+  description: "She is an A-list movie star with a dark past",
+  age: 31,
+  occupation: "Actor"
+};
+
+var mrsPeacock = {
+  first_name: "Eleanor",
+  last_name: "Peacock",
+  color: "blue",
+  description:
+    "She is from a wealthy family and uses her status and money to earn popularity",
+  age: 36,
+  image: "https//:metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+  occupation: "Socialité"
+};
+
+var mrMustard = {
+  first_name: "Jack",
+  last_name: "Mustard",
+  color: "yellow",
+  description:
+    "2He is a former football player who tries to get by on his former glory",
+  age: 62,
+  image: "https//:metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",
+  occupation: "Retired Football player"
+};
+var charactersArray = [
+  mrGreen,
+  drOrchid,
+  profPlum,
+  missScarlet,
+  mrsPeacock,
+  mrMustard
+];
 
 // Rooms' Collection
-var roomsArray = [];
+
+var dinningRoom = {};
+var conservatory = {};
+var kitchen = {};
+var study = {};
+var library = {};
+var billiardRoom = {};
+var lounge = {};
+var ballroom = {};
+var hall = {};
+var spa = {};
+var livingRoom = {};
+var observatory = {};
+var theater = {};
+var guestHouse = {};
+var patio = {};
+
+var roomsArray = {
+  dinningRoom,
+  conservatory,
+  kitchen,
+  study,
+  library,
+  billiardRoom,
+  lounge,
+  ballroom,
+  hall,
+  spa,
+  livingRoom,
+  observatory,
+  theater,
+  guestHouse,
+  patio
+};
 
 // Weapons Collection
-var weaponsArray = [];
+
+var rope = {
+  name: "rope",
+  weight: 10
+};
+var knife = {
+  name: "knife",
+  weight: 8
+};
+var candlestick = {
+  name: "candlestick",
+  weight: 2
+};
+var dumbbell = {
+  name: "dumbbell",
+  weight: 30
+};
+var poison = {
+  name: "poison",
+  weight: 2
+};
+var axe = {
+  name: "axe",
+  weight: 15
+};
+var bat = {
+  name: "bat",
+  weight: 13
+};
+var trophy = {
+  name: "trophy",
+  weight: 25
+};
+var pistol = {
+  name: "pistol",
+  weight: 20
+};
+var weaponsArray = [
+  rope,
+  knife,
+  candlestick,
+  dumbbell,
+  poison,
+  axe,
+  bat,
+  trophy,
+  pistol
+];
+
+var cards = [charactersArray, roomsArray, weaponsArray];
+
+function randomSelector(array) {
+  if (array.length == 0) {
+    return undefined;
+  }
+
+  var index = Math.floor(Math.random() * array.length);
+
+  return array[index];
+}
+
+var misteryEnvelope = [];
+
+function pickMistery() {}
