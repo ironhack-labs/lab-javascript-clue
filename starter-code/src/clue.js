c557a8831e41b3bb3e3b20ee30603503c3e6e9ea
Miguel Rodríguez Román <miguel.arr085@gmail.com>
2018-05-05 04:37:34 +0200

    completado

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -84,11 +84,213 @@ name: Guest House
 name: Patio
 */
 
+//Personajes
+var mrGreen = {
+  firstName:   'Jacob',
+  lastName:    'Green',
+  color:       'green',
+  description: 'He has a lot of connections',
+  age:          45,
+  image:       'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+  occupation:   'Entrepreneur'
+};
+var drOrchid = {
+  firstName:   'Doctor',
+  lastName:    'Orchid',
+  color:       'white',
+  description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
+  age:          26,
+  image:       'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
+  occupation:   'Scientist'
+};
+var profPlum = {
+  firstName:   'Victor',
+  lastName:    'Plum',
+  color:       'purple',
+  description: 'Billionare video game designer',
+  age:          22,
+  image:       'https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg',
+  occupation:   'Designer'
+};
+var missScarlet = {
+  firstName:   'Kasandra',
+  lastName:    'Scarlet',
+  color:       'red',
+  description: 'She is an A-list movie star with a dark past',
+  age:          31,
+  image:       'https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg',
+  occupation:   'Actor'
+};
+var mrsPeacock = {
+  firstName:   'Eleanor',
+  lastName:    'Peacock',
+  color:       'blue',
+  description: 'She is from a wealthy family and uses her status and money to earn popularity',
+  age:         36,
+  image:       'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
+  occupation:   'Socialité'
+};
+var mrMustard = {
+  firstName:   'Jack',
+  lastName:    'Mustard',
+  color:       'yellow',
+  description: 'He is a former football player who tries to get by on his former glory',
+  age:         62,
+  image:       'https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg',
+  occupation:   'Retired Football player'
+};
+var profPlum = {
+  firstName:   'Victor',
+  lastName:    'Plum',
+  color:       'purple',
+  description: 'Billionare video game designer',
+  age:         22,
+  image:       'https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg',
+  occupation:   'Designe'
+};
+
+
+// Armas
+var rope = {
+  name: "rope",
+  weight: 10
+}
+
+var knife = {
+  name: "knife",
+  weight: 8
+}
+
+var candlestick = {
+  name: "candlestick",
+  weight: 2
+}
+
+var dumbbell = {
+  name: "dumbbell",
+  weight: 30
+}
+
+var poison = {
+  name: "poison",
+  weight: 2
+}
+
+var axe = {
+  name: "axe",
+  weight: 15
+}
+
+var bat = {
+  name: "bat",
+  weight: 13
+}
+
+var trophy = {
+  name: "trophy",
+  weight: 25
+}
+
+var pistol = {
+  name: "pistol",
+  weight: 20
+}
+
+// Salas
+var dinningRoom = {
+  name: "Dinning Room"
+}
+
+var conservatory = {
+ name: "Conservatory"
+}
+
+var kitchen = {
+  name: "Kitchen"
+}
+
+var study =  {
+  name: "Study"
+}
+
+var library = {
+  name: "Library"
+}
+
+var billiardRoom =  {
+  name: "Billiard Room"
+}
+
+var lounge = {
+  name: "Lounge"
+}
+
+var ballroom = {
+  name: "Ballroom"
+}
+
+var hall =  {
+  name: "Hall"
+}
+
+var spa = {
+  name: "Spa"
+}
+
+var livingRoom =  {
+  name: "Living Room"
+}
+
+var observatory = {
+  name: "Observatory"
+}
+
+var theater = {
+  name: "Theater"
+}
+
+var guestHouse = {
+  name: "Guest House"
+}
+
+var patio = {
+  name: "Patio"
+}
+
 // Characters Collection
-var charactersArray = [];
+var charactersArray = [mrGreen, drOrchid, profPlum, missScarlet, mrsPeacock, mrMustard];
 
 // Rooms' Collection
-var roomsArray = [];
+var roomsArray = [dinningRoom, conservatory, kitchen, study, library, billiardRoom, lounge, ballroom, hall, spa,
+  livingRoom, observatory, theater, guestHouse, patio];
 
 // Weapons Collection
-var weaponsArray = [];
+var weaponsArray = [rope, knife, candlestick, dumbbell, poison, axe, bat, trophy, pistol];
+
+function randomSelector(array){
+  return array[Math.floor(Math.random()*array.length)];
+}
+
+function pickMistery(){
+  var arrayRandom=[];
+  var character = randomSelector(charactersArray);
+  arrayRandom.push(character);
+  var weapons = randomSelector(weaponsArray);
+  arrayRandom.push(weapons);
+  var room = randomSelector(roomsArray);
+  arrayRandom.push(room);
+
+  return arrayRandom;
+}
+var arrayRandom = pickMistery();
+//console.log(arrayRandom);
+
+function revealMistery(arrayRandom){
+  return arrayRandom[0].firstName + " " + arrayRandom[0].lastName + " killed Mr.Boddy using the " + arrayRandom[1].name + " in the " + arrayRandom[2].name + "!!!!";
+} 
+revealMistery(arrayRandom);
+
+
+
+
+
--- starter-code/tests/ClueSpec.js
+++ starter-code/tests/ClueSpec.js
@@ -73,10 +73,10 @@ describe('Reveal the mistery - revealMistery', function () {
   });
 
   it('Return an array', function () {
-    expect(typeof revealMistery([{ first_name: 'aa', last_name: 'abc' }, { name: 'abd' }, { name: 'abb' }])).toEqual('string');
+    expect(typeof revealMistery([{ firstName: 'aa', lastName: 'abc' }, { name: 'abd' }, { name: 'abb' }])).toEqual('string');
   });
 
   it('Return <FIRST NAME> <LAST NAME> killed Mr.Boddy using the <WEAPON> in the <PLACE>!!!!', function () {
-    expect(revealMistery([{ first_name: 'Victor', last_name: 'Plum' }, { name: 'poison' }, { name: 'Billiard Room' }])).toEqual('Victor Plum killed Mr.Boddy using the poison in the Billiard Room!!!!');
+    expect(revealMistery([{ firstName: 'Victor', lastName: 'Plum' }, { name: 'poison' }, { name: 'Billiard Room' }])).toEqual('Victor Plum killed Mr.Boddy using the poison in the Billiard Room!!!!');
   });
 });
