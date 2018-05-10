aa236446455599530b2a3db4817fee8adc669a0b
Ravi Kalia <ravkalia@gmail.com>
2018-05-10 16:16:04 -0500

    randomSelector

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,123 +1,116 @@
 // Characters
 const mrGreen = {
-first_name:   'Jacob',
-last_name:    'Green',
-color:        'green',
-description:  'He has a lot of connections',
-age:          45,
-image:        'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg,',
-occupation:   'Entrepreneur',
+  first_name: 'Jacob',
+  last_name: 'Green',
+  color: 'green',
+  description: 'He has a lot of connections',
+  age: 45,
+  image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg,',
+  occupation: 'Entrepreneur',
 };
 
 const drOrchid = {
-frst_name:   'Doctor',
-lst_name:    'Orchid',
-color:        'white',
-description:  'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
-age:          26,
-image:        'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
-ocupation:   'Scientist',
+  frst_name: 'Doctor',
+  lst_name: 'Orchid',
+  color: 'white',
+  description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
+  age: 26,
+  image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
+  ocupation: 'Scientist',
 };
 
 const profPlum = {
-first_name:   'Victor',
-last_name:    'Plum',
-color:        'purple',
-description:  'Billionare video game designer',
-age:          22,
-image:        'https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg',
-occupation:   'Designer',
+  first_name: 'Victor',
+  last_name: 'Plum',
+  color: 'purple',
+  description: 'Billionare video game designer',
+  age: 22,
+  image: 'https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg',
+  occupation: 'Designer',
 };
 
 const missScarlet = {
-first_name:   'Kasandra',
-last_name:    'Scarlet',
-color:        'red',
-description:  'She is an A-list movie star with a dark past',
-age:          31,
-image:        'https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg',
-occupation:   'Actor',
+  first_name: 'Kasandra',
+  last_name: 'Scarlet',
+  color: 'red',
+  description: 'She is an A-list movie star with a dark past',
+  age: 31,
+  image: 'https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg',
+  occupation: 'Actor',
 };
 
 const mrsPeacock = {
-first_name:   'Eleanor',
-last_name:    'Peacock',
-color:        'blue',
-description:  'She is from a wealthy family and uses her status and money to earn popularity',
-age:          36,
-image:        'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
-occupation:   'Socialité',
+  first_name: 'Eleanor',
+  last_name: 'Peacock',
+  color: 'blue',
+  description: 'She is from a wealthy family and uses her status and money to earn popularity',
+  age: 36,
+  image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
+  occupation: 'Socialité',
 };
 
 const mrMustard = {
-first_name:   'Jack',
-last_name:    'Mustard',
-color:        'yellow',
-description:  'He is a former football player who tries to get by on his former glory',
-age:          62,
-image:        'https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg',
-occupation:   'Retired Football player',
+  first_name: 'Jack',
+  last_name: 'Mustard',
+  color: 'yellow',
+  description: 'He is a former football player who tries to get by on his former glory',
+  age: 62,
+  image: 'https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg',
+  occupation: 'Retired Football player',
 };
 
 // Characters Collection
-const charactersArray = [mrGreen, drOrchid, profPlum, missScarlett, MrsPeacock, mrMustard];
+const charactersArray = [mrGreen, drOrchid, profPlum, missScarlet, mrsPeacock, mrMustard];
 
 // Weapons
 
-const rope = {name: 'rope', weight: 10};
-const knife = {name: 'knife', weight: 8};
-const candlestick = {name: 'candlestick', weight: 2};
-const dumbbell = {name: 'dumbbell', weight: 30};
-const poison = {name: 'poison', weight: 2};
-const axe = {name: 'axe', weight: 15};
-const trophy = {name: 'trophy', weight: 25};
-const bat = {name: 'bat', weight: 13};
-const pistol = {name: 'pistol', weight: 20};// Rooms
+const rope = { name: 'rope', weight: 10 };
+const knife = { name: 'knife', weight: 8 };
+const candlestick = { name: 'candlestick', weight: 2 };
+const dumbbell = { name: 'dumbbell', weight: 30 };
+const poison = { name: 'poison', weight: 2 };
+const axe = { name: 'axe', weight: 15 };
+const trophy = { name: 'trophy', weight: 25 };
+const bat = { name: 'bat', weight: 13 };
+const pistol = { name: 'pistol', weight: 20 };// Rooms
 
 // Rooms' Collection
 const roomsArray = [
-    rope,
-    knife,
-    candlestick,
-    dumbbell,
-    poison,
-    axes,
-    trophy,
-    bat,
-    pistol,
+  rope,
+  knife,
+  candlestick,
+  dumbbell,
+  poison,
+  axe,
+  trophy,
+  bat,
+  pistol,
 ];
-
-{ name: 'Conservatory'} 
-{ name: 'Dinning Room'} 
-{ name: 'Kitchen'} 
-{ name: 'Study'} 
-{ name: 'Library'} 
-{ name: 'Billiard Room'} 
-{ name: 'Lounge'} 
-{ name: 'Ballroom'} 
-{ name: 'Hall'} 
-{ name: 'Spa'} 
-{ name: 'Living Room'} 
-{ name: 'Observatory'} 
-{ name: 'Theater'} 
-{ name: 'Guest House'} 
-{ name: 'Patio'} 
 
 // Weapons Collection
 const weaponsArray = [
-  { name: 'Conservatory'}, 
-  { name: 'Dinning Room'},
-  { name: 'Kitchen'},
-  { name: 'Study'},
-  { name: 'Library'}, 
-  { name: 'Billiard Room'}, 
-  { name: 'Lounge'},
-  { name: 'Ballroom'}, 
-  { name: 'Hall'},
-  { name: 'Spa'},
-  { name: 'Living Room'},
-  { name: 'Observatory'},
-  { name: 'Theater'},
-  { name: 'Guest House'}, 
-  { name: 'Patio'},
+  { name: 'Conservatory' },
+  { name: 'Dinning Room' },
+  { name: 'Kitchen' },
+  { name: 'Study' },
+  { name: 'Library' },
+  { name: 'Billiard Room' },
+  { name: 'Lounge' },
+  { name: 'Ballroom' },
+  { name: 'Hall' },
+  { name: 'Spa' },
+  { name: 'Living Room' },
+  { name: 'Observatory' },
+  { name: 'Theater' },
+  { name: 'Guest House' },
+  { name: 'Patio' },
 ];
+
+const randomSelector = (array) => {
+  if (array.length === 0) return undefined;
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return (array[randomIndex]);
+};
+
+// const randArray = randomSelector(weaponsArray);
+// randArray;
