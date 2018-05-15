66e38b79120efa55a09de44c6097e3dc6c08a429
Mariona Roca <mariona.roca@gmail.com>
2018-05-16 00:19:52 +0200

    done

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -153,3 +153,17 @@ function randomSelector (array){
     var indexRand=Math.floor(Math.random()*array.length);
     return array[indexRand];
 }
+
+function pickMistery(){
+    var arr=[];
+    arr.push(randomSelector(charactersArray));
+    arr.push(randomSelector(weaponsArray));
+    arr.push(randomSelector(roomsArray));
+    return arr;
+}
+var misteryEnvelope = pickMistery();
+
+function revealMistery(mistEnv){
+//<FIRST NAME> <LAST NAME> killed Mr.Boddy using the <WEAPON> in the <PLACE>!!!!
+    return mistEnv[0].first_name + ' ' + mistEnv[0].last_name + ' killed Mr.Boddy using the ' + mistEnv[1].name + ' in the ' + mistEnv[2].name + '!!!!';
+}
