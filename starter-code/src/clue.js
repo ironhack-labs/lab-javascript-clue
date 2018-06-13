92093f26ac6095f3ef67e127861eca5d392919fe
YokoYolo <y.ravlyk@gmail.com>
2018-06-13 14:32:34 -0400

    error fixed

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -101,6 +101,7 @@ var roomsArray = [
 ]
 
 
+
 function randomSelector (array) {
     if (array.length === 0) {
         return undefined;}
@@ -121,6 +122,6 @@ return misteryEnvelope
 pickMistery ()
 
 function  revealMistery (misteryEnvelope){
-  return misteryEnvelope[0].first_name + ' ' + misteryEnvelope[0].last_name + ' killed Mr.Boddy using the ' + misteryEnvelope[1].name + ' in the ' + misteryEnvelope[2].name};
+  return misteryEnvelope[0].first_name + ' ' + misteryEnvelope[0].last_name + ' killed Mr.Boddy using the ' + misteryEnvelope[1].name + ' in the ' + misteryEnvelope[2].name+'!!!!' };
 
 console.log (revealMistery (misteryEnvelope))
