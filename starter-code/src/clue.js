8dc9170964214d61af1a4d79b890003b4d5b76ad
Jackson Mades <jacksonmades@gmail.com>
2018-06-13 14:31:31 -0400

    Now it is 7 errors, instead of 8.

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -287,7 +287,7 @@ function pickMistery() {
 }
 
 
-function revealMistery() {
+function revealMistery(misteryEnvelope) {
     return (misteryEnvelope[0].first_name + " " + misteryEnvelope[0].last_name + "killed Mr.Boddy using the " + misteryEnvelope[1].name + "in the" + misteryEnvelope[2].name + "!!!")
 
 }
