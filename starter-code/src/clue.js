88890e5244d2dd36f9bd21ec70945361b9308659
Verónica Reina <veroreinah@gmail.com>
2018-06-13 12:55:55 +0200

    Iteration 3 - Revealing the mistery

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -110,3 +110,9 @@ function pickMistery () {
 }
 
 var misteryEnvelope = pickMistery();
+
+function revealMistery (misteryEnvelope) {
+  return misteryEnvelope[0].first_name + 
+  " " + misteryEnvelope[0].last_name + " killed Mr.Boddy using the " + 
+  misteryEnvelope[1].name + " in the " + misteryEnvelope[2].name + "!!!!";
+};
