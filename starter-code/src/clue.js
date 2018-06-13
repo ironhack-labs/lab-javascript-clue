d204e358693f4f2637527b74dda35253a5653ff3
John Doe <johndoe@example.com>
2018-06-13 14:28:49 -0400

    No Errors

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -41,7 +41,7 @@ var roomsArray = [
 
 
 var randomPick = [];
-var misteryEnvelope= [];
+
 
 function randomSelector(randomArray){
     randomPick = (randomArray[Math.floor(Math.random() * randomArray.length)]);
@@ -50,6 +50,7 @@ function randomSelector(randomArray){
 }
 
 function pickMistery(){
+    var misteryEnvelope= [];
     misteryEnvelope.push(randomSelector(charactersArray))
     misteryEnvelope.push(randomSelector(weaponsArray))
     misteryEnvelope.push(randomSelector(roomsArray))
@@ -59,6 +60,6 @@ function pickMistery(){
 pickMistery()
 
 function revealMistery(misteryEnvelope){
-  return (misteryEnvelope[0].first_name + ' ' + misteryEnvelope[0].last_name +  " killed Mr. Boddy using the " + misteryEnvelope[1].name + " in the " + misteryEnvelope[2].name + " !!!" );
+  return (misteryEnvelope[0].first_name + ' ' + misteryEnvelope[0].last_name +  " killed Mr.Boddy using the " + misteryEnvelope[1].name + " in the " + misteryEnvelope[2].name + "!!!!" );
 }
 revealMistery()
