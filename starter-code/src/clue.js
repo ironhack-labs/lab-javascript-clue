7bbebd4369082edcc9af4add0c29f3a732c7e2b9
Helene <Helene@Helenes-MacBook-Pro.local>
2018-08-01 15:52:31 +0200

    BER Helene & OjunaClue

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -84,11 +84,102 @@ name: Guest House
 name: Patio
 */
 
-// Characters Collection
+
 var charactersArray = [];
-
-// Rooms' Collection
-var roomsArray = [];
-
-// Weapons Collection
 var weaponsArray = [];
+var roomsArray = [{name: "Dinning Room"}, {name: "Conservatory"}, {name: "Kitchen"}];
+
+var mrGreen = {
+
+firstName:   "Jacob",
+lastName:    "Green",
+colorPerson:        "green",
+description:  "He has a lot of connections",
+age:          45,
+imagePerson: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg"  ,      
+occupation:   "Entrepreneur"
+  }
+
+var drOrchid = {
+firstName:   "Doctor",
+lastName:    "Orchid",
+colorPerson:        "white",
+description:  "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+age:          26,
+imagePerson:        "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+ocupation:   "Scientist"
+}
+
+var profPlum = {
+firstName:   "Victor",
+lastName:    "Plum",
+colorPerson:        "purple",
+description:  "Billionare video game designer",
+age:          22,
+imagePerson:  "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
+occupation:   "Designer"
+}
+
+
+
+var rope = {
+  name: "rope", 
+  weight: 10
+} ;
+
+var knife = {
+  name: "knife",
+  weight: 8
+};
+
+var candlestick = {
+  name: "candlestick",
+  weight: 2
+};
+
+
+
+
+charactersArray.push(mrGreen, drOrchid, profPlum);
+weaponsArray.push(rope, knife, candlestick);
+
+
+
+/* Defines randomSelector
+Return undefined if the array is empty
+Return undefined if the array is empty
+Return the element of a single value array
+Should return an element of the array
+Return a random element of the array
+ */
+
+var getRandomCharacter;
+var getRandomWeapon;
+var getRandomRoom;
+
+
+function randomSelector(arr) {
+  var randomIndex = Math.floor(Math.random() * arr.length);
+  
+  return arr[randomIndex]
+}
+
+function pickMystery() {
+  var arr = []
+
+  arr.push(
+  randomSelector(charactersArray),
+  randomSelector(weaponsArray),
+  randomSelector(roomsArray))
+
+  return arr
+};
+
+var mysteryEnvelope = pickMystery();
+
+function revealMystery(envelope) {
+  console.log(mysteryEnvelope[0].firstName + " " + mysteryEnvelope[0].lastName + " killed Mr.Boddy using the " + mysteryEnvelope[1].name + " in the "  + mysteryEnvelope[2].name + "!!!");
+};
+
+revealMystery(mysteryEnvelope) ;
+
