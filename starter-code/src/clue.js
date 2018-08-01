576863bb9ab5ac8a001ce1f6c7f7ac714e403e99
Manuel Navarro <manuel.alejandro.navarro@gmail.com>
2018-07-31 22:14:27 -0500

    iteracion 2 done

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,69 +1,60 @@
-// Characters
-
 var mrGreen = {
-};
-mrGreen
-first_name:   'Jacob'
-last_name:    'Green'
-color:        'green'
-description:  'He has a lot of connections'
-age:          45
-image:        'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg'
+first_name:   'Jacob',
+last_name:    'Green',
+color:        'green',
+description:  'He has a lot of connections',
+age:          45,
+image:        'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
 occupation:   'Entrepreneur'
 };
 
 var drOrchid = {
-drOrchid
-frst_name:   'Doctor'
-last_name:    'Orchid'
-color:        'white'
-description:  'PhD in plant toxicology. Adopted daughter of Mr. Boddy'
-age:          26
-image:        'http://www.radiotimes.com/uploads/images/Original/111967.jpg'
+frst_name:   'Doctor',
+last_name:    'Orchid',
+color:        'white',
+description:  'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
+age:          26,
+image:        'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
 ocupation:   'Scientist'
 };
 
 var profPlum = {
-profPlum
-first_name:   'Victor'
-last_name:    'Plum'
-color:        'purple'
-description:  'Billionare video game designer'
-age:          22
-image:        'https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg'
+first_name:   'Victor',
+last_name:    'Plum',
+color:        'purple',
+description:  'Billionare video game designer',
+age:          22,
+image:        'https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg',
 occupation:   'Designer'
 };
 
 var missScarlet = {
-missScarlet
-first_name:   'Kasandra'
-last_name:    'Scarlet'
-color:        'red'
-description:  'She is an A-list movie star with a dark past'
-age:          31
-image:        'https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg'
+first_name:   'Kasandra',
+last_name:    'Scarlet',
+color:        'red',
+description:  'She is an A-list movie star with a dark past',
+age:          31,
+image:        'https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg',
 occupation:   'Actor'
 };
 
 var mrsPeacock = {
-mrsPeacock
-first_name:   'Eleanor'
-last_name:    'Peacock'
-color:        'blue'
-description:  'She is from a wealthy family and uses her status and money to earn popularity'
-age:          36
-image:        'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg'
+first_name:   'Eleanor',
+last_name:    'Peacock',
+color:        'blue',
+description:  'She is from a wealthy family and uses her status and money to earn popularity',
+age:          36,
+image:        'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
 occupation:   'Socialité'
 };
 
 var mrMustard = {
-mrMustard
-first_name:   'Jack'
-last_name:    'Mustard'
-color:        'yellow'
-description:  'He is a former football player who tries to get by on his former glory'
-age:          62
-image:        'https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg'
+first_name:   'Jack',
+last_name:    'Mustard',
+color:        'yellow',
+description:  'He is a former football player who tries to get by on his former glory',
+age:          62,
+image:        'https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg',
 occupation:   'Retired Football player'
 };
 
@@ -98,3 +89,22 @@ var weaponsArray = [
 {name: 'trophy', weight: 25},
 {name: 'pistol', weight: 20},
 ];
+
+function randomSelector(array) {
+var randomNumber = Math.floor(Math.random() * array.length);
+return array[randomNumber];
+};
+
+function pickMistery(){
+    var misteryEnvelope = [];
+    var character = randomSelector(charactersArray);
+    var weapon = randomSelector(weaponsArray);
+    var room = randomSelector(roomsArray);
+    misteryEnvelope.push(character, weapon, room);
+    return misteryEnvelope;
+}
+
+function revealMystery(){
+
+    return (first-name + last_name + "killed Mr. Boddy using the " + weapon + "in the " place + "!!!")
+}
