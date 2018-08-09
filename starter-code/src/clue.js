3927942d7db4f648e7f027d2c9141ac992343e1e
Pauline Brothier <paulinebrothier@gmail.com>
2018-08-09 17:10:18 +0200

    done

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -176,6 +176,7 @@ var weaponsArray = [];
 weaponsArray.push(rope, knife, candlestick, dumbbell, poison, axe, bat, trophy, pistol);
 
 
+// Create random
 
 function randomSelector (myArray) {
     var randomNumber = Math.floor(Math.random() * myArray.length);
@@ -187,105 +188,25 @@ randomSelector(roomsArray);
 
 // Create the mystery
 
-var misteryEnvelope = [];
-
 function pickMistery () {
-    
+
+    var misteryCard = [];
+
     var misteryChar = randomSelector(charactersArray);
     var misteryWeapons = randomSelector(weaponsArray);
     var misteryRooms = randomSelector(roomsArray);
 
-    misteryEnvelope.push(misteryChar, misteryWeapons, misteryRooms); 
+    misteryCard.push(misteryChar, misteryWeapons, misteryRooms);
 
-    return misteryEnvelope;
+    return misteryCard;
 }
 
 pickMistery();
 
+var misteryEnvelope = pickMistery ();
 
+function revealMistery(misteryEnvelope) {
+    return (misteryEnvelope[0].firstName + " " + misteryEnvelope[0].lastName + " killed Mr.Boddy unsing the " + misteryEnvelope[1].name + " in the " + misteryEnvelope[2].name + "!!!!");
+}
 
-// Characters
-/*
-mrGreen
-first_name:   Jacob
-last_name:    Green
-color:        green
-description:  He has a lot of connections
-age:          45
-image:        https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg
-occupation:   Entrepreneur
-
-drOrchid
-frst_name:   Doctor
-lst_name:    Orchid
-color:        white
-description:  PhD in plant toxicology. Adopted daughter of Mr. Boddy
-age:          26
-image:        http://www.radiotimes.com/uploads/images/Original/111967.jpg
-ocupation:   Scientist
-
-profPlum
-first_name:   Victor
-last_name:    Plum
-color:        purple
-description:  Billionare video game designer
-age:          22
-image:        https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg
-occupation:   Designer
-
-missScarlet
-first_name:   Kasandra
-last_name:    Scarlet
-color:        red
-description:  She is an A-list movie star with a dark past
-age:          31
-image:        https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg
-occupation:   Actor
-
-mrsPeacock
-first_name:   Eleanor
-last_name:    Peacock
-color:        blue
-description:  She is from a wealthy family and uses her status and money to earn popularity
-age:          36
-image:        https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg
-occupation:   Socialité
-
-mrMustard
-first_name:   Jack
-last_name:    Mustard
-color:        yellow
-description:  He is a former football player who tries to get by on his former glory
-age:          62
-image:        https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg
-occupation:   Retired Football player
-
-// Weapons
-
-name: rope        ---   weight: 10
-name: knife       ---   weight: 8
-name: candlestick ---   weight: 2
-name: dumbbell    ---   weight: 30
-name: poison      ---   weight: 2
-name: axe         ---   weight: 15
-name: bat         ---   weight: 13
-name: trophy      ---   weight: 25
-name: pistol      ---   weight: 20
-
-// Rooms
-name: Dinning Room
-name: Conservatory
-name: Kitchen
-name: Study
-name: Library
-name: Billiard Room
-name: Lounge
-name: Ballroom
-name: Hall
-name: Spa
-name: Living Room
-name: Observatory
-name: Theater
-name: Guest House
-name: Patio
-*/
+revealMistery(misteryEnvelope);
