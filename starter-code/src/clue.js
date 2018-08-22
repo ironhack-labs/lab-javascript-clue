39998ae1db5bf52f5fbf6314c9b45535d60a9f2d
agdwm <almudenaguerraswm@gmail.com>
2018-08-22 11:28:12 +0200

    Create structure of weapons

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -68,36 +68,28 @@ var charactersArray = [
 	}
 ];
 
+// Weapons Collection
+var weaponsArray = [
+	{ name: rope ,weight: 10 },
+	{ name: candlestick, weight: 2 },
+	{ name: dumbbell, weight: 30 },
+	{ name: poison, weight: 2 },
+	{ name: axe, weight: 15 },
+	{ name: bat, weight: 13 },
+	{ name: trophy, weight: 25 },
+	{ name: pistol, weight: 20 }
+];
+
+// Rooms' Collection
+var roomsArray = [
+
+];
+
+
 /*
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Weapons
 
-name: rope        ---   weight: 10
-name: knife       ---   weight: 8
-name: candlestick ---   weight: 2
-name: dumbbell    ---   weight: 30
-name: poison      ---   weight: 2
-name: axe         ---   weight: 15
-name: bat         ---   weight: 13
-name: trophy      ---   weight: 25
-name: pistol      ---   weight: 20
+
 
 // Rooms
 name: Dinning Room
@@ -119,8 +111,6 @@ name: Patio
 
 
 
-// Rooms' Collection
-var roomsArray = [];
 
-// Weapons Collection
-var weaponsArray = [];
+
+
