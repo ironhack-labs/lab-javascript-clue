691555016ae7adcd3150db69568da42f7f516772
Ana Moreno <anamariamoreno@gmail.com>
2018-09-19 15:24:24 +0200

    update

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -115,15 +115,15 @@ var roomsArray = [
   { name: 'Patio'}
 ];
 //Number 1
-function randomSelector(charactersArray){
-  if (!charactersArray.length){
+function randomSelector(array){
+  if (!array.length){
     return undefined;
   }
-  console.log(charactersArray);
-  if (charactersArray.length === 1){
-    return charactersArray[0];
+  
+  if (array.length === 1){
+    return array[0];
   }
-  return charactersArray[Math.floor(Math.random()*charactersArray.length)];
+  return array[Math.floor(Math.random()*array.length)];
  
 }
 
@@ -136,3 +136,9 @@ function pickMistery(charactersArray, weaponsArray, roomsArray){
   return misteryEnvelope;
 }
 
+// Number 3
+function revealMistery(misteryEnvelope){
+  return misteryEnvelope[0].first_name + " " + misteryEnvelope[0].last_name + 
+  " killed Mr.Boddy using the " +misteryEnvelope[1].name + " in the " + 
+  misteryEnvelope[2].name + "!!!";
+}
