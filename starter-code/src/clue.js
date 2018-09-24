5e3c4fcff048c6971c5aef8cb623187302604912
Rita Mernissi <rita.mernissi@hotmail.com>
2018-09-25 00:22:39 +0200

    2 failures remaining

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,6 +1,6 @@
 // Characters Collection
-var charactersArray = {
-    'MrGreen': {
+var charactersArray = [
+    {   name:         "mrGreen",
         first_name:   "Jacob",
         last_name:    "Green",
         color:        "green",
@@ -8,17 +8,19 @@ var charactersArray = {
         age:          45,
         image:        "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
         occupation:   "Entrepreneur"
-        }
-    , 'drOrchid': {
-        frst_name:   'Doctor',
-        lst_name:    'Orchid',
-        color:        'white',
-        description:  'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
-        age:          26,
-        image:        'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
-        ocupation:   'Scientist'
     }
-    , 'ProfPlum': {
+    , 
+    {   name:         "drOrchid",
+        first_name:   "Doctor",
+        last_name:    "Orchid",
+        color:        "white",
+        description:  "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+        age:          26,
+        image:        "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+        ocupation:    "Scientist"
+    }
+    ,
+    {   name:        "profPlum",
         first_name:  "Victor",
         last_name:   "Plum",
         color:       "purple",
@@ -27,7 +29,8 @@ var charactersArray = {
         image:       "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
         occupation:  "Designer"
     }
-    , 'missScarlet': {
+    ,
+    {   name:        "missScarlet",
         first_name:  "Kasandra",
         last_name:   "Scarlet",
         color:       "red",
@@ -36,38 +39,59 @@ var charactersArray = {
         image:       "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",
         occupation:  "Actor"
     }
-};
+    ,
+    {   name:        "mrsPeacock",
+        first_name:  "Eleanor",
+        last_name:   "Peacock",
+        color:       "blue",
+        description: "She is from a wealthy family and uses her status and money to earn popularity",
+        age:         36,
+        image:       "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+        occupation:  "Socialité"
+    }
+    ,
+    {   name:        "mrMustard",
+        first_name:  "Jack",
+        last_name:   "Mustard",
+        color:       "yellow",
+        description: "He is a former football player who tries to get by on his former glory",
+        age:         62,
+        image:       "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",
+        occupation:  "Retired Football player"
+
+    }
+];
 
 // Rooms' Collection
 var weaponsArray = [
-   {name:'rope', weight: 10},
-   {name:'knife', weight: 8},
-   {name:'candlestick', weight: 2},
-   {name:'dumbbell', weight: 30},
-   {name:'poiso', weight: 2},
-   {name:'axe', weight: 15},
-   {name:'bat', weight: 13},
-   {name:'trophy', weight: 25},
-   {name:'pistol', weight: 20}
+   {name:"rope", weight: 10},
+   {name:"knife", weight: 8},
+   {name:"candlestick", weight: 2},
+   {name:"dumbbell", weight: 30},
+   {name:"poison", weight: 2},
+   {name:"axe", weight: 15},
+   {name:"bat", weight: 13},
+   {name:"trophy", weight: 25},
+   {name:"pistol", weight: 20}
 ];
 
 // Weapons Collection
 var roomsArray = [
-    {name:'Dining Room'},
-    {name:'Conservatory'},
-    {name:'Kitchen'},
-    {name:'Study'},
-    {name:'Library'},
-    {name:'Billiard Room'},
-    {name:'Lounge'},
-    {name:'Ballroom'},
-    {name:'Hall'},
-    {name:'Spa'},
-    {name:'Living Room'},
-    {name:'Observatory'},
-    {name:'Theater'},
-    {name:'Guest House'},
-    {name:'Patio'}
+    {name:"Dining Room"},
+    {name:"Conservatory"},
+    {name:"Kitchen"},
+    {name:"Study"},
+    {name:"Library"},
+    {name:"Billiard Room"},
+    {name:"Lounge"},
+    {name:"Ballroom"},
+    {name:"Hall"},
+    {name:"Spa"},
+    {name:"Living Room"},
+    {name:"Observatory"},
+    {name:"Theater"},
+    {name:"Guest House"},
+    {name:"Patio"}
 ];
 
 /* Creating a method randomSelector to select randomly one element from a card stack.
@@ -88,8 +112,9 @@ Our mystery should be stored on a misteryEnvelope variable.
 */
 
 function pickMistery(charactersArray, weaponsArray, roomsArray) {
-    var misteryEnvelope = new Array(3);
-    misteryEnvelope[0] = randomSelector(charactersArray);
+    var misteryEnvelope = [];
+    var character = randomSelector(charactersArray);
+    misteryEnvelope[0] = [character.first_name, character.last_name];
     misteryEnvelope[1] = randomSelector(weaponsArray);
     misteryEnvelope[2] = randomSelector(roomsArray);
     return misteryEnvelope;
@@ -101,5 +126,5 @@ misteryEnvelope array as an argument and return the revealed mystery like this:
 */
 
 function revealMistery(misteryEnvelope) {
-    return "Mr " + misteryEnvelope[0] + " killed Mr Body using the " + misteryEnvelope[1] + " in the " + misteryEnvelope[2] + "."
+    return misteryEnvelope[0][0] + " " + misteryEnvelope[0][1] + " killed Mr.Boddy using the " + misteryEnvelope[1] + " in the " + misteryEnvelope[2] + "!!!!"
 };
