bbca0a8a32901784325a587a98544398668fb196
sylvia_mavuba <smavuba@voyages-sncf.com>
2018-09-25 13:14:08 +0200

    fix error

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -194,7 +194,6 @@ function pickMistery() {
 }
 
 function revealMistery(misteryEnvelope) {
-    var misteryEnvelope = pickMistery();
-    var resp = misteryEnvelope[0].first_name + ' ' + misteryEnvelope[0].last_name + ' killed Mr.Boddy using the ' + misteryEnvelope[1].name + 'in the ' + misteryEnvelope[2] + '!!!!';
+    var resp = misteryEnvelope[0].first_name + ' ' + misteryEnvelope[0].last_name + ' killed Mr.Boddy using the ' + misteryEnvelope[1].name + ' in the ' + misteryEnvelope[2].name + '!!!!';
     return resp;
 }
