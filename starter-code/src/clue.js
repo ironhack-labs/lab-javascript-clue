6ffa7d9632dfa0ce66138d30b46bcee693d4afb7
gerard0jr <gerard.os.jr@gmail.com>
2018-10-31 15:05:41 -0600

    hecho

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,94 +1,159 @@
-// Characters
-/*
-mrGreen
-first_name:   Jacob
-last_name:    Green
-color:        green
-description:  He has a lot of connections
-age:          45
-image:        https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg
-occupation:   Entrepreneur
+var charaterArray = [
+  {
+    first_name:   "Jacob",
+    last_name:    "Green",
+    color:        "green",
+    description:  "He has a lot of connections",
+    age:          45,
+    image:        "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+    occupation:   "Entrepreneur"
+  },
+  {
+    first_name:   "Doctor",
+    last_name:    "Orchid",
+    color:        "white",
+    description:  "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+    age:          26,
+    image:        "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+    ocupation:    "Scientist"
 
-drOrchid
-first_name:   Doctor
-last_name:    Orchid
-color:        white
-description:  PhD in plant toxicology. Adopted daughter of Mr. Boddy
-age:          26
-image:        http://www.radiotimes.com/uploads/images/Original/111967.jpg
-ocupation:   Scientist
-
-profPlum
-first_name:   Victor
-last_name:    Plum
-color:        purple
-description:  Billionare video game designer
-age:          22
-image:        https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg
-occupation:   Designer
-
-missScarlet
-first_name:   Kasandra
-last_name:    Scarlet
-color:        red
-description:  She is an A-list movie star with a dark past
-age:          31
-image:        https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg
-occupation:   Actor
-
-mrsPeacock
-first_name:   Eleanor
-last_name:    Peacock
-color:        blue
-description:  She is from a wealthy family and uses her status and money to earn popularity
-age:          36
-image:        https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg
-occupation:   Socialité
-
-mrMustard
-first_name:   Jack
-last_name:    Mustard
-color:        yellow
-description:  He is a former football player who tries to get by on his former glory
-age:          62
-image:        https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg
-occupation:   Retired Football player
+  },
+  {
+    first_name:   "Victor",
+    last_name:    "Plum",
+    color:        "purple",
+    description:  "Billionare video game designer",
+    age:          22,
+    image:        "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
+    occupation:   "Designer"
+  },
+  {
+    first_name:   "Kasandra",
+    last_name:    "Scarlet",
+    color:        "red",
+    description:  "She is an A-list movie star with a dark past",
+    age:          31,
+    image:        "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",
+    occupation:   "Actor"
+  },
+  {
+    first_name:   "Eleanor",
+    last_name:    "Peacock",
+    color:        "blue",
+    description:  "She is from a wealthy family and uses her status and money to earn popularity",
+    age:          36,
+    image:        "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+    occupation:   "Socialité"
+  },
+  {
+    first_name:   "Jack",
+    last_name:    "Mustard",
+    color:        "yellow",
+    description:  "He is a former football player who tries to get by on his former glory",
+    age:          62,
+    image:        "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",
+    occupation:   "Retired Football player"
+  }
+]
 
 // Weapons
 
-name: rope        ---   weight: 10
-name: knife       ---   weight: 8
-name: candlestick ---   weight: 2
-name: dumbbell    ---   weight: 30
-name: poison      ---   weight: 2
-name: axe         ---   weight: 15
-name: bat         ---   weight: 13
-name: trophy      ---   weight: 25
-name: pistol      ---   weight: 20
+var weaponsArray = [
+  {
+    name: "rope",
+    weigth: 10
+  },
+  {
+    name: "knife",
+    weigth: 8
+  },
+  {
+    name: "candlestick",
+    weigth: 2
+  },
+  {
+    name: "dumbell", 
+    weigth: 30
+  },
+  {
+    name: "poison", 
+    weigth: 2
+  },
+  {
+    name: "axe",
+    weigth: 15
+  },
+  {
+    name: "bat", 
+    weigth: 13
+  },
+  {
+    name: "trophy",
+    weigth: 25
+  },
+  {
+    name: "pistol",
+    weigth: 20
+  }
+]
 
 // Rooms
-name: Dinning Room
-name: Conservatory
-name: Kitchen
-name: Study
-name: Library
-name: Billiard Room
-name: Lounge
-name: Ballroom
-name: Hall
-name: Spa
-name: Living Room
-name: Observatory
-name: Theater
-name: Guest House
-name: Patio
-*/
+var roomsArray = [
+"Dinning Room",
+"Conservatory",
+"Kitchen",
+"Study",
+"Library",
+"Billiard Room",
+"Lounge",
+"Ballroom",
+"Hall",
+"Spa",
+"Living Room",
+"Observatory",
+"Theater",
+"Guest House",
+"Patio"
+]
 
-// Characters Collection
-var charactersArray = [];
+var datos = [
+  charaterArray,
+  weaponsArray,
+  roomsArray
+]
 
-// Rooms' Collection
-var roomsArray = [];
+function randomSelector (datos){
+  if(datos.length === 0){
+    return undefined
+  }
+  var lenChar = Math.floor( Math.random() * datos[0].length )
+  var lenWeapon = Math.floor( Math.random() * datos[1].length)
+  var lenRoom = Math.floor( Math.random() * datos[2].length)  
 
-// Weapons Collection
-var weaponsArray = [];
+  return [lenChar,lenWeapon,lenRoom]
+}
+
+function pickMistery(datos){
+  var randomArray = randomSelector(datos)
+  var info = [datos[0][randomArray[0]],datos[1][randomArray[1]],datos[2][randomArray[2]]]
+  return info;
+  
+}
+
+var misteryEnvelope = pickMistery(datos)
+
+function revealMistery (misteryEnvelope){
+  var solucion = [{
+    first_name: misteryEnvelope[0].first_name,
+    last_name: misteryEnvelope[0].last_name
+  },
+  {
+    name: misteryEnvelope[1].name
+  },
+  {
+    name: misteryEnvelope[2]
+  }
+  ]
+  return solucion
+}
+
