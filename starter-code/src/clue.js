ac169e351a30990ced7456fb0367d9b40e09e069
Salazar <antonio.salazar@oliverwyman.com>
2018-10-31 19:33:33 -0600

    done

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,50 +1,5 @@
 // Characters
 /*
-mrGreen
-first_name:   Jacob
-last_name:    Green
-color:        green
-description:  He has a lot of connections
-age:          45
-image:        https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg
-occupation:   Entrepreneur
-
-drOrchid
-first_name:   Doctor
-last_name:    Orchid
-color:        white
-description:  PhD in plant toxicology. Adopted daughter of Mr. Boddy
-age:          26
-image:        http://www.radiotimes.com/uploads/images/Original/111967.jpg
-ocupation:   Scientist
-
-profPlum
-first_name:   Victor
-last_name:    Plum
-color:        purple
-description:  Billionare video game designer
-age:          22
-image:        https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg
-occupation:   Designer
-
-missScarlet
-first_name:   Kasandra
-last_name:    Scarlet
-color:        red
-description:  She is an A-list movie star with a dark past
-age:          31
-image:        https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg
-occupation:   Actor
-
-mrsPeacock
-first_name:   Eleanor
-last_name:    Peacock
-color:        blue
-description:  She is from a wealthy family and uses her status and money to earn popularity
-age:          36
-image:        https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg
-occupation:   Socialité
-
 mrMustard
 first_name:   Jack
 last_name:    Mustard
@@ -54,17 +9,6 @@ age:          62
 image:        https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg
 occupation:   Retired Football player
 
-// Weapons
-
-name: rope        ---   weight: 10
-name: knife       ---   weight: 8
-name: candlestick ---   weight: 2
-name: dumbbell    ---   weight: 30
-name: poison      ---   weight: 2
-name: axe         ---   weight: 15
-name: bat         ---   weight: 13
-name: trophy      ---   weight: 25
-name: pistol      ---   weight: 20
 
 // Rooms
 name: Dinning Room
@@ -83,61 +27,200 @@ name: Theater
 name: Guest House
 name: Patio
 */
-
-var character = [];
-
-character.push(Object.values(charactersArray))
-
-console.log(character)
-
-function randomSelector( array ){
-  for(let i = 0; i < array.length; i++){
-    if(array.length === 0){
-      return 
-    } else if( array.length === 1){
-      console.log(array[i]);
-      return array[i];
-    } else if( array.length ){
-      console.log(array[i])
-      return array[i]
-    } else if( array.length){
-      return array - Math.random();
-    }
+// Characters Collection
+var charactersArray = [
+  {
+   
+      first_name:   "Jacob",
+      last_name:    "Green",
+      color:        "green",
+      description:  "He has a lot of connections",
+      age:          45,
+      image:        "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+      occupation:   "Entrepreneur"
+      
     
+  },
+  {
+    
+        first_name:   "Doctor",
+        last_name:    "Orchid",
+        color:        "white",
+        description:  "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+        age:          26,
+        image:        "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+        ocupation:   "Scientist"
+      
+  },
+  {
+    
+        first_name:   "Victor",
+        last_name:    "Plum",
+        color:        "purple",
+        description:  "Billionare video game designer",
+        age:          22,
+        image:        "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
+        occupation:   "Designer"
+      
+
+  },
+  {
+    
+        first_name:   "Kasandra",
+        last_name:    "Scarlet",
+        color:        "red",
+        description:  "She is an A-list movie star with a dark past",
+        age:          31,
+        image:        "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",
+        occupation:   "Actor"
+      
+  },
+  {
+    
+        first_name:   "Eleanor",
+        last_name:    "Peacock",
+        color:        "blue",
+        description:  "She is from a wealthy family and uses her status and money to earn popularity",
+        age:          36,
+        image:        "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+        occupation:   "Socialité"
+      
+  },
+  {
+    
+      first_name:   "Jack",
+      last_name:    "Mustard",
+      color:        "yellow",
+      description:  "He is a former football player who tries to get by on his former glory",
+      age:          62,
+      image:        "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",
+      occupation:   "Retired Football player"
+    
+  }
+];
+
+
+  
+
+
+// Rooms' Collection
+var roomsArray = [
+  {
+    name: "Dinning Room"
+  },
+  {
+    name: "Conservatory"
+  },
+  {
+    name: "Kitchen"
+  },
+  {
+    name: "Study"
+  },
+  {
+    name: "Library"
+  },
+  {
+    name: "Billiard Room"
+  },
+  {
+    name: "Lounge"
+  },
+  {
+    name: "Ballroom"
+  }, 
+  {
+    name: "Hall"
+  },
+  {
+    name: "Spa"
+  },
+  {
+    name: "Living Room"
+  },
+  {
+    name: "Observatory"
+  },
+  {
+    name: "Theater"
+  },
+  {
+    name: "Guest House"
+  },
+  {
+    name: "Patio"
+  }
+];
+
+// Weapons Collection
+var weaponsArray = [
+  {
+    name: "rope",
+    weight: 10,
+  },
+  {
+    name: "knife",
+    weight: 8
+  },
+  {
+    name: "candlestick",
+    weight: 2
+  },
+  {
+    name: "dumbbell",
+    weight: 30
+  },
+  {
+    name: "poison",
+    weight: 2
+  },
+  {
+    name: "axe",
+    weight: 15
+  },
+  {
+    name: "bat",
+    weight: 13
+  },
+  {
+    name: "trophy",
+    weight: 25
+  },
+  {
+    name: "pistol",
+    weight: 20
+  }
+];
+
+
+function randomSelector(charactersArray){
+  for(let i = 0; i <= charactersArray.length; i++){
+      if(charactersArray.length === 0){
+        return 
+      } else if( charactersArray.length === 1){
+        return charactersArray[i];
+      } else{
+        return charactersArray[Math.floor(Math.random()*charactersArray.length)];
+      }
   }
 }
 
+function pickMistery( ){
+  var newArray = [];
 
-// Characters Collection
-var charactersArray = {
-  mrGreen : [
-    { 
-    first_name:   "Jacob",
-    last_name:    "Green",
-    color:        "green",
-    description:  "He has a lot of connections",
-    age:          45,
-    image:        "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
-    occupation:   "Entrepreneur"
-    }
-  ], 
+  newArray.push(randomSelector(charactersArray))
+  newArray.push(randomSelector(weaponsArray))
+  newArray.push(randomSelector(roomsArray))
+  
+  
+  return newArray
 
-  drOrchid:
-  [
-    {
-      first_name:   "Doctor",
-      last_name:    "Orchid",
-      color:        "white",
-      description:  "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
-      age:          26,
-      image:        "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
-      ocupation:   "Scientist"
-    }
-  ]
-};
+}
 
-// Rooms' Collection
-var roomsArray = [];
+var misteryArray = pickMistery();
 
-// Weapons Collection
-var weaponsArray = [];
+function revealMistery( truth ){
+   
+  misteryResolved = `${misteryArray[0].first_name} ${misteryArray[0].last_name}  killed Mr. Boddy using the ${misteryArray[1].name} in the ${misteryArray[2].name}`
+  return  misteryResolved
+}
