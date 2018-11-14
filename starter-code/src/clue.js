50951c61021449ff40ba166fecc0bc0d625cd1aa
Mariana Macedo Rodrigues da Silva <mariana.macedo@anbima.com>
2018-11-14 17:05:49 -0200

    feito, mas com erro no jasmine

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -85,17 +85,17 @@ name: Patio
 */
 
 // Characters Collection
-var charactersArray = [
-    {first_name: 'Jacob', last_name:'Green', color: 'green', description: 'He has a lot of connections',
-age: 45, image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
- occupation:'Scientit'}
-];
+var charactersArray = [];
 addCharactersArray = (fn, ln, color, description, age, image, occupation) => {
     const character =  {first_name: fn, last_name:ln, color: color, description: description,
     age: age, image: image,
      occupation:occupation}
      charactersArray.push(character);
 }
+//add mrGreen
+addCharactersArray('Jacob','Green','green', 'He has a lot of connections', 45, 
+'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg', 'Scientit' );
+
 //add drOrchid
 addCharactersArray('Doctor','Orchid','white','PhD in plant toxicology. Adopted daughter of Mr. Boddy',
 26,'http://www.radiotimes.com/uploads/images/Original/111967.jpg','Scientist');
@@ -116,7 +116,7 @@ addCharactersArray('Eleanor', 'Peacock', 'blue', 'She is from a wealthy family a
 addCharactersArray('Jack', 'Mustard', 'yellow', 'He is a former football player who tries to get by on his former glory',
 62, 'https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg','Retired Football player')
 
-
+// console.log(charactersArray);
 // Rooms' Collection
 var roomsArray = [];
 addRooms = (name) => {
@@ -138,6 +138,7 @@ addRooms('Observatory');
 addRooms('Theater');
 addRooms('Guest House');
 addRooms('Patio');
+// console.log(roomsArray);
 
 // Weapons Collection
 var weaponsArray = [];
@@ -155,5 +156,34 @@ addWeapons('axe', 15);
 addWeapons('bat', 13);
 addWeapons('thophy', 25);
 addWeapons('pistol', 20);
-console.log(weaponsArray);
+// console.log(weaponsArray);
+
+randomSelector = (cardStack) => {
+    let randomNumber = Math.random()*cardStack.length;
+    const card = cardStack[Math.floor(randomNumber)] ; 
+    // console.log(card);
+    return card;
+}
+
+pickMistery = (charactersArray, weaponsArray, roomsArray) => {
+    const misteryEnvelope = [];
+    if(charactersArray.length != 0 || weaponsArray.length !=0 || roomsArray.length !=0){
+    const characterCard = randomSelector(charactersArray);
+    const weaponCard = randomSelector(weaponsArray);
+    const roomCard = randomSelector(roomsArray);
+    misteryEnvelope.push(characterCard, weaponCard, roomCard);
+    return misteryEnvelope ;
+}
+
+}
+revealMistery = (envelope) => {
+    const firstName = envelope[0].first_name;
+    const lastName = envelope[0].last_name;
+    const weapon = envelope[1].name;
+    const room = envelope[2].name;
+    return `${firstName} ${lastName} killed Mr.Boddy using the ${weapon} in the ${room}!!!!`
+}
+const misteryEnvelope = pickMistery(charactersArray, weaponsArray, roomsArray);
+
+console.log(revealMistery(misteryEnvelope));
 
