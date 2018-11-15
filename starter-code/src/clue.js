c6a939b5b4a3adfd561baee47f24c0663617b449
Diego Ramos <diegoramos.media@gmail.com>
2018-11-15 10:03:51 -0200

    JS | Clue - Mixing objects and arrays - wip

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -87,58 +87,58 @@ name: Patio
 // Characters Collection
 var charactersArray = [
     {
-        first_name:   "Jacob",
-        last_name:    "Green",
-        color:        "green",
-        description:  "He has a lot of connections",
-        age:          45,
-        image:        "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
-        occupation:   "Entrepreneur"
+        first_name: "Jacob",
+        last_name: "Green",
+        color: "green",
+        description: "He has a lot of connections",
+        age: 45,
+        image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+        occupation: "Entrepreneur"
     },
     {
-        first_name:   "Doctor",
-        last_name:    "Orchid",
-        color:        "white",
-        description:  "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
-        age:          26,
-        image:        "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
-        ocupation:   "Scientist"
+        first_name: "Doctor",
+        last_name: "Orchid",
+        color: "white",
+        description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+        age: 26,
+        image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+        ocupation: "Scientist"
     },
     {
-        first_name:   "Victor",
-        last_name:    "Plum",
-        color:        "purple",
-        description:  "Billionare video game designer",
-        age:          22,
-        image:        "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
-        occupation:   "Designer"
+        first_name: "Victor",
+        last_name: "Plum",
+        color: "purple",
+        description: "Billionare video game designer",
+        age: 22,
+        image: "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
+        occupation: "Designer"
     },
     {
-        first_name:   "Kasandra",
-        last_name:    "Scarlet",
-        color:        "red",
-        description:  "She is an A-list movie star with a dark past",
-        age:          31,
-        image:        "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",
-        occupation:   "Actor"   
+        first_name: "Kasandra",
+        last_name: "Scarlet",
+        color: "red",
+        description: "She is an A-list movie star with a dark past",
+        age: 31,
+        image: "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",
+        occupation: "Actor"
     },
     {
-        first_name:   "Eleanor",
-        last_name:    "Peacock",
-        color:        "blue",
-        description:  "She is from a wealthy family and uses her status and money to earn popularity",
-        age:          36,
-        image:        "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
-        occupation:   "Socialité"
+        first_name: "Eleanor",
+        last_name: "Peacock",
+        color: "blue",
+        description: "She is from a wealthy family and uses her status and money to earn popularity",
+        age: 36,
+        image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+        occupation: "Socialité"
     },
     {
-        first_name:   "Jack",
-        last_name:    "Mustard",
-        color:        "yellow",
-        description:  "He is a former football player who tries to get by on his former glory",
-        age:          62,
-        image:        "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",
-        occupation:   "Retired Football player"
+        first_name: "Jack",
+        last_name: "Mustard",
+        color: "yellow",
+        description: "He is a former football player who tries to get by on his former glory",
+        age: 62,
+        image: "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",
+        occupation: "Retired Football player"
 
     }
 
@@ -146,56 +146,57 @@ var charactersArray = [
 
 // Rooms' Collection
 var roomsArray = [
-    {name: "Dinning Room"},
-    {name: "Conservatory"},
-    {name: "Kitchen"},
-    {name: "Study"},
-    {name: "Library"},
-    {name: "Billiard Room"},
-    {name: "Lounge"},
-    {name: "Ballroom"},
-    {name: "Hall"},
-    {name: "Spa"},
-    {name: "Living Room"},
-    {name: "Observatory"},
-    {name: "Theater"},
-    {name: "Guest House"},
-    {name: "Patio"}
-    
+    { name: "Dinning Room" },
+    { name: "Conservatory" },
+    { name: "Kitchen" },
+    { name: "Study" },
+    { name: "Library" },
+    { name: "Billiard Room" },
+    { name: "Lounge" },
+    { name: "Ballroom" },
+    { name: "Hall" },
+    { name: "Spa" },
+    { name: "Living Room" },
+    { name: "Observatory" },
+    { name: "Theater" },
+    { name: "Guest House" },
+    { name: "Patio" }
+
 ];
 
 // Weapons Collection
 var weaponsArray = [
-        {name: "rope"        ,   weight: 10},
-        {name: "knife"       ,   weight: 8},
-        {name: "candlestick" ,   weight: 2},
-        {name: "dumbbell"    ,   weight: 30},
-        {name: "poison"      ,   weight: 2},
-        {name: "axe"         ,   weight: 15},
-        {name: "bat"         ,   weight: 13},
-        {name: "trophy"      ,   weight: 25},
-        {name: "pistol"      ,   weight: 20}
+    { name: "rope", weight: 10 },
+    { name: "knife", weight: 8 },
+    { name: "candlestick", weight: 2 },
+    { name: "dumbbell", weight: 30 },
+    { name: "poison", weight: 2 },
+    { name: "axe", weight: 15 },
+    { name: "bat", weight: 13 },
+    { name: "trophy", weight: 25 },
+    { name: "pistol", weight: 20 }
 
 ];
 
-console.log(charactersArray.length-1);
+// console.log(charactersArray.length - 1);
 var objectChosen;
-const randomSelector = (array) =>{
+const randomSelector = (array) => {
     objectChosen = Math.floor(Math.random() * (array.length)) + 0;
     return array[objectChosen];
 };
 
 var mysteryEnvelope = [];
-const pickMistery = () =>{
-    mysteryEnvelope.push(randomSelector(weaponsArray),randomSelector(roomsArray),randomSelector(charactersArray));
+const pickMistery = (arrayW, arrayR, arrayC) => {
+    mysteryEnvelope.push(randomSelector(arrayW), randomSelector(arrayR), randomSelector(arrayC));
 
     return mysteryEnvelope;
 }
 
-console.log(pickMistery.values());
+console.log('MYSTERY ENVELOPE')
+console.log(pickMistery(weaponsArray, roomsArray, charactersArray));
 
 
-console.log(randomSelector(weaponsArray));
+// console.log(randomSelector(weaponsArray));
 
 
 //randomSelector({charactersArray,roomsArray,weaponsArray});
