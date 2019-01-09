e750d961eb33b7eaaa44d75fa5085902008d5de3
Pedro Arantes <pedro.hga90@gmail.com>
2019-01-09 18:14:18 +0100

    Submission

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -67,28 +67,137 @@ name: trophy      ---   weight: 25
 name: pistol      ---   weight: 20
 
 // Rooms
-name: Dinning Room
-name: Conservatory
-name: Kitchen
-name: Study
-name: Library
-name: Billiard Room
-name: Lounge
-name: Ballroom
-name: Hall
-name: Spa
-name: Living Room
-name: Observatory
-name: Theater
-name: Guest House
-name: Patio
+" Dinning Room"
+" Conservatory"
+" Kitchen"
+ "Study"
+ "Library"
+ "Billiard Room"
+" Lounge"
+ "Ballroom"
+ Hall
+ Spa
+ Living Room"
+" Observatory"
+" Theater
+ Guest House"
+" Patio
 */
 
 // Characters Collection
-var charactersArray = [];
+var charactersArray = [
+  {first_name:   'Jacob',
+  last_name:    'Green',
+  color:        'green',
+  description:  'He has a lot of connections',
+  age:          45,
+  image:        'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+  occupation:   'Entrepreneur'
+  },
+
+  {first_name:   'Doctor',
+  last_name:    'Orchid',
+  color:        'white',
+  description:  'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
+  age:          26,
+  image:        'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
+  ocupation:   'Scientist'
+  },
+
+  {first_name:   'Victor',
+  last_name:    'Plum',
+  color:        'purple',
+  description:  'Billionare video game designer',
+  age:          22,
+  image:        'https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg',
+  occupation:   'Designer'
+  },
+
+  {first_name:   'Kasandra',
+  last_name:    'Scarlet',
+  color:        'red',
+  description:  'She is an A-list movie star with a dark past',
+  age:          31,
+  image:        'https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg',
+  occupation:   'Actor'
+  },
+
+  {first_name:   'Eleanor',
+  last_name:    'Peacock',
+  color:        'blue',
+  description:  'She is from a wealthy family and uses her status and money to earn popularity',
+  age:          36,
+  image:        'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
+  occupation:   'Socialité'
+  },
+
+  {first_name:   'Jack',
+  last_name:    'Mustard',
+  color:        'yellow',
+  description:  'He is a former football player who tries to get by on his former glory',
+  age:          62,
+  image:        'https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg',
+  occupation:   'Retired Football player'
+  }
+];
 
 // Rooms' Collection
-var roomsArray = [];
+var roomsArray =
+  ["DinningRoom",
+    "Conservatory",
+    "Kitchen",
+    "Study",
+    "Library",
+    "BilliardRoom",
+    "Lounge",
+    "Ballroom",
+    "Hall",
+    "Spa",
+    "LivingRoom",
+    "Observatory",
+    "Theater",
+    "GuestHouse",
+    "Patio"];
 
 // Weapons Collection
-var weaponsArray = [];
+var weaponsArray = [
+  {name: "rope", weight: 10},
+  {name: "knife", weight: 8},
+  {name: "candlestick", weight: 2},
+  {name: "dumbbell", weight: 30},
+  {name: "poison", weight: 2},
+  {name: "axe", weight: 15},
+  {name: "bat", weight: 13},
+  {name: "trophy", weight: 25},
+  {name: "pistol", weight: 20},
+];
+
+
+function randomSelector(array){
+  var a = Math.floor(Math.random()*array.length)
+  if(array === undefined){
+    return array[0];
+  }else {
+    return array[a]
+  }
+}
+
+
+
+function pickMistery(){
+  
+  var suspect = randomSelector(charactersArray)
+  var room = randomSelector(roomsArray)
+  var weapon = randomSelector(weaponsArray)
+  var misteryEvelope = [suspect, weapon, room]
+  return misteryEvelope
+}
+
+function revealMistery(misteryEnvelope){
+  console.log(misteryEnvelope[0].first_name + " " + misteryEnvelope[0].last_name + " killed Mr. Boddy using the " + misteryEnvelope[1].name + " in the " + misteryEnvelope[2] + "!!!!" 
+  )
+  //<FIRST NAME> <LAST NAME> killed Mr.Boddy using the <WEAPON> in the <PLACE>!!!!
+  return misteryEnvelope[0].first_name + " " + misteryEnvelope[0].last_name + " killed Mr. Boddy using the " + misteryEnvelope[1].name + " in the " + misteryEnvelope[2] + "!!!!" 
+  
+}
+revealMistery(misteryEnvelope)
