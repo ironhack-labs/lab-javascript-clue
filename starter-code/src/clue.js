2286964dbeae5c36f0d7a1d3ad1f00f469383897
Alfonso Silva <alfsilfer@gmail.com>
2019-01-09 17:50:23 +0100

    ppobjects2

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,4 +1,5 @@
 // Characters
+/*
 var mrGreen ={
   first_name:   "Jacob",
   last_name:    "Green",
@@ -79,35 +80,133 @@ var pistol = {name: "pistol",weight: 20};
 
 
 //Rooms
-function rooms(){
-  var dinningRoom = {name: "Dinning room"};
-  var Conservatory = {name: "Conservatory"};
-  var Kitchen = {name: "Kitchen"};
-  var study = {name:"Study"};
-  var library = {name:"Library"};
-  var billiardRoom = {name:"Billiard Room"};
-  var lounge = {name:"Lounge"};
-  var ballroom = {name:"Ballroom"};
-  var hall = {name: "Hall"};
-  var spa = {name:"Spa"};
-  var livingRoom = {name:"Living Room"};
-  var observatory = {name:"Observatory"};
-  var theater = {name:"Theater"};
-  var guestHouse = {name:"Guest House"};
-  var patio = {name: "patio"}
-  };
 
-
-console.log(rooms);
+var dinningRoom = {name: "Dinning room"};
+var Conservatory = {name: "Conservatory"};
+var Kitchen = {name: "Kitchen"};
+var study = {name:"Study"};
+var library = {name:"Library"};
+var billiardRoom = {name:"Billiard Room"};
+var lounge = {name:"Lounge"};
+var ballroom = {name:"Ballroom"};
+var hall = {name: "Hall"};
+var spa = {name:"Spa"};
+var livingRoom = {name:"Living Room"};
+var observatory = {name:"Observatory"};
+var theater = {name:"Theater"};
+var guestHouse = {name:"Guest House"};
+var patio = {name: "patio"}
+*/
 
 
 
 // Characters Collection
-var charactersArray = [];
+var charactersArray = [
+  {first_name:   "Jacob",
+  last_name:    "Green",
+  color:        "green",
+  description:  "He has a lot of connections",
+  age:          45,
+  image:        "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+  occupation:   "Entrepreneur"
+},
+{
+  first_name:   "Doctor",
+  last_name:    "Orchid",
+  color:        "white",
+  description:  "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+  age:          26,
+  image:        "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+  ocupation:   "Scientist"
+},
+{
+  first_name:   "Victor",
+  last_name:    "Plum",
+  color:        "purple",
+  description:  "Billionare video game designer",
+  age:          22,
+  image:        "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
+  occupation:   "Designer"
+},
+{
+  first_name:   "Kasandra",
+  last_name:    "Scarlet",
+  color:        "red",
+  description:  "She is an A-list movie star with a dark past",
+  age:          31,
+  image:        "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",
+  occupation:   "Actor"
+},
+{
+  first_name:   "Eleanor",
+  last_name:    "Peacock",
+  color:        "blue",
+  description:  "She is from a wealthy family and uses her status and money to earn popularity",
+  age:          36,
+  image:        "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+  occupation:   "Socialité"
+},
+{
+  first_name:   "Jack",
+  last_name:    "Mustard",
+  color:        "yellow",
+  description:  "He is a former football player who tries to get by on his former glory",
+  age:          62,
+  image:        "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",
+  occupation:   "Retired Football player",
+}
+];
+
 
 // Rooms' Collection
-var roomsArray = [];
-roomsArray.push(rooms())
-console.log(roomsArray);
+var roomsArray = [
+  {name: "Dinning room"},
+  {name: "Conservatory"},
+  {name: "Kitchen"},
+  {name:"Study"},
+  {name:"Library"},
+  {name:"Billiard Room"},
+  {name:"Lounge"},
+  {name:"Ballroom"},
+  {name: "Hall"},
+  {name:"Spa"},
+  {name:"Living Room"},
+  {name:"Observatory"},
+  {name:"Theater"},
+  {name:"Guest House"},
+  {name: "patio"}
+];
+
 // Weapons Collection
-var weaponsArray = [];
+var weaponsArray = [
+  {name: "rope", weight: 10},
+  {name: "knife", weight: 8},
+  {name: "candlestick", weight: 2}, 
+  {name:"dumbbell",weight: 30},
+  {name: "poison", weight: 2},
+  {name: "axe", weight: 15},
+  {name: "bat",weight: 13},
+  {name: "trophy", weight: 25},
+  {name: "pistol",weight: 20}
+];
+
+function randomSelector(arrayQueRecibe){
+    var miNumeroRandom=Math.floor(Math.random()*(arrayQueRecibe.length-0)+0)
+ 
+    return  arrayQueRecibe[miNumeroRandom] 
+}
+
+function pickMistery(){
+  var randomWeapon=randomSelector(weaponsArray)
+  var randomCharacters = randomSelector(charactersArray)
+  var randomRoom = randomSelector(roomsArray)
+  
+    var misteryEnvelope = [randomCharacters, randomWeapon,randomRoom];
+    return misteryEnvelope
+  // var misteryEnvelope = [randomSelector(charactersArray),randomSelector(weaponsArray), randomSelector(roomsArray)];
+  
+}
+
+function revealMistery(val){
+  return val[0].first_name + " " +val[0].last_name + " killed Mr.Boddy using the "+ val[1].name+ " in the "+val[2].name+ "!!!!";
+}
