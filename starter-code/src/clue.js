82a23ec1e1668a37142a52e02d28abed24879bb2
dreazz <agustinsilveira.96@gmail.com>
2019-01-11 09:50:19 +0100

    fix revealMistery, now code pass

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -163,9 +163,8 @@ function pickMistery(){
 
 }
 
-function revealMistery(){
-  var misteryEnvelope = [];
-  misteryEnvelope = pickMistery();
+function revealMistery(misteryEnvelope){
+//  misteryEnvelope = pickMistery();
 
   
   return `${misteryEnvelope[0].first_name} ${misteryEnvelope[0].last_name} killed Mr.Boddy using the ${misteryEnvelope[1].name} in the ${misteryEnvelope[2].name}!!!!`;
