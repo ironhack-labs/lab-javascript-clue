153141f6229f328b737c1f89942659b4a253581e
Rafael Torres Escobar <torresc.rafael@gmail.com>
2019-01-23 19:57:41 -0600

    Javasript Objects

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -84,11 +84,157 @@ name: Guest House
 name: Patio
 */
 
+let mrGreen = {
+    first_name:   "Jacob",
+    last_name:    "Green",
+    color:        "green",
+    description:  "He has a lot of connections",
+    age:          45,
+    image:        "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+    occupation:   "Entrepreneur",
+}
+
+
+let drOrchid = {
+    first_name:   "Doctor",
+    last_name:    "Orchid",
+    color:        "white",
+    description:  "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+    age:          26,
+    image:        "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+    ocupation:   "Scientist"
+}
+
+
+let profPlum ={
+    first_name:   "Victor",
+    last_name:    "Plum",
+    color:        "purple",
+    description:  "Billionare video game designer",
+    age:          22,
+    image:        "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
+    occupation:   "Designer"
+}
+
+let missScarlet = {
+    first_name:   "Kasandra",
+    last_name:    "Scarlet",
+    color:        "red",
+    description:  "She is an A-list movie star with a dark past",
+    age:          31,
+    image:        "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",
+    occupation:   "Actor"
+}
+
+let mrsPeacock = {
+    first_name:   "Eleanor",
+    last_name:    "Peacock",
+    color:        "blue",
+    description:  "She is from a wealthy family and uses her status and money to earn popularity",
+    age:          36,
+    image:        "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+    occupation:   "Socialité"
+}
+
+let mrMustard = {
+    first_name:   "Jack",
+    last_name:    "Mustard",
+    color:        "yellow",
+    description:  "He is a former football player who tries to get by on his former glory",
+    age:          62,
+    image:        "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",
+    occupation:   "Retired Football player"
+}
+
+
+
+let rope = {
+    name: "rope",
+    weight: 10,
+}
+let knife = {
+    name: "knife",
+    weigth: 8,
+}
+let candlestick = {
+    name: "candlestick",
+    weigth: 2,
+}
+let dumbbell = {
+    name:"dumbbell",
+    weigth: 30,
+}
+let poison = {
+    name:"poison",
+    weigth: 2,
+}
+let axe = {
+    name:"axe",
+    weigth:15,
+}
+let bat = {
+    name:"bat",
+    weigth: 13,
+}
+let trophy = {
+    name:"trophy",
+    weigth: 25,
+}
+let pistol = { 
+    name:"pistol",
+    weigth: 20,
+}
+
+// Rooms
+let dinningRoom = {
+    name: Dinning "Room",
+}
+
+let conservatoryRoom={
+    name: "Conservatory"
+}
+
+
+let kitcherRoom = {name: "Kitchen"}
+let studyRoom = {name: "Study"}
+let libraryRoom = {name: "Library"}
+let billiardRoom = {name: "Billiard Room"}
+let loungeRoom {name: "Lounge"}
+let ballRoom = {name: "Ballroom"}
+let hallRoom = {name: "Hall "}
+name: Spa
+name: Living Room
+name: Observatory
+name: Theater
+name: Guest House
+name: Patio
+*/
+
+
 // Characters Collection
-var charactersArray = [];
+var charactersArray = [mrGreen, drOrchid , profPlum,missScarlet, mrsPeacock,mrMustard];
 
 // Rooms' Collection
-var roomsArray = [];
+var roomsArray = ["Dinning Room", "Conservatory", "Kitchen", "Study", "Library", "Billard Room", "Lounge", "Ballroom", "Hall", "Spa", "Living Room", "Observatory", "Theater", "Guest  House", "Patio"];
 
 // Weapons Collection
-var weaponsArray = [];
+var weaponsArray = [rope, knife, candlestick, dumbbell, poison, axe, bat, trophy, pistol];
+
+function randomSelector(arr){
+    if(arr.length == 0) return undefined
+    var selector = Math.floor(Math.random() * arr.length);
+    return arr[selector]
+}
+
+function pickMistery(){
+    return [randomSelector(charactersArray),randomSelector(weaponsArray),randomSelector(roomsArray)]
+  }
+  
+let misteryEnvelope = pickMistery();
+
+
+function revealMistery(mistery){
+    return mistery[0].first_name+ " " + mistery[0].last_name + " " + "killed Mr.Boddy using the " + mistery[1].name +  " in the "+ mistery[2]  + "!!!!"
+}
+
+revealMistery(misteryEnvelope)
