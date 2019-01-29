6a9f35f1aef4ac33ac44fcc4804a331e8b07d045
David Gallego Gomez <davizs@hotmail.com>
2019-01-29 23:45:47 +0100

    Terminadas las funciones a falta de pasar todos los tests

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -150,7 +150,8 @@ var mrMustard = {
 charactersArray = [mrGreen, drOrchid, profPlum, missScarlet, mrsPeacock, mrMustard];
 
 // Rooms' Collection
-var roomsArray = ['Dinning Room', 'Conservatory', 'Kitchen', 'Study', 'Library', 'Billiard Room', 'Lounge', 'Ballroom', 'Hall', 'Spa', 'Living Room', 'Observatory', 'Theater', 'Guest House', 'Patio'];
+var roomsArray = ['Dinning Room', 'Conservatory', 'Kitchen', 'Study', 'Library', 'Billiard Room', 'Lounge'
+                  , 'Ballroom', 'Hall', 'Spa', 'Living Room', 'Observatory', 'Theater', 'Guest House', 'Patio'];
 
 // Weapons Collection
 var weaponsArray = [];
@@ -167,10 +168,20 @@ var pistol = { name: 'pistol' , weight: 20 }
 
 weaponsArray = [rope, knife, candlestick, dumbbell, poison, axe, bat, trophy, pistol];
 
-function randomSelector () {
-
+function randomSelector (gameArray) {
+  var pos = Math.floor(Math.random() * gameArray.length);
+  return gameArray[pos];
 }
 
 function pickMistery () {
-  var misteryEnvelope = [];
+  var misteryEnvelope = [randomSelector(charactersArray), randomSelector(roomsArray), randomSelector(weaponsArray)];
+  return misteryEnvelope;
 }
+
+function revealMistery (misteryEnvelope) {
+  console.log( misteryEnvelope[0].first_name + ' ' + misteryEnvelope[0].last_name + ' killed Mr.Boddy using the ' 
+              + misteryEnvelope[2].name + ' in the ' + misteryEnvelope[1] + '!!!!' );
+}
+
+var mistery = pickMistery();
+revealMistery(mistery);
