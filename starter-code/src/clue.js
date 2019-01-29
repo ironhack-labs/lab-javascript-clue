cb3034273280a3bde08e80f5538ad43811e07615
SANDRA LERMA MARTIN <t151744@t0.inet>
2019-01-29 12:22:46 +0100

    Ejercicio JS Cluedo Día 4

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -92,3 +92,147 @@ var roomsArray = [];
 
 // Weapons Collection
 var weaponsArray = [];
+
+
+//People
+
+var mrGreen={
+first_name:   "Jacob",
+last_name:    "Green",
+color:        "green",
+description:  "He has a lot of connections",
+age:          45,
+image:        "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+occupation:   "Entrepreneur"
+};
+
+var drOrchid={
+first_name:   "Doctor",
+last_name:    "Orchid",
+color:        "white",
+description:  "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+age:          26,
+image:        "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+ocupation:   "Scientist"
+};
+
+var profPlum={
+first_name:   "Victor",
+last_name:    "Plum",
+color:        "purple",
+description:  "Billionare video game designer",
+age:          22,
+image:        "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
+occupation:   "Designer"
+};
+
+var missScarlet={
+first_name:   "Kasandra",
+last_name:    "Scarlet",
+color:        "red",
+description:  "She is an A-list movie star with a dark past",
+age:          31,
+image:        "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",
+occupation:   "Actor"
+};
+
+var mrsPeacock={
+first_name:   "Eleanor",
+last_name:    "Peacock",
+color:        "blue",
+description:  "She is from a wealthy family and uses her status and money to earn popularity",
+age:          36,
+image:        "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+occupation:   "Socialité"
+};
+
+var mrMustard={
+first_name:   "Jack",
+last_name:    "Mustard",
+color:        "yellow",
+description:  "He is a former football player who tries to get by on his former glory",
+age:          62,
+image:        "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",
+occupation:   "Retired Football player"
+};
+charactersArray.push( mrGreen, drOrchid, profPlum, missScarlet, mrsPeacock,mrMustard);
+
+weaponsArray.push({name: "rope",weight:10},
+{name: "knife", weight: 8},
+{name: "candlestick", weight: 2},
+{name: "dumbbell",weight: 30},
+{name: "poison",weight: 2},
+{name: "axe",weight: 15},
+{name: "bat",weight: 13},
+{name: "trophy",weight: 25},
+{name: "pistol",weight: 20} );
+
+
+roomsArray.push(
+    {name: "Dinning Room"},
+    {name: "Conservatory"},
+    {name: "Kitchen"},
+    {name: "Study"},
+    {name: "Library"},
+    {name: "Billiard Room"},
+    {name: "Lounge"},
+    {name: "Ballroom"},
+    {name: "Hall"},
+    {name: "Spa"},
+    {name: "Living Room"},
+    {name: "Observatory"},
+    {name: "Theater"},
+    {name: "Guest House"},
+    {name: "Patio"}
+);
+
+
+//PICK MISTERY
+
+function randomSelector(stack){
+if(stack !=''){
+var max=stack.length; //min lo incluye pero max no, por eso no hay que poner stack.length-1
+var min = 0;
+var i=Math.floor(Math.random() * (max - min) + min); 
+
+return stack[i];
+}
+return undefined;
+};
+
+
+function pickMistery(){
+
+var envelope=[];
+
+//escoger personaje
+   envelope.push(randomSelector(charactersArray));
+
+//escoger arma
+
+envelope.push(randomSelector(weaponsArray));
+
+//escoger habitación
+envelope.push(randomSelector(roomsArray));
+
+return envelope;
+
+} 
+
+var misteryEnvelope =pickMistery();
+
+
+//REVEAL MISTERY
+
+function revealMistery(misteryEnvelope){
+
+var firstName=misteryEnvelope[0].first_name;
+var lastName=misteryEnvelope[0].last_name;
+var weapon=misteryEnvelope[1].name;
+var place=misteryEnvelope[2].name;
+
+var misterio=firstName+" "+lastName+" killed Mr.Boddy using the "+weapon+" in the "+place+"!!!!";
+
+return misterio;
+
+};
