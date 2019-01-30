6caca52c8cdee03d0c3c7a63a2d7bfaf789a414a
Javi L <jlopezro@gmail.com>
2019-01-30 18:51:46 +0100

    done

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,89 +1,3 @@
-// Characters
-/*
-mrGreen
-first_name: Jacob
-last_name: Green
-color: green
-description: He has a lot of connections
-age: 45
-image: https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg
-occupation: Entrepreneur
-
-drOrchid
-first_name: Doctor
-last_name: Orchid
-color: white
-description: PhD in plant toxicology. Adopted daughter of Mr. Boddy
-age: 26
-image: http://www.radiotimes.com/uploads/images/Original/111967.jpg
-ocupation: Scientist
-
-profPlum
-first_name: Victor
-last_name: Plum
-color: purple
-description: Billionare video game designer
-age: 22
-image: https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg
-occupation: Designer
-
-missScarlet
-first_name: Kasandra
-last_name: Scarlet
-color: red
-description: She is an A-list movie star with a dark past
-age: 31
-image: https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg
-occupation: Actor
-
-mrsPeacock
-first_name: Eleanor
-last_name: Peacock
-color: blue
-description: She is from a wealthy family and uses her status and money to earn popularity
-age: 36
-image: https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg
-occupation: Socialité
-
-mrMustard
-first_name: Jack
-last_name: Mustard
-color: yellow
-description: He is a former football player who tries to get by on his former glory
-age: 62
-image: https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg
-occupation: Retired Football player
-
-// Weapons
-
-name: rope --- weight: 10
-name: knife --- weight: 8
-name: candlestick --- weight: 2
-name: dumbbell --- weight: 30
-name: poison --- weight: 2
-name: axe --- weight: 15
-name: bat --- weight: 13
-name: trophy --- weight: 25
-name: pistol --- weight: 20
-
-// Rooms
-name: Dinning Room
-name: Conservatory
-name: Kitchen
-name: Study
-name: Library
-name: Billiard Room
-name: Lounge
-name: Ballroom
-name: Hall
-name: Spa
-name: Living Room
-name: Observatory
-name: Theater
-name: Guest House
-name: Patio
-*/
-
 //Iteration 1 - Creating the cards
 
 // Characters
@@ -216,32 +130,16 @@ var mrGreen = {
     
     roomsArray.push(dinningRoom, conservatory, kitchen, study, library, billiardRoom, lounge, ballRoom, hall, spa, livingRoom, observatory, theater, guestHouse, patio);
     
+    
     //Iteration 2 - Creating the mistery
     
 randomSelector = function(array) { return array[Math.floor(array.length * Math.random())]; };
 
-//var randomValue = charactersArray[Math.floor(charactersArray.length * Math.random())];
-
-//console.log(randomValue);
-
-
-        /*return charactersArray[Math.floor(charactersArray.length * Math.random())];
-        };*/
-        //var randomValue = charactersArray[Math.floor(charactersArray.length * Math.random())];
-        //console.log(randomValue);
-        /*randomSelector(charactersArray);
-        console.log(randomSelector);*/ 
-        
-
-
-    
-    
-    
     var pickMistery = function() {
 
         var a = randomSelector(charactersArray);
-        var b = randomSelector(roomsArray);
-        var c = randomSelector(weaponsArray);
+        var b = randomSelector(weaponsArray);
+        var c = randomSelector(roomsArray);
 
         var send = [ a, b, c ];
 
@@ -253,22 +151,30 @@ randomSelector = function(array) { return array[Math.floor(array.length * Math.r
 
     console.log(misteryEnvelope);
     
-
     
 //Iteration 3 - Revealing the mistery
 
     var revealMistery = function(misteryEnvelope) {
 
+      var fname = misteryEnvelope[0].first_name;
+      var lname = misteryEnvelope[0].last_name;
+      var wname = misteryEnvelope[1].name;
+      var rname = misteryEnvelope[2].name;
 
-       /* var x = misteryEnvelope;
+      var add0 = " ";
+      var add1 = " killed Mr.Boddy using the ";
+      var add2 = " in the ";
+      var add3 = "!!!!";
 
-        return x;*/
+      var phrase = fname + add0 + lname + add1 + wname + add2 + rname + add3;
+
+        return phrase;
+      
     }
 
-    revealMistery();
+   var rf = revealMistery(misteryEnvelope);
+   console.log(rf);
 
-   // var algo = revealMistery();
 
-   /*var y = revealMistery();
+       
 
-    console.log(y);*/
