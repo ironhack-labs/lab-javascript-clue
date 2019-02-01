58fa1944d46b8b2f3fcb7d262a9baa32b6b1fd23
Martin <martin.mihalik.nr@gmail.com>
2019-02-01 12:57:59 +0100

    Clue as a constructor and DOM manipulation

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,122 +1,149 @@
 // Characters Collection
-var charactersArray = [];
 
-var mrGreen = {
-    first_name: 'Jacob',
-    last_name: 'Green',
-    color: 'green',
-    description: 'He has a lot of connections',
-    age: '45',
-    image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
-    occupation: 'Entrepreneur'
+function Characters(first, last, color, description, age, image, occupation) {
+    this.first_name = first;
+    this.last_name = last;
+    this.color = color;
+    this.description = description;
+    this.age = age;
+    this.image = image;
+    this.occupation = occupation;
 }
 
-var drOrchid = {
-    first_name: 'Doctor',
-    last_name: 'Orchid',
-    color: 'white',
-    description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
-    age: '26',
-    image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
-    ocupation: 'Scientist'
-}
+var mrGreen = new Characters (
+    'Jacob',
+    'Green',
+    'green',
+    'He has a lot of connections',
+    '45',
+    'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+    'Entrepreneur'
+)
 
-var profPlum = {
-    first_name: 'Victor',
-    last_name: 'Plum',
-    color:'purple',
-    description: 'Billionare video game designer',
-    age: '22',    
-    image: 'https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg',
-    occupation: 'Designer'
-}
+var drOrchid = new Characters (
+    'Doctor',
+    'Orchid',
+    'white',
+    'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
+    '26',
+    'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
+    'Scientist'
+)
 
-var missScarlet = {
-    first_name: 'Kasandra',
-    last_name: 'Scarlet',
-    color: 'red',
-    description: 'She is an A-list movie star with a dark past',
-    age: '31',
-    image: 'https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg',
-    occupation: 'Actor'
-}
+var profPlum = new Characters (
+    'Victor',
+    'Plum',
+    'purple',
+    'Billionare video game designer',
+    '22',    
+    'https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg',
+    'Designer'
+)
 
-var mrsPeacock = {
-    first_name: 'Eleanor',
-    last_name: 'Peacock',
-    color: 'blue',
-    description: 'She is from a wealthy family and uses her status and money to earn popularity',
-    age: '36',
-    image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
-    occupation: 'Socialité'
-}
+var missScarlet = new Characters (
+    'Kasandra',
+    'Scarlet',
+    'red',
+    'She is an A-list movie star with a dark past',
+    '31',
+    'https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg',
+    'Actor'
+)
 
-var mrMustard = {
-    first_name: 'Jack',
-    last_name: 'Mustard',
-    color: 'yellow',
-    description: 'He is a former football player who tries to get by on his former glory',
-    age: '62',
-    image: 'https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg',
-    occupation: 'Retired Football player'
-}
+var mrsPeacock = new Characters (
+    'Eleanor',
+    'Peacock',
+    'blue',
+    'She is from a wealthy family and uses her status and money to earn popularity',
+    '36',
+    'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
+    'Socialité'
+)
 
-charactersArray.push(mrGreen, drOrchid, profPlum, missScarlet, mrsPeacock, mrMustard);
+var mrMustard = new Characters (
+    'Jack',
+    'Mustard',
+    'yellow',
+    'He is a former football player who tries to get by on his former glory',
+    '62',
+    'https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg',
+    'Retired Football player'
+)
 
 // Weapons Collection
-var weaponsArray = [];
 
-var rope = {name: 'rope', weight: 10}
-var knife = {name: 'knife', weight: 8}
-var candlestick = {name: 'candlestick', weight: 2}
-var dumbbell = {name: 'dumbbell', weight: 30}
-var poison = {name: 'poison', weight: 2}
-var bat = {name: 'bat', weight: 13}
-var axe = {name: 'axe',  weight: 15}
-var pistol = {name: 'pistol', weight: 20}
-var trophy = {name: 'trophy', weight: 25}
+function Weapon(name, weight) {
+    this.name = name;
+    this.weight = weight;
+}
 
-weaponsArray.push(rope, knife, candlestick, dumbbell, poison, axe, bat, trophy, pistol);
+var rope = new Weapon ('rope', 10)
+var knife = new Weapon ('knife', 8)
+var candlestick = new Weapon ('candlestick', 2)
+var dumbbell = new Weapon ('dumbbell', 30)
+var poison = new Weapon ('poison', 2)
+var bat = new Weapon ('bat', 13)
+var axe = new Weapon ('axe', 15)
+var pistol = new Weapon ('pistol', 20)
+var trophy = new Weapon ('trophy', 25)
 
 // Rooms Collection
-var roomsArray = [];
 
-var diningRoom = { name: 'Dinning Room'}
-var conservatory = { name: 'Conservatory'}
-var kitchen = { name: 'Kitchen'}
-var study = { name: 'Study'}
-var library = { name: 'Library'}
-var billiardRoom = { name: 'Billiard Room'}
-var lounge = { name: 'Lounge'}
-var ballroom = { name: 'Ballroom'}
-var hall = { name: 'Hall'}
-var spa = { name: 'Spa'}
-var livingRoom = { name: 'Living Room'}
-var observatory = { name: 'Observatory'}
-var theater = { name: 'Theater'}
-var guestHouse = { name: 'Guest House'}
-var patio = { name: 'Patio'}
-
-roomsArray.push(diningRoom, conservatory, kitchen, study, library, billiardRoom, lounge, ballroom, hall, spa, livingRoom, observatory, theater, guestHouse, patio);
-
-// Random Selector function
-function randomSelector(arr) {
-    var randomNum = Math.floor(Math.random() * arr.length);
-    return randomNum;
+function Room(name) {
+    this.name = name;
 }
 
-var oneArrRuleThemAll = [charactersArray, weaponsArray, roomsArray];
-var secretEnvelope = []; 
+var diningRoom = new Room ('Dinning Room');
+var conservatory = new Room ('Conservatory');
+var kitchen = new Room ('Kitchen');
+var study = new Room ('Study');
+var library = new Room ('Library');
+var billiardRoom = new Room ('Billiard Room');
+var lounge = new Room ('Lounge');
+var ballroom = new Room ('Ballroom');
+var hall = new Room ('Hall');
+var spa = new Room ('Spa');
+var livingRoom = new Room ('Living Room');
+var observatory = new Room ('Observatory');
+var theater = new Room ('Theater');
+var guestHouse = new Room ('Guest House');
+var patio = new Room ('Patio');
 
-// Loop through the oneArrRuleThemAll array and in each iteration it calls the Random Selector function
-function pickMystery(arr) {
-    for(let i = 0; i < arr.length; i++) {
-        secretEnvelope.push(arr[i][randomSelector(arr)]);
+// Arrays
+let charactersArray = [mrGreen, drOrchid, profPlum, missScarlet, mrsPeacock, mrMustard];
+let weaponsArray = [rope, knife, candlestick, dumbbell, poison, axe, bat, trophy, pistol];
+let roomsArray = [diningRoom, conservatory, kitchen, study, library, billiardRoom, lounge, ballroom, hall, spa, livingRoom, observatory, theater, guestHouse, patio];
+let allArr = [charactersArray, weaponsArray, roomsArray]; 
+
+// Badass constructor
+function Game(allArr) {
+    this.allArr = allArr,
+    this.secretEnvelope = [],
+    this.randomSelector = function(arr) {
+        var randomNum = Math.floor(Math.random() * arr.length);
+        return randomNum;
+    }
+    this.pickMystery = function(arr) {
+        this.secretEnvelope = [];
+        for(let i = 0; i < arr.length; i++) {
+            this.secretEnvelope.push(arr[i][this.randomSelector(arr[i])]);
+        }
+    }
+    this.revealMistery = function() {
+        return `${this.secretEnvelope[0].first_name} ${this.secretEnvelope[0].last_name} killed Mr. Boddy using the ${this.secretEnvelope[1].name} in the ${this.secretEnvelope[2].name}.`;
+    }
+    this.init = function() {
+        console.log(this.pickMystery(allArr));
     }
 }
-pickMystery(oneArrRuleThemAll);
 
-// Final sentence
-var revealMistery = `${secretEnvelope[0].first_name} ${secretEnvelope[0].last_name} killed Mr. Boddy using the ${secretEnvelope[1].name} in the ${secretEnvelope[2].name}.`;
+let game = new Game(allArr);
 
-console.log(revealMistery);
+// DOM manipulation
+document.getElementById("button").addEventListener("click", function(){
+    game.init();
+    let imgPath = game.secretEnvelope[0].image;
+    document.getElementById("img").setAttribute("src", imgPath);
+    document.getElementById("sentence").innerHTML = game.revealMistery();
+
+})
