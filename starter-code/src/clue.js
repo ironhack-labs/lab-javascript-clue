ef39394f07f506325b4953d4f3fd95856aa1e60b
Daniel Herrero Hernando <danielherrerohernando@gmail.com>
2019-02-06 16:35:01 +0100

    done

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -56,15 +56,15 @@ occupation:   Retired Football player
 
 // Weapons
 
-name: rope        ---   weight: 10
-name: knife       ---   weight: 8
-name: candlestick ---   weight: 2
-name: dumbbell    ---   weight: 30
-name: poison      ---   weight: 2
-name: axe         ---   weight: 15
-name: bat         ---   weight: 13
-name: trophy      ---   weight: 25
-name: pistol      ---   weight: 20
+name: rope        ,   weight: 10
+name: knife       ,   weight: 8
+name: candlestick ,   weight: 2
+name: dumbbell    ,   weight: 30
+name: poison      ,   weight: 2
+name: axe         ,   weight: 15
+name: bat         ,   weight: 13
+name: trophy      ,   weight: 25
+name: pistol      ,   weight: 20
 
 // Rooms
 name: Dinning Room
@@ -84,11 +84,122 @@ name: Guest House
 name: Patio
 */
 
+var mrGreen = {
+    first_name:   "Jacob",
+    last_name:    "Green",
+    color:        "green",
+    description:  "He has a lot of connections",
+    age:          45,
+    image:        "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+    occupation:   "Entrepreneur"
+}
+
+var drOrchid = {
+    first_name:   "Doctor",
+    last_name:    "Orchid",
+    color:        "white",
+    description:  "PhD in plant toxicology. Adopted daughter of Mr. Boddy has a lot of connections",
+    age:          26,
+    image:        "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+    occupation:   "Scientist"
+}
+
+var profPlum = {
+    first_name:   "Victor",
+    last_name:    "Plum",
+    color:        "purple",
+    description:  "PhD Billionare video game designer plant toxicology. Adopted daughter of Mr. Boddy has a lot of connections",
+    age:          22,
+    image:        "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
+    occupation:   "Designer"
+}
+
+var missScarlet = {
+    first_name:   "Kasandra",
+    last_name:    "Scarlet",
+    color:        "red",
+    description:  "She is an A-list movie star with a dark past",
+    age:          31,
+    image:        "https://metrouk2.https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",
+    occupation:   "Actor"
+}
+
+
+var mrsPeacock = {
+    first_name:   "Eleanor",
+    last_name:    "Peacock",
+    color:        "blue",
+    description:  "She is from a wealthy family and uses her status and money to earn popularity ",
+    age:          36,
+    image:        "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+    occupation:   "Socialité"
+}
+
+var mrMustard = {
+    first_name:   "Jack",
+    last_name:    "Mustard",
+    color:        "yellow",
+    description:  "She is from a wealthy He is a former football player who tries to get by on his former glory",
+    age:          62,
+    image:        "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",
+    occupation:   "Retired Football player"
+}
+
+
 // Characters Collection
-var charactersArray = [];
+var charactersArray = [mrGreen,mrMustard,drOrchid,mrsPeacock,missScarlet,profPlum];
 
 // Rooms' Collection
-var roomsArray = [];
+var roomsArray = [
+    {name: "Dinning Room"},
+    {name: "Conservatory"},
+    {name: "Kitchen"},
+    {name: "Study"},
+    {name: "Library"},
+    {name: "Billiard Room"},
+    {name: "Lounge"},
+    {name: "Ballroom"},
+    {name: "Hall"},
+    {name: "Spa"},
+    {name: "Living Room"},
+    {name: "Observatory"},
+    {name: "Theater"},
+    {name: "Guest House"},
+    {name: "Patio"}
+
+];
 
 // Weapons Collection
-var weaponsArray = [];
+var weaponsArray = [
+    {name: "rope",  weight: 10},
+    {name: "knife"       ,   weight: 8},
+    {name: "candlestick" ,   weight: 2},
+    {name: "dumbbell"    ,   weight: 30},
+    {name: "poison"     ,   weight: 2},
+    {name: "axe"         ,   weight: 15},
+    {name: "bat"        ,   weight: 13},
+    {name: "trophy"      ,   weight: 25},
+    {name: "pistol"      ,   weight: 20}
+];
+
+
+function randomSelector(stack){
+    var size = stack.length
+    var num = Math.floor(Math.random()*size)
+    return stack[num]
+
+}
+
+function pickMistery(){
+    var mysteryEnvelope = []
+    mysteryEnvelope.push(randomSelector(charactersArray))
+    mysteryEnvelope.push(randomSelector(weaponsArray))
+    mysteryEnvelope.push(randomSelector(roomsArray))
+    return mysteryEnvelope
+}
+
+
+function revealMistery(envelope){
+    var mistery = envelope[0].first_name + " " + envelope[0].last_name + " killed Mr.Boddy using the " + envelope[1].name + " in the " + envelope[2].name + "!!!!"
+    return mistery
+}
