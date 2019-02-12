013447f95b4a3d6aafc8715fb80c61d70ef77395
Abel Rodriguez <abelrodriguez199@gmail.com>
2019-02-12 11:24:47 +0100

    Ejercicio terminado

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,94 +1,108 @@
-// Characters
-/*
-mrGreen
-first_name:   Jacob
-last_name:    Green
-color:        green
-description:  He has a lot of connections
-age:          45
-image:        https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg
-occupation:   Entrepreneur
+var mrGreen = {
+    first_name: "Jacob",
+    last_name: "Green",
+    color: "green",
+    description: "He has a lot of connections",
+    age: 45,
+    image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+    occupation: "Entrepreneur"
+  };
+  var drOrchid = {
+    first_name: "Doctor",
+    last_name: "Orchid",
+    color: "white",
+    description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+    age: "26",
+    image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+    occupation: "Scientist"
+  };
+  var profPlum = {
+    first_name: "Victor",
+    last_name: "Plum",
+    color: "purple",
+    description: "Billionare video game designer",
+    age: "22",
+    image: "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
+    occupation: "Designer"
+  };
+  var missScarvar = {
+    first_name: "Kasandra",
+    last_name: "Scarvar",
+    color: "red",
+    description: "She is an A-list movie star with a dark past,",
+    age: "31",
+    image: "https://metrouk2.files.wordpress.com/2016/07/miss-scarvart.jpg,",
+    occupation: "Actor"
+  };
+  var mrsPeacock = {
+    first_name: "Eleanor",
+    last_name: "Peacock",
+    color: "blue",
+    description: "She is from a wealthy family and uses her status and money to earn popularity",
+    age: "36",
+    image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+    occupation: "Socialité"
+  };
+  var mrMustard = {
+    first_name: "Jack",
+    last_name: "Mustard",
+    color: "yellow",
+    description: "He is a former football player who tries to get by on his former glory",
+    age: "62",
+    image: "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",
+    occupation: "Retired Football player",
+  };
 
-drOrchid
-first_name:   Doctor
-last_name:    Orchid
-color:        white
-description:  PhD in plant toxicology. Adopted daughter of Mr. Boddy
-age:          26
-image:        http://www.radiotimes.com/uploads/images/Original/111967.jpg
-ocupation:   Scientist
+  var rope = { name: "rope", weight: 10 };
+  var knife = { name: "knife", weight: 8 };
+  var candlestick = { name: "Candlestick", weight: 2 };
+  var dumbbell = { name: "Dumbbell", weight: 30 };
+  var poison = { name: "Poison", weight: 2 };
+  var axe = { name: "axe", weight: 15 };
+  var bat = { name: "bat", weight: 13 };
+  var trophy = { name: "trophy", weight: 25 };
+  var pistol = { name: "pistol", weight: 20 };
 
-profPlum
-first_name:   Victor
-last_name:    Plum
-color:        purple
-description:  Billionare video game designer
-age:          22
-image:        https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg
-occupation:   Designer
+  var dinningRoom = { name: 'Dinning Room' }
+  var conservatory = { name: 'Conservatory' }
+  var study = { name: 'Study' }
+  var library = { name: 'Library' }
+  var billiardRoom = { name: 'Billiard Room' }
+  var lounge = { name: 'Lounge' }
+  var ballroom = { name: 'Ballroom' }
+  var hall = { name: 'Hall' }
+  var spa = { name: 'Spa' }
+  var observatory = { name: 'Observatory' }
+  var theater = { name: 'Theater' }
+  var guestHouse = { name: 'Guest House' }
+  var patio = { name: 'Patio' }
+  var kitchen = { name: 'Kitchen' }
 
-missScarlet
-first_name:   Kasandra
-last_name:    Scarlet
-color:        red
-description:  She is an A-list movie star with a dark past
-age:          31
-image:        https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg
-occupation:   Actor
+  var charactersArray = [mrGreen,drOrchid,profPlum,missScarvar,mrsPeacock,mrMustard];
 
-mrsPeacock
-first_name:   Eleanor
-last_name:    Peacock
-color:        blue
-description:  She is from a wealthy family and uses her status and money to earn popularity
-age:          36
-image:        https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg
-occupation:   Socialité
+  var roomsArray = [dinningRoom,conservatory,study,library,billiardRoom,lounge,ballroom,hall,spa,observatory,theater,guestHouse,patio];
 
-mrMustard
-first_name:   Jack
-last_name:    Mustard
-color:        yellow
-description:  He is a former football player who tries to get by on his former glory
-age:          62
-image:        https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg
-occupation:   Retired Football player
+  var weaponsArray = [rope,knife,candlestick,dumbbell,poison,axe,bat,trophy,pistol];
 
-// Weapons
+function randomSelector(cardArray) {
+    if (cardArray===0){
+        return undefined;
+    }
+    else {
+        return cardArray[Math.floor((Math.random())*(cardArray.length))]
+    }
+}
 
-name: rope        ---   weight: 10
-name: knife       ---   weight: 8
-name: candlestick ---   weight: 2
-name: dumbbell    ---   weight: 30
-name: poison      ---   weight: 2
-name: axe         ---   weight: 15
-name: bat         ---   weight: 13
-name: trophy      ---   weight: 25
-name: pistol      ---   weight: 20
+var cards = [charactersArray,weaponsArray,roomsArray];
 
-// Rooms
-name: Dinning Room
-name: Conservatory
-name: Kitchen
-name: Study
-name: Library
-name: Billiard Room
-name: Lounge
-name: Ballroom
-name: Hall
-name: Spa
-name: Living Room
-name: Observatory
-name: Theater
-name: Guest House
-name: Patio
-*/
+function pickMistery(){
+    misteryEnvelope= [];
+        for (i=0; i<cards.length; i++){
+            misteryEnvelope.push(randomSelector(cards[i]));
+    }
+    return misteryEnvelope;
+}
 
-// Characters Collection
-var charactersArray = [];
-
-// Rooms' Collection
-var roomsArray = [];
-
-// Weapons Collection
-var weaponsArray = [];
+function revealMistery (misteryEnvelope){
+    return `${misteryEnvelope[0].first_name} ${misteryEnvelope[0].last_name} killed Mr.Boddy using the ${misteryEnvelope[1].name} in the ${misteryEnvelope[2].name}!!!!`
+} 
