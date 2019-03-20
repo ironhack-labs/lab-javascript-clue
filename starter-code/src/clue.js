383fbfe1ec37d3640d0793ecd0d15d7c1a2f15cd
ernestogl88 <ernestogomezlopez@gmail.com>
2019-03-20 17:02:04 +0100

    Estructura de datos creada

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,37 +1,3 @@
-// Characters
-
-
-
-// Weapons
-
-name: rope        ---   weight: 10
-name: knife       ---   weight: 8
-name: candlestick ---   weight: 2
-name: dumbbell    ---   weight: 30
-name: poison      ---   weight: 2
-name: axe         ---   weight: 15
-name: bat         ---   weight: 13
-name: trophy      ---   weight: 25
-name: pistol      ---   weight: 20
-
-// Rooms
-name: Dinning Room
-name: Conservatory
-name: Kitchen
-name: Study
-name: Library
-name: Billiard Room
-name: Lounge
-name: Ballroom
-name: Hall
-name: Spa
-name: Living Room
-name: Observatory
-name: Theater
-name: Guest House
-name: Patio
-
-
 // Characters Collection
 var charactersArray = [{
   first_name: "Jacob",
@@ -89,7 +55,7 @@ var charactersArray = [{
 }
 ];
 
-// Rooms' Collection
+// Weapons Collection
 var weaponsArray =  [
   { name: "rope", weight: 10 },
   { name: "knife", weight: 8 },
@@ -102,7 +68,7 @@ var weaponsArray =  [
   { name: "pistol", weight: 20 }
 ];
 
-// Weapons Collection
+// Rooms Collection
 var roomsArray = [
   {name: "Dinning Room"},
   {name: "Conservatory"},
@@ -129,4 +95,6 @@ function randomSelector (array){
   }
 }
 
+function pickMistery(){
 
+}
