3287235d41c475e3660226121a44a826ed25be5e
Jose Luis <jlavilesmolina@gmail.com>
2019-03-20 18:39:01 +0100

    First function completed

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,87 +1,79 @@
-Characters Collection
 var charactersArray = [
-    var mrGreen = {
-        first_name: Jacob,
-        last_name: Green,
-        color: green,
-        description: He has a lot of connections,
+    mrGreen = {
+        first_name: "Jacob",
+        last_name: "Green",
+        color: "green",
+        description: "He has a lot of connections",
         age: 45,
-        image: https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg,
-        occupation: Entrepreneur,
-    }
-        
-    var drOrchid = {
-        first_name: Doctor,
-        last_name: Orchid,
-        color: white,
-        description: PhD in plant toxicology. Adopted daughter of Mr. Boddy,
+        image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+        occupation: "Entrepreneur",
+    },
+    drOrchid = {
+        first_name: "Doctor",
+        last_name: "Orchid",
+        color: "white",
+        description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
         age: 26,
-        image: http://www.radiotimes.com/uploads/images/Original/111967.jpg,
-        ocupation: Scientist,
-    }
-
-    var profPlum = {
-        first_name: Victor,
-        last_name: Plum,
-        color: purple,
-        description: Billionare video game designer,
+        image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+        ocupation: "Scientist",
+    },
+    profPlum = {
+        first_name: "Victor",
+        last_name: "Plum",
+        color: "purple",
+        description: "Billionare video game designer",
         age: 22,
-        image: https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg,
-        occupation: Designer,
-    }
-
-    var missScarlet = {
-        first_name: Kasandra,
-        last_name: Scarlet,
-        color: red,
-        description: She is an A-list movie star with a dark past,
+        image: "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
+        occupation: "Designer",
+    },
+    missScarlet = {
+        first_name: "Kasandra",
+        last_name: "Scarlet",
+        color: "red",
+        description: "She is an A-list movie star with a dark past",
         age: 31,
-        image: https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg,
-        occupation: Actor,
-    }
-
-    var mrsPeacock = {
-        first_name: Eleanor,
-        last_name: Peacock,
-        color: blue,
-        description: She is from a wealthy family and uses her status and money to earn popularity,
+        image: "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",
+        occupation: "Actor",
+    },
+    mrsPeacock = {
+        first_name: "Eleanor",
+        last_name: "Peacock",
+        color: "blue",
+        description: "She is from a wealthy family and uses her status and money to earn popularity",
         age: 36,
-        image: https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg,
-        occupation: Socialité,
-    }
-
-    var mrMustard = {
-        first_name: Jack,
-        last_name: Mustard,
-        color: yellow,
-        description: He is a former football player who tries to get by on his former glory,
+        image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+        occupation: "Socialité",
+    },
+    mrMustard = {
+        first_name: "Jack",
+        last_name: "Mustard",
+        color: "yellow",
+        description: "He is a former football player who tries to get by on his former glory",
         age: 62,
-        image: https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg,
-        occupation: Retired Football player,
+        image: "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",
+        occupation: "Retired Football player",
     }
 ];
 
 
-// Rooms' Collection
 var roomsArray = [ 
-    "Dinning Room",
-    "Conservatory",
-    "Kitchen",
-    "Study",
-    "Library",
-    "Billiard Room",
-    "Lounge",
-    "Ballroom",
-    "Hall",
-    "Spa",
-    "Living Room",
-    "Observatory",
-    "Theater",
-    "Guest House",
-    "Patio",
+    {name:"Dinning Room"},
+    {name:"Conservatory"},
+    {name:"Kitchen"},
+    {name:"Study"},
+    {name:"Library"},
+    {name:"Billiard Room"},
+    {name:"Lounge"},
+    {name:"Ballroom"},
+    {name:"Hall"},
+    {name:"Spa"},
+    {name:"Living Room"},
+    {name:"Observatory"},
+    {name:"Theater"},
+    {name:"Guest House"},
+    {name:"Patio"}
 ];
 
-// Weapons Collection
 var weaponsArray = [
     {name: "rope", weight: 10},
     {name: "knife", weight: 8},
@@ -91,17 +83,13 @@ var weaponsArray = [
     {name: "axe", weight: 15},
     {name: "bat",weight: 13},
     {name: "trophy", weight: 25},
-    {name: "pistol", weight: 20},
+    {name: "pistol", weight: 20}
 ];
 
-function selectorRandom(cards) {
-    var cards = []:
-
-    if(charactersArray.length > 0 && roomsArray.length > 0 && weaponsArray.length > 0) {
-        return { 
-        cards[Math.floor(Math.random()*charactersArray)],
-        cards[Math.floor(Math.random()*roomsArray)],
-        cards[Math.floor(Math.random()*weaponsArray)],
-        }
+function randomSelector(array){
+if (array.length === 0) return undefined
+    else {
+      return array[Math.floor(Math.random()*array.length)]
     }
 }
+
