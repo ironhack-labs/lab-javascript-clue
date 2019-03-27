3e601b44dcc71680585e512a4cb39acc3a43a5bb
deredhuzent <hakurascot@gmail.com>
2019-03-27 13:43:13 -0600

    Done, Alberto y Regina

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -67,7 +67,7 @@ name: trophy      ---   weight: 25
 name: pistol      ---   weight: 20
 
 // Rooms
-name: Dinning Room
+{name: "Dinning Room"}
 name: Conservatory
 name: Kitchen
 name: Study
@@ -84,11 +84,237 @@ name: Guest House
 name: Patio
 */
 
+//character
+const mrGreen = {
+    first_name : "Jacob", 
+    last_name : "Green",
+    color: "Green",
+    description: "He has a lot of connections",
+    age: "45",
+    image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+    occupation: "Entrepeneur"
+}
+
+const drOrchid = {
+    first_name : "Doctor", 
+    last_name : "Orchid",
+    color: "white",
+    description: "Phd in plant toxicology. Adopted daughter of Mr. Boddy",
+    age: "26",
+    image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+    occupation: "Scientist"
+}
+
+const porfPlum = {
+    first_name : "Victor", 
+    last_name : "Plum",
+    color: "purple",
+    description: "Billionare video game designer",
+    age: "22",
+    image: "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
+    occupation: "Designer"
+}
+
+const missScarlet = {
+    first_name : "Kasandra", 
+    last_name : "Scarlet",
+    color: "red",
+    description: "She is an A-list movie with a dark past",
+    age: "31",
+    image: "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",
+    occupation: "Actor"
+}
+
+const mrsPeacock = {
+    first_name : "Eleanor", 
+    last_name : "Peacock",
+    color: "blue",
+    description: "She is from a wealthy family and uses her status and money to earn popularity",
+    age: "36",
+    image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+    occupation: "Socialité"
+}
+
+const mrMustard = {
+    first_name : "Jack", 
+    last_name : "Mustard",
+    color: "yellow",
+    description: "He is a former football player who tries to get by on his former glory",
+    age: "62",
+    image: "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",
+    occupation: "Retired Football player"
+}
+
+//weapons
+const rope = {
+    name: "rope",
+    weight: "10"
+}
+
+const knife = {
+    name: "knife",
+    weight: "8"
+}
+
+const candlestick = {
+    name: "candlestick",
+    weight: "2"
+}
+
+const dumbbell = {
+    name: "dumbbell",
+    weight: "30"
+}
+
+const poison = {
+    name: "poison",
+    weight: "2"
+}
+
+const axe = {
+    name: "axe",
+    weight: "15"
+}
+
+const bat = {
+    name: "bat",
+    weight: "13"
+}
+
+const trophy = {
+    name: "trophy",
+    weight: "25"
+}
+
+const pistol = {
+    name: "pistol",
+    weight: "20"
+}
+
+//Rooms
+const dinningRoom = {
+    name: "Dinning Room",
+}
+
+const conservatory = {
+    name: "conservatory",
+}
+
+const kitchen = {
+    name: "kitchen",
+}
+
+const study = {
+    name: "study",
+}
+
+const library = {
+    name: "Library",
+}
+
+const billiardRoom = {
+    name: "Billiard Room",
+}
+
+const lounge = {
+    name: "Lounge",
+}
+
+const ballRoom = {
+    name: "Ball Room",
+}
+
+const hall  = {
+    name: "Hall",
+}
+
+const spa = {
+    name: "Spa",
+}
+
+const livingRoom = {
+    name: "Living Room",
+}
+
+const observatory = {
+    name: "Observatory",
+}
+
+const theater = {
+    name: "Theater",
+}
+
+const guestHouse = {
+    name: "Guest House",
+}
+
+const patio = {
+    name: "Patio",
+}
+
+
 // Characters Collection
 var charactersArray = [];
+
+charactersArray.push(mrGreen)
+charactersArray.push(drOrchid)
+charactersArray.push(porfPlum)
+charactersArray.push(missScarlet)
+charactersArray.push(mrsPeacock)
+charactersArray.push(mrMustard)
+
 
 // Rooms' Collection
 var roomsArray = [];
 
+roomsArray.push(dinningRoom)
+roomsArray.push(conservatory)
+roomsArray.push(kitchen)
+roomsArray.push(study)
+roomsArray.push(library)
+roomsArray.push(billiardRoom)
+roomsArray.push(lounge)
+roomsArray.push(ballRoom)
+roomsArray.push(hall)
+roomsArray.push(spa)
+roomsArray.push(livingRoom)
+roomsArray.push(observatory)
+roomsArray.push(theater)
+roomsArray.push(guestHouse)
+roomsArray.push(patio)
+
+
 // Weapons Collection
 var weaponsArray = [];
+
+weaponsArray.push(rope)
+weaponsArray.push(knife)
+weaponsArray.push(candlestick)
+weaponsArray.push(dumbbell)
+weaponsArray.push(poison)
+weaponsArray.push(axe)
+weaponsArray.push(bat)
+weaponsArray.push(trophy)
+weaponsArray.push(pistol)
+
+//function random selector
+
+function randomSelector(arr) {
+    const random = Math.floor(Math.random() * arr.length)
+    return arr[random]
+}
+
+function pickMistery(){
+
+let misteryEnvelope = [] 
+
+    misteryEnvelope.push (randomSelector(charactersArray))
+    misteryEnvelope.push(randomSelector(weaponsArray))
+    misteryEnvelope.push(randomSelector(roomsArray))
+    
+    return misteryEnvelope;
+}
+
+function revealMistery(arr){
+    return `${arr[0].first_name} ${arr[0].last_name} killed Mr.Boddy using the ${arr[1].name} in the ${arr[2].name}!!!!`
+}
