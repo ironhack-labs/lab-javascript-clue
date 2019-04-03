0f10040f1194aff496161efda4aa3308326b395a
Samuel Yamamoto <yamamoto.samuel@gmail.com>
2019-04-03 17:13:03 -0400

    Final Sub

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -85,10 +85,43 @@ name: Patio
 */
 
 // Characters Collection
-var charactersArray = [];
+var charactersArray = [
+{mrGreen:{first_name:"Jacob", last_name:"Green",color:"Green",description:"He has a lot of connections",age:45, occupation:"Entrepeneur"}}, 
+{drOrchid:{first_name:"Doctor", last_name:"Orchid", color:"white", description:"PhD in plant toxicology. Adopted daughter of Mr. Boddy", age:26, occupation:"Scientist"}}, 
+{profPlum:{first_name:"Victor", last_name:"Plum", color:"purple", description:"Billionare video game designer", age:22, occupation:"Designer"}},
+{missScarlet:{first_name:"Kassandra", last_name:"Scarlett", color:"red", description:"She is an A-list movie star with a dark past", age:31, occupation:"Actor"}}, 
+{mrsPeacock:{first_name:"Eleanor", last_name:"Peacock", color:"blue", description:"She is from a wealthy family and uses her status and money to earn popularity", age:"36", occupation:"Socialité"}}, 
+{mrMustard:{first_name:"Jack", last_name:"Mustard", color:"yellow", description:"He is a former football player who tries to get by on his former glory", age:"62", occupation:"Retired Football player"}}];
 
 // Rooms' Collection
-var roomsArray = [];
+var roomsArray = ["Dinning Room", "Conservatory", "Kitchen", "Study", "Library", "Billiard Room", "Lounge", "Ballroom", "Hall", "Spa", "Living Room", "Observatory", "Theater", "Guest House", "Patio"];
 
 // Weapons Collection
-var weaponsArray = [];
+var weaponsArray = [
+{name:"rope", weight:10},
+{name:"knife", weight:8}, 
+{name:"candlestick", weight:2}, 
+{name:"dumbbell", weight:30}, 
+{name:"poison", weight:2}, 
+{name:"axe", weight:15}, 
+{name:"bat", weight:13}, 
+{name:"trophy", weight:25}, 
+{name:"pistol", weight:20}];
+
+// Shuffled Deck
+
+function randomSelector(arr){
+    return(arr[(Math.floor(Math.random() * arr.length))]);
+}
+function pickMistery(){
+    var misteryEnvelope = [];
+    misteryEnvelope[0] = randomSelector(charactersArray);
+    misteryEnvelope[1] = randomSelector(weaponsArray);
+    misteryEnvelope[2] = randomSelector(roomsArray);
+    return misteryEnvelope;
+}
+
+// THE BIG REVEAL
+function revealMistery(arr){
+    return(`${arr[0].first_name} ${arr[0].last_name} killed Mr.Boddy using the ${arr[1].name} in the ${arr[2].name}!!!!`);
+}
