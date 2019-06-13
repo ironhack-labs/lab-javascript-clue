7fa5a39052c0dd236d0e168ec7704bb871d255fe
Uira Oliveira <uira.sampaio@hotmail.com>
2019-06-13 20:06:07 -0300

    Done

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,94 +1,96 @@
-// Characters
-/*
-mrGreen
-first_name:   Jacob
-last_name:    Green
-color:        green
-description:  He has a lot of connections
-age:          45
-image:        https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg
-occupation:   Entrepreneur
+class Character {
+    constructor(fname, lname, color, description, age, image, occupation) {
+        this.first_name = fname;
+        this.last_name = lname;
+        this.color = color;
+        this.description = description;
+        this.age = age;
+        this.image = image;
+        this.occupation = occupation;
+    }
+}
 
-drOrchid
-first_name:   Doctor
-last_name:    Orchid
-color:        white
-description:  PhD in plant toxicology. Adopted daughter of Mr. Boddy
-age:          26
-image:        http://www.radiotimes.com/uploads/images/Original/111967.jpg
-ocupation:   Scientist
 
-profPlum
-first_name:   Victor
-last_name:    Plum
-color:        purple
-description:  Billionare video game designer
-age:          22
-image:        https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg
-occupation:   Designer
+const mrGreen = new Character('Jacob', 'Green', 'green', 'He has a lot of connections', 45, 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg', 'Entrepreneur');
+const drOrchid = new Character('Doctor', 'Orchid', 'white', 'PhD in plant toxicology. Adopted daughter of Mr. Boddy', 26, 'http://www.radiotimes.com/uploads/images/Original/111967.jpg', 'Scientist');
+const profPlum = new Character('Victor', 'Plum', 'purple', 'Billionare video game designer', 22, 'https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg', 'Designer');
+const missScarlet = new Character('Kasandra', 'Scarlet', 'red', 'She is an A-list movie star with a dark past', 31, 'https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg', 'Actor');
+const mrsPeacock = new Character('Eleanor', 'Peacock', 'blue', 'She is from a wealthy family and uses her status and money to earn popularity', 36, 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg', 'Socialité');
+const mrMustard = new Character('Jack', 'Mustard', 'yellow', 'He is a former football player who tries to get by on his former glory', 62, 'https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg', 'Retired Football player');
 
-missScarlet
-first_name:   Kasandra
-last_name:    Scarlet
-color:        red
-description:  She is an A-list movie star with a dark past
-age:          31
-image:        https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg
-occupation:   Actor
 
-mrsPeacock
-first_name:   Eleanor
-last_name:    Peacock
-color:        blue
-description:  She is from a wealthy family and uses her status and money to earn popularity
-age:          36
-image:        https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg
-occupation:   Socialité
+class Weapon {
+    constructor(name, weight) {
+        this.name = name;
+        this.weight = weight;
+    }
 
-mrMustard
-first_name:   Jack
-last_name:    Mustard
-color:        yellow
-description:  He is a former football player who tries to get by on his former glory
-age:          62
-image:        https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg
-occupation:   Retired Football player
+}
 
-// Weapons
+const rope = new Weapon('rope', 10);
+const knife = new Weapon('knife', 8);
+const candlestick = new Weapon('candlestick', 2);
+const dumbbell = new Weapon('dumbbell', 30);
+const poison = new Weapon('poison', 2);
+const axe = new Weapon('axe', 15);
+const bat = new Weapon('bat', 13);
+const trophy = new Weapon('trophy', 25);
+const pistol = new Weapon('pistol', 20);
 
-name: rope        ---   weight: 10
-name: knife       ---   weight: 8
-name: candlestick ---   weight: 2
-name: dumbbell    ---   weight: 30
-name: poison      ---   weight: 2
-name: axe         ---   weight: 15
-name: bat         ---   weight: 13
-name: trophy      ---   weight: 25
-name: pistol      ---   weight: 20
+class Room {
+    constructor(name) {
+        this.name = name;
+    }
 
-// Rooms
-name: Dinning Room
-name: Conservatory
-name: Kitchen
-name: Study
-name: Library
-name: Billiard Room
-name: Lounge
-name: Ballroom
-name: Hall
-name: Spa
-name: Living Room
-name: Observatory
-name: Theater
-name: Guest House
-name: Patio
-*/
+}
+
+const dinningRoom = new Room('Dinning Room');
+const conservatory = new Room('Conservatory');
+const kitchen = new Room('Kitchen');
+const study = new Room('Study');
+const library = new Room('Library');
+const billiardRoom = new Room('Billiard Room');
+const lounge = new Room('Lounge');
+const ballroom = new Room('Ballroom');
+const hall = new Room('Hall');
+const spa = new Room('Spa');
+const livingRoom = new Room('Living Room');
+const observatory = new Room('Observatory');
+const theater = new Room('Theater');
+const guestHouse = new Room('Guest House');
+const patio = new Room('Patio');
+
+
+
 
 // Characters Collection
-var charactersArray = [];
+let charactersArray = [];
 
 // Rooms' Collection
-var roomsArray = [];
+let roomsArray = [];
 
 // Weapons Collection
-var weaponsArray = [];
+let weaponsArray = [];
+charactersArray.push(mrGreen, drOrchid, profPlum, missScarlet, mrsPeacock, mrMustard);
+weaponsArray.push(rope, knife, candlestick, dumbbell, poison, axe, bat, trophy, pistol);
+roomsArray.push(dinningRoom, conservatory, kitchen, study, library, billiardRoom, lounge, ballroom, hall, spa, livingRoom, observatory, theater, guestHouse, patio);
+
+
+
+const randomSelector = (arr) => {
+    let random = arr[Math.floor(Math.random() * arr.length)]
+    return random;
+}
+
+const pickMistery = () => {
+    let mysteryEnvelope = [];
+    mysteryEnvelope.push(randomSelector(charactersArray), randomSelector(weaponsArray), randomSelector(roomsArray))
+    return mysteryEnvelope;
+}
+
+
+const revealMistery = (arr) => {
+    return `${arr[0].first_name} ${arr[0].last_name} killed Mr.Boddy using the ${arr[1].name} in the ${arr[2].name}!!!!`;
+}
+
+console.log(revealMistery(pickMistery()))
