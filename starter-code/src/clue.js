71bc0c3c1af77253a6a6e36aa2ec590663c4aac0
Victor Thoma <victor@mrundmrsthoma.de>
2019-06-25 21:59:34 +0200

    almost done

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -85,10 +85,173 @@ name: Patio
 */
 
 // Characters Collection
-var charactersArray = [];
+var charactersArray = [
+  {
+    character: "mrGreen",
+    first_name: "Jacob",
+    last_name: "Green",
+    color: "green",
+    description: "He has a lot of connections",
+    age: 45,
+    image:
+      "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+    occupation: "Entrepreneur"
+  },
+  {
+    character: "drOrchid",
+    first_name: "Doctor",
+    last_name: "Orchid",
+    color: "white",
+    description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+    age: 26,
+    image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+    occupation: "Scientist"
+  },
+  {
+    character: "profPlum",
+    first_name: "Victor",
+    last_name: "Plum",
+    color: "purple",
+    description: "Billionare video game designer",
+    age: 22,
+    image: "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
+    occupation: "Designer"
+  },
+  {
+    character: "missScarlet",
+    first_name: "Kasandra",
+    last_name: "Scarlet",
+    color: "red",
+    description: "She is an A-list movie star with a dark past",
+    age: 31,
+    image: "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",
+    occupation: "Actor"
+  },
+  {
+    character: "mrsPeacock",
+    first_name: "Eleanor",
+    last_name: "Peacock",
+    color: "blue",
+    description:
+      "She is from a wealthy family and uses her status and money to earn popularity",
+    age: 36,
+    image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+    occupation: "Socialité"
+  },
+  {
+    character: "mrMustard",
+    first_name: "Jack",
+    last_name: "Mustard",
+    color: "yellow",
+    description:
+      "He is a former football player who tries to get by on his former glory",
+    age: 62,
+    image: "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",
+    occupation: "Retired Football player"
+  }
+];
+
+var rope = {
+  name: "rope",
+  weight: 10
+};
+
+var knife = {
+  name: "knife",
+  weight: 8
+};
+
+var candlestick = {
+  name: "candlestick",
+  weight: 2
+};
+
+var dumbbell = {
+  name: "dumbbell",
+  weight: 30
+};
+
+var poison = {
+  name: "poison",
+  weight: 2
+};
+
+var axe = {
+  name: "axe",
+  weight: 15
+};
+
+var bat = {
+  name: "bat",
+  weight: 13
+};
+
+var trophy = {
+  name: "trophy",
+  weight: 25
+};
+
+var pistol = {
+  name: "pistol",
+  weight: 20
+};
 
 // Rooms' Collection
-var roomsArray = [];
+var roomsArray = [
+  "Dinning Room",
+  "Conservatory",
+  "Kitchen",
+  "Study",
+  "Library",
+  "Billiard Room",
+  "Lounge",
+  "Ballroom",
+  "Hall",
+  "Spa",
+  "Living Room",
+  "Observatory",
+  "Theater",
+  "Guest House",
+  "Patio"
+];
 
 // Weapons Collection
 var weaponsArray = [];
+
+weaponsArray.push(
+  rope,
+  knife,
+  candlestick,
+  dumbbell,
+  poison,
+  axe,
+  bat,
+  trophy,
+  pistol
+);
+
+function randomSelector(array) {
+  var myRandomElement = array[Math.floor(Math.random() * array.length)];
+  return myRandomElement;
+}
+
+function pickMistery(character, room, weapon) {
+  var misteryEnvelope = [];
+  var character = randomSelector(character);
+  var room = randomSelector(room);
+  var weapon = randomSelector(weapon);
+  misteryEnvelope.push(character, room, weapon);
+  return misteryEnvelope;
+}
+
+let misteryEnvelope = pickMistery(charactersArray, weaponsArray, roomsArray);
+
+function revealMistery(mistery) {
+  var solution = [
+    `<FIRST NAME> <LAST NAME> killed Mr.Boddy using the <WEAPON> in the ${
+      mistery[2]
+    }!!!!`
+  ];
+}
+
+revealMistery(misteryEnvelope);
