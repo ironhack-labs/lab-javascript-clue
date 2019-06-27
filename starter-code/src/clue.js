d4e00fff1e3bc1db7e3ebb492694311a144dd892
Niklas Eicker <git@nikl.me>
2019-06-27 10:29:38 +0200

    done

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,94 +1,88 @@
-// Characters
-/*
-mrGreen
-first_name:   Jacob
-last_name:    Green
-color:        green
-description:  He has a lot of connections
-age:          45
-image:        https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg
-occupation:   Entrepreneur
+function Character(first_name, last_name, color, description, age, image, occupation) {
+  this.first_name = first_name;
+  this.last_name = last_name;
+  this.color = color;
+  this.description = description;
+  this.age = age;
+  this.image = image;
+  this.occupation = occupation;
+}
 
-drOrchid
-first_name:   Doctor
-last_name:    Orchid
-color:        white
-description:  PhD in plant toxicology. Adopted daughter of Mr. Boddy
-age:          26
-image:        http://www.radiotimes.com/uploads/images/Original/111967.jpg
-ocupation:   Scientist
+function Weapon(name, weight) {
+  this.name = name;
+  this.weight = weight;
+}
 
-profPlum
-first_name:   Victor
-last_name:    Plum
-color:        purple
-description:  Billionare video game designer
-age:          22
-image:        https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg
-occupation:   Designer
-
-missScarlet
-first_name:   Kasandra
-last_name:    Scarlet
-color:        red
-description:  She is an A-list movie star with a dark past
-age:          31
-image:        https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg
-occupation:   Actor
-
-mrsPeacock
-first_name:   Eleanor
-last_name:    Peacock
-color:        blue
-description:  She is from a wealthy family and uses her status and money to earn popularity
-age:          36
-image:        https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg
-occupation:   Socialité
-
-mrMustard
-first_name:   Jack
-last_name:    Mustard
-color:        yellow
-description:  He is a former football player who tries to get by on his former glory
-age:          62
-image:        https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg
-occupation:   Retired Football player
-
-// Weapons
-
-name: rope        ---   weight: 10
-name: knife       ---   weight: 8
-name: candlestick ---   weight: 2
-name: dumbbell    ---   weight: 30
-name: poison      ---   weight: 2
-name: axe         ---   weight: 15
-name: bat         ---   weight: 13
-name: trophy      ---   weight: 25
-name: pistol      ---   weight: 20
-
-// Rooms
-name: Dinning Room
-name: Conservatory
-name: Kitchen
-name: Study
-name: Library
-name: Billiard Room
-name: Lounge
-name: Ballroom
-name: Hall
-name: Spa
-name: Living Room
-name: Observatory
-name: Theater
-name: Guest House
-name: Patio
-*/
+function Room(name) {
+  this.name = name;
+}
 
 // Characters Collection
 var charactersArray = [];
 
-// Rooms' Collection
-var roomsArray = [];
+charactersArray.push(new Character("Jacob", "Green", "green", "He has a lot of connections", 45
+, "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg", "Entrepreneur"));
+
+charactersArray.push(new Character("Doctor", "Orchid", "white", "PhD in plant toxicology. Adopted daughter of Mr. Boddy", 26
+, "http://www.radiotimes.com/uploads/images/Original/111967.jpg", "Scientist"));
+
+charactersArray.push(new Character("Victor", "Plum", "purple", "Billionare video game designer", 22
+, "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg", "Designer"));
+
+charactersArray.push(new Character("Kasandra", "Scarlet", "red", "She is an A-list movie star with a dark past", 31
+, "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg", "Actor"));
+
+charactersArray.push(new Character("Eleanor", "Peacock", "blue", "She is from a wealthy family and uses her status and money to earn popularity", 36
+, "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg", "Socialité"));
+
+charactersArray.push(new Character("Jack", "Mustard", "yellow", "He is a former football player who tries to get by on his former glory", 62
+, "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg", "Retired Football player"));
+
 
 // Weapons Collection
 var weaponsArray = [];
+
+weaponsArray.push(new Weapon("rope", 10));
+weaponsArray.push(new Weapon("knife", 8));
+weaponsArray.push(new Weapon("candlestick", 2));
+weaponsArray.push(new Weapon("dumbbell", 30));
+weaponsArray.push(new Weapon("poison", 2));
+weaponsArray.push(new Weapon("axe", 15));
+weaponsArray.push(new Weapon("bat", 13));
+weaponsArray.push(new Weapon("trophy", 25));
+weaponsArray.push(new Weapon("pistol", 20));
+
+// Rooms' Collection
+var roomsArray = [];
+
+roomsArray.push(new Room("Dinning Room"));
+roomsArray.push(new Room("Conservatory"));
+roomsArray.push(new Room("Kitchen"));
+roomsArray.push(new Room("Study"));
+roomsArray.push(new Room("Library"));
+roomsArray.push(new Room("Billiard Room"));
+roomsArray.push(new Room("Lounge"));
+roomsArray.push(new Room("Ballroom"));
+roomsArray.push(new Room("Hall"));
+roomsArray.push(new Room("Spa"));
+roomsArray.push(new Room("Living Room"));
+roomsArray.push(new Room("Observatory"));
+roomsArray.push(new Room("Theater"));
+roomsArray.push(new Room("Guest House"));
+roomsArray.push(new Room("Patio"));
+
+function randomSelector(array) {
+  return array[ Math.floor(Math.random() * array.length) ];
+}
+
+function pickMistery() {
+  return [randomSelector(charactersArray), randomSelector(weaponsArray), randomSelector(roomsArray)];
+}
+
+let misteryEnvelope = []
+
+misteryEnvelope = pickMistery();
+
+function revealMistery(mistery) {
+  return `${mistery[0].first_name} ${mistery[0].last_name} killed Mr.Boddy using the ${mistery[1].name} in the ${mistery[2].name}!!!!`
+}
