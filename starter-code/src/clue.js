6455b98c5fd7350dddbb43ed6bbcf2afcb7d8711
Mark Trevail <trevail.m@gmail.com>
2019-06-27 10:49:01 +0200

    Initial commit

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,88 +1,73 @@
+// -----------------------------------------------------------------------------
+// 1. Create data structures for Characters, Weapons, Rooms
+// -----------------------------------------------------------------------------
+
+// Character constructor
+function Character(first_name,last_name,color,description,age,image,occupation) {
+  this.first_name = first_name;
+  this.last_name = last_name;
+  this.color = color;
+  this.description = description;
+  this.age = age;
+  this.image = image;
+  this.occupation = occupation;
+};
+
+// Weapon constructor
+function Weapon(name, weight) {
+  this.name = name;
+  this.weight = weight;
+};
+
+// Room constructor
+function Room(name) {
+  this.name = name;
+};
+
+// -----------------------------------------------------------------------------
+// 2. Create the instances
+// -----------------------------------------------------------------------------
+
 // Characters
-/*
-mrGreen
-first_name:   Jacob
-last_name:    Green
-color:        green
-description:  He has a lot of connections
-age:          45
-image:        https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg
-occupation:   Entrepreneur
-
-drOrchid
-first_name:   Doctor
-last_name:    Orchid
-color:        white
-description:  PhD in plant toxicology. Adopted daughter of Mr. Boddy
-age:          26
-image:        http://www.radiotimes.com/uploads/images/Original/111967.jpg
-ocupation:   Scientist
-
-profPlum
-first_name:   Victor
-last_name:    Plum
-color:        purple
-description:  Billionare video game designer
-age:          22
-image:        https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg
-occupation:   Designer
-
-missScarlet
-first_name:   Kasandra
-last_name:    Scarlet
-color:        red
-description:  She is an A-list movie star with a dark past
-age:          31
-image:        https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg
-occupation:   Actor
-
-mrsPeacock
-first_name:   Eleanor
-last_name:    Peacock
-color:        blue
-description:  She is from a wealthy family and uses her status and money to earn popularity
-age:          36
-image:        https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg
-occupation:   Socialité
-
-mrMustard
-first_name:   Jack
-last_name:    Mustard
-color:        yellow
-description:  He is a former football player who tries to get by on his former glory
-age:          62
-image:        https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg
-occupation:   Retired Football player
+var mrGreen = new Character('Jacob',	'Green',	'green',	'He has a lot of connections',	'45',	'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',	'Entrepreneur');
+var drOrchid = new Character('Doctor',	'Orchid',	'white',	'PhD in plant toxicology. Adopted daughter of Mr. Boddy',	'26',	'http://www.radiotimes.com/uploads/images/Original/111967.jpg',	'Scientist');
+var profPlum = new Character('Victor',	'Plum',	'purple',	'Billionare video game designer',	'22',	'https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg',	'Designer');
+var missScarlet = new Character('Kasandra',	'Scarlet',	'red',	'She is an A-list movie star with a dark past',	'31',	'https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg',	'Actor');
+var mrsPeacock = new Character('Eleanor',	'Peacock',	'blue',	'She is from a wealthy family and uses her status and money to earn popularity',	'36',	'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',	'Socialité');
+var mrMustard = new Character('Jack',	'Mustard',	'yellow',	'He is a former football player who tries to get by on his former glory',	'62',	'https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg',	'Retired Football player');
 
 // Weapons
-
-name: rope        ---   weight: 10
-name: knife       ---   weight: 8
-name: candlestick ---   weight: 2
-name: dumbbell    ---   weight: 30
-name: poison      ---   weight: 2
-name: axe         ---   weight: 15
-name: bat         ---   weight: 13
-name: trophy      ---   weight: 25
-name: pistol      ---   weight: 20
+var rope = new Weapon('rope', 10);
+var knife = new Weapon('knife', 8);
+var candlestick = new Weapon('candlestick', 2);
+var dumbbell = new Weapon('dumbbell', 30);
+var poison = new Weapon('poison', 2);
+var axe = new Weapon('axe', 15);
+var bat = new Weapon('bat', 13);
+var trophy = new Weapon('trophy', 25);
+var pistol = new Weapon('pistol', 20);
 
 // Rooms
-name: Dinning Room
-name: Conservatory
-name: Kitchen
-name: Study
-name: Library
-name: Billiard Room
-name: Lounge
-name: Ballroom
-name: Hall
-name: Spa
-name: Living Room
-name: Observatory
-name: Theater
-name: Guest House
-name: Patio
-*/
+var dinningRoom = new Room('Dinning Room');
+var conervatory = new Room('Conservatory');
+var kitchen = new Room('Kitchen');
+var study = new Room('Study');
+var library = new Room('Library');
+var billiardRoom = new Room('Billiard Room');
+var lounge = new Room('Lounge');
+var ballroom = new Room('Ballroom');
+var hall = new Room('Hall');
+var spa = new Room('Spa');
+var livingRoom = new Room('Living Room');
+var observatory = new Room('Observatory');
+var theater = new Room('Theater');
+var guestHouse = new Room('Guest House');
+var patio = new Room('Patio');
+
+
+// -----------------------------------------------------------------------------
+// 3. Create the collections
+// -----------------------------------------------------------------------------
 
 // Characters Collection
 var charactersArray = [];
