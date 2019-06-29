8bab3e7a5da682d6327dc4d1e98f9c9ee518abcc
Julia Ramos <juliaramosguedes@gmail.com>
2019-06-29 15:02:21 -0300

    correct syntax for jasmine

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,116 +1,104 @@
 // Characters
-const characters = {
-  mrGreen: {
-    firstName: 'Jacob',
-    lastName: 'Green',
-    color: 'green',
-    description: 'He has a lot of connections',
-    age: 45,
-    image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
-    occupation: 'Entrepreneur',
-  },
-  drOrchid: {
-    firstName: 'Doctor',
-    lastName: 'Orchid',
-    color: 'white',
-    description:  `PhD in plant toxicology. Adopted daughter of Mr. Bo'dy`,
-    age: 26,
-    image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
-    ocupation: 'Scientist',
-  },
-  profPlum: {
-    firstName: 'Victor', 
-    lastName: 'Plum',
-    color: 'purple',
-    description: `Billionare video game desi'ner`,
-    age: 22,
-    image:'https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg',
-    occupation: 'Designer',
-  },
-  missScarlet: {
-    firstName: 'Kasandra',
-    lastName: 'Scarlet',
-    color: 'red',
-    description: `She is an A-list movie star' with a dark past`,
-    age: 31,
-    image: 'https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg',
-    occupation: 'Actor',
-  },
-  mrsPeacock: {
-    firstName: 'Eleanor',
-    lastName: 'Peacock',
-    color: 'blue',
-    description: `She is from a wealthy famil'y and uses her status and money to earn popularity`,
-    age: 36,
-    image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
-    occupation: 'Socialité',
-  },
-  mrMustard: {
-    firstName: 'Jack',
-    lastName: 'Mustard',
-    color: 'yellow',
-    description: `He is a former football pla'yer who tries to get by on his former glory`,
-    age: 62,
-    image: 'https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg',
-    occupation: 'Retired Football player'
-  },
+const mrGreen = {
+  first_name: 'Jacob',
+  last_name: 'Green',
+  color: 'green',
+  description: 'He has a lot of connections',
+  age: 45,
+  image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+  occupation: 'Entrepreneur',
+}
+
+const drOrchid = {
+  first_name: 'Doctor',
+  last_name: 'Orchid',
+  color: 'white',
+  description:  `PhD in plant toxicology. Adopted daughter of Mr. Bo'dy`,
+  age: 26,
+  image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
+  ocupation: 'Scientist',
+}
+
+const profPlum = {
+  first_name: 'Victor', 
+  last_name: 'Plum',
+  color: 'purple',
+  description: `Billionare video game desi'ner`,
+  age: 22,
+  image:'https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg',
+  occupation: 'Designer',
+}
+
+const missScarlet = {
+  first_name: 'Kasandra',
+  last_name: 'Scarlet',
+  color: 'red',
+  description: `She is an A-list movie star' with a dark past`,
+  age: 31,
+  image: 'https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg',
+  occupation: 'Actor',
+}
+
+const mrsPeacock = {
+  first_name: 'Eleanor',
+  last_name: 'Peacock',
+  color: 'blue',
+  description: `She is from a wealthy famil'y and uses her status and money to earn popularity`,
+  age: 36,
+  image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
+  occupation: 'Socialité',
+  }
+
+const mrMustard = {
+  first_name: 'Jack',
+  last_name: 'Mustard',
+  color: 'yellow',
+  description: `He is a former football pla'yer who tries to get by on his former glory`,
+  age: 62,
+  image: 'https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg',
+  occupation: 'Retired Football player'
 }
 
 // Weapons
-const weapons = {
-  rope: {weight: 10},
-  knife: {weight: 8},
-  candlestick: {weight: 2},
-  dumbbell: {weight: 30},
-  poison: {weight: 2},
-  axe: {weight: 15},
-  bat: {weight: 13},
-  trophy: {weight: 25},
-  pistol: {weight: 20},
-}
+const weapons = [
+  {name: 'rope', weight: 10},
+  {name: 'knife', weight: 8},
+  {name: 'candlestick', weight: 2},
+  {name: 'dumbbell', weight: 30},
+  {name: 'poison', weight: 2},
+  {name: 'axe', weight: 15},
+  {name: 'bat', weight: 13},
+  {name: 'trophy', weight: 25},
+  {name: 'pistol', weight: 20},
+]
 
 // Rooms
-const rooms = {
-dinning: 'dinning room',
-conservatory: 'conservatory',
-kitchen: 'kitchen',
-study: 'study',
-library: 'library',
-billiard: 'billiard room',
-lounge: 'lounge',
-ballroom: 'ballroom',
-hall: 'hall',
-spa: 'spa',
-living: 'living room',
-observatory: 'observatory',
-theater: 'theater',
-guest: 'guest house',
-patio: 'patio',
-}
-
-// console.log(characters.mrMustard.age)
-// console.log(weapons.trophy.weight)
-// console.log(rooms.lounge)
-
-
-/* After creating the data structure, push every document into their corresponding array. charactersArray, weaponsArray, roomsArray */
-
-function objectsArray(object) {
-  let objectArray = [];
-  for (const char in object) {
-    objectArray.push(char)
-    }
-    return objectArray;
-}
+const rooms = [
+  {name: 'Dinning Room'},
+  {name: 'Conservatory'},
+  {name: 'Kitchen'},
+  {name: 'Study'},
+  {name: 'Library'},
+  {name: 'Billiard Room'},
+  {name: 'Lounge'},
+  {name: 'Ballroom'},
+  {name: 'Hall'},
+  {name: 'Spa'},
+  {name: 'Living Room'},
+  {name: 'Observatory'},
+  {name: 'Theater'},
+  {name: 'Guest House'},
+  {name: 'Patio'}
+]
 
 // Characters Collection
-let charactersArray = objectsArray(characters);
+let charactersArray = [mrGreen, drOrchid, profPlum, missScarlet, mrsPeacock, mrMustard]
 
 // Rooms' Collection
-let roomsArray = objectsArray(rooms);
+let roomsArray = rooms
 
 // Weapons Collection
-let weaponsArray = objectsArray(weapons);
+let weaponsArray = weapons
 
 /* Random Selector
 Create a method randomSelector to randomly select one element from a card stack. The method should receive an array as an argument, and return randomly one of the elements of the array. */
@@ -131,20 +119,14 @@ function pickMistery() {
   return misteryEnvelope;
 }
 
-// console.log(pickMistery())
-
 /* Finally, we need to reveal the mystery. Create a revealMistery method, that will receive our misteryEnvelope array as an argument and return the revealed mystery like this:
-
 <FIRST NAME> <LAST NAME> killed Mr.Boddy using the <WEAPON> in the <PLACE>!!!! */
-let misteryEnvelope = pickMistery();
-
-console.log(misteryEnvelope[0])
-
 function revealMistery (array) {
   let char = array[0];
   let weapon = array[1];
   let room = array[2];
-  return `${characters[char].firstName} ${characters[char].lastName} killed Mr.Boddy using the ${weapon} in the ${rooms[room]}!!!!`;
+  return `${char.first_name} ${char.last_name} killed Mr.Boddy using the ${weapon.name} in the ${room.name}!!!!`;
 }
 
-console.log(revealMistery(misteryEnvelope))
+// let misteryEnvelope = pickMistery();
+// console.log(revealMistery(misteryEnvelope))
