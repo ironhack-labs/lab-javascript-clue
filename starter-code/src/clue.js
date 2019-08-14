1366f3f29ffd22fad7f4cafa872298e04a025ac6
kaiser <cesar.lpz@gmail.com>
2019-08-14 15:06:31 -0500

    Done :octocat:

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -85,10 +85,112 @@ name: Patio
 */
 
 // Characters Collection
-var charactersArray = [];
+const mrGreen = {
+    first_name:   'Jacob',
+    last_name:    'Green',
+    color:        'green',
+    description:  'He has a lot of connections',
+    age:          45,
+    image:        'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+    occupation:   'Entrepreneur'
+}
+const drOrchid = {
+    first_name:   'Doctor',
+    last_name:    'Orchid',
+    color:        'white',
+    description:  'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
+    age:          26,
+    image:        'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
+    ocupation:   'Scientist'
+}
+const profPlum = {
+    first_name:   'Victor',
+    last_name:    'Plum',
+    color:        'purple',
+    description:  'Billionare video game designer',
+    age:          22,
+    image:        'https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg',
+    occupation:   'Designer'
+}
+
+const missScarlet = {
+    first_name:   'Kasandra',
+    last_name:    'Scarlet',
+    color:        'red',
+    description:  'She is an A-list movie star with a dark past',
+    age:          31,
+    image:        'https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg',
+    occupation:   'Actor'
+}
+
+const mrsPeacock = {
+    first_name:   'Eleanor',
+    last_name:    'Peacock',
+    color:        'blue',
+    description:  'She is from a wealthy family and uses her status and money to earn popularity',
+    age:          36,
+    image:        'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
+    occupation:   'Socialité'
+}
+
+const mrMustard = {
+    first_name:   'Jack',
+    last_name:    'Mustard',
+    color:        'yellow',
+    description:  'He is a former football player who tries to get by on his former glory',
+    age:          62,
+    image:        'https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg',
+    occupation:   'Retired Football player'
+}
+
+const charactersArray = [mrGreen, drOrchid, profPlum, missScarlet, mrsPeacock, mrMustard]
 
 // Rooms' Collection
-var roomsArray = [];
+var roomsArray = [
+    {name: 'Dinning Room'},
+    {name: 'Conservatory'},
+    {name: 'Kitchen'},
+    {name: 'Study'},
+    {name: 'Library'},
+    {name: 'Billiard Room'},
+    {name: 'Lounge'},
+    {name: 'Ballroom'},
+    {name: 'Hall'},
+    {name: 'Spa'},
+    {name: 'Living Room'},
+    {name: 'Observatory'},
+    {name: 'Theater'},
+    {name: 'Guest House'},
+    {name: 'Patio'}
+    
+];
 
 // Weapons Collection
-var weaponsArray = [];
+const rope = {name: 'rope', weight:10}
+const knife = {name: 'knife', weight:8}
+const candlestick = {name: 'candlestick', weight:2}
+const dumbbell = {name: 'dumbbell', weight:30}
+const poison = {name: 'poison', weight:2}
+const axe = {name: 'axe', weight:15}
+const bat = {name: 'bat', weight:13}
+const trophy = {name: 'trophy', weight:25}
+const pistol = {name: 'pistol', weight:20}
+
+const weaponsArray = [rope, knife, candlestick, dumbbell, poison, axe, bat, trophy, pistol];
+
+const randomSelector = (array) => {
+    const randomCard = Math.floor(Math.random() * array.length);
+    return array[randomCard]
+}
+
+const pickMistery = () => {
+    const character = randomSelector(charactersArray)
+    const weapon = randomSelector(weaponsArray)
+    const room = randomSelector(roomsArray)
+    const misteryEnvelope = [character, weapon, room]
+    return misteryEnvelope
+}
+
+const revealMistery = (misteryEnvelope) => {
+    return `${misteryEnvelope[0].first_name} ${misteryEnvelope[0].last_name} killed Mr.Boddy using the ${misteryEnvelope[1].name} in the ${misteryEnvelope[2].name}!!!!`
+}  
