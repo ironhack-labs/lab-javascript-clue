f475a62cf68106f66f07f71653c3a74bc8e194fc
Stefan Brückl <stefanbrueckl@Stefans-MacBook-Pro.local>
2019-08-23 15:07:27 +0200

    finished the jasmin test

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -85,10 +85,128 @@ name: Patio
 */
 
 // Characters Collection
-const charactersArray = [];
+const charactersArray = [
+    {
+    character:    "mrGreen",
+    first_name:   "Jacob",
+    last_name:    "Green",
+    color:        "green",
+    description:  "He has a lot of connections",
+    age:          "45",
+    image:        "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+    occupation:   "Entrepreneur"
+    },
+    {
+    character:    "drOrchid",
+    first_name:   "Doctor",
+    last_name:    "Orchid",
+    color:        "white",
+    description:  "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+    age:          "26",
+    image:        "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+    occupation:    "Scientist"
+    },
+    {
+    character:    "profPlum",
+    first_name:   "Victor",
+    last_name:    "Plum",
+    color:        "purple",
+    description:  "Billionaire video game designer",
+    age:          "22",
+    image:        "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
+    occupation:   "Designer"
+    },
+    {
+    character:    "missScarlet",
+    first_name:   "Kasandra",
+    last_name:    "Scarlet",
+    color:        "red",
+    description:  "She is an A-list movie star with a dark past",
+    age:          "31",
+    image:        "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",
+    occupation:   "Actor"
+    },
+    {
+    character:    "mrsPeacock",
+    first_name:   "Eleanor",
+    last_name:    "Peacock",
+    color:        "blue",
+    description:  "She is from a wealthy family and uses her status and money to earn popularity",
+    age:          "36",
+    image:        "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+    occupation:   "Socialité"
+    },
+    {
+    character:    "mrMustard",
+    first_name:   "Jack",
+    last_name:    "Mustard",
+    color:        "yellow",
+    description:  "He is a former football player who tries to get by on his former glory",
+    age:          "62",
+    image:        "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",
+    occupation:   "Retired Football player"
+    }
+];
 
 // Rooms Collection
-const roomsArray = [];
+const roomsArray = [
+    "Dining Room","Conservatory","Kitchen","Study","Library","Billiard Room","Lounge","Lounge","Ballroom",
+    "Hall","Spa","Living Room","Observatory","Theater","Guest House","Patio"
+];
 
 // Weapons Collection
-const weaponsArray = [];
+const weaponsArray = [
+    {
+        name: "rope",
+        weight: 10
+    },
+    {
+        name: "knife",
+        weight: 8
+    },
+    {
+        name: "candlestick",
+        weight: 2
+    },
+    {
+        name: "dumbbell",
+        weight: 30
+    },
+    {
+        name: "poison",
+        weight: 2
+    },
+    {
+        name: "axe",
+        weight: 15
+    },
+    {
+        name: "bat",
+        weight: 13
+    },
+    {
+        name: "trophy",
+        weight: 25
+    },
+    {
+        name: "pistol",
+        weight: 20
+    }
+];
+
+function randomSelector (oneArray) {
+    let rand = oneArray[Math.floor(Math.random() * oneArray.length)];
+    return rand;
+}
+
+function pickMystery() {
+    let mysteryEnvelope = {};
+    mysteryEnvelope.weapon = randomSelector(weaponsArray);
+    mysteryEnvelope.room = randomSelector(roomsArray);
+    mysteryEnvelope.suspect = randomSelector(charactersArray);
+    return mysteryEnvelope;
+}
+
+function revealMystery(mysteryEnvelope){
+    return `${mysteryEnvelope.char.first_name} ${mysteryEnvelope.char.last_name} killed Mr. Boddy using the ${mysteryEnvelope.weapon.name} in the ${mysteryEnvelope.room}`;
+}
