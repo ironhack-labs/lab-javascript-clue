875a579dfd59cf0c6324e06936915eef8bcedf3a
Pascal Hanke <pascal.hanke@yahoo.de>
2019-10-16 15:08:30 +0200

    done

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,73 +1,114 @@
 // Characters
-/*
-mrGreen
-first_name:   Jacob
-last_name:    Green
-color:        green
-description:  He has a lot of connections
-age:          45
-image:        https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg
-occupation:   Entrepreneur
 
-drOrchid
-first_name:   Doctor
-last_name:    Orchid
-color:        white
-description:  PhD in plant toxicology. Adopted daughter of Mr. Boddy
-age:          26
-image:        http://www.radiotimes.com/uploads/images/Original/111967.jpg
-occupation:    Scientist
+const mrGreen = {
+    first_name: "Jacob",
+    last_name: "Green",
+    color: "green",
+    description: "He has a lot of connections",
+    age: 45,
+    image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+    occupation: "Entrepreneur",
+};
 
-profPlum
-first_name:   Victor
-last_name:    Plum
-color:        purple
-description:  Billionaire video game designer
-age:          22
-image:        https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg
-occupation:   Designer
+const drOrchid = {
+    first_name: "Doctor",
+    last_name: "Orchid",
+    color: "white",
+    description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+    age: 26,
+    image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+    occupation: "Scientist"
+};
 
-missScarlet
-first_name:   Kasandra
-last_name:    Scarlet
-color:        red
-description:  She is an A-list movie star with a dark past
-age:          31
-image:        https://www.radiotimes.com/uploads/images/Original/111967.jpg
-occupation:   Actor
+const profPlum = {
+    first_name: "Victor",
+    last_name: "Plum",
+    color: "purple",
+    description: "Billionaire video game designer",
+    age: 22,
+    image: "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg",
+    occupation: "Designer"
+};
 
-mrsPeacock
-first_name:   Eleanor
-last_name:    Peacock
-color:        blue
-description:  She is from a wealthy family and uses her status and money to earn popularity
-age:          36
-image:        https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg
-occupation:   Socialité
+const missScarlet = {
+    first_name: "Kasandra",
+    last_name: "Scarlet",
+    color: "red",
+    description: "She is an A-list movie star with a dark past",
+    age: 31,
+    image: "https://www.radiotimes.com/uploads/images/Original/111967.jpg",
+    occupation: "Actor"
+};
 
-mrMustard
-first_name:   Jack
-last_name:    Mustard
-color:        yellow
-description:  He is a former football player who tries to get by on his former glory
-age:          62
-image:        https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg
-occupation:   Retired Football player
+const mrsPeacock = {
+    first_name: "Eleanor",
+    last_name: "Peacock",
+    color: "blue",
+    description: "She is from a wealthy family and uses her status and money to earn popularity",
+    age: 36,
+    image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+    occupation: "Socialité"
+};
+
+const mrMustard = {
+    first_name: "Jack",
+    last_name: "Mustard",
+    color: "yellow",
+    description: "He is a former football player who tries to get by on his former glory",
+    age: 62,
+    image: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg",
+    occupation: "Retired Football player"
+};
 
 // Weapons
+const rope = {
+    name: "rope",
+    weight: 10
+};
 
-name: rope        ---   weight: 10
-name: knife       ---   weight: 8
-name: candlestick ---   weight: 2
-name: dumbbell    ---   weight: 30
-name: poison      ---   weight: 2
-name: axe         ---   weight: 15
-name: bat         ---   weight: 13
-name: trophy      ---   weight: 25
-name: pistol      ---   weight: 20
+const knife = {
+    name: "knife",
+    weight: 8
+};
+
+const candlestick = {
+    name: "candlestick",
+    weight: 2
+};
+
+const dumbbell = {
+    name: "dumbbell",
+    weight: 30
+};
+
+const poison = {
+    name: "poison",
+    weight: 2
+};
+
+const axe = {
+    name: "axe",
+    weight: 15
+};
+
+const bat = {
+    name: "bat",
+    weight: 13
+};
+
+const trophy = {
+    name: "trophy",
+    weight: 25
+};
+
+const pistol = {
+    name: "pistol",
+    weight: 20
+};
+
 
 // Rooms
-name: Dining Room
+/* name: Dining Room
 name: Conservatory
 name: Kitchen
 name: Study
@@ -82,13 +123,98 @@ name: Observatory
 name: Theater
 name: Guest House
 name: Patio
-*/
+ */
+
+
+const DiningRoom = {
+    name: "Dining Room"
+};
+const Conservatory = {
+    name: "Conservatory"
+};
+const Kitchen = {
+    name: "Kitchen"
+};
+const Study = {
+    name: "Study"
+};
+const Library = {
+    name: "Library"
+};
+const BilliardRoom = {
+    name: "Billiard Room"
+};
+const Lounge = {
+    name: "Lounge"
+};
+const Ballroom = {
+    name: "Ballroom"
+};
+const Hall = {
+    name: "Hall"
+};
+const Spa = {
+    name: "Spa"
+};
+const LivingRoom = {
+    name: "Living Room"
+};
+const Observatory = {
+    name: "Observatory"
+};
+const Theater = {
+    name: "Theater"
+};
+const GuestHouse = {
+    name: "Guest House"
+};
+const Patio = {
+    name: "Patio"
+};
 
 // Characters Collection
-const charactersArray = [];
+const charactersArray = [mrGreen, drOrchid, profPlum, missScarlet, mrsPeacock, mrMustard];
 
 // Rooms Collection
-const roomsArray = [];
+const roomsArray = [DiningRoom, Conservatory, Kitchen, Study, Library, BilliardRoom, Lounge, Ballroom, Hall, Spa, LivingRoom, Observatory, Theater, GuestHouse, Patio];
 
 // Weapons Collection
-const weaponsArray = [];
+const weaponsArray = [rope, knife, candlestick, dumbbell, poison, axe, bat, trophy, pistol];
+
+
+/* const randomSelector = (cardDeck) => {
+    return cardDeck[Math.floor(Math.random()*cardDeck.length)]
+} */
+const randomSelector = (cardDeck) => {
+    let randomNumber = Math.floor(Math.random() * cardDeck.length);
+    return cardDeck[randomNumber];
+};
+
+/* const pickMystery = (characters, weapons, rooms) => {
+
+    let mysteryEnvelope = {
+        character: randomSelector(characters),
+        weapons: randomSelector(weapons),
+        rooms: randomSelector(rooms)
+    };
+
+    return mysteryEnvelope;
+} */
+const pickMystery = () => {
+
+    let mysteryEnvelope = {
+        suspect: randomSelector(charactersArray),
+        weapon: randomSelector(weaponsArray),
+        room: randomSelector(roomsArray)
+    }
+
+    return mysteryEnvelope;
+};
+
+
+const revealMystery = (mystery) => {
+    console.log(mystery)
+    return `${mystery.suspect.first_name} ${mystery.suspect.last_name} killed Mr. Boddy using the ${mystery.weapon.name} in the ${mystery.room.name}!!!!`
+};
+
+revealMystery(pickMystery());
