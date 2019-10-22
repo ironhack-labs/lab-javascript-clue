9f11fa1e406c77e4eeebdc5701b7c41fd4043d00
mendosaurus <masaki.endo@nex.nikkei.com>
2019-10-22 16:46:57 -0400

    done

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,120 +1,150 @@
+// Iteration 1 - Creating the cards
 // Characters
-
 const mrGreen = {
-    first_name:   "Jacob",
-    last_name:    "Green",
-    color:        "green",
-    description:  'He has a lot of connections',
-    age:          45,
-    image:        "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
-    occupation:   "Entrepreneur",
+    first_name: "Jacob",
+    last_name: "Green",
+    color: "green",
+    description: 'He has a lot of connections',
+    age: 45,
+    image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+    occupation: "Entrepreneur",
 }
 
 const drOrchid = {
-    first_name:   "Doctor",
-    last_name:    "Orchid",
-    color:        "white",
-    description:  "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
-    age:          26,
-    image:        "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
-    occupation:    "Scientist"
+    first_name: "Doctor",
+    last_name: "Orchid",
+    color: "white",
+    description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+    age: 26,
+    image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+    occupation: "Scientist"
 }
 
 const profPlum = {
-    first_name:   "Victor",
-    last_name:    "Plum",
-    color:        "purple",
-    description:  "Billionaire video game designer",
-    age:          22,
-    image:        "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg",
-    occupation:   "Designer"
+    first_name: "Victor",
+    last_name: "Plum",
+    color: "purple",
+    description: "Billionaire video game designer",
+    age: 22,
+    image: "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg",
+    occupation: "Designer"
 }
 
 const missScarlet = {
-    first_name:   "Kasandra",
-    last_name:    "Scarlet",
-    color:        "red",
-    description:  "She is an A-list movie star with a dark past",
-    age:          31,
-    image:        "https://www.radiotimes.com/uploads/images/Original/111967.jpg",
-    occupation:   "Actor"
+    first_name: "Kasandra",
+    last_name: "Scarlet",
+    color: "red",
+    description: "She is an A-list movie star with a dark past",
+    age: 31,
+    image: "https://www.radiotimes.com/uploads/images/Original/111967.jpg",
+    occupation: "Actor"
 }
 
 const mrsPeacock = {
-    first_name:   "Eleanor",
-    last_name:    "Peacock",
-    color:        "blue",
-    description:  "She is from a wealthy family and uses her status and money to earn popularity",
-    age:          36,
-    image:        "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
-    occupation:   "Socialité"
+    first_name: "Eleanor",
+    last_name: "Peacock",
+    color: "blue",
+    description: "She is from a wealthy family and uses her status and money to earn popularity",
+    age: 36,
+    image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+    occupation: "Socialité"
 }
 
 const mrMustard = {
-    first_name:   "Jack",
-    last_name:    "Mustard",
-    color:        "yellow",
-    description:  "He is a former football player who tries to get by on his former glory",
-    age:          62,
-    image:        "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg",
-    occupation:   "Retired Football player"
+    first_name: "Jack",
+    last_name: "Mustard",
+    color: "yellow",
+    description: "He is a former football player who tries to get by on his former glory",
+    age: 62,
+    image: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg",
+    occupation: "Retired Football player"
 }
 
 // Weapons
 const rope = {
-    neme:"rope",
-    wight:10
+    neme: "rope",
+    wight: 10
 }
 const knife = {
-    name:"knife",
-    wight:8
+    name: "knife",
+    wight: 8
 }
 const candlestick = {
-    name:"candlestick",
-    weight:2
+    name: "candlestick",
+    weight: 2
 }
 
 const dumbbell = {
-    name:"dumbbell",
-    weight:30
+    name: "dumbbell",
+    weight: 30
 }
 const poison = {
-    name:"poison",
-    weight:2
+    name: "poison",
+    weight: 2
 }
 const axe = {
-    name:"axe",
-    weight:15
+    name: "axe",
+    weight: 15
 }
 const bat = {
-    name:"bat",
-    weight:13
+    name: "bat",
+    weight: 13
 }
 const trophy = {
-    name:"trophy",
-    weight:25
+    name: "trophy",
+    weight: 25
 }
 const pistol = {
-    name:"pistol",
-    weight:20
+    name: "pistol",
+    weight: 20
 }
 
 // Rooms
-const DinningRoom = { name: "DinningRoom" };
-const Conservatory = { name: "Conservatory" };
-const Kitchen = { name: "Kitchen" };
-const Study = { name: "Study" };
-const Library = { name: "Library" };
-const BilliardRoom = { name: "BilliardRoom" };
-const Lounge = { name: "Lounge" };
-const Ballroom = { name: "Ballroom" };
-const Hall = { name: "Hall" };
-const Spa = { name: "Spa" };
-const LivingRoom = { name: "LivingRoom" };
-const Observatory = { name: "Observatory" };
-const Theater = { name: "Theater" };
-const GuestHouse = { name: "GuestHouse" };
-const Patio = { name: "Patio" };
+const DinningRoom = {
+    name: "DinningRoom"
+};
+const Conservatory = {
+    name: "Conservatory"
+};
+const Kitchen = {
+    name: "Kitchen"
+};
+const Study = {
+    name: "Study"
+};
+const Library = {
+    name: "Library"
+};
+const BilliardRoom = {
+    name: "BilliardRoom"
+};
+const Lounge = {
+    name: "Lounge"
+};
+const Ballroom = {
+    name: "Ballroom"
+};
+const Hall = {
+    name: "Hall"
+};
+const Spa = {
+    name: "Spa"
+};
+const LivingRoom = {
+    name: "LivingRoom"
+};
+const Observatory = {
+    name: "Observatory"
+};
+const Theater = {
+    name: "Theater"
+};
+const GuestHouse = {
+    name: "GuestHouse"
+};
+const Patio = {
+    name: "Patio"
+};
 
 // Characters Collection
 const charactersArray = [
@@ -158,31 +188,41 @@ const weaponsArray = [
     pistol
 ];
 
+// Iteration 2 - Creating the mystery
 // Random Selector
 // Create a function randomSelector to randomly select one element from a card stack. The method should receive an array as an argument, and return randomly one of the elements of the array.
-function randomSelector(array){
-    let randomIndex = Math.floor(Math.random()*array.length)
+function randomSelector(array) {
+    let randomIndex = Math.floor(Math.random() * array.length)
     return array[randomIndex]
 }
 
 // Create the mystery
 // We need to create a pickMystery function that will call randomSelector for each card stack, and return an object with the 3 picked cards, a suspect, a weapon and a room. Our mystery should be stored on a mysteryEnvelope variable.
 
-function pickMystery(characters, weapons, rooms) {
-    let mysteryEnvelope = [];
-    mysteryEnvelope.push(randomSelector(characters));
-    mysteryEnvelope.push(randomSelector(weapons));
-    mysteryEnvelope.push(randomSelector(rooms));
-    return mysteryEnvelope;
+//did not work
+// function pickMystery() {
+    //     let cardStack = [charactersArray, weaponsArray, roomsArray]
+    //     let mysteryEnvelope = [];
+    //     return cardStack.forEach(randomSelector)
+    // }
+    
+    // function pickMystery() {
+    //     let cardStack = [charactersArray, weaponsArray, roomsArray]
+    //     return mysteryEnvelope = cardStack.map(randomSelector)
+    // }
+//did not work
+        
+function pickMystery() {
+    let mysteryEnvelope = {};
+    mysteryEnvelope.suspect = randomSelector(charactersArray)
+    mysteryEnvelope.room = randomSelector(roomsArray)
+    mysteryEnvelope.weapon = randomSelector(weaponsArray)
+    return mysteryEnvelope
 }
 
 // Finally, we need to reveal the mystery. Create a revealMystery function, that will receive our mysteryEnvelope object as an argument and return the revealed mystery like this:
 // <FIRST NAME> <LAST NAME> killed Mr. Boddy using the <WEAPON> in the <ROOM>!!!!
 
-function revealMystery(mysteryEnvelope){
-    let firstName = mysteryEnvelope[0].first_name
-    let lastName = mysteryEnvelope[0].last_name
-    let weapon = mysteryEnvelope[1].name
-    let room = mysteryEnvelope[2].name
-    return `${firstName} + " " + ${lastName} + " " + "killed Mr. Boddy using the" + " " +${weapon} + " " + "in the" + " " + ${room} + " " + "!!!!"`
+function revealMystery(mysteryEnvelope) {
+    return (mysteryEnvelope.suspect.first_name + ' ' + mysteryEnvelope.suspect.last_name + ' killed Mr. Boddy using the ' + mysteryEnvelope.weapon.name + ' in the ' + mysteryEnvelope.room.name+'!!!!')
 }
