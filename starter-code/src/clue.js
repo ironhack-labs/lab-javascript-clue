abeda2bd241e4f42dc78b2838f7031ad633e18a8
Daphne <daphne.feresin@gmail.com>
2019-10-22 22:46:58 -0400

    done

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,94 +1,136 @@
-// Characters
-/*
-mrGreen
-first_name:   Jacob
-last_name:    Green
-color:        green
-description:  He has a lot of connections
-age:          45
-image:        https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg
-occupation:   Entrepreneur
-
-drOrchid
-first_name:   Doctor
-last_name:    Orchid
-color:        white
-description:  PhD in plant toxicology. Adopted daughter of Mr. Boddy
-age:          26
-image:        http://www.radiotimes.com/uploads/images/Original/111967.jpg
-occupation:    Scientist
-
-profPlum
-first_name:   Victor
-last_name:    Plum
-color:        purple
-description:  Billionaire video game designer
-age:          22
-image:        https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg
-occupation:   Designer
-
-missScarlet
-first_name:   Kasandra
-last_name:    Scarlet
-color:        red
-description:  She is an A-list movie star with a dark past
-age:          31
-image:        https://www.radiotimes.com/uploads/images/Original/111967.jpg
-occupation:   Actor
-
-mrsPeacock
-first_name:   Eleanor
-last_name:    Peacock
-color:        blue
-description:  She is from a wealthy family and uses her status and money to earn popularity
-age:          36
-image:        https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg
-occupation:   Socialité
-
-mrMustard
-first_name:   Jack
-last_name:    Mustard
-color:        yellow
-description:  He is a former football player who tries to get by on his former glory
-age:          62
-image:        https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg
-occupation:   Retired Football player
-
-// Weapons
-
-name: rope        ---   weight: 10
-name: knife       ---   weight: 8
-name: candlestick ---   weight: 2
-name: dumbbell    ---   weight: 30
-name: poison      ---   weight: 2
-name: axe         ---   weight: 15
-name: bat         ---   weight: 13
-name: trophy      ---   weight: 25
-name: pistol      ---   weight: 20
-
-// Rooms
-name: Dining Room
-name: Conservatory
-name: Kitchen
-name: Study
-name: Library
-name: Billiard Room
-name: Lounge
-name: Ballroom
-name: Hall
-name: Spa
-name: Living Room
-name: Observatory
-name: Theater
-name: Guest House
-name: Patio
-*/
 
 // Characters Collection
-const charactersArray = [];
-
+const charactersArray = [
+  {
+    first_name:   'Jacob',
+    last_name:    'Green',
+    color:        'green',
+    description:  'He has a lot of connections',
+    age:          '45',
+    image:        'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+    occupation:   'Entrepreneur'
+  },
+  {
+    first_name:   'Doctor',
+    last_name:    'Orchid',
+    color:        'white',
+    description:  'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
+    age:          '26',
+    image:        'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
+    occupation:   'Scientist'
+  },
+  
+  {
+    first_name:   'Victor',
+    last_name:    'Plum',
+    color:        'purple',
+    description:  'Billionare video game designer',
+    age:          '22',
+    image:        'https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg',
+    occupation:   'Designer'
+  },
+  {
+    first_name:   'Kasandra',
+    last_name:    'Scarlet',
+    color:        'red',
+    description:  'She is an A-list movie star with a dark past',
+    age:          '31',
+    image:        'https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg',
+    occupation:   'Actor'
+  },
+  {
+    first_name:   'Eleanor',
+    last_name:    'Peacock',
+    color:        'blue',
+    description:  'She is from a wealthy family and uses her status and money to earn popularity',
+    age:          '36',
+    image:        'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
+    occupation:   'Socialité'
+  },
+  {
+    first_name:   'Jack',
+    last_name:    'Mustard',
+    color:        'yellow',
+    description:  'He is a former football player who tries to get by on his former glory',
+    age:          '62',
+    image:        'https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg',
+    occupation:   'Retired Football player'
+  }
+];
 // Rooms Collection
-const roomsArray = [];
+const roomsArray = [ 
+  { 
+    name: 'Dinning Room' },
+  { name: 'Conservatory' },
+  { name: 'Kitchen' },
+  { name: 'Study' },
+  { name: 'Library' },
+  { name: 'Billiard Room' },
+  { name: 'Lounge' },
+  { name: 'Ballroom' },
+  { name: 'Hall' },
+  { name: 'Spa' },
+  { name: 'Living Room' },
+  { name: 'Observatory' },
+  { name: 'Theater' },
+  { name: 'Guest House' },
+  { name: 'Patio' }
+  ];
 
 // Weapons Collection
-const weaponsArray = [];
+const weaponsArray = [
+  { 
+  name: 'rope',         
+  weight: 10 },
+{ 
+  name: 'knife',
+  weight: 8 },
+{ 
+  name: 'candlestick',  
+  weight: 2 },
+{ 
+  name: 'dumbbell',     
+  weight: 30 },
+{ 
+  name: 'poison',       
+  weight: 2 },
+{ 
+  name: 'axe',          
+  weight: 15 },
+{ 
+  name: 'bat',          
+  weight: 13 },
+{ 
+  name: 'trophy',       
+  weight: 25 },
+{ 
+  name: 'pistol',       
+  weight: 20 }
+];
+
+//Random Selector
+function randomSelector(arr){
+let randomObject = arr[Math.floor((Math.random()* arr.length))];
+return randomObject;
+  }
+
+//Create the mistery
+function pickMystery(arr1,arr2,arr3){
+  mysteryEnvelope = {};
+  
+  let card1 = randomSelector(arr1);
+  let card2 = randomSelector(arr2);
+  let card3 = randomSelector(arr3);
+  
+  mysteryEnvelope = [card1,card2,card3];
+  return mysteryEnvelope;
+      }
+
+mysteryEnvelope = pickMystery(charactersArray,roomsArray,weaponsArray);
+
+//Revealing the mistery
+function revealMystery(envelope){
+  console.log(`${envelope[0].first_name} ${envelope[0].last_name} killed Mr. Boddy using the ${envelope[2].name}  in the ${envelope[1].name}!!!!`)
+}
+
+
