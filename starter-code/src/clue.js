e8b5b55e97773d5d690e8f6e83f60b5743c814f0
Imanol Bernardino Alonso <imanolbernardino92@gmail.com>
2019-10-25 08:46:53 +0200

    clueImanol

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,6 +1,9 @@
 // Characters
 /*
-mrGreen
+let charactersArray = [
+
+{
+   
 first_name:   Jacob
 last_name:    Green
 color:        green
@@ -8,8 +11,9 @@ description:  He has a lot of connections
 age:          45
 image:        https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg
 occupation:   Entrepreneur
+}
+{
 
-drOrchid
 first_name:   Doctor
 last_name:    Orchid
 color:        white
@@ -17,8 +21,9 @@ description:  PhD in plant toxicology. Adopted daughter of Mr. Boddy
 age:          26
 image:        http://www.radiotimes.com/uploads/images/Original/111967.jpg
 occupation:    Scientist
+}
+{
 
-profPlum
 first_name:   Victor
 last_name:    Plum
 color:        purple
@@ -26,8 +31,8 @@ description:  Billionaire video game designer
 age:          22
 image:        https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg
 occupation:   Designer
-
-missScarlet
+}
+{
 first_name:   Kasandra
 last_name:    Scarlet
 color:        red
@@ -35,8 +40,9 @@ description:  She is an A-list movie star with a dark past
 age:          31
 image:        https://www.radiotimes.com/uploads/images/Original/111967.jpg
 occupation:   Actor
+}
+{
 
-mrsPeacock
 first_name:   Eleanor
 last_name:    Peacock
 color:        blue
@@ -44,8 +50,9 @@ description:  She is from a wealthy family and uses her status and money to earn
 age:          36
 image:        https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg
 occupation:   Socialité
+}
+{
 
-mrMustard
 first_name:   Jack
 last_name:    Mustard
 color:        yellow
@@ -53,42 +60,64 @@ description:  He is a former football player who tries to get by on his former g
 age:          62
 image:        https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg
 occupation:   Retired Football player
-
+}
+];
 // Weapons
-
-name: rope        ---   weight: 10
-name: knife       ---   weight: 8
-name: candlestick ---   weight: 2
-name: dumbbell    ---   weight: 30
-name: poison      ---   weight: 2
-name: axe         ---   weight: 15
-name: bat         ---   weight: 13
-name: trophy      ---   weight: 25
-name: pistol      ---   weight: 20
+let weaponsArray=[
+(name: rope ,weight: 10 ),
+(name: knife , weight: 8),
+(name: candlestick , weight: 2),
+(name: dumbbell , weight: 30),
+(name: poison ,  weight: 2),
+(name: axe , weight: 15),
+(name: bat , weight: 13),
+(name: trophy , weight: 25),
+(name: pistol , weight: 20)
+];
 
 // Rooms
-name: Dining Room
-name: Conservatory
-name: Kitchen
-name: Study
+let roomsArray=[
+name: Dining Room ,
+name: Conservatory , 
+name: Kitchen , 
+name: Study , 
 name: Library
-name: Billiard Room
-name: Lounge
-name: Ballroom
-name: Hall
-name: Spa
-name: Living Room
-name: Observatory
-name: Theater
-name: Guest House
+name: Billiard Room ,
+name: Lounge , 
+name: Ballroom ,
+name: Hall ,
+name: Spa ,
+name: Living Room ,
+name: Observatory ,
+name: Theater ,
+name: Guest House ,
 name: Patio
+];
 */
 
 // Characters Collection
-const charactersArray = [];
+let charactersArray = [];
 
 // Rooms Collection
-const roomsArray = [];
+let roomsArray = [];
 
 // Weapons Collection
-const weaponsArray = [];
+let weaponsArray = [];
+// CardsClue
+let cardsArray = [];
+
+
+function misteryEnvelope() {
+    let cardsClue = [
+        let charactersArray = []
+        let weaponsArray = []
+        let roomsArray = []
+    ];
+    return cardsClue;
+  }
+  
+
+  function getRandom(cardsClue) {
+     return Math.random(misteryEnvelope);
+  }
+  
