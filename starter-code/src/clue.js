1497fcdf2bd5907a77b9e35db5b0d64be23e6cda
Veronica <veronica1506@gmail.com>
2019-10-31 10:54:51 -0400

    Create weapons-array

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -62,9 +62,47 @@ const suspectsArray = [
 
 // Rooms Collection
 const roomsArray = [];
+    
 
 // Weapons Collection
-const weaponsArray = [];
+const weaponsArray = [
+    {
+        name: "rope",
+        weight: 10
+    },
+    {
+        name: "knife",
+        weight: 8
+    },
+    {
+        name: "candlestick",
+        weight: 2
+    },
+    {
+        name: "dumbbell",
+        weight: 30
+    },
+    {
+        name: "poison",
+        weight: 2
+    },
+    {
+        name: "axe",
+        weight: 15
+    }, 
+    {
+        name: "bat",
+        weight: 13
+    }, 
+    {
+        name: "trophy",
+        weight: 25
+    }, 
+    {
+        name: "pistol",
+        weight: 20
+    }
+];
 
 // ITERATION 2
 
