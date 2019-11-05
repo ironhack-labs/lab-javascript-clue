e36f1c57f98dd1f15b54f18f6c019149bfd908fd
Kayla Martinez <kaylamartinez1201@gmail.com>
2019-11-04 21:01:57 -0500

    format code

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -152,3 +152,5 @@ function pickMystery(){
 function revealMystery(envelope){
     return `${envelope.suspect.firstName} ${envelope.suspect.lastName} killed Mr. Boddy using the ${envelope.weapon.name} in the ${envelope.room.name}!`
 }
+
+
