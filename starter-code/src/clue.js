4061b451df00a40da55ed14cee9b2075ab8a290e
Peacock-Co <pavaoleonardo@hotmail.com>
2019-11-16 13:55:45 +0100

    First iteration

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,13 +1,98 @@
+import { url } from "inspector";
+
 // ITERATION 1
 
 // Suspects Collection
-const suspectsArray = [];
+const suspectsArray = [
+  {
+    firstName: "Jacob",
+    lastName: "mrGreen",
+    ocupation: "Entrepeneur",
+    age: 45,
+    description: "He has a lot of connections",
+    image: url(
+      "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg"
+    )
+  },
+  {
+    firstName: "Doctor",
+    lastName: "Orchid",
+    ocupation: "Scientist",
+    age: 26,
+    description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+    image: url("http://www.radiotimes.com/uploads/images/Original/111967.jpg")
+  },
+  {
+    firstName: "Victor",
+    lastName: "Plum",
+    ocupation: "Designer",
+    age: 22,
+    description: "Billionaire video game designer",
+    image: url(
+      "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg"
+    )
+  },
+  {
+    firstName: "Kasandra",
+    lastName: "Scarlet",
+    ocupation: "Actor",
+    age: 31,
+    description: "She is an A-list movie star with a dark past",
+    image: url("https://www.radiotimes.com/uploads/images/Original/111967.jpg")
+  },
+  {
+    firstName: "Eleanor",
+    lastName: "Peacock",
+    ocupation: "Socialité",
+    age: 36,
+    description:
+      "She is from a wealthy family and uses her status and money to earn popularity",
+    image: url("https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg")
+  },
+  {
+    firstName: "Jack",
+    lastName: "Mustard",
+    ocupation: "Retired Football player",
+    age: 62,
+    description:
+      "He is a former football player who tries to get by on his former glory",
+    image: url(
+      "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg"
+    )
+  }
+];
 
 // Rooms Collection
-const roomsArray = [];
+const roomsArray = [
+  { name: "Dining Room" },
+  { name: "Conservatory" },
+  { name: "Kitchen" },
+  { name: "Study" },
+  { name: "Library" },
+  { name: "Billiard Room" },
+  { name: "Lounge" },
+  { name: "Ballroom" },
+  { name: "Hall" },
+  { name: "Spa" },
+  { name: "Living Room" },
+  { name: "Observatory" },
+  { name: "Theater" },
+  { name: "Guest House" },
+  { name: "Patio" }
+];
 
 // Weapons Collection
-const weaponsArray = [];
+const weaponsArray = [
+  { name: "rope", weight: 10 },
+  { name: "knife", weight: 8 },
+  { name: "candlestic", weight: 2 },
+  { name: "dumbbell", weight: 30 },
+  { name: "poiso", weight: 2 },
+  { name: "axe", weight: 15 },
+  { name: "bat", weight: 13 },
+  { name: "trophy", weight: 25 },
+  { name: "pistol", weight: 20 }
+];
 
 // ITERATION 2
 
