85f8dec507e13282dae0c7245a964e91f7e3bad2
Román Méndez <mendezbeck@gmail.com>
2019-11-16 14:17:40 +0100

    iteration 2 done

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -16,7 +16,7 @@ const suspectsArray = [
     occupation: "Entrepreneur",
     age: 45,
     description: "He has a lot of connections",
-    image: url("https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg"),
+    image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
     color: "green",
   },
   {
@@ -25,7 +25,7 @@ const suspectsArray = [
     occupation: "Scientist",
     age: 26,
     description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
-    image: url("http://www.radiotimes.com/uploads/images/Original/111967.jpg"),
+    image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
     color: "white",
   },
   {
@@ -34,7 +34,7 @@ const suspectsArray = [
     occupation: "Designer",
     age: 22,
     description: "Billionaire video game designer",
-    image: url("https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg"),
+    image: "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg",
     color: "purple",
   },
   {
@@ -43,7 +43,7 @@ const suspectsArray = [
     occupation: "Actor",
     age: 31,
     description: "She is an A-list movie star with a dark past",
-    image: url("https://www.radiotimes.com/uploads/images/Original/111967.jpg"),
+    image: "https://www.radiotimes.com/uploads/images/Original/111967.jpg",
     color: "red",
   },
   {
@@ -52,7 +52,7 @@ const suspectsArray = [
     occupation: "Socialité",
     age: 36,
     description: "She is from a wealthy family and uses her status and money to earn popularity",
-    image: url("https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg"),
+    image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
     color: "blue",
   },
   {
@@ -61,7 +61,7 @@ const suspectsArray = [
     occupation: "Retired Football player",
     age: 62,
     description: "He is a former football player who tries to get by on his former glory",
-    image: url("https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg"),
+    image: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg",
     color: "yellow",
   }
 ];
@@ -87,15 +87,15 @@ const roomsArray = [
 
 // Weapons Collection
 const weaponsArray = [
-    {name: rope, weight: 10},
-    {name: knife, weight: 8},
-    {name: candlestick, weight: 2},
-    {name: dumbbell, weight: 30},
-    {name: poison, weight: 2},
-    {name: axe, weight: 15},
-    {name: bat, weight: 13},
-    {name: trophy, weight: 25},
-    {name: pistol, weight: 20},
+    {name: "rope", weight: 10},
+    {name: "knife", weight: 8},
+    {name: "candlestick", weight: 2},
+    {name: "dumbbell", weight: 30},
+    {name: "poison", weight: 2},
+    {name: "axe", weight: 15},
+    {name: "bat", weight: 13},
+    {name: "trophy", weight: 25},
+    {name: "pistol", weight: 20},
 ];
 
 // ITERATION 2
