987710605906b688a265be1cc926b86874879020
Ruben Vaquero <rvaquero@elpublicamaleon.com>
2019-11-16 14:27:46 +0100

    iteration 3
    q
    :qw
    
    clear
    q
    
    wq
    :wq
    :wq
    q
    quit

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -2,12 +2,12 @@
 
 // Suspects Collection
 const suspectsArray = [
-  {firstName: 'Jacob', lastname: 'Green', occupation: 'Entrepreneur', age: 45, description: 'He has a lot of connections', image: 'https://pbs.twimg.com/profile_images/', color:'green'},
-  {firstName: 'Doctor', lastname: 'Orchid', occupation: 'Scientist', age: 26, description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy', image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',  color:'white'},
-  {firstName: 'Victor', lastname: 'Plum', occupation: 'Designer', age: 22, description: 'Billionaire video game designer', image: 'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',  color:'purple'},
-  {firstName: 'Kasandra', lastname: 'Scarlet', occupation: 'Actor', age: 31, description: 'She is an A-list movie star with a dark past', image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',  color:'red'},
-  {firstName: 'Eleanor', lastname: 'Peacock', occupation: 'Socialité', age: 36, description: 'She is from a wealthy family and uses her status and money to earn popularity', image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',  color:'blue'},
-  {firstName: 'Jack', lastname: 'Mustard', occupation: 'Retired Football player', age: 62, description: 'He is a former football player who tries to get by on his former glory', image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',  color:'yellow'}
+  {firstName: 'Jacob', lastName: 'Green', occupation: 'Entrepreneur', age: 45, description: 'He has a lot of connections', image: 'https://pbs.twimg.com/profile_images/', color:'green'},
+  {firstName: 'Doctor', lastName: 'Orchid', occupation: 'Scientist', age: 26, description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy', image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',  color:'white'},
+  {firstName: 'Victor', lastName: 'Plum', occupation: 'Designer', age: 22, description: 'Billionaire video game designer', image: 'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',  color:'purple'},
+  {firstName: 'Kasandra', lastName: 'Scarlet', occupation: 'Actor', age: 31, description: 'She is an A-list movie star with a dark past', image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',  color:'red'},
+  {firstName: 'Eleanor', lastName: 'Peacock', occupation: 'Socialité', age: 36, description: 'She is from a wealthy family and uses her status and money to earn popularity', image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',  color:'blue'},
+  {firstName: 'Jack', lastName: 'Mustard', occupation: 'Retired Football player', age: 62, description: 'He is a former football player who tries to get by on his former glory', image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',  color:'yellow'}
 ];
 
 // Rooms Collection
@@ -65,3 +65,9 @@ function pickMystery(){
 }
 
 // ITERATION 3
+
+function revealMystery(object){
+  
+  return object.suspect['firstName'] + ' ' + object.suspect['lastName'] + ' ' + 'killed Mr. Boddy using the' + ' ' + object.weapon['name'] + ' ' + 'in the' + ' ' + object.room['name'] + '!';
+  
+}
