609ea9443d78187ace0fb2402b0613c7a8a52734
Giu Roperto <giu.roperto@gmail.com>
2019-11-28 20:53:20 -0300

    finish iteration 2

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -2,147 +2,172 @@
 
 // Suspects Collection
 const suspectsArray = [
-
-  firstName: Jacob,
-  lastName: Green,
-  occupation: Entrepreneur,
-  age: 45,
-  description: He has a lot of connections,
-  image: https: //pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg ,
-  color: green < br >
-
-  firstName: Doctor,
-  lastName: Orchid,
-  occupation: Scientist,
-  age: 26,
-  description: PhD in plant toxicology.Adopted daughter of Mr.Boddy,
-  image: http: //www.radiotimes.com/uploads/images/Original/111967.jpg,
-  color: white < br >
-
-  firstName: Victor,
-  lastName: Plum,
-  occupation: Designer,
-  age: 22,
-  description: Billionaire video game designer,
-  image: https: //66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg ,
-  color: purple < br >
-
-  firstName: Kasandra,
-  lastName: Scarlet,
-  occupation: Actor,
-  age: 31,
-  description: She is an A - list movie star with a dark past,
-  image: https: //www.radiotimes.com/uploads/images/Original/111967.jpg,
-  color: red < br >
-
-  firstName: Eleanor,
-  lastName: Peacock,
-  occupation: Socialité,
-  age: 36,
-  description: She is from a wealthy family and uses her status and money to earn popularity,
-  image: https: //metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg,
-  color: blue < br >
-
-  firstName: Jack,
-  lastName: Mustard,
-  occupation: Retired Football player,
-  age: 62,
-  description: He is a former football player who tries to get by on his former glory,
-  image: https: //static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg,
-  color: yellow < br >
-
-
-
+  {
+    firstName: 'Jacob',
+    lastName: 'Green',
+    occupation: 'Entrepreneur',
+    age: 45,
+    description: 'He has a lot of connections',
+    image: 'https: //pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+    color: 'green',
+  },
+  {
+    firstName: 'Doctor',
+    lastName: 'Orchid',
+    occupation: 'Scientist',
+    age: 26,
+    description: 'PhD in plant toxicology.Adopted daughter of Mr.Boddy',
+    image: 'http: //www.radiotimes.com/uploads/images/Original/111967.jpg',
+    color: 'white',
+  },
+  {
+    firstName: 'Victor',
+    lastName: 'Plum',
+    occupation: 'Designer',
+    age: 22,
+    description: 'Billionaire video game designer',
+    image: 'https: //66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg ',
+    color: 'purple',
+  },
+  {
+    firstName: 'Kasandra',
+    lastName: 'Scarlet',
+    occupation: 'Actor',
+    age: 31,
+    description: 'She is an A - list movie star with a dark past',
+    image: 'https: //www.radiotimes.com/uploads/images/Original/111967.jpg',
+    color: 'red',
+  },
+  {
+    firstName: 'Eleanor',
+    lastName: 'Peacock',
+    occupation: 'Socialité',
+    age: 36,
+    description: 'She is from a wealthy family and uses her status and money to earn popularity',
+    image: 'https: //metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
+    color: 'blue',
+  },
+  {
+    firstName: 'Jack',
+    lastName: 'Mustard',
+    occupation: 'Retired Football player',
+    age: 62,
+    description: 'He is a former football player who tries to get by on his former glory',
+    image: 'https: //static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
+    color: 'yellow',
+  }
 ];
 
 // Rooms Collection
-const roomsArray = [{
-    name: Dining Room
+const roomsArray = [
+  {
+    name: 'Dining Room',
   },
   {
-    name: Conservatory
+    name: 'Conservatory',
   },
   {
-    name: Kitchen
+    name: 'Kitchen',
   },
   {
-    name: Study
+    name: 'Study',
   },
   {
-    name: Library
+    name: 'Library',
   },
   {
-    name: Billiard Room
+    name: 'Billiard Room',
   },
   {
-    name: Lounge
+    name: 'Lounge',
   },
   {
-    name: Ballroom
+    name: 'Ballroom',
   },
   {
-    name: Hall
+    name: 'Hall',
   },
   {
-    name: Spa
+    name: 'Spa',
   },
   {
-    name: Living Room
+    name: 'Living Room',
   },
   {
-    name: Observatory
+    name: 'Observatory',
   },
   {
-    name: Theater
+    name: 'Theater',
   },
   {
-    name: Guest House
+    name: 'Guest House',
   },
   {
-    name: Patio
+    name: 'Patio',
   }
 ];
 
 // Weapons Collection
 const weaponsArray = [
   {
-    name: rope,
-    weight: 10
+    name: 'rope',
+    weight: 10,
   },
   {
-    name: knife,
-    weight: 8
+    name: 'knife',
+    weight: 8,
   },
   {
-    name: candlestick,
-    weight: 2
+    name: 'candlestick',
+    weight: 2,
   },
   {
-    name: dumbbell,
-    weight: 30
+    name: 'dumbbell',
+    weight: 30,
   },
   {
-    name: poison,
-    weight: 2
+    name: 'poison',
+    weight: 2,
   },
   {
-    name: axe,
-    weight: 15
+    name: 'axe',
+    weight: 15,
   },
   {
-    name: bat,
-    weight: 13
+    name: 'bat',
+    weight: 13,
   },
   {
-    name: trophy,
-    weight: 25
+    name: 'trophy',
+    weight: 25,
   },
   {
-    name: pistol,
-    weight: 20
+    name: 'pistol',
+    weight: 20,
   }
 ];
 
 // ITERATION 2
 
+const selectRandom = (array) => {
+  let randomNumber = Math.floor(Math.random() * array.length);
+  // console.log(randomNumber);
+  return array[randomNumber];
+}
+
+// console.log(selectRandom(weaponsArray));
+
+const pickMystery = () => {
+  let killer = selectRandom(suspectsArray);
+  let weapon = selectRandom(weaponsArray);
+  let room = selectRandom(roomsArray);
+
+  console.log(killer);
+  console.log(weapon);
+  console.log(room);
+
+  // return obj {suspectsArray, weaponsArray, roomsArray};
+}
+
+pickMystery();
 // ITERATION 3
