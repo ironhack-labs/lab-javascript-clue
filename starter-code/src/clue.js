25141f417e059147b948d437d052e46d0d686853
Fernando Lima Borrelli <flborrelli@gmail.com>
2019-11-29 00:07:07 -0300

    Done

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,14 +1,126 @@
 // ITERATION 1
 
 // Suspects Collection
-const suspectsArray = [];
+const suspectsArray = [
+{
+    firstName: 'Jacob',
+    lastName: 'Green',
+    occupation: 'Entrepreneur',
+    age: 45,
+    description: 'He has a lot of connections',
+    color: 'green'
+},
+{
+    firstName: 'Doctor',
+    lastName: 'Orchid',
+    occupation: 'Scientist',
+    age: 26,
+    description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy has a lot of connections',
+    color: 'green'
+},
+{
+    firstName: 'Victor',
+    lastName: 'Plum',
+    occupation: 'Designer',
+    age: 22,
+    description: 'Billionaire video game designer',
+    color: 'purple'
+},
+{
+    firstName: 'Kasandra',
+    lastName: 'Scarlet',
+    occupation: 'Actor',
+    age: 31,
+    description: 'She is an A-list movie star with a dark past',
+    color: 'red'
+},
+{
+    firstName: 'Eleanor',
+    lastName: 'Peacock',
+    occupation: 'Socialité',
+    age: 36,
+    description: 'She is from a wealthy family and uses her status and money to earn popularity',
+    color: 'blue'
+},
+{
+    firstName: 'Jack',
+    lastName: 'Mustard',
+    occupation: 'Retired Football player',
+    age: 62,
+    description: 'He is a former football player who tries to get by on his former glory',
+    color: 'yellow'
+}
+];
 
 // Rooms Collection
-const roomsArray = [];
-
+const roomsArray = [
+    {name: 'Dining Room'}, 
+    {name: 'Conservatory'}, 
+    {name: 'Kitchen'}, 
+    {name: 'Study'}, 
+    {name: 'Library'}, 
+    {name: 'Billiard Room'}, 
+    {name: 'Lounge'}, 
+    {name: 'Ballroom'}, 
+    {name: 'Hall'}, 
+    {name: 'Spa'}, 
+    {name: 'Living Room'}, 
+    {name: 'Observatory'}, 
+    {name: 'Theater'}, 
+    {name: 'Guest House'}, 
+    {name: 'Patio'}
+];
 // Weapons Collection
-const weaponsArray = [];
-
+const weaponsArray = [
+{
+    name: 'rope',
+    weight: 10,
+},
+{
+    name: 'knife', 
+    weight: 8
+},
+{
+    name: 'candlestick', 
+    weight: 2
+},
+{
+    name: 'dumbbell', 
+    weight: 30
+},
+{
+    name: 'poison', 
+    weight: 2
+},
+{
+    name: 'axe', 
+    weight: 15
+},
+{
+    name: 'bat', 
+    weight: 13
+},
+{
+    name: 'pistol', 
+    weight: 20
+},
+];
 // ITERATION 2
+function selectRandom(arr){
+    let randomIndexOfArr = Math.floor((Math.random() * arr.length));
+    return arr[randomIndexOfArr];
+};
+
+let gameObject = {suspect: selectRandom(suspectsArray), weapon: selectRandom(weaponsArray), room: selectRandom(roomsArray)};
+
+function pickMystery(){
+    return gameObject;
+};
 
 // ITERATION 3
+function revealMystery(obj){
+    let message = `${obj.suspect.firstName} ${obj.suspect.lastName} killed Mr. Boddy using the ${obj.weapon.name} in the ${obj.room.name}`;
+    return message;
+}
+
+revealMystery(gameObject);
