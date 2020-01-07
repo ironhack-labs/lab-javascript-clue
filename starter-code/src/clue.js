1d52bce619037855b7432335acb7bdce907c312f
javier Varela <javivarela@javiers-MacBook-Pro.local>
2020-01-08 00:04:49 +0100

    Iteration 2: Create the mystery

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -21,7 +21,7 @@ const suspectsArray = [
         color: 'white'
     },
     {
-        firstName: 'Victor'
+        firstName: 'Victor',
         lastName: 'Plum',
         occupation: 'Designer',
         age: '22',
@@ -60,21 +60,21 @@ const suspectsArray = [
 
 // Rooms Collection
 const roomsArray = [
-    {name: Dining Room},
-    {name: Conservatory},
-    {name: Kitchen},
-    {name: Study},
-    {name: Library},
-    {name: Billiard Room},
-    {name: Lounge},
-    {name: Ballroom},
-    {name: Hall},
-    {name: Spa},
-    {name: Living Room},
-    {name: Observatory},
-    {name: Theater},
-    {name: Guest House},
-    {name: Patio}
+    {name: 'Dining Room'},
+    {name: 'Conservatory'},
+    {name: 'Kitchen'},
+    {name: 'Study'},
+    {name: 'Library'},
+    {name: 'Billiard Room'},
+    {name: 'Lounge'},
+    {name: 'Ballroom'},
+    {name: 'Hall'},
+    {name: 'Spa'},
+    {name: 'Living Room'},
+    {name: 'Observatory'},
+    {name: 'Theater'},
+    {name: 'Guest House'},
+    {name: 'Patio'}
 ];
 
 // Weapons Collection
@@ -118,5 +118,19 @@ const weaponsArray = [
 ];
 
 // ITERATION 2
+
+const selectRandom = array => (
+    array[Math.floor(Math.random()*array.length)]
+);
+
+const pickMystery = () => (
+    {
+        suspect: selectRandom(suspectsArray),
+        weapon: selectRandom(weaponsArray),
+        room: selectRandom(roomsArray)
+    }
+);
+
+console.log (pickMystery());
 
 // ITERATION 3
