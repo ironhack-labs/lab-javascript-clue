685199a3dfb5ca3d5e98315f801638d31731fda0
Javier Varela <jvarela@strands.com>
2020-01-07 21:27:32 +0100

    creating collections

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,7 +1,26 @@
 // ITERATION 1
 
 // Suspects Collection
-const suspectsArray = [];
+const suspectsArray = [
+    {
+        firstName: 'Jacob',
+        lastName: 'Green',
+        occupation: 'Entrepreneur',
+        age: '45',
+        description: 'He has a lot of connections',
+        image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+        color: 'green'
+    },
+    {
+        firstName: 'Doctor',
+        lastName: 'Orchid',
+        occupation: 'Scientist',
+        age: '26',
+        description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
+        image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
+        color: 'white'
+    },
+];
 
 // Rooms Collection
 const roomsArray = [];
