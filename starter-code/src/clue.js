ef34d85b6ea7d4d3976634f6016207072f57cfc8
Marta Mullor <Marta@MacBook-Air-de-marta.local>
2020-01-08 23:48:02 +0100

    1 and 3 done 2 with errors

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -109,5 +109,10 @@ function pickMystery(){
   }
   
  
-
 // ITERATION 3
+
+function revealMystery (envelope){
+    return `${envelope.suspect.firstName} ${envelope.suspect.lastName} killed Mr. Boddy using the ${envelope.weapon.name} in the ${envelope.room.name}!`;
+  }
+  
+  revealMystery();
