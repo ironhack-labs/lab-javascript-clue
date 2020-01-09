720b377a3523839e886ab0f336a267fa66c3be64
alexfc96 <alex@LAPTOP-ELMBT3E0.localdomain>
2020-01-09 10:22:29 +0100

    add the class for the rooms(rigth)

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -86,7 +86,21 @@ const suspectsArray = [
 
 // Rooms Collection
 const roomsArray = [
-
+    {name: "Dining Room"},
+    {name: "Conservatory"},
+    {name: "Kitchen"},
+    {name: "Study"},
+    {name: "Library"},
+    {name: "Billiard Room"},
+    {name: "Lounge"},
+    {name: "Ballroom"},
+    {name: "Hall"},
+    {name: "Spa"},
+    {name: "Living Room"},
+    {name: "Observatory"},
+    {name: "Theater"},
+    {name: "Guest House"},
+    {name: "Patio"},
 ];
 
 // Weapons Collection
