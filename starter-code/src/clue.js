cd57cc85c26d92143f2f34732ba19d9d335d28ff
alexfc96 <alex@LAPTOP-ELMBT3E0.localdomain>
2020-01-09 09:30:17 +0100

    two first sentences

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,5 +1,14 @@
 // ITERATION 1
+function selectRandom(array){
+    if (array === []){
+        return undefined
+    }
+    else if (array.length === 1){
+        return array[0]
+    }
 
+
+}
 // Suspects Collection
 const suspectsArray = [];
 
