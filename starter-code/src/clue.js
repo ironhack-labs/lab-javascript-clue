79a44f09e3923b8bfb4cefbd76f4a7816f1c4c28
carlamt25 <57832951+carlamt25@users.noreply.github.com>
2020-01-11 10:24:02 +0100

    done

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,14 +1,62 @@
 // ITERATION 1
 
 // Suspects Collection
-const suspectsArray = [];
+const suspectsArray = [{
+    firstName: "Jacob",
+    lastName: "Green",
+    occupation: "Entrepreneur",
+    age: 45,
+    description: "He has a lot of connections",
+    image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+    color: "green",}, {
+
+        //otro suspitos
+    }
+
+
+
+];
 
 // Rooms Collection
-const roomsArray = [];
+const roomsArray = [{
+    name: "Dining Room",}, {
+
+        //room2
+
+    }];
 
 // Weapons Collection
-const weaponsArray = [];
+const weaponsArray = [{
+    name: "rope", 
+     weight: 10},{
+
+        //weapon2
+     }
+
+];
 
 // ITERATION 2
 
+function selectRandom (array){
+return array[Math.floor(Math.random() * array.length)];
+//array.length xk quiero que el random esté entre la longitud del array
+//math.random coge un num de 0 a 1 aleatoriamente
+//* array.length: lo multiplica por la long de la array
+//math.floor lo redondea hacia abajo/ math.average lo redondea normal/ math.ceiling
+};
+
+function pickMystery (){
+    return {
+        suspect: selectRandom(suspectsArray),
+        weapon: selectRandom(weaponsArray),
+        room: selectRandom(roomsArray),
+    }
+}
+
+
 // ITERATION 3
+
+function revealMystery (envelope){
+    return envelope.suspect.firstName + " " +  envelope.suspect.lastName + " " + "killed Mr. Boddy using the" + " " + envelope.weapon.name + " " + "in the" + " " + envelope.room.name + "!"
+
+}
