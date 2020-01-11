d5c787389a534a0d9a9903b0027f2a9d46e51ab8
Leinadm <danielmonteru@gmail.com>
2020-01-11 06:45:44 +0100

    done

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -33,7 +33,7 @@ const suspectsArray = [
         age: '31',
         description: 'She is an A-list movie star with a dark past',
         image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
-        color: 'red',
+        color: 'red'
     }, {
         firstName: 'Eleanor',
         lastName: 'Peacock',
@@ -41,15 +41,15 @@ const suspectsArray = [
         age: '36',
         description: 'She is from a wealthy family and uses her status and money to earn popularity',
         image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
-        color: 'blue',
+        color: 'blue'
     }, {
         firstName: 'Jack',
         lastName: 'Mustard',
         occupation: 'Retired Football player',
         age: '62',
         description: 'He is a former football player who tries to get by on his former glory',
-        image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg','
-        color: yellow<br>
+        image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
+        color: 'yellow'
     }
 ];
 
@@ -74,17 +74,42 @@ const roomsArray = [
 
 // Weapons Collection
 const weaponsArray = [
-    {name: rope, weight: 10},
-    {name: knife, weight: 8},
-    {name: candlestick, weight: 2},
-    {name: dumbbell, weight: 30},
-    {name: poison, weight: 2},
-    {name: axe, weight: 15},
-    {name: bat, weight: 13},
-    {name: trophy, weight: 25},
-    {name: pistol, weight: 20}
+    {name: 'rope', weight: 10},
+    {name: 'knife', weight: 8},
+    {name: 'candlestick', weight: 2},
+    {name: 'dumbbell', weight: 30},
+    {name: 'poison', weight: 2},
+    {name: 'axe', weight: 15},
+    {name: 'bat', weight: 13},
+    {name: 'trophy', weight: 25},
+    {name: 'pistol', weight: 20}
 ];
 
 // ITERATION 2
 
+function selectRandom(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function pickMystery(arr1, arr2, arr3){
+    var suspect = selectRandom(arr1);
+    var weapon = selectRandom(arr2);
+    var room = selectRandom(arr3);
+
+    var mistery = {Suspect: suspect, Weapon: weapon, Room: room};
+
+    return mistery;
+}
+
 // ITERATION 3
+
+function revealMistery(obj){
+    var firstName = obj.Suspect.firstName;
+    var lastName = obj.Suspect.lastName;
+    var weapon = obj.Weapon.name;
+    var room = obj.Room.name;
+
+    var revealMistery = firstName.concat(' ').concat(lastName).concat(' killed Mr. Boddy using the ').concat(weapon).concat(' in the ').concat(room).concat('!');
+    
+    return revealMistery;
+}
