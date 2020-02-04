fc7202fdf0c290662babc6018d5001c55ab41bf4
PiaG <piagmdf@gmail.com>
2020-02-04 21:37:22 +0100

    fill up arrays with suspects, weapons, rooms. set random-function.

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,14 +1,189 @@
 // ITERATION 1
 
 // Suspects Collection
-const suspectsArray = [];
+const suspectsArray = [
+    //mrGreen <br>
+    {
+        firstName: "Jacob",
+        lastName: "Green",
+        occupation: "Entrepreneur",
+        age: 45,
+        description: "He has a lot of connections",
+        image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+        color: "green",
+    },
+
+    //drOrchid, 
+    {
+        firstName: "Doctor",
+        lastName: "Orchid",
+        occupation: "Scientist",
+        age: 26,
+        description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+        image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+        color: "white",
+    },
+
+    //profPlum<br>
+    {
+        firstName: "Victor",
+        lastName: "Plum",
+        occupation: "Designer",
+        age: 22,
+        description: "Billionaire video game designer",
+        image: "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg",
+        color: "purple",
+    },
+
+    //missScarlet<br>
+    {
+        firstName: "Kasandra",
+        lastName: "Scarlet",
+        occupation: "Actor",
+        age: 31,
+        description: "She is an A-list movie star with a dark past",
+        image: "https://www.radiotimes.com/uploads/images/Original/111967.jpg",
+        color: "red",
+    },
+
+    //mrsPeacock<br>
+    {
+        firstName: "Eleanor",
+        lastName: "Peacock",
+        occupation: "Socialité",
+        age: 36,
+        description: "She is from a wealthy family and uses her status and money to earn popularity",
+        image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+        color: "blue",
+    },
+
+    //mrMustard<br>
+    {
+        firstName: "Jack",
+        lastName: "Mustard",
+        occupation: "Retired Football player",
+        age: 62,
+        description: "He is a former football player who tries to get by on his former glory",
+        image: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg",
+        color: "yellow",
+    }
+];
 
 // Rooms Collection
-const roomsArray = [];
+const roomsArray = [{
+        name: "Dining Room"
+    },
+    {
+        name: "Conservatory"
+    },
+    {
+        name: "Kitchen"
+    },
+    {
+        name: "Study"
+    },
+    {
+        name: "Library"
+    },
+    {
+        name: "Billiard Room"
+    },
+    {
+        name: "Lounge"
+    },
+    {
+        name: "Ballroom"
+    },
+    {
+        name: "Hall"
+    },
+    {
+        name: "Spa"
+    },
+    {
+        name: "Living Room"
+    },
+    {
+        name: "Observatory"
+    },
+    {
+        name: "Theater"
+    },
+    {
+        name: "Guest House"
+    },
+    {
+        name: "Patio"
+    }
+];
+
 
 // Weapons Collection
-const weaponsArray = [];
+const weaponsArray = [{
+        name: "rope",
+        weight: 10
+    },
+    {
+        name: "knife",
+        weight: 8
+    },
+    {
+        name: "candlestick",
+        weight: 2
+    },
+    {
+        name: "dumbbell",
+        weight: 30
+    },
+    {
+        name: "poison",
+        weight: 2
+    },
+    {
+        name: "axe",
+        weight: 15
+    },
+    {
+        name: "bat",
+        weight: 13
+    },
+    {
+        name: "trophy",
+        weight: 25
+    },
+    {
+        name: "pistol",
+        weight: 20
+    }
+];
 
 // ITERATION 2
+/* 
+At the beginning of the game, players shuffle each of the card 
+stacks to create a combination of suspect, weapon and room. This will 
+be the mystery to solve.
+
+Random selector
+Declare a function named selectRandom to randomly select one element 
+from a card stack. The function should expect an array as an argument, 
+and should return a random element from the array.
+
+Create the mystery
+Declare a function named pickMystery that takes no arguments and 
+returns an object with three properties, suspect, weapon and room, 
+each holding as a value a card of that specific type. You can get a 
+random card of each type by calling selectRandom on each card stack.
+*/
+
+function selectRandom(theSuspect) {
+    let ourSuspect = Math.floor(Math.random() * theSuspect.length);
+    // console.log("ourSuspect",ourSuspect)
+    // console.log("mir",theSuspect[ourSuspect])
+    return theSuspect[ourSuspect].firstName
+}
+
+selectRandom(suspectsArray)
+console.log(selectRandom(suspectsArray))
+
 
 // ITERATION 3
