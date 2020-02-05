160ea0d711bcfebc232dc548adf17b96c74f22a8
marie-pascale hardy <mariepascalehardy@mariepacalesmbp.localdomain>
2020-02-05 10:47:37 +0100

    iteration 3 completed

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,103 +1,187 @@
 // ITERATION 1
 
-// Suspects Collection
-let suspectsArray = [
-    {
-    nickName: "mrGreen",
-    firstName: "Jacob",
-    lastName: "Green",
-    occupation: "Entrepreneur",
-    age: 45,
-    description: "He has a lot of connections",
-    image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
-    color: "green" 
+
+let suspectsArray = [{
+        nickName: "mrGreen",
+        firstName: "Jacob",
+        lastName: "Green",
+        occupation: "Entrepreneur",
+        age: 45,
+        description: "He has a lot of connections",
+        image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+        color: "green"
     },
     {
-    nickName: "drOrchid",
-    firstName: "Doctor",
-    lastName: "Orchid",
-    occupation: "Scientist",
-    age: 26,
-    description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
-    image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
-    color: "white"
+        nickName: "drOrchid",
+        firstName: "Doctor",
+        lastName: "Orchid",
+        occupation: "Scientist",
+        age: 26,
+        description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+        image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+        color: "white"
     },
     {
-    nickName: "profPlum",
-    firstName: "Victor",
-    lastName: "Plum",
-    occupation: "Designer",
-    age: 22,
-    description: "Billionaire video game designer",
-    image: "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg",
-    color: "purple"
+        nickName: "profPlum",
+        firstName: "Victor",
+        lastName: "Plum",
+        occupation: "Designer",
+        age: 22,
+        description: "Billionaire video game designer",
+        image: "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg",
+        color: "purple"
+    },
+    {
+        nickname: "missScarlet",
+        firstName: "Kasandra",
+        lastName: "Scarlet",
+        occupation: "Actor",
+        age: 31,
+        description: "She is an A-list movie star with a dark past",
+        image: "https://www.radiotimes.com/uploads/images/Original/111967.jpg",
+        color: "red"
+    },
+    {
+        nickname: "mrsPeacock",
+        firstName: "Eleanor",
+        lastName: "Peacock",
+        occupation: "Socialité",
+        age: 36,
+        description: "She is from a wealthy family and uses her status and money to earn popularity",
+        image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+        color: "blue"
+    },
+    {
+        nickname: "mrMustard",
+        firstName: "Jack",
+        lastName: "Mustard",
+        occupation: "Retired Football player",
+        age: 62,
+        description: "He is a former football player who tries to get by on his former glory",
+        image: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg",
+        color: "yellow"
     }
-    /*missScarlet<br>
-    firstName: Kasandra<br>
-    lastName: Scarlet<br>
-    occupation: Actor<br>
-    age: 31<br>
-    description: She is an A-list movie star with a dark past<br>
-    image: https://www.radiotimes.com/uploads/images/Original/111967.jpg<br>
-    color: red<br>
-    
-    mrsPeacock<br>
-    firstName: Eleanor<br>
-    lastName: Peacock<br>
-    occupation: Socialité<br>
-    age: 36<br>
-    description: She is from a wealthy family and uses her status and money to earn popularity<br>
-    image: https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg<br>
-    color: blue<br>
-    
-    mrMustard<br>
-    firstName: Jack<br>
-    lastName: Mustard<br>
-    occupation: Retired Football player<br>
-    age: 62<br>
-    description: He is a former football player who tries to get by on his former glory<br>
-    image: https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg<br>
-    color: yellow<br>*/
-    ]; 
-    
-    // Rooms Collection
-    let roomsArray = [
-        {name: "Dining Room"},
-        {name: "Conservatory"},
-        {name: "Kitchen"},
-        {name: "Study"},
-        {name: "Library"},
-        {name: "Billiard Room"},
-        {name: "Lounge"},
-        {name: "Ballroom"},
-        {name: "Hall"},
-        {name: "Spa"},
-        {name: "Living Room"},
-        {name: "Observatory"},
-        {name: "Theater"},
-        {name: "Guest House"},
-        {name: "Patio"}
-        
-    ];
-    // Weapons Collection
-    let weaponsArray = [
-        {name: "rope", weight: 10},
-        {name: "knife", weight: 8},
-        {name: "candlestick", weight: 2},
-        {name: "dumbbell", weight: 30},
-        {name: "poison", weight: 2},
-        {name: "axe", weight: 15},
-        {name: "bat", weight: 13},
-        {name: "trophy", weight: 25},
-        {name: "pistol", weight: 20}
-    ];
-    
-    
-    function selectRandom(mySuspectsArray){
-        let result = Math.floor(Math.random() * mySuspectsArray.length)
-        //console.log(mySuspectsArray[result])
-        return mySuspectsArray[result]
+];
+
+// Rooms Collection
+let roomsArray = [{
+        name: "Dining Room"
+    },
+    {
+        name: "Conservatory"
+    },
+    {
+        name: "Kitchen"
+    },
+    {
+        name: "Study"
+    },
+    {
+        name: "Library"
+    },
+    {
+        name: "Billiard Room"
+    },
+    {
+        name: "Lounge"
+    },
+    {
+        name: "Ballroom"
+    },
+    {
+        name: "Hall"
+    },
+    {
+        name: "Spa"
+    },
+    {
+        name: "Living Room"
+    },
+    {
+        name: "Observatory"
+    },
+    {
+        name: "Theater"
+    },
+    {
+        name: "Guest House"
+    },
+    {
+        name: "Patio"
     }
-    console.log(selectRandom(suspectsArray))
+
+];
+// Weapons Collection
+let weaponsArray = [{
+        name: "rope",
+        weight: 10
+    },
+    {
+        name: "knife",
+        weight: 8
+    },
+    {
+        name: "candlestick",
+        weight: 2
+    },
+    {
+        name: "dumbbell",
+        weight: 30
+    },
+    {
+        name: "poison",
+        weight: 2
+    },
+    {
+        name: "axe",
+        weight: 15
+    },
+    {
+        name: "bat",
+        weight: 13
+    },
+    {
+        name: "trophy",
+        weight: 25
+    },
+    {
+        name: "pistol",
+        weight: 20
+    }
+];
+
+// ITERATION 2
+
+function selectRandom(cardStackArray) {
+    let result = Math.floor(Math.random() * cardStackArray.length)
+    //console.log(cardStackArray[result])
+    return cardStackArray[result]
+}
+//console.log(selectRandom(suspectsArray))
+
+
+
+
+function pickMystery() {
+    let objectWithThreeProperties = {
+        suspect: selectRandom(suspectsArray),
+        weapon: selectRandom(weaponsArray),
+        room: selectRandom(roomsArray)
+    };
+    //console.log(objectWithThreeProperties)
+    return objectWithThreeProperties
+}
+
+//pickMystery()
+
 
 // ITERATION 3
+
+let envelope = pickMystery()
+
+function revealMystery(object) {
+    let revealMessage = `${object.suspect.firstName} ${object.suspect.lastName} killed Mr. Boddy using the ${object.weapon.name} in the ${object.room.name.toLowerCase()}!`
+    return revealMessage
+}
+
+revealMystery(envelope)
