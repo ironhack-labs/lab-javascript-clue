17f251aad90488d318637bd7579d46bf1caaca4e
Ben Balderas <benbalderas3@gmail.com>
2020-02-11 23:17:53 -0600

    started suspects array

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,14 +1,73 @@
 // ITERATION 1
 
 // Suspects Collection
-const suspectsArray = [];
+const suspectsArray = [{
+        firstName: "Jacob",
+        lastName: "Green",
+        occupation: "Entrepreneur",
+        age: 45,
+        description: "He has a lot of connections",
+        image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+        color: "green"
+    },
+    {
+        firstName: "Doctor",
+        lastName: "Orchid",
+        occupation: "Scientist",
+        age: 26,
+        description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+        image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+        color: "white"
+    },
+    {
+        firstName: "Victor",
+        lastName: "Plum",
+        occupation: "Designer",
+        age: 22,
+        description: "Billionaire video game designer",
+        image: "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg",
+        color: "purple"
+    },
+    {
+        firstName: "",
+        lastName: "",
+        occupation: "",
+        age: 0,
+        description: "",
+        image: "",
+        color: ""
+    },
+    {
+        firstName: "",
+        lastName: "",
+        occupation: "",
+        age: 0,
+        description: "",
+        image: "",
+        color: ""
+    },
+    {
+        firstName: "",
+        lastName: "",
+        occupation: "",
+        age: 0,
+        description: "",
+        image: "",
+        color: ""
+    }
+];
 
 // Rooms Collection
-const roomsArray = [];
+const roomsArray = [
+
+];
 
 // Weapons Collection
-const weaponsArray = [];
+const weaponsArray = [
+
+];
 
 // ITERATION 2
+
 
 // ITERATION 3
