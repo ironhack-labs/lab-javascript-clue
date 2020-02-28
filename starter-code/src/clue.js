edb387930d88a45ca9c81646f40c470a64a9a48a
brunobsb <tavares.bruno@gmail.com>
2020-02-28 04:10:45 -0300

    only one left

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -3,63 +3,63 @@
 // Suspects Collection
 const suspectsArray = [
     {
-        firstName: Jacob ,
-        lastName: Green,
-        occupation: Entrepreneur,
+        firstName: 'Jacob' ,
+        lastName: 'Green',
+        occupation: 'Entrepreneur',
         age: 45 ,
-        description: He has a lot of connections,
-        image: https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg ,
-        color: green 
-    }
+        description: 'He has a lot of connections',
+        image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+        color: 'green'
+    },
     {
-        firstName: Doctor,
-        lastName: Orchid,
-        occupation: Scientist,
+        firstName: 'Doctor',
+        lastName: 'Orchid',
+        occupation: 'Scientist',
         age: 26,
-        description: PhD in plant toxicology. Adopted daughter of Mr. Boddy,
-        image: http://www.radiotimes.com/uploads/images/Original/111967.jpg,
-        color: white
-    }
+        description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
+        image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
+        color: 'white'
+    },
     {
-        firstName: Victor,
-        lastName: Plum,
-        occupation: Designer,
+        firstName: 'Victor',
+        lastName: 'Plum',
+        occupation: 'Designer',
         age: 22,
-        description: Billionaire video game designer,
-        image: https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg ,
-        color: purple
-    }
+        description: 'Billionaire video game designer',
+        image: 'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg' ,
+        color: 'purple'
+    },
     {
 
-        firstName: Kasandra,
-        lastName: Scarlet,
-        occupation: Actor,
+        firstName: 'Kasandra',
+        lastName: 'Scarlet',
+        occupation: 'Actor',
         age: 31,
-        description: She is an A-list movie star with a dark past,
-        image: https://www.radiotimes.com/uploads/images/Original/111967.jpg,
-        color: red
+        description: 'She is an A-list movie star with a dark past',
+        image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
+        color: 'red'
 
-    }
+    },
     {
 
-        firstName: Eleanor,
-        lastName: Peacock,
-        occupation: Socialité,
+        firstName: 'Eleanor',
+        lastName: 'Peacock',
+        occupation: 'Socialité',
         age: 36,
-        description: She is from a wealthy family and uses her status and money to earn popularity,
-        image: https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg,
-        color: blue
+        description: 'She is from a wealthy family and uses her status and money to earn popularity',
+        image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
+        color: 'blue'
 
-    }
+    },
     {
 
-        firstName: Jack,
-        lastName: Mustard,
-        occupation: Retired Football player,
+        firstName: 'Jack',
+        lastName: 'Mustard',
+        occupation: 'Retired Football player',
         age: 62,
-        description: He is a former football player who tries to get by on his former glory,
-        image: https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg,
-        color: yellow
+        description: 'He is a former football player who tries to get by on his former glory',
+        image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
+        color: 'yellow'
 
     }
     // ...
@@ -67,54 +67,54 @@ const suspectsArray = [
 // Rooms Collection
 const roomsArray = [
     {
-        name: Dining Room,
-    } 
+        name: 'Dining Room',
+    },
     {
-        name: Conservatory, 
-    } 
+        name: 'Conservatory', 
+    },
     {  
-        name: Conservatory,
-    }
+        name: 'Conservatory',
+    },
     {    
-        name: Kitchen,
-    }
+        name: 'Kitchen',
+    },
     {    
-        name: Study,
-    }    
+        name: 'Study',
+    },   
     { 
     
-        name: Library,
-    }
+        name: 'Library',
+    },
     { 
-        name: Billiard Room,
-   }
+        name: 'Billiard Room',
+   },
    { 
-        name: Lounge,
-   }
+        name: 'Lounge',
+   },
    { 
-        name: Ballroom,
-   }
+        name: 'Ballroom',
+   },
    { 
-        name: Hall,
-   }
+        name: 'Hall',
+   },
    { 
-        name: Spa,
-   }
+        name: 'Spa',
+   },
    { 
-        name: Living Room,
-   }
+        name: 'Living Room',
+   },
    { 
-        name: Observatory,
-   }
+        name: 'Observatory',
+   },
    { 
-        name: Theater,
-   }
+        name: 'Theater',
+   },
    { 
-        name: Guest House,
-   }
+        name: 'Guest House',
+   },
    { 
-        name: Patio
-   }
+        name: 'Patio'
+   },
 
 
 
@@ -125,60 +125,74 @@ const roomsArray = [
 const weaponsArray = [
 
 {
-
-    name: rope --- weight: 10,
-}
+    name: 'rope' , weight: 10,
+},
 {    
-    name: knife --- weight: 8,
-}
+    name: 'knife' , weight: 8,
+},
 {    
-    name: candlestick --- weight: 2,
-}
+    name: 'candlestick' , weight: 2,
+},
 {    
-    name: dumbbell --- weight: 30,
-}
+    name: 'dumbbell' , weight: 30,
+},
 {    
-    name: poison --- weight: 2,
-}
+    name: 'poison' , weight: 2,
+},
 {    
-    name: axe --- weight: 15,
-}
+    name: 'axe' , weight: 15,
+},
 {    
-    name: bat --- weight: 13,
-}
+    name: 'bat' , weight: 13,
+},
 {    
-    name: trophy --- weight: 25,
-}
+    name: 'trophy' , weight: 25,
+},
 {    
-    name: pistol --- weight: 20
-}
+    name: 'pistol', weight:20
+},
 
 
 ];
+let envelope = "";
+let suspect = "";
+let weapon = "";
+let room = "";
+let arrayContainer = "";
+let mysteryEnvelope ="";
+ function selectRandom(arrayContainer) {
+ if(arrayContainer.length == 0){
 
-function randompick(min,max){
-
-    return Math.random(0 - -1) + 0;
-
+    return undefined; //Se o arrayContainer está vazio, ela ja não retonaria undefined por default?
 }
-console.log(suspectsArray);
-return randompick(suspectsArray);
-// container = suspectsArray.length, index;
+
+    return arrayContainer[Math.floor(Math.random()*arrayContainer.length)];
     
-// while (container > 0){
+}
 
-//     index = Math.floor(Math.random()* container);
 
-//     container -= 1;
-
-//     arqTemporario = suspectsArray[container];
-//     suspectsArray[container] = suspectsArray[index];
-//     suspectsArray[index] = arqTemporario;
-
-// }
-// return suspectsArray;
-// }
-// console.log(randompick(suspectsArray));
 // ITERATION 2
 
+function pickMystery(){ 
+
+   let mysteryEnvelope = {        suspect: selectRandom(suspectsArray),        weapon: selectRandom(weaponsArray),        room: selectRandom(roomsArray) }   
+
+    return mysteryEnvelope;    
+}
+
+
 // ITERATION 3
+
+function revealMystery(){
+
+
+ 
+ let envelope =`${suspect.firstName} ${suspect.lastName} killed Mr. Boddy using the ${weapon.name} in the ${room.name}!`;
+console.log(envelope);
+ return envelope;
+
+}
+
+
+
+revealMystery(envelope);
