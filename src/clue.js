4b81f99b1e7592c5b1353755724a919644c733c4
Elvira Ramírez <elvirarp92@gmail.com>
2020-03-26 22:09:35 +0100

    switch names of weaponsArray and roomsArray, formerly mistaken for each other

--- src/clue.js
+++ src/clue.js
@@ -65,19 +65,6 @@ const suspectsArray = [
 
 // Rooms Collection
 const roomsArray = [
-  { name: 'rope', weight: 10 },
-  { name: 'knife', weight: 8 },
-  { name: 'candlestick', weight: 2 },
-  { name: 'dumbbell', weight: 30 },
-  { name: 'poison', weight: 2 },
-  { name: 'axe', weight: 15 },
-  { name: 'bat', weight: 13 },
-  { name: 'trophy', weight: 25 },
-  { name: 'pistol', weight: 20 }
-];
-
-// Weapons Collection
-const weaponsArray = [
   { name: 'Dining Room' },
   { name: 'Conservatory' },
   { name: 'Kitchen' },
@@ -95,6 +82,18 @@ const weaponsArray = [
   { name: 'Patio' }
 ];
 
+// Weapons Collection
+const weaponsArray = [
+  { name: 'rope', weight: 10 },
+  { name: 'knife', weight: 8 },
+  { name: 'candlestick', weight: 2 },
+  { name: 'dumbbell', weight: 30 },
+  { name: 'poison', weight: 2 },
+  { name: 'axe', weight: 15 },
+  { name: 'bat', weight: 13 },
+  { name: 'trophy', weight: 25 },
+  { name: 'pistol', weight: 20 }
+];
 // ITERATION 2
 
 
