b87a620dace35eaad46e70be1b99621f0a37821a
Jesus Jurado <jesusjbarrau@gmail.com>
2020-03-26 21:51:15 +0100

    Ejercicio js

--- src/clue.js
+++ src/clue.js
@@ -1,7 +1,21 @@
 // ITERATION 1
-
+function selectRandom(arr){
+    return undefined
+}
 // Suspects Collection
-const suspectsArray = [];
+const suspectsArray = [
+
+    {
+        "firstName" : "Jacob",
+        "lastName" : "Green",
+        "occupation" : "Entrepreneur",
+        "age" : 45,
+        "description" : "He has a lot of connections",
+        "image" : "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+        "color" : "green" 
+    }
+    
+];
 
 // Rooms Collection
 const roomsArray = [];
