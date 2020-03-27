a362dcfaa7e1609a40d18d7a9e88309ee7542726
DayanRojas <dayan18_13@hotmail.com>
2020-03-27 08:35:53 +0100

    done

--- src/clue.js
+++ src/clue.js
@@ -1,13 +1,46 @@
 // ITERATION 1
 
-// Suspects Collection
-const suspectsArray = [];
+const suspectsArray = [
+    {
+        firstName: 'Jacob',
+        lastName: 'Green',
+        occupation: 'Entrepreneur'
+    }
+];
+const roomsArray = [2, 5, 7, 5, 3, 7, 9];
+const weaponsArray = ["Paper", "water", "poison"];
 
-// Rooms Collection
-const roomsArray = [];
+const selectRandom = (array) => {
+    let randomIdx = 0;
+    if (array.length == 0) {
+        return undefined
+    } else if (array.length == 1) {
+        return array[0];
+    } else {
+        randomIdx = Math.floor((Math.random() * array.length));
+        return array[randomIdx]
+    }
+}
+const pickMystery = () => {
+    let object = {
+        suspect: selectRandom(suspectsArray),
+        weapon: selectRandom(weaponsArray),
+        room: selectRandom(roomsArray)
+    }
 
-// Weapons Collection
-const weaponsArray = [];
+    return object
+}
+
+
+let envelope = pickMystery()
+
+
+const revealMystery = (envelope) => {
+    return `${envelope.suspect.firstName} ${envelope.suspect.lastName} killed Mr. Boddy using the ${envelope.weapon.name} in the ${envelope.room.name}!`
+}
+
+
+revealMystery(envelope);
 
 // ITERATION 2
 
