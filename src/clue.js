2a8705b99b124e9da6d4d961b63aeaca4c49de7a
Marc De Mena <mdemena@gmail.com>
2020-04-01 11:00:00 +0200

    Finished exercise

--- src/clue.js
+++ src/clue.js
@@ -132,3 +132,6 @@ function pickMystery(){
     return misteryEnvelope;
 }
 // ITERATION 3
+function revealMystery(envelope){
+    return `${envelope.suspect.firstName} ${envelope.suspect.lastName} killed Mr. Boddy using the ${envelope.weapon.name} in the ${envelope.room.name}!`;
+}
