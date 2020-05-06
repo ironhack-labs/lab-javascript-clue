6f9cf6350a8c6512e1f4def734898719f3d59976
Andrea Astorga <andrea.astorga95@gmail.com>
2020-05-06 21:09:49 +0200

    Iteration 3

--- src/clue.js
+++ src/clue.js
@@ -159,3 +159,9 @@ function pickMystery () {
 }
 
 // ITERATION 3
+
+function revealMystery (envelope) {
+
+    return `${envelope.suspect.firstName} ${envelope.suspect.lastName} killed Mr. Boddy using the ${envelope.weapon.name} in the ${envelope.room.name}!`
+
+}
