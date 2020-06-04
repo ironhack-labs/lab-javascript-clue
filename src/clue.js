54afd662851fa1db77d1f803da2365828b37537f
Diego Ortiz <diegortiz988@gmail.com>
2020-06-04 22:10:55 +0200

    done

--- src/clue.js
+++ src/clue.js
@@ -9,7 +9,7 @@ const suspectsArray = [{
     description: 'He has a lot of connections',
     image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
     color: 'green',
-} {
+}, {
     firstName: 'Doctor',
     lastName: 'Orchid',
     occupation: 'Scientist',
@@ -17,7 +17,7 @@ const suspectsArray = [{
     description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
     image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
     color: 'white',
-} {
+}, {
     firstName: 'Victor',
     lastName: 'Plum',
     occupation: 'Designer',
@@ -25,7 +25,7 @@ const suspectsArray = [{
     description: 'Billionaire video game designer',
     image: 'https: //66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
     color: 'purple',
-} {
+}, {
     firstName: 'Kasandra',
     lastName: 'Scarlet',
     occupation: 'Actor',
@@ -33,7 +33,7 @@ const suspectsArray = [{
     description: 'She is an A-list movie star with a dark past',
     image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
     color: 'red',
-} {
+}, {
     firstName: 'Eleanor',
     lastName: 'Peacock',
     occupation: 'Socialité',
@@ -41,7 +41,7 @@ const suspectsArray = [{
     description: 'She is from a wealthy family and uses her status and money to earn popularity',
     image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
     color: 'blue',
-} {
+}, {
     firstName: 'Jack',
     lastName: 'Mustard',
     occupation: 'Retired Football player',
@@ -54,23 +54,51 @@ const suspectsArray = [{
 
 // Rooms Collection
 const roomsArray = [{
-
-        name: 'Dining Room',
-        name: 'Conservatory',
-        name: 'Kitchen',
-        name: 'Study',
-        name: 'library',
-        name: 'Billiard Room',
-        name: 'Lounge',
-        name: 'Ballroom',
-        name: 'Hall',
-        name: 'Spa',
-        name: 'Living Room',
-        name: 'Observatory',
-        name: 'Theater',
-        name: 'Guest House',
+        name: 'Dining Room'
+    },
+    {
+        name: 'Conservatory'
+    },
+    {
+        name: 'Kitchen'
+    },
+    {
+        name: 'Study'
+    },
+    {
+        name: 'library'
+    },
+    {
+        name: 'Billiard Room'
+    },
+    {
+        name: 'Lounge'
+    },
+    {
+        name: 'Ballroom'
+    },
+    {
+        name: 'Hall'
+    },
+    {
+        name: 'Spa'
+    },
+    {
+        name: 'Living Room'
+    },
+    {
+        name: 'Observatory'
+    },
+    {
+        name: 'Theater'
+    },
+    {
+        name: 'Guest House'
+    },
+    {
         name: 'Patio'
     }
+
 
 ];
 
@@ -78,28 +106,28 @@ const roomsArray = [{
 const weaponsArray = [{
     name: 'rope',
     weight: '10',
-} {
+}, {
     name: 'knife',
     weight: '8',
-} {
+}, {
     name: 'candelstick',
     weight: '2',
-} {
+}, {
     name: 'dumbbell',
     weight: '30',
-} {
+}, {
     name: 'poison',
     weight: '2',
-} {
+}, {
     name: 'axe',
     weight: '15',
-} {
+}, {
     name: 'bat',
     weight: '13',
-} {
+}, {
     name: 'trophy',
     weight: '25',
-} {
+}, {
     name: 'pistol',
     weight: '20',
 }];
@@ -111,28 +139,36 @@ function selectRandom() {
 }
 
 function selectRandom([]) {
-
+    result null
 }
 
-function selectRandom(Array) {
-    index = parseInt(math.random()) * Array.length
-    card = Array[index]
-    return card
-}
 
-Array = []
-for (i = 1; 1 <= 30; i++) {
-    Array.push(i)
-}
 
-card = selectRandom(Array)
+// var suspects = suspectsArray[Math.floor(Math.random() * suspectsArray.length)];
+// var weapons = weaponsArray[Math.floor(Math.random() * weaponsArray.length)];
+// var rooms = roomsArray[Math.floor(Math.random() * roomsArray.length)];
 
-console.log(`The selected card is ${card}`)
+
+
+
+
+
+function selectRandom(randomArr) {
+    return randomArr[Math.floor(Math.random() * randomArr.length)];
+};
+
 
 // --- PICK MYSTERY --- ///
 
-function pickMystery() {
-    
+function pickMystery(){
+    return {
+        suspect: selectRandom(suspectsArray), 
+        weapon: selectRandom(weaponsArray), 
+        room: selectRandom(roomsArray)
+    }
 }
+
+console.log()
+
 
 // ITERATION 3
