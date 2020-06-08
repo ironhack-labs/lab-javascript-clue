9bec441230e536baf5c5acbe3cfd10fc160b983c
RFeldman84 <rachel.feldman84@gmail.com>
2020-06-07 22:01:16 -0400

    Changed final function pickMystery() from reviewing ways to make new objects; CLEANER READ: TOP- game code ONLY. BOTTOM- all code & steps used while in the making.

--- src/clue.js
+++ src/clue.js
@@ -1,11 +1,8 @@
-function print(...param){
-  console.log(...param)
-}
-///////////////////////
-// ITERATION 1
+// CLUE GAME CODE
+
+//Iteration 1: Create the cards
 
 //Suspects Collection
-//mrGreen drOrchid profPlum missScarlet mrsPeacock mrMustard
 const suspectsArray = [
   {
     firstName: "Jacob",
@@ -63,8 +60,6 @@ const suspectsArray = [
   }
 ];
 
-// print(suspectsArray)
-
 // Rooms Collection
 const roomsArray = [
   {name: "Dinning Room"},
@@ -84,8 +79,6 @@ const roomsArray = [
   {name: "Patio"}
   ];
   
-// print(roomsArray)
-
 // Weapons Collection
 const weaponsArray = [
   {name: "rope", weight: 10},
@@ -99,45 +92,192 @@ const weaponsArray = [
   {name: "pistol", weight: 20}
 ];
 
-// print(weaponsArray)
+// Iteration 2: Create the mystery
 
-
-// ITERATION 2
-
-// let arr = [1,2,6,9,"yo",true]
-// let arr2 = "word"
-//  let arr3 = []
-// function selectRandom(array){
-//   if(Array.isArray(array) === false || array.length === 0) return undefined; 
-//   let randomPick = array[Math.floor(Math.random() * array.length)];
-//   return randomPick
-
-// }
-
-//or//
-
+//Random selector
 function selectRandom(array){
   let randomPick = array[Math.floor(Math.random() * array.length)];
   return Array.isArray(array) === true && array.length !== 0 ?  randomPick : undefined;
 }
 
-// print(selectRandom(arr))
-// print(selectRandom(arr2))
-// print(selectRandom(arr3))
-
-
-
-
+//Create the mystery
 function pickMystery(){
-  let cardsObj = {suspect: selectRandom(suspectsArray), weapon: selectRandom(weaponsArray), room: selectRandom(roomsArray)};
+  const cardsObj = new Object()
+  cardsObj.suspect = selectRandom(suspectsArray)
+  cardsObj.room = selectRandom(roomsArray)
+  cardsObj.weapon = selectRandom(weaponsArray)
   return cardsObj;
 }
-//print(pickMystery())
 
 
+//Iteration 3: Reveal the mystery
 function revealMystery(envelope){
 return `${envelope.suspect.firstName} ${envelope.suspect.lastName} killed Mr. Boddy using the ${envelope.weapon.name} in the ${envelope.room.name}!`
 
 }
 
-//print(revealMystery(pickMystery()))
+console.log(revealMystery(pickMystery()))
+
+
+
+
+
+//////////////////// ALL CODE WORK /////////////////////
+// function print(...param){
+//   console.log(...param)
+// }
+// ///////////////////////
+// // ITERATION 1
+
+// //Suspects Collection
+// //mrGreen drOrchid profPlum missScarlet mrsPeacock mrMustard
+// const suspectsArray = [
+//   {
+//     firstName: "Jacob",
+//     lastName: "Green",
+//     occupation: "Entrepreneur",
+//     age: 45,
+//     description: "He has a lot of connections",
+//     image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+//     color: "green",  
+//     },
+//   {
+//     firstName: "Doctor",
+//     lastName: "Orchid",
+//     occupation: "Scientist",
+//     age: 26,
+//     description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+//     image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+//     color: "white"
+//   },
+//   {
+//     firstName: "Victor",
+//     lastName: "Plum",
+//     occupation: "Designer",
+//     age: 22,
+//     description: "Billionaire video game designer",
+//     image: "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg", 
+//     color: "purple"
+//   },
+//   {
+//     firstName: "Kasandra",
+//     lastName: "Scarlet",
+//     occupation: "Actor",
+//     age: 31,
+//     description: "She is an A-list movie star with a dark past",
+//     image: "https://www.radiotimes.com/uploads/images/Original/111967.jpg",
+//     color: "red"
+//   },
+//   {
+//     firstName: "Eleanor",
+//     lastName: "Peacock",
+//     occupation: "Socialité",
+//     age: 36,
+//     description: "She is from a wealthy family and uses her status and money to earn popularity",
+//     image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+//     color: "blue"
+//   },
+//   {
+//     firstName: "Jack",
+//     lastName: "Mustard",
+//     occupation: "Retired Football player",
+//     age: 62,
+//     description: "He is a former football player who tries to get by on his former glory",
+//     image: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg",
+//     color: "yellow"
+//   }
+// ];
+
+// // print(suspectsArray)
+
+// // Rooms Collection
+// const roomsArray = [
+//   {name: "Dinning Room"},
+//   {name: "Conservatory"},
+//   {name: "Kitchen"},
+//   {name: "Study"},
+//   {name: "Library"},
+//   {name: "Billiard Room"},
+//   {name: "Lounge"},
+//   {name: "Ballroom"},
+//   {name: "Hall"},
+//   {name: "Spa"},
+//   {name: "Living Room"},
+//   {name: "Observatory"},
+//   {name: "Theater"},
+//   {name: "Guest House"},
+//   {name: "Patio"}
+//   ];
+  
+// // print(roomsArray)
+
+// // Weapons Collection
+// const weaponsArray = [
+//   {name: "rope", weight: 10},
+//   {name: "knife", weight: 8},
+//   {name: "candlestick", weight: 2},
+//   {name: "dumbbell", weight: 30},
+//   {name: "poison", weight: 2},
+//   {name: "axe", weight: 15},
+//   {name: "bat", weight: 13},
+//   {name: "trophy", weight: 25},
+//   {name: "pistol", weight: 20}
+// ];
+
+// // print(weaponsArray)
+
+
+// // ITERATION 2
+
+// // let arr = [1,2,6,9,"yo",true]
+// // let arr2 = "word"
+// //  let arr3 = []
+// // function selectRandom(array){
+// //   if(Array.isArray(array) === false || array.length === 0) return undefined; 
+// //   let randomPick = array[Math.floor(Math.random() * array.length)];
+// //   return randomPick
+
+// // }
+
+// //or//
+
+// function selectRandom(array){
+//   let randomPick = array[Math.floor(Math.random() * array.length)];
+//   return Array.isArray(array) === true && array.length !== 0 ?  randomPick : undefined;
+// }
+
+// // print(selectRandom(arr))
+// // print(selectRandom(arr2))
+// // print(selectRandom(arr3))
+
+
+
+
+// // function pickMystery(){
+// //   let cardsObj = {suspect: selectRandom(suspectsArray), weapon: selectRandom(weaponsArray), room: selectRandom(roomsArray)};
+// //   return cardsObj;
+// // }
+// print(pickMystery())
+
+///OR//
+
+// function pickMystery(){
+//   const cardsObj = new Object()
+//   cardsObj.suspect = selectRandom(suspectsArray)
+//   cardsObj.room = selectRandom(roomsArray)
+//   cardsObj.weapon = selectRandom(weaponsArray)
+//   return cardsObj;
+// }
+// print(pickMystery())
+
+
+// // ITERATION 3
+
+// function revealMystery(envelope){
+// return `${envelope.suspect.firstName} ${envelope.suspect.lastName} killed Mr. Boddy using the ${envelope.weapon.name} in the ${envelope.room.name}!`
+
+// }
+
+// print(revealMystery(pickMystery()))
+
+
