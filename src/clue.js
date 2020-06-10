45d141a9ce213d15b1e257050e930f120b13d3cf
unknown <you@example.com>
2020-06-10 20:52:25 +0200

    ITERATION 2

--- src/clue.js
+++ src/clue.js
@@ -58,6 +58,21 @@ const suspectsArray = [
     color:        "yellow"},
 ];
 
+// Weapons Collection
+const weaponsArray = [
+
+
+    {name: "rope", weight: 10},
+    {name: "knife", weight: 8},
+    {name: "candlestick", weight: 2},
+    {name: "dumbbell" , weight: 30},
+    {name: "poison", weight: 2},
+    {name: "poison", weight: 2},
+    {name: "axe", weight: 15},
+    {name: "bat", weight: 13},
+    {name: "trophy", weight: 25},
+    {name: "pistol", weight: 20}
+    ];
     
 // Rooms Collection
 const roomsArray = [
@@ -77,34 +92,36 @@ const roomsArray = [
     "Theater",
     "Guest House",
     "Patio"   
-
-
 ];
-
-// Weapons Collection
-const weaponsArray = [
-
-
-{name: "rope", weight: 10},
-{name: "knife", weight: 8},
-{name: "candlestick", weight: 2},
-{name: "dumbbell" , weight: 30},
-{name: "poison", weight: 2},
-{name: "poison", weight: 2},
-{name: "axe", weight: 15},
-{name: "bat", weight: 13},
-{name: "trophy", weight: 25},
-{name: "pistol", weight: 20}
-];
-
-
-
-
-
-
-
-
 // ITERATION 2
+
+//Random selector
+function selectRandomSuspectsArray (arr) { 
+    const murderer = Math.random() * (max - min) + min;
+    return murderer;
+
+}
+
+function selectRandomWeaponsArray (arr) { 
+    let weapon = Math.random() * (max - min) + min;
+    return weapon;
+
+}
+
+function selectRandomRoomsArray (arr) { 
+    let room =  Math.random() * (max - min) + min;
+    return room;
+}
+
+//Create the mystery
+
+function pickMystery () {
+
+   
+
+}
+
+
 
 // ITERATION 3
 
