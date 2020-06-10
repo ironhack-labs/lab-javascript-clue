e65f144c2c37971aac421b4e5e03f861aaa911cf
Willem Prins <willemprins93@gmail.com>
2020-06-10 12:07:17 +0100

    solved final iteration

--- src/clue.js
+++ src/clue.js
@@ -111,3 +111,8 @@ function pickMystery(){
 }
 
 // ITERATION 3
+
+function revealMystery(object){
+    const str = object.suspect.firstName+' '+object.suspect.lastName+' killed Mr. Boddy using the '+object.weapon.name+' in the '+object.room.name+'!'
+    return str;
+}
