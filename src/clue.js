07c1709f764976ba628ef5f9c091a21119341c1d
anna dodo <anna.dorenskaya@gmail.com>
2020-07-07 22:06:22 +0200

    finished task + all tests are good

--- src/clue.js
+++ src/clue.js
@@ -5,57 +5,57 @@
 const suspectsArray = [
     {
         firstName: 'Jacob',
-    lastName: 'Green',
-    occupation: 'Entrepreneur',
-    age: 45,
-    description: 'He has a lot of connections',
-    image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
-    color: 'green'
+        lastName: 'Green',
+        occupation: 'Entrepreneur',
+        age: 45,
+        description: 'He has a lot of connections',
+        image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+        color: 'green'
     },
     {
-        firstName: Doctor
-    lastName: 'Orchid',
-    occupation: 'Scientist',
-    age: 26,
-    description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
-    image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
-    color: 'white'
+        firstName: 'Doctor',
+        lastName: 'Orchid',
+        occupation: 'Scientist',
+        age: 26,
+        description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
+        image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
+        color: 'white'
     },
     {
         firstName: 'Victor',
-    lastName: 'Plum',
-    occupation: 'Designer',
-    age: 22,
-    description: 'Billionaire video game designer',
-    image: 'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
-    color: 'purple'
+        lastName: 'Plum',
+        occupation: 'Designer',
+        age: 22,
+        description: 'Billionaire video game designer',
+        image: 'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
+        color: 'purple'
     },
     {
         firstName: 'Kasandra',
-    lastName: 'Scarlet',
-    occupation: 'Actor',
-    age: 31
-    description: 'She is an A-list movie star with a dark past',
-    image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
-    color: 'red'
+        lastName: 'Scarlet',
+        occupation: 'Actor',
+        age: 31,
+        description: 'She is an A-list movie star with a dark past',
+        image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
+        color: 'red'
     },
     {
         firstName: 'Eleanor',
-    lastName: 'Peacock',
-    occupation: 'Socialité',
-    age: 36
-    description: 'She is from a wealthy family and uses her status and money to earn popularity',
-    image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg'
-    color: 'blue'
+        lastName: 'Peacock',
+        occupation: 'Socialité',
+        age: 36,
+        description: 'She is from a wealthy family and uses her status and money to earn popularity',
+        image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
+        color: 'blue'
     },
     {
         firstName: 'Jack',
-    lastName: 'Mustard',
-    occupation: 'Retired Football player',
-    age: 62
-    description: 'He is a former football player who tries to get by on his former glory',
-    image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
-    color: 'yellow'
+        lastName: 'Mustard',
+        occupation: 'Retired Football player',
+        age: 62,
+        description: 'He is a former football player who tries to get by on his former glory',
+        image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
+        color: 'yellow'
     }
 ];
 
@@ -112,7 +112,7 @@ const roomsArray = [
 
 // Weapons Collection
 const weaponsArray = [
-    {
+   {
         name: 'rope',
         weight: 10
     },
@@ -151,8 +151,38 @@ const weaponsArray = [
 ];
 
 // ITERATION 2
+function selectRandom (array) {
+    if (array.length) {
+        if (array.length === 1) {
+            return array[0];
+        } else {
+            return array[Math.floor(Math.random() * array.length)];
+        }
+    } else {
+        return undefined
+    }
+
+}
 
 
+function pickMystery() {
+    let randomSuspect = selectRandom(suspectsArray);
+    let randomRoom = selectRandom(roomsArray);
+    let  randomWeapons = selectRandom(weaponsArray);
+    //let finalObject = {randomSuspect, randomRoom, randomWeapons};
+    const finalObject = { 
+        suspect: randomSuspect, 
+        weapon: randomWeapons, 
+        room: randomRoom} 
+    return finalObject;
+}
 
 
 // ITERATION 3
+function revealMystery(object) {
+    return `${object.suspect.firstName} ${object.suspect.lastName} killed Mr. Boddy using the ${object.weapon.name} in the ${object.room.name}!`
+
+}
+
+console.log(revealMystery(pickMystery()));
+
