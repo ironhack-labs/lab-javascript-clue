cff4a146f44fb620adee4977427a14f15186d80f
Gala Torres <galatorresrovelo@gmail.com>
2020-09-24 17:57:26 -0500

    First Draft

--- src/clue.js
+++ src/clue.js
@@ -1,14 +1,15 @@
 // ITERATION 1
-
+//COMING SOON
 // Suspects Collection
 const suspectsArray = [];
-
+//COMING SOON
 // Rooms Collection
 const roomsArray = [];
-
+//COMING SOON
 // Weapons Collection
 const weaponsArray = [];
-
+//COMING SOON
 // ITERATION 2
-
+//COMING SOON
 // ITERATION 3
+//COMING SOON
