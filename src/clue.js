a451fd9ac4a88913f501b8cb8807d7490e04d157
Pablo Fernandez <pablo_fz@hotmail.com>
2020-10-08 16:19:17 +0200

    Clue completado

--- src/clue.js
+++ src/clue.js
@@ -101,4 +101,7 @@ function selectRandom(array) {
 
 // ITERATION 3
 
-
+function revealMystery(mystery){
+    
+    return  `${mystery.suspect.firstName} ${mystery.suspect.lastName } killed Mr. Boddy using the ${mystery.weapon.name} in the ${mystery.room.name}!`;
+}
