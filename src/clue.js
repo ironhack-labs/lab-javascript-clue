abc1dd7512997095713b2132f8223efbd18c1720
Nina van den Braak <ninavandenbraak@gmail.com>
2020-10-14 12:11:51 +0200

    created suspects, rooms and weapons

--- src/clue.js
+++ src/clue.js
@@ -3,60 +3,110 @@
 // Suspects Collection
 const suspectsArray = [
 
-    {firstName: "Scarlett",
-    lastName: "Ruby",
-    occupation: "Singer",
-    age: 28,
-    description: "Scarlett Ruby is a gold digger",
-    image: "Scarlett looking in the mirror",
-    color: "red" },
+    mrGreen: 
+    {firstName: 'Jacob', 
+    lastName: 'Green',
+    occupation: 'Entrepreneur'
+    age: 45
+    description: 'He has a lot of connections'
+    image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+    color: 'green'},
 
-    {firstName: "Piet",
-    lastName: "van Geel",
-    occupation: "Painter",
-    age: 48,
-    description: "Piet van Geel is a painter who's very jealous",
-    image: "Piet painting something",
-    color: "yellow" },
+    drOrchid: 
+    {firstName: 'Doctor',
+    lastName: 'Orchid',
+    occupation: 'Scientist',
+    age: 26,
+    description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
+    image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
+    color: 'white' },
 
-    {firstName: "Susan",
-    lastName: "Waterson",
-    occupation: "Tech CEO",
-    age: 55,
-    description: "Susan wants to rule the world",
-    image: "Susan behind a computer",
-    color: "blue" },
+    profPlum: 
+    {firstName: 'Victor',
+    lastName: 'Plum',
+    occupation: 'Designer',
+    age: 22,
+    description: 'Billionaire video game designer',
+    image: 'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
+    color: 'purple'},
 
-    {firstName: "Henrick",
-    lastName: "Rooting",
-    occupation: "Ranger",
-    age: 17,
-    description: "Henrick would do anything to save the planet",
-    image: "Henrick walking through the forest",
-    color: "green" },
+    missScarlet: 
+    {firstName: 'Kasandra',
+    lastName: 'Scarlet',
+    occupation: 'Actor',
+    age: 31,
+    description: 'She is an A-list movie star with a dark past',
+    image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
+    color: 'red'},
 
-    {firstName: "Jennifer",
-    lastName: "Bigmouth",
-    occupation: "Chef",
-    age: 37,
-    description: "Don't interrupt Jennifer when's she's busy with her knives..",
-    image: "Jennifer cooking in the kitchen",
-    color: "purple" },
+    mrsPeacock:
+    {firstName: 'Eleanor',
+    lastName: 'Peacock',
+    occupation: 'Socialité',
+    age: 36,
+    description: 'She is from a wealthy family and uses her status and money to earn popularity',
+    image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
+    color: 'blue'},
 
-    {firstName: "Thom",
-    lastName: "Brightlight",
-    occupation: "Marketing consultant",
+    mrMustard:
+    {firstName: 'Jack',
+    lastName: 'Mustard',
+    occupation: 'Retired Football player',
     age: 62,
-    description: "Don't believe the bullshit Thom is selling for a living",
-    image: "Thom giving a lecture to a company board",
-    color: "orange" }
+    description: 'He is a former football player who tries to get by on his former glory',
+    image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
+    color: 'yellow'}
 ];
 
 // Rooms Collection
-const roomsArray = [];
+const roomsArray = [
+    {name: 'Dining Room'},
+{name: 'Conservatory'},
+{name: 'Kitchen'},
+{name: 'Study'},
+{name: 'Library'},
+{name: 'Billiard Room'},
+{name: 'Lounge'},
+{name: 'Ballroom'},
+{name: 'Hall'},
+{name: 'Spa'},
+{name: 'Living Room'},
+[name: 'Observatory'],
+{name: 'Theater'},
+{name: 'Guest House'},
+{name: 'Patio'}
+];
 
 // Weapons Collection
-const weaponsArray = [];
+const weaponsArray = [
+    {name: 'rope',
+    weight: 10
+    },
+    {name: 'knife',
+    weight: 8
+    },
+    {name: 'candlestick',
+    weight: 2
+    },
+    {name: 'dumbbell',
+    weight: 30
+    },
+    {name: 'poison',
+    weight: 2
+    },
+    {name: 'axe',
+    weight: 15
+    },
+    {name: 'bat',
+    weight: 13
+    },
+    {name: 'trophy',
+    weight: 25
+    },
+    {name: 'pistol',
+    weight: 20
+    }
+];
 
 // ITERATION 2
 
