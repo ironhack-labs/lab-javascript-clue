1790eb5f6e4f2d8449334664978ddff778f4c3ce
Nina van den Braak <ninavandenbraak@gmail.com>
2020-10-14 11:08:06 +0200

    created an array of suspects

--- src/clue.js
+++ src/clue.js
@@ -1,7 +1,56 @@
 // ITERATION 1
 
 // Suspects Collection
-const suspectsArray = [];
+const suspectsArray = [
+
+    {firstName: "Scarlett",
+    lastName: "Ruby",
+    occupation: "Singer",
+    age: 28,
+    description: "Scarlett Ruby is a gold digger",
+    image: "Scarlett looking in the mirror",
+    color: "red" },
+
+    {firstName: "Piet",
+    lastName: "van Geel",
+    occupation: "Painter",
+    age: 48,
+    description: "Piet van Geel is a painter who's very jealous",
+    image: "Piet painting something",
+    color: "yellow" },
+
+    {firstName: "Susan",
+    lastName: "Waterson",
+    occupation: "Tech CEO",
+    age: 55,
+    description: "Susan wants to rule the world",
+    image: "Susan behind a computer",
+    color: "blue" },
+
+    {firstName: "Henrick",
+    lastName: "Rooting",
+    occupation: "Ranger",
+    age: 17,
+    description: "Henrick would do anything to save the planet",
+    image: "Henrick walking through the forest",
+    color: "green" },
+
+    {firstName: "Jennifer",
+    lastName: "Bigmouth",
+    occupation: "Chef",
+    age: 37,
+    description: "Don't interrupt Jennifer when's she's busy with her knives..",
+    image: "Jennifer cooking in the kitchen",
+    color: "purple" },
+
+    {firstName: "Thom",
+    lastName: "Brightlight",
+    occupation: "Marketing consultant",
+    age: 62,
+    description: "Don't believe the bullshit Thom is selling for a living",
+    image: "Thom giving a lecture to a company board",
+    color: "orange" }
+];
 
 // Rooms Collection
 const roomsArray = [];
