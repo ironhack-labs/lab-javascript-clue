9db2c2de2a072c03694d0ae8bb19a5f2530ef91d
Anna Mosley <annahmosley@gmail.com>
2020-10-15 16:35:30 -0400

    complete all of iteration 1

--- src/clue.js
+++ src/clue.js
@@ -1,16 +1,86 @@
 // ITERATION 1
 
 // Suspects Collection
-const suspectsArray = [firstName: 'Jacob',
+const suspectsArray = [{firstName: 'Jacob',
 lastName: 'Green',
-occupation: 'Entrepreneur'];
+occupation: 'Entrepreneur'
+age: '60',
+color: 'Green'}
+{ firstName: 'Paul',
+lastName: 'Plum',
+occupation: 'Professor'
+age: '45'
+color: 'purple'}
+{firstName: 'Patricia',
+lastName: 'Peacock',
+occupation: 'Socialite',
+age: 70,
+color: 'blue'}
+{firstName: 'Wilma',
+lastName: 'White',
+occupation: 'Maid',
+age: '75',
+color: 'white'}
+{firstName: 'Murphy',
+lastName: 'Musturd',
+occupation: 'Colonel',
+age: '55',
+color: 'yellow'}
+firstName: 'Sara'
+lastName: 'Scarlett'
+occupation: 'Model'
+age: '30'
+color: 'red'];
 
 // Rooms Collection
 const roomsArray = ['bathroom', 'master bedroom', 'billards room', 'library', 'dining room','study', 'cellar','lounge', 'attic', 'home gym', 'kitchen', 'conservatory','theater room', 'basement','ball room'];
 
 // Weapons Collection
-const weaponsArray = ['Weapon 1: knife',''];
-
+const weaponsArray = ['rope','knife', 'candlestick', 'dumbell', 'poison', 'axe','bat' 'trophy', 'pistol'];
+let rope = {
+    name: 'rope',
+    weight: 10,
+  };
+  
+  let knife = {
+    name: 'knife',
+    weight: 8,
+  };
+  
+  let candlestick = {
+    name: 'candlestick',
+    weight: 2,
+  };
+  
+  let dumbbell = {
+    name: 'dumbbell',
+    weight: 30,
+  };
+  
+  let poison = {
+    name: 'poison',
+    weight: 2,
+  };
+  
+  let axe = {
+    name: 'axe',
+    weight: 15,
+  };
+  
+  let bat = {
+    name: 'bat',
+    weight: 13,
+  };
+  
+  let trophy = {
+    name: 'trophy',
+    weight: 25,
+  };
+  
+  let pistol = {
+    name: 'pistol',
+    weight: 20,
+  };
 // ITERATION 2
 
 // ITERATION 3
