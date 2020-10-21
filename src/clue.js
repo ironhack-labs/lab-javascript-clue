1df7de289122b73f59f679fc0ad817219833cdfd
ararita <munjizavesna@gmail.com>
2020-10-21 16:10:45 +0200

    Iteration 2

--- src/clue.js
+++ src/clue.js
@@ -10,7 +10,7 @@ const suspectsArray = [
     description: "He has a lot of connections",
     image:
       "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
-    color: "green"
+    color: "green",
   },
   {
     firstName: "Doctor",
@@ -19,7 +19,7 @@ const suspectsArray = [
     age: 26,
     description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
     image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
-    color: "white"
+    color: "white",
   },
   {
     firstName: "Victor",
@@ -29,7 +29,7 @@ const suspectsArray = [
     description: "Billionaire video game designer",
     image:
       "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg",
-    color: "purple"
+    color: "purple",
   },
 
   {
@@ -40,7 +40,7 @@ const suspectsArray = [
     description:
       "She is from a wealthy family and uses her status and money to earn popularity",
     image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
-    color: "blue"
+    color: "blue",
   },
   {
     firstName: "Jack",
@@ -51,8 +51,8 @@ const suspectsArray = [
       "He is a former football player who tries to get by on his former glory",
     image:
       "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg",
-    color: "yellow"
-  }
+    color: "yellow",
+  },
 ];
 
 // Rooms Collection
@@ -71,22 +71,51 @@ const roomsArray = [
   { name: "Observatory" },
   { name: "Theater" },
   { name: "Guest House" },
-  { name: "Patio" }
+  { name: "Patio" },
 ];
 
 // Weapons Collection
 const weaponsArray = [
   { name: "rope", weight: 10 },
-  { name: "rope", weight: 10 },
-  { name: "rope", weight: 10 },
-  { name: "rope", weight: 10 },
-  { name: "rope", weight: 10 },
-  { name: "rope", weight: 10 },
-  { name: "rope", weight: 10 },
-  { name: "rope", weight: 10 },
-  { name: "rope", weight: 10 }
+  { name: "rope", weight: 8 },
+  { name: "rope", weight: 2 },
+  { name: "rope", weight: 30 },
+  { name: "rope", weight: 2 },
+  { name: "rope", weight: 15 },
+  { name: "rope", weight: 13 },
+  { name: "rope", weight: 25 },
+  { name: "rope", weight: 20 },
 ];
 
 // ITERATION 2
+
+// Random selector
+
+// Declare a function named selectRandom to randomly select one element from a card stack. The function should expect an array as an argument, and should return a random element from the array.
+
+// Create the mystery
+
+// Declare a function named pickMystery that takes no arguments and returns an object with three properties: suspect, weapon and room, each holding as a value a card of that specific type. You can get a random card of each type by calling selectRandom on each card stack.
+function selectRandom(array) {
+  if (array.length === 0) return;
+
+  let random = array[Math.floor(Math.random() * array.length)];
+  return random;
+}
+
+function pickMystery() {
+  let randomSuspect = selectRandom(suspectsArray);
+  let randomWeapon = selectRandom(weaponsArray);
+  let randomRoom = selectRandom(roomsArray);
+
+  let mysteryObj = {
+    suspect: randomSuspect,
+    weapon: randomWeapon,
+    room: randomRoom,
+  };
+  console.log(mysteryObj);
+
+  return mysteryObj;
+}
 
 // ITERATION 3
