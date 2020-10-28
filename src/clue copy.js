f54070f1b4138ce16194561fd0bb9f347d724288
Volney Faustini <volney.faustini@gmail.com>
2020-10-27 23:01:51 -0300

    done

---
+++ src/clue copy.js
@@ -0,0 +1,14 @@
+// ITERATION 1
+
+// Suspects Collection
+const suspectsArray = [];
+
+// Rooms Collection
+const roomsArray = [];
+
+// Weapons Collection
+const weaponsArray = [];
+
+// ITERATION 2
+
+// ITERATION 3
