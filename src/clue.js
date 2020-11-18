d221a86c1ccefb85702644acf7a9e636ee47b955
Discordante <diegocerratos@gmail.com>
2020-11-18 20:18:08 +0100

    weaponsArray added

--- src/clue.js
+++ src/clue.js
@@ -1,6 +1,5 @@
 // ITERATION 1
-//MODIFICACIÓN COMENT INÉS
-//modification 2 DIego
+
 // Suspects Collection
 const suspectsArray = [];
 
@@ -8,8 +7,15 @@ const suspectsArray = [];
 const roomsArray = [];
 
 // Weapons Collection
-const weaponsArray = [];
+const weaponsArray = [{name: 'rope', weight: 10},
+    {name: 'knife', weight: 8},{name: 'candlestick', weight: 2},
+    {name: 'dumbbell', weight: 30},{name: 'poison', weight: 2},
+    {name: 'axe', weight: 15},{name: 'bat', weight: 13},
+    {name: 'trophy', weight: 25},{name: 'pistol', weight: 20}
+    ]
 
 // ITERATION 2
 
 // ITERATION 3
+
+console.log(weaponsArray)
