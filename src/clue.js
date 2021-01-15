ff62363506009eaf70355c70c2da533fc093bd5f
Abel Andrés <abelandres.gtc@gmail.com>
2021-01-15 02:04:37 +0100

    not done

--- src/clue.js
+++ src/clue.js
@@ -1,8 +1,18 @@
 // ITERATION 1
 
 // Suspects Collection
-const suspectsArray = [];
-
+const suspectsArray = [
+    {
+        mrGreen
+        firstName: Jacob 
+        lastName: Green 
+        occupation: Entrepreneur 
+        age: 45 
+        description: He has a lot of connections 
+        image: https: //pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg <br>
+        color: green
+    }
+]
 // Rooms Collection
 const roomsArray = [];
 
