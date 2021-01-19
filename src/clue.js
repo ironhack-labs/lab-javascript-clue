5eb73a7647da22404f36050f01eafc69d3ca09ea
Aleksandra Porada <aleksandra.porada92@gmail.com>
2021-01-20 00:22:24 +0100

    Finish first iteration

--- src/clue.js
+++ src/clue.js
@@ -1,14 +1,33 @@
-// ITERATION 1
 
 // Suspects Collection
-const suspectsArray = [];
+const suspectsArray = ["Kate", "Jim"];
 
 // Rooms Collection
-const roomsArray = [];
+const roomsArray = ["bathroom", "kitchen"];
 
 // Weapons Collection
-const weaponsArray = [];
+const weaponsArray = ["TV cable", "pills"];
+
+
+// ITERATION 1
+function selectRandom(arr) {
+    if (arr.length === 0) {
+        return undefined;
+    } return arr[Math.floor(Math.random() * arr.length)];
+}
 
 // ITERATION 2
+
+function pickMystery(suspectsArray, roomsArray, weaponsArray) {
+    let crime = {
+        suspect:"?",
+        room:"?",
+        weapon:"?"
+    };
+    crime["suspect"] = suspectsArray[Math.floor(Math.random() * suspectsArray.length)];
+    crime["room"] = roomsArray[Math.floor(Math.random() * roomsArray.length)];
+    crime["weapon"] = weaponsArray[Math.floor(Math.random() * weaponsArray.length)];
+    return crime;
+};
 
 // ITERATION 3
