2ddc25ccf3efc4c937fafb239fab94e7d10352df
Jeroen van Bart <jeroenvanbart83@gmail.com>
2021-03-23 23:58:34 +0100

    changed 1 thing

---
+++ src/test.js
@@ -0,0 +1,19 @@
+const roomsArray = [
+    "Dining Room", 
+    "Conservatory", 
+    "Kitchen", 
+    "Study", 
+    "Library", 
+    "Billiard Room", 
+    "Lounge", 
+    "Ballroom", 
+    "Hall", 
+    "Spa", 
+    "Living Room", 
+    "Observatory", 
+    "Theater", 
+    "Guest House", 
+    "Patio"
+];
+
+    console.log(roomsArray);
