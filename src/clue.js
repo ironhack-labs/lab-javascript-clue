553bbbe177bbdd283cfb825030cec3958e396915
Silinde87 <paurodriguezmolina@gmail.com>
2021-03-24 11:58:46 +0100

    Add Logic global structure

--- src/clue.js
+++ src/clue.js
@@ -1,14 +1,57 @@
 // ITERATION 1
 
 // Suspects Collection
-const suspectsArray = [];
+const suspectsArray = [
+	{
+		firstName: "Jacob",
+		lastName: "Green",
+		occupation: "Entrepreneur",
+		age: 45,
+		description: "He has a lot of connections",
+		image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+		color: "green",
+	},
+];
 
 // Rooms Collection
-const roomsArray = [];
+const roomsArray = [
+	{
+		name: "Dinning Room",
+	},
+];
 
 // Weapons Collection
-const weaponsArray = [];
+const weaponsArray = [
+	{
+		name: "rope",
+		weight: 10,
+	},
+];
 
 // ITERATION 2
+let selectRandom = (array) => {
+	let randomOutput = {};
+	return randomOutput;
+};
 
-// ITERATION 3
+//done
+let pickMystery = () => {
+	let suspectSelected = selectRandom(suspectsArray);
+	let roomSelected = selectRandom(roomsArray);
+	let weaponSelected = selectRandom(weaponsArray);
+
+	return {
+		suspect: suspectSelected,
+		room: roomSelected,
+		weapon: weaponSelected,
+	};
+};
+
+// ITERATION 3 - done
+let revealMystery = (murder) => {
+	let output = `${murder.suspect.firstName} ${murder.suspect.lastName} killed Mr. Boddy using the
+    ${murder.weapon.name} in the ${murder.room.name}!`;
+	return output;
+};
+
+//revealMystery(pickMystery());
