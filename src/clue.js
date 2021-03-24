caa3e7e424fe9ce653ac0d9b0f673c8250e69a6f
Alex Escofet <alex.escofet96@gmail.com>
2021-03-24 14:12:02 +0100

    Iteration 3 done

--- src/clue.js
+++ src/clue.js
@@ -111,3 +111,6 @@ function pickMystery(){
 }
 
 // ITERATION 3
+function revealMystery(object){
+    return(`${object.suspect.firstName} ${object.suspect.lastName} killed Mr. Boddy using the ${object.weapon.name} in the ${object.room.name}!`)
+}
