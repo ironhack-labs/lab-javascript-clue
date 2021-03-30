ca72a6a6c9e88a7ecac7211243daa803e39b66f7
amelol <75670435+amelol@users.noreply.github.com>
2021-03-31 00:02:16 +0200

    added subfunctions iteratio n 2

--- src/clue.js
+++ src/clue.js
@@ -10,7 +10,7 @@ const suspects =
   occupation: 'Entrepreneur',
   age: 45,
   description: 'He has a lot of connections',
-  image: URL('https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg') ,
+  image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
   color: 'green'
   },
 
@@ -21,7 +21,7 @@ const suspects =
   occupation: 'Scientist',
   age: 26,
   description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
-  image: URL('http://www.radiotimes.com/uploads/images/Original/111967.jpg'),
+  image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
   color: 'white' 
   },
 
@@ -32,7 +32,7 @@ const suspects =
   occupation: 'Designer',
   age: 22,
   description: 'Billionaire video game designer',
-  image: URL('https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg'),
+  image: 'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
   color: 'purple' 
   },
 
@@ -43,7 +43,7 @@ const suspects =
   occupation: 'Actor',
   age: 31,
   description: 'She is an A-list movie star with a dark past',
-  image: URL('https://www.radiotimes.com/uploads/images/Original/111967.jpg'),
+  image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
   color: 'red'  
   },
 
@@ -54,7 +54,7 @@ const suspects =
   occupation: 'Socialité',
   age: 36,
   description: 'She is from a wealthy family and uses her status and money to earn popularity',
-  image: URL('https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg'),
+  image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
   color: 'blue'  
   },
 
@@ -65,7 +65,7 @@ const suspects =
   occupation: 'Retired Football player',
   age: 62,
   description: 'He is a former football player who tries to get by on his former glory',
-  image: URL('https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg'),
+  image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
   color: 'yellow' 
   }
 
@@ -143,17 +143,38 @@ const weapons =
 
 // ITERATION 2
 
- function selectRandom([suspects, rooms, weapons]) {
-   let mystery = []
-       if (!mystery.length) {
+ function selectRandom(suspects, rooms, weapons) {
+
+ let mystery = `${randomSuspect(suspects)}, ${randomRoom(rooms)}, ${randomWeapon(weapons)}`
+
+  function randomSuspect(suspects) {
+    suspect = suspects[Math.floor(Math.random() * suspects.length)]
+    if (suspects===[]){
       return undefined
     }
-  
-      if (mystery.length === 1) {
-      return 
-      }
-    }
-    return mystery
+    return suspect
+  }
 
- }
+  function randomRoom(rooms) {
+    room = rooms[Math.floor(Math.random() * rooms.length)]
+    if (rooms===[]){
+      return undefined
+    }
+    return room
+  }
+
+    function randomWeapon(weapons) {
+    weapon = weapons[Math.floor(Math.random() * weapons.length)]
+    if (weapons===[]){
+      return undefined
+    }    
+    return weapon
+  }
+
+
+    return mystery
+    }
+    
+console.log(selectRandom())
+ 
 // ITERATION 3
