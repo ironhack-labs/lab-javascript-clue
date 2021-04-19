7f05eb91094350ba54f0d2c4d7fc934c611c7b21
Andrei <andrei_1002@hotmail.com>
2021-04-19 21:22:05 +0200

    terminada iteración 1

--- src/clue.js
+++ src/clue.js
@@ -3,31 +3,31 @@
 // Suspects Collection
 const suspectsArray = [
     {
-        firstname: Kasandra,
-        lastName: Scarlet,
-        occupation: Actor,
+        firstname: "Kasandra",
+        lastName: "Scarlet",
+        occupation: "Actor",
         age: 31,
         description: "She is an A-list movie star with a dark past",
         image: "https://www.radiotimes.com/uploads/images/Original/111967.jpg" ,
-        color: red
+        color: "red"
     },
     {
-        firstName: Eleanor,
-        lastName: Peacock,
-        occupation: Socialité,
+        firstName:"Eleanor",
+        lastName: "Peacock",
+        occupation: "Socialité",
         age: 36,
         description: "She is from a wealthy family and uses her status and money to earn popularity",
         image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
-        color: blue
+        color: "blue"
     },
     {
-        firstName: Jack,
-        lastName: Mustard,
-        occupation: Retired Football player,
+        firstName: "Jack",
+        lastName: "Mustard",
+        occupation: "Retired Football player",
         age: 62,
-        description: He is a former football player who tries to get by on his former glory,
+        description: "He is a former football player who tries to get by on his former glory",
         image: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg",
-        color: yellow
+        color: "yellow"
     }
 ];
 
@@ -77,5 +77,13 @@ const weaponsArray = [
 
 
 // ITERATION 2
+/*var randomItem = myArray[Math.floor(Math.random()*myArray.length)].semillas;*/
+function selectRandom(array){
+    console.log(array[Math.floor(Math.random()*array.length)])
+}
+selectRandom(suspectsArray);
+selecRandom(weaponArray);
+selectRandom(roomsArray)
+
 
 // ITERATION 3
