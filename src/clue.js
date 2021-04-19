9938b01cf4c1de15ecbf0cd0a04f793e62a690f5
Andrei <andrei_1002@hotmail.com>
2021-04-19 20:35:36 +0200

    añadido primer array

--- src/clue.js
+++ src/clue.js
@@ -1,7 +1,35 @@
 // ITERATION 1
 
 // Suspects Collection
-const suspectsArray = [];
+const suspectsArray = [
+    {
+        firstname: Kasandra,
+        lastName: Scarlet,
+        occupation: Actor,
+        age: 31,
+        description: "She is an A-list movie star with a dark past",
+        image: "https://www.radiotimes.com/uploads/images/Original/111967.jpg" ,
+        color: red
+    },
+    {
+        firstName: Eleanor,
+        lastName: Peacock,
+        occupation: Socialité,
+        age: 36,
+        description: "She is from a wealthy family and uses her status and money to earn popularity",
+        image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+        color: blue
+    },
+    {
+        firstName: Jack,
+        lastName: Mustard,
+        occupation: Retired Football player,
+        age: 62,
+        description: He is a former football player who tries to get by on his former glory,
+        image: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg",
+        color: yellow
+    }
+];
 
 // Rooms Collection
 const roomsArray = [];
