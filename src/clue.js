adf7c85d2e3d7bb8a808237385813e640cad5d67
“Jaime <jsevillano23@gmail.com>
2021-04-20 18:15:13 -0500

    done

--- src/clue.js
+++ src/clue.js
@@ -1,4 +1,28 @@
+//JAIME SEVILLANO
 // ITERATION 1
+let suspectsArray = [{
+    firstName: "Jacob",
+    lastName: "Green",
+    occupation: "Entrepreneur",
+    age: 45,
+    description: "He has a lot of connections",    
+    image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+    color: "green"
+}]
+let weaponsArray = [{
+    rope: 10,
+    knife: 8,
+    candlestick: 2,
+    dumbbell: 30,
+    poison: 2,
+    axe: 15,
+    bat: 13,
+    trophy: 25,
+    pistol: 20
+}]
+let roomsArray = [{
+    name: "Dining Room"
+}]
 
 // Suspects Collection
 const suspectsArray = [];
