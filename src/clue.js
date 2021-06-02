12c1f6acbc315f897d663dce99c0abc835a74b6b
Irina <ifalko@protonmail.com>
2021-06-02 14:54:23 +0200

    First commit

--- src/clue.js
+++ src/clue.js
@@ -2,7 +2,18 @@
 
 // Suspects Array
 
-const suspectsArray = [];
+const suspectsArray = [
+  {
+    firstName: 'Jacob',
+    lastName: 'Green',
+    occupation: 'Entrepreneur',
+    age: 45,
+    description: 'He has a lot of connections',
+    image:
+      'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+    color: 'green'
+  }
+];
 
 // Rooms Array
 
@@ -12,19 +23,15 @@ const roomsArray = [];
 
 const weaponsArray = [];
 
-
 // ITERATION 2
 
 function selectRandom() {}
 
 function pickMystery() {}
 
-
 // ITERATION 3
 
 function revealMystery() {}
-
-
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
