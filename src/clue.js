3f2afff63b20527ef63c4f34d2ad119a02cf5ed1
Luca TC <lucatuveri@outlook.it>
2021-06-06 13:21:38 +0200

    random selector done

--- src/clue.js
+++ src/clue.js
@@ -1,13 +1,6 @@
 // ITERATION 1
 
 // Suspects Array
-// Miss Scarlett().She is represented by a  token.
-// Rev Green("Mr. Green" in North America).He is represented by a green token.
-// Colonel Mustard.He is represented by a yellow token.
-// Professor Plum.He is represented by a purple token.
-//   Mrs.Peacock.She is represented by a blue token.
-//     Mrs.White.She is represented by a white token.
-// should have 6 suspects
 const suspectsArray = [
   {
     firstName: 'Miss',
@@ -19,124 +12,107 @@ const suspectsArray = [
     color: 'red'
 
   }, {
-    firstName: 'Miss',
-    lastName: 'Scarlett',
-    occupation: 'Entrepreneur',
-    age: 22,
+    firstName: 'Colonel',
+    lastName: 'Mustard',
+    occupation: 'Colonel',
+    age: 68,
     description: '',
     image: '',
-    color: ''
+    color: 'yellow'
 
   }, {
-    firstName: 'Miss',
-    lastName: 'Scarlett',
-    occupation: 'Entrepreneur',
-    age: 22,
-    description: '',
+    firstName: 'Rev',
+    lastName: 'Green',
+    occupation: 'Reverendo',
+    age: 76,
+    description: 'Mr. Green" in North America',
     image: '',
-    color: ''
+    color: 'green'
 
   }, {
-    firstName: 'Miss',
-    lastName: 'Scarlett',
-    occupation: 'Entrepreneur',
-    age: 22,
+    firstName: 'Professor',
+    lastName: 'Plum',
+    occupation: 'Professor',
+    age: 45,
     description: '',
     image: '',
-    color: ''
+    color: 'purple'
 
   }, {
-    firstName: 'Miss',
-    lastName: 'Scarlett',
-    occupation: 'Entrepreneur',
-    age: 22,
+    firstName: 'Mrs.',
+    lastName: 'Peacock',
+    occupation: 'Dancer',
+    age: 45,
     description: '',
     image: '',
-    color: ''
+    color: 'blue'
 
   }, {
-    firstName: 'Miss',
-    lastName: 'Scarlett',
+    firstName: 'Mrs',
+    lastName: 'White',
     occupation: 'Entrepreneur',
     age: 22,
     description: '',
     image: '',
-    color: ''
+    color: 'white'
 
   },
 ];
 
 // Rooms Array
-//	 †	   		Ballroom		   	‡ 	
-// Kitchen	Conservatory
-// Dining Room		Cellar
-//   (with
-// envelope) Billiard Room
-// Library
-// Hall
-// Lounge	Study
-// ‡	†
-// should have 15 rooms
-// should have a string in property "name"
+	
+
 const roomsArray = [
   {
-    name:'Ballrom',
+    name:'Ballrom1',
     
   }, {
-    name: 'Ballrom',
+    name: 'Kitchen',
 
   }, {
-    name: 'Ballrom',
+    name: 'Conservatory',
 
   }, {
-    name: 'Ballrom',
+    name: 'Dining',
 
   }, {
-    name: 'Ballrom',
+    name: 'Cellar',
 
   }, {
-    name: 'Ballrom',
+    name: 'Billiard Room',
 
   }, {
-    name: 'Ballrom',
+    name: 'Library',
 
   }, {
-    name: 'Ballrom',
+    name: 'Hall',
 
   }, {
-    name: 'Ballrom',
+    name: 'Lounge',
 
   }, {
-    name: 'Ballrom',
+    name: 'Study',
 
   }, {
-    name: 'Ballrom',
+    name: 'Garden',
 
   }, {
-    name: 'Ballrom',
+    name: 'Bathroom',
 
   }, {
-    name: 'Ballrom',
+    name: 'Theatre',
 
   }, {
-    name: 'Ballrom',
+    name: 'Ballrom2',
 
   }, {
-    name: 'Ballrom',
+    name: 'Ballrom3',
 
   },
 ];
 
 // Weapons Array
-// Candlestick
-// (Knife in some North American editions)
-// (called lead piping in earlier UK editions; the early tokens were made out of actual lead and therefore posed a risk of lead )
-// (first depicted in the UK as a Dreyse M1907 semi - automatic pistol, [14] and in North America as a .)
-// 
-// Wrench(depicted as a monkey wrench in North American editions; and an open - ended spanner in some traditional UK versions)
-// should have a string in property "name"
-// should have a string in property "name"
-// should have a number in property "weight"
+
 const weaponsArray = [
   {
     name: 'Candlestick',
@@ -168,18 +144,47 @@ weight:69,
   },
 ];
 
-
+//console.log(roomsArray)
+ console.log(suspectsArray)
+//console.log(weaponsArray)
 // ITERATION 2
-
-function selectRandom() { }
-
+// Declare a function named `selectRandom` 
+//to randomly select one element from a card stack.
+//The function should expect an`array` as an argument, 
+//and should return a random element from the array.
+// should return undefined if the array is empty
+// should return the element of a single value array
+// should return an element of the array
+// should return a random element of the array
+function selectRandom() {
+  const cardNumber = Math.floor(Math.random()*6)
+  // suspectsArray.forEach(element => {
+     console.log(`'I'm ${suspectsArray[cardNumber].firstName} ${suspectsArray[cardNumber].lastName}'`)
+  // });
+ };
+selectRandom();
+// should return an object
+// should return an object with 3 properties: `suspect`, `weapon`, `room`
+// should return a suspect in the suspect property of the object
+// should return a weapon in the weapon property of the object
+// should return a room in the room property of the object
+// Declare a function named `pickMystery` that takes no arguments
+// and returns an object with three properties: _suspect_, _weapon_ and _room_, 
+//each holding as a value a card of that specific type.
+//You can get a random card of each type by calling `selectRandom` on each card stack.
 function pickMystery() { }
-
+pickMystery()
 
 // ITERATION 3
+// should return a string
+// should return "<FIRST NAME> <LAST NAME> killed Mr. Boddy using the <WEAPON> in the <PLACE>!"
 
+//Declare a function named `revealMystery`
+// that receives an _envelope_`object`(with the shape of the object returned by`pickMystery`) as the single argument,
+// and returns a reveal message in the following format:
+//**\<FIRST NAME\> \<LAST NAME\> killed Mr. Boddy using the \<WEAPON\> in the \<ROOM\>!**
 function revealMystery() { }
-
+revealMystery()
 
 
 // The following is required to make unit tests work.
