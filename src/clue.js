345bbae4ad0a920667479e7f71297e7f9f6380d9
Stephen Gallagher <sgallagher92@gmail.com>
2021-08-11 15:27:43 +0200

    done

--- src/clue.js
+++ src/clue.js
@@ -2,29 +2,182 @@
 
 // Suspects Array
 
-const suspectsArray = [];
-
-// Rooms Array
-
-const roomsArray = [];
+const suspectsArray = [
+  {
+    firstName: 'Jacob',
+    lastName: 'Green',
+    occupation: 'Entrepreneur',
+    age: 45,
+    description: 'He has a lot of connections',
+    image: (
+      <img src="https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg"></img>
+    ),
+    color: 'green'
+  },
+  {
+    firstName: 'Doctor',
+    lastName: 'Orchid',
+    occupation: 'Scientist',
+    age: 26,
+    description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
+    image: (
+      <img src="http://www.radiotimes.com/uploads/images/Original/111967.jpg"></img>
+    ),
+    color: 'white'
+  },
+  {
+    firstName: 'Victor',
+    lastName: 'Plum',
+    occupation: 'Designer',
+    age: 22,
+    description: 'Billionaire video game designer',
+    image: (
+      <img src="https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg"></img>
+    ),
+    color: 'purple'
+  },
+  {
+    firstName: 'Eleanor',
+    lastName: 'Peacock',
+    occupation: 'Socialité',
+    age: 36,
+    description:
+      'She is from a wealthy family and uses her status and money to earn popularity',
+    image: (
+      <img src="https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg"></img>
+    ),
+    color: 'blue'
+  },
+  {
+    firstName: 'Jack',
+    lastName: 'Mustard',
+    occupation: 'Retired Football player',
+    age: 62,
+    description:
+      'He is a former football player who tries to get by on his former glory',
+    image: (
+      <img src="https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg"></img>
+    ),
+    color: 'yellow'
+  }
+];
 
 // Weapons Array
 
-const weaponsArray = [];
+const weaponsArray = [
+  {
+    name: 'rope',
+    weight: 10
+  },
+  {
+    name: 'knife',
+    weight: 8
+  },
+  {
+    name: 'candlestick',
+    weight: 2
+  },
+  {
+    name: 'dumbbell',
+    weight: 30
+  },
+  {
+    name: 'poison',
+    weight: 2
+  },
+  {
+    name: 'axe',
+    weight: 15
+  },
+  {
+    name: 'bat',
+    weight: 13
+  },
+  {
+    name: 'trophy',
+    weight: 25
+  },
+  {
+    name: 'pistol',
+    weight: 20
+  }
+];
 
+// Rooms Array
+
+const roomsArray = [
+  {
+    name: 'Dining Room'
+  },
+  {
+    name: 'Conservatory'
+  },
+  {
+    name: 'Kitchen'
+  },
+  {
+    name: 'Study'
+  },
+  {
+    name: 'Library'
+  },
+  {
+    name: 'Billiard Room'
+  },
+  {
+    name: 'Lounge'
+  },
+  {
+    name: 'Ballroom'
+  },
+  {
+    name: 'Hall'
+  },
+  {
+    name: 'Spa'
+  },
+  {
+    name: 'Living Room'
+  },
+  {
+    name: 'Observatory'
+  },
+  {
+    name: 'Theater'
+  },
+  {
+    name: 'Guest House'
+  },
+  {
+    name: 'Patio'
+  }
+];
 
 // ITERATION 2
 
-function selectRandom() {}
+function selectRandom(array) {
+  const randNum = Math.floor(Math.random() * array.length);
+  const randElement = array[randNum];
+  return randElement;
+}
 
-function pickMystery() {}
+function pickMystery() {
+  const randomSuspect = selectRandom(suspectsArray);
+  const randomRoom = selectRandom(roomsArray);
+  const randomWeapon = selectRandom(weaponsArray);
 
+  const envelope = {};
+
+  envelope.suspect = randomSuspect.firstName + ' ' + randomSuspect.lastName;
+  envelope.room = randomRoom.name;
+  envelope.weapon = randomWeapon.name;
+}
 
 // ITERATION 3
 
-function revealMystery() {}
-
-
+function revealMystery(object) {
+  return `${envelope.suspect} killed Mr. Boddy using the ${envelope.weapon} in the ${envelope.room}`;
+}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
