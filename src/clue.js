8782c5a89ed680bc2eec445cef0fd4f71437e05f
Bárbara Quecedo <quecedobarbara@gmail.com>
2021-10-18 22:24:37 +0200

    done

--- src/clue.js
+++ src/clue.js
@@ -1,40 +1,178 @@
-// ITERATION 1
-
 // Suspects Array
 
-const suspectsArray = [];
+const suspectsArray = [
+    
+  {
+  firstName: "Jacob",
+  lastName: "Green",
+  occupation: "Entrepreneur",
+  age: 45,
+  description: "He has a lot of connections",
+  image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpg",
+  color: "green"
+  },
+  
+  {
+  firstName: "Doctor",
+  lastName: "Orchid",
+  occupation: "Scientist",
+  age: 26,
+  description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+  image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+  color: "white"
+  },
+  
+  
+  {
+  firstName: "Victor",
+  lastName: "Plum",
+  occupation: "Designer",
+  age: 22,
+  description: "Billionaire video game designer",
+  image: "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg",
+  color: "purple"
+  },
+  
+  {
+  firstName: "Kasandra",
+  lastName: "Scarlet",
+  occupation: "Actor",
+  age: 31,
+  description: "She is an A-list movie star with a dark past",
+  image: "https://www.radiotimes.com/uploads/images/Original/111967.jpg",
+  color: "red"
+  },
+
+  {
+  firstName: "Eleanor",
+  lastName: "Peacock",
+  occupation: "Socialité",
+  age: 36,
+  description: "She is from a wealthy family and uses her status and money to earn popularity",
+  image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+  color: "blue"
+  },
+  
+  
+  {
+  firstName: "Jack",
+  lastName: "Mustard",
+  occupation: "Retired Football player",
+  age: 62,
+  description: "He is a former football player who tries to get by on his former glory",
+  image: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg",
+  color: "yellow"
+}
+];
 
 // Rooms Array
 
-const roomsArray = [];
+const roomsArray = [ 
+
+{
+name: "Dining Room"
+},
+
+{
+name: "Conservatory"
+},
+
+{
+name: "Kitchen"
+},
+
+{
+name: "Study"
+},
+
+{
+name: "Library"
+},
+
+{
+name: "Billiard Room"
+},
+
+{
+name: "Lounge"
+},
+
+{
+name: "Ballroom"
+},
+
+{
+name: "Hall"
+},
+
+{
+name: "Spa"
+},
+
+{
+name: "Living Room"
+},
+
+{
+name: "Observatory"
+},
+
+{
+name: "Theater"
+},
+
+{
+name: "Guest House"
+},
+
+{
+name: "Patio"
+}
+];
 
 // Weapons Array
 
-const weaponsArray = [];
 
+const weaponsArray = [ 
+{name: "rope" , weight: 10},
+{name: "knife" , weight: 8},
+{name: "candlestick" , weight: 2},
+{name: "poison" , weight: 2},
+{name: "axe" , weight: 15},
+{name: "bat", weight: 13},
+{name: "trophy" , weight: 25},
+{name: "pistol" , weight: 20},
+{name: "dumbbell" , weight: 30}
+];
 
 // ITERATION 2
 
-function selectRandom() {}
+/// Declare a function named selectRandom to randomly select one element from a card stack. The function should expect an array as an argument, and should return a random element from the array.
 
-function pickMystery() {}
-
-
-// ITERATION 3
-
-function revealMystery() {}
-
-
-
-// The following is required to make unit tests work.
-/* Environment setup. Do not modify the below code. */
-if (typeof module !== 'undefined') {
-  module.exports = {
-    suspectsArray,
-    roomsArray,
-    weaponsArray,
-    pickMystery,
-    revealMystery,
-    selectRandom
-  };
+function selectRandom(arr) {
+const randomIndex = Math.floor(Math.random() * arr.length);
+return arr[randomIndex]
 }
+
+// Declare a function named pickMystery that takes no arguments and returns an object with three properties: suspect, weapon and room, each holding as a value a card of that specific type. You can get a random card of each type by calling selectRandom on each card stack.
+
+function pickMystery() {
+return {
+  suspect: selectRandom(suspectsArray),
+  weapon: selectRandom(weaponsArray),
+  room: selectRandom(roomsArray)
+}
+}
+
+
+// At last, we are ready to reveal the mystery.
+
+// Declare a function named revealMystery that receives an envelope object (with the shape of the object returned by pickMystery) as the single argument, and returns a reveal message in the following format:
+
+// <FIRST NAME> <LAST NAME> killed Mr. Boddy using the <WEAPON> in the <ROOM>!
+
+function revealMystery(envelope) {
+return `${envelope.suspect.firstName} ${envelope.suspect.lastName} killed Mr. Boddy using the ${envelope.weapon.name} in the ${envelope.room.name}!`
+
+}
+
