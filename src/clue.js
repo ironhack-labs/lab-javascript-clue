779139467b7945d451b2251310375e2f1bb98458
Fernando Cases <nandocasesgarcia@gmail.com>
2021-10-21 16:50:37 +0200

    Iteration1

--- src/clue.js
+++ src/clue.js
@@ -3,22 +3,24 @@
 // Suspects Array
 
 const suspectsArray = [
-   {
+  {
     firstName: 'Jacob',
     lastName: 'Green',
-    occupation:'Entrepreneur',
+    occupation: 'Entrepreneur',
     age: 45,
-    description:'He has a lot of connections',
-    image:'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
-    color: 'green',},
-   {
+    description: 'He has a lot of connections',
+    image:
+      'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+    color: 'green'
+  },
+  {
     firstName: 'Doctor',
     lastName: 'Orchid',
     occupation: 'Scientist',
     age: 26,
     description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
     image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
-    color: 'white',
+    color: 'white'
   },
   {
     firstName: 'Victor',
@@ -26,8 +28,9 @@ const suspectsArray = [
     occupation: 'Designer',
     age: 22,
     description: 'Billonaire video game designer',
-    image: 'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
-    color: 'purple',
+    image:
+      'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
+    color: 'purple'
   },
   {
     firstName: 'Kasandra',
@@ -36,37 +39,65 @@ const suspectsArray = [
     age: 31,
     description: 'She is an A-list movie star with a dark past',
     image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
-    color: 'red',
+    color: 'red'
   },
   {
     firstName: 'Eleanor',
     lastName: 'Peacock',
     occupation: 'Socialite',
     age: 36,
-    description: 'She is from a wealthy family and uses her status and money to earn popularity',
+    description:
+      'She is from a wealthy family and uses her status and money to earn popularity',
     image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
-    color: 'blue',
+    color: 'blue'
   },
-   {
+  {
     firstName: 'Jacl',
     lastName: 'Mustard',
-    occupation:'Retired Fotball player',
+    occupation: 'Retired Fotball player',
     age: 62,
-    description:'He is a former football player who tries to get by on his former glory',
-    image:'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
-    color: 'yellow',
-    },
+    description:
+      'He is a former football player who tries to get by on his former glory',
+    image:
+      'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
+    color: 'yellow'
+  }
 ];
 
-/* Rooms Array 
-*/
+// Rooms Array
 
-const roomsArray = [];
+const roomsArray = [
+{ name: 'Dining Room' },
+{ name: 'Conservatory' },
+{ name: 'Kitchen' },
+{ name: 'Study' },
+{ name: 'Library' },
+{ name: 'Billiard Room' },
+{ name: 'Lounge' },
+{ name: 'Ballroom' },
+{ name: 'Hall' },
+{ name: 'Spa' },
+{ name: 'Living Room'},
+{ name: 'Observatory' },
+{ name: 'Theater' },
+{ name: 'Guest House' },
+{ name: 'Patio' },
+];
+
 
 // Weapons Array
 
-const weaponsArray = [];
-
+const weaponsArray = [
+  { name: 'rope', weight: 10 },
+  { name: 'knife', weight: 8 },
+  { name: 'candlestick', weight: 2 },
+  { name: 'dumbbell', weight: 30 },
+  { name: 'poison', weight: 2 },
+  { name: 'axe', weight: 15 },
+  { name: 'bat', weight: 13 },
+  { name: 'trophy', weight: 25 },
+  { name: 'pistol', weight: 20 }
+];
 
 // ITERATION 2
 
@@ -74,12 +105,9 @@ function selectRandom() {}
 
 function pickMystery() {}
 
-
 // ITERATION 3
 
 function revealMystery() {}
-
-
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
