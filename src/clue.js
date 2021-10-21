bf1e0649997ea83ef6fa748e1d7ec3350d514352
Raquel <raqhidcor@gmail.comgit commit --amend --reset-authorgit statusgit config --global user.name Raquelraqhidcor@gmail.com>
2021-10-22 01:17:40 +0200

    Iteration 3 completed

--- src/clue.js
+++ src/clue.js
@@ -197,8 +197,9 @@ function pickMystery() {
 
 
 // ITERATION 3
-
-function revealMystery() {
+function revealMystery(envelope) {
+  let revealMessage= `${envelope.suspect.firstName} ${envelope.suspect.lastName} killed Mr. Boddy using the ${envelope.weapon.name} in the ${envelope.room.name}!`;
+  return (revealMessage)
 }
 
 
