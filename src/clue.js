066a4ae7472008740b6dd7b4116c4a7529352cf9
besaga <bsgallardo93@gmail.com>
2021-10-24 12:36:17 +0200

    comentarios del ejercicio

--- src/clue.js
+++ src/clue.js
@@ -2,130 +2,131 @@
 // Suspects Array
 
 const suspectsArray = [
-    {
-      firstName: 'Jacob',
-      lastName: 'Green',
-      occupation: 'Entrepreneur',
-      age: 45,
-      description: 'He has a lot of connections',
-      image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
-      color: 'green',
-    }
-    , {
-      firstName: 'Doctor',
-      lastName: 'Orchid',
-      occupation: 'Scientist',
-      age: 26,
-      description: 'PhD in plant toxicology.Adopted daughter of Mr.Boddy',
-      image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
-      color: 'white',
-    }
-    , {
-      firstName: 'Victor',
-      lastName: 'Plum',
-      occupation: 'Designer',
-      age: 22,
-      description: 'Billionaire video game designer',
-      image: 'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
-      color: 'purple',
-    }
-    ,{
-      firstName: 'Kasandra',
-      lastName: 'Scarlet',
-      occupation: 'Actor',
-      age: 31,
-      description: 'She is an A- list movie star with a dark past',
-      image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
-      color: 'red',
-    }
-    ,{
-      firstName: 'Eleanor',
-      lastName: 'Peacock',
-      occupation: 'Socialité',
-      age: 36,
-      description: 'She is from a wealthy family and uses her status and money to earn popularity',
-      image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
-      color: 'blue',
-    }
-    ,{
-      firstName: 'Jack',
-      lastName: 'Mustard',
-      occupation: 'Retired Football player',
-      age: 62,
-      description: 'He is a former football player who tries to get by on his former glory',
-      image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
-      color: 'yellow',
-    }
-  ];
+  {
+    firstName: 'Jacob',
+    lastName: 'Green',
+    occupation: 'Entrepreneur',
+    age: 45,
+    description: 'He has a lot of connections',
+    image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+    color: 'green',
+  }
+  , {
+    firstName: 'Doctor',
+    lastName: 'Orchid',
+    occupation: 'Scientist',
+    age: 26,
+    description: 'PhD in plant toxicology.Adopted daughter of Mr.Boddy',
+    image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
+    color: 'white',
+  }
+  , {
+    firstName: 'Victor',
+    lastName: 'Plum',
+    occupation: 'Designer',
+    age: 22,
+    description: 'Billionaire video game designer',
+    image: 'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
+    color: 'purple',
+  }
+  , {
+    firstName: 'Kasandra',
+    lastName: 'Scarlet',
+    occupation: 'Actor',
+    age: 31,
+    description: 'She is an A- list movie star with a dark past',
+    image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
+    color: 'red',
+  }
+  , {
+    firstName: 'Eleanor',
+    lastName: 'Peacock',
+    occupation: 'Socialité',
+    age: 36,
+    description: 'She is from a wealthy family and uses her status and money to earn popularity',
+    image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
+    color: 'blue',
+  }
+  , {
+    firstName: 'Jack',
+    lastName: 'Mustard',
+    occupation: 'Retired Football player',
+    age: 62,
+    description: 'He is a former football player who tries to get by on his former glory',
+    image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
+    color: 'yellow',
+  }
+];
 
 // Rooms Array
 
-    const roomsArray = [
-      {
-name: 'Dining Room'}
-      ,{name: 'Conservatory'}
-      ,{name: 'Kitchen'}
-      ,{name: 'Study'}
-      ,{name: 'Library'}
-      ,{name: 'Billiard Room'}
-      ,{name: 'Lounge'}
-      ,{name: 'Ballroom'}
-      ,{name: 'Hall'}
-      ,{name: 'Spa'}
-      ,{name: 'Living Room'}
-      ,{name: 'Observatory'}
-      ,{name: 'Theater'}
-      ,{name: 'Guest House'}
-      ,{name: 'Patio'},
-     ];
+const roomsArray = [
+  {
+    name: 'Dining Room'
+  }
+  , { name: 'Conservatory' }
+  , { name: 'Kitchen' }
+  , { name: 'Study' }
+  , { name: 'Library' }
+  , { name: 'Billiard Room' }
+  , { name: 'Lounge' }
+  , { name: 'Ballroom' }
+  , { name: 'Hall' }
+  , { name: 'Spa' }
+  , { name: 'Living Room' }
+  , { name: 'Observatory' }
+  , { name: 'Theater' }
+  , { name: 'Guest House' }
+  , { name: 'Patio' },
+];
 
 // Weapons Array
 
-    const weaponsArray = [
-      {name: 'rope', weight: 10},
-      {name: 'knife', weight: 8},
-      {name: 'candlestick', weight: 2},
-      {name: 'dumbbell', weight: 30},
-      {name: 'poison', weight: 2},
-      {name: 'axe', weight: 15},
-      {name: 'bat', weight: 13}, 
-      {name: 'trophy', weight: 25}, 
-      {name: 'pistol', weight: 20}, 
-      
-    ];
+const weaponsArray = [
+  { name: 'rope', weight: 10 },
+  { name: 'knife', weight: 8 },
+  { name: 'candlestick', weight: 2 },
+  { name: 'dumbbell', weight: 30 },
+  { name: 'poison', weight: 2 },
+  { name: 'axe', weight: 15 },
+  { name: 'bat', weight: 13 },
+  { name: 'trophy', weight: 25 },
+  { name: 'pistol', weight: 20 },
+
+];
 
 // ITERACIÓN 2
 
-function  selectRandom(array){
-  let randoms=( Math.floor(Math.random() * (array.length - 0)+0))
-  return array[randoms]
- }
- 
-function  pickMystery()  {
-  let new_object={
-    suspect: selectRandom(suspectsArray),
+function selectRandom(array) { // selecciona un objeto  random del array que le paso
+  let randoms = (Math.floor(Math.random() * (array.length - 0) + 0))// calcula un numero randdom con valor min 0 y masimo la logitus del array
+  return array[randoms]// devuelve un objeto del array en posicion random
+}
+
+function pickMystery() {// Devuelve un objetto random juntando sospechoso,armaa y habitafcion
+  let new_object = {
+    suspect: selectRandom(suspectsArray),//llamamos a la funcion selectRandom que se encarga de eleir al azar un elemento del array que le pasamos
     weapon: selectRandom(weaponsArray),
-    room: selectRandom (roomsArray),
+    room: selectRandom(roomsArray),
   }
   //console.log(new_object)
-  return new_object
-  }
-  //console.log(pickMystery() )
+  return new_object// devuelve el objeto al azar
+}
+//console.log(pickMystery() )
 
 // ITERACIÓN 3
 
-function revealMystery(object)  {
+function revealMystery(object) {//le paso el objeto que me da pickMistery que utilizo para completar la frase
   //console.log(object.room.name)
-  return `${object.suspect.firstName } ${object.suspect.lastName } killed Mr. Boddy using the ${object.weapon.name } in the ${object.room.name }!`
+  return `${object.suspect.firstName} ${object.suspect.lastName} killed Mr. Boddy using the ${object.weapon.name} in the ${object.room.name}!`
 }
 console.log(revealMystery(object = pickMystery()))
 
-  
 
 
 
-  // The following is required to make unit tests work.
-  /* Environment setup. Do not modify the below code. */
+
+// The following is required to make unit tests work.
+/* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
   module.exports = {
     suspectsArray,
