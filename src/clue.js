33e32745d5826acd617c2b7e28db60455705ff66
Alejandra Lopez <alejandralopez91@gmail.com>
2021-11-14 16:01:02 +0100

    done

--- src/clue.js
+++ src/clue.js
@@ -3,125 +3,131 @@
 // Suspects Array
 
 const suspectsArray = [
-  
-  mrGreen:{
-firstName: "Jacob",
-lastName: "Green",
-occupation: "Entrepreneur",
-age: 45,
-description: "He has a lot of connections",
-image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
-color: "green",
+  {
+  firstName: "Jacob",
+  lastName: "Green",
+  occupation: "Entrepreneur",
+  age: 45,
+  description: "He has a lot of connections",
+  image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+  color: "green",
   },
 
-drOrchid:{
-firstName: "Doctor",
-lastName: "Orchid",
-occupation: "Scientist",
-age: 26,
-description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
-image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
-color: "white",
-},
+  {
+  firstName: "Doctor",
+  lastName: "Orchid",
+  occupation: "Scientist",
+  age: 26,
+  description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+  image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+  color: "white",
+  },
 
-profPlum:{
-firstName: "Victor",
-lastName: "Plum",
-occupation: "Designer",
-age: 22,
-description: "Billionaire video game designer",
-image: "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg",
-color: "purple",
-},
+  {
+  firstName: "Victor",
+  lastName: "Plum",
+  occupation: "Designer",
+  age: 22,
+  description: "Billionaire video game designer",
+  image: "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg",
+  color: "purple",
+  },
 
-missScarlet:{
-firstName: "Kasandra",
-lastName: "Scarlet",
-occupation: "Actor",
-age: 31,
-description: "She is an A-list movie star with a dark past",
-image: "https://www.radiotimes.com/uploads/images/Original/111967.jpg",
-color: "red"
-},
+  {
+  firstName: "Kasandra",
+  lastName: "Scarlet",
+  occupation: "Actor",
+  age: 31,
+  description: "She is an A-list movie star with a dark past",
+  image: "https://www.radiotimes.com/uploads/images/Original/111967.jpg",
+  color: "red"
+  },
 
-mrsPeacock:{
-firstName: "Eleanor",
-lastName: "Peacock",
-occupation: "Socialité",
-age: 36,
-description: "She is from a wealthy family and uses her status and money to earn popularity",
-image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
-color: "blue",
-},
+  {
+  firstName: "Eleanor",
+  lastName: "Peacock",
+  occupation: "Socialité",
+  age: 36,
+  description: "She is from a wealthy family and uses her status and money to earn popularity",
+  image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+  color: "blue",
+  },
 
-mrMustard:{
-firstName: "Jack",
-lastName: "Mustard",
-occupation: "Retired Football player",
-age: 62,
-description: "He is a former football player who tries to get by on his former glory",
-image: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg",
-color: "yellow",
-}
+  {
+  firstName: "Jack",
+  lastName: "Mustard",
+  occupation: "Retired Football player",
+  age: 62,
+  description: "He is a former football player who tries to get by on his former glory",
+  image: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg",
+  color: "yellow",
+  },
 ];
 
 // Rooms Array
 
 const roomsArray = [
- { name: rope, weight: 10},
-{name: knife , weight: 8},
-{name: candlestick , weight: 2},
-{name: dumbbell , weight: 30},
-{name: poison , weight: 2},
-{name: axe , weight: 15},
-{name: bat , weight: 13},
-{name: trophy , weight: 25},
-{name: pistol , weight: 20}
+  {name: "Dining Room"},
+  {name: "Conservatory"},
+  {name: "Kitchen"},
+  {name: "Study"},
+  {name: "Library"},
+  {name: "Billiard Room"},
+  {name: "Lounge"},
+  {name: "Ballroom"},
+  {name: "Hall"},
+  {name: "Spa"},
+  {name: "Living Room"},
+  {name: "Observatory"},
+  {name: "Theater"},
+  {name: "Guest House"},
+  {name: "Patio"},
 ];
 
 // Weapons Array
 
 const weaponsArray = [
-{name: "Dining Room"},
-{name: "Conservatory"},
-{name: "Kitchen"},
-{name: "Study"},
-{name: "Library"},
-{name: "Billiard Room"},
-{name: "Lounge"},
-{name: "Ballroom"},
-{name: "Hall"},
-{name: "Spa"},
-{name: "Living Room"},
-{name: "Observatory"},
-{name: "Theater"},
-{name: "Guest House"},
-{name: "Patio"}
+  {name: "rope", weight: 10},
+  {name: "knife" , weight: 8},
+  {name: "candlestick" , weight: 2},
+  {name: "dumbbell" , weight: 30},
+  {name: "poison" , weight: 2},
+  {name: "axe" , weight: 15},
+  {name: "bat" , weight: 13},
+  {name: "trophy" , weight: 25},
+  {name: "pistol" , weight: 20},
 ];
 
 
 // ITERATION 2
+/*Select one element from a card stack at random. 
+The function should expect an array as an argument, and should return a random element from the array.*/
+function selectRandom(array) {
+let randomCard=Math.floor(Math.random()*array.length);
+return array[randomCard];
+};
 
-function selectRandom() {
-let random[];
-for (let i=0; i<suspectsArray.length;i++){
-  for(let j=0; j<roomsArray.length; j++){
-    for(let g=0; g<weaponsArray.length; g++){
-      random.push()
-    }
-  }
-}
+/*takes no arguments and returns an object with three properties: suspect, weapon and room, 
+each holding as a value a card of that specific type*/
 
-}
-
-
-
-function pickMystery() {}
+function pickMystery() {
+suspect=selectRandom(suspectsArray);
+weapon=selectRandom(weaponsArray);
+room=selectRandom(roomsArray);
+return {suspect, weapon, room};
+};
 
 
 // ITERATION 3
+/*Declare a function named revealMystery that receives an envelope object 
+(with the shape of the object returned by pickMystery) as the single argument, 
+and returns a reveal message in the following format:
+<FIRST NAME> <LAST NAME> killed Mr. Boddy using the <WEAPON> in the <ROOM>!*/
 
-function revealMystery() {}
+function revealMystery(envelope) {
+  const {suspect, weapon, room}=envelope;
+    return `${suspect.firstName} ${suspect.lastName} killed Mr. Boddy using the ${weapon.name} in the ${room.name}!`;
+}
 
 
 
