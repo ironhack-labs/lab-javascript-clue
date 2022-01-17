650082db1e2cc3495a20f332eeeeccd576fc827e
Miguel Ortega <miguel.owd@gmail.com>
2022-01-17 08:22:34 +0100

    Lab solved

--- src/clue.js
+++ src/clue.js
@@ -73,21 +73,21 @@ const suspectsArray = [
 
 const roomsArray = [
 
-  { name: Dining - Room },
-  { name: Conservatory },
-  { name: Kitchen },
-  { name: Study },
-  { name: Library },
-  //{ name: Billiard Room },
-  { name: Lounge },
-  { name: Ballroom },
-  { name: Hall },
-  { name: Spa },
-  //{ name: Living Room },
-  { name: Observatory },
-  { name: Theater },
-  //{ name: Guest House },
-  { name: Patio },
+  { name: "Dining - Room" },
+  { name: "Conservatory" },
+  { name: "Kitchen" },
+  { name: "Study" },
+  { name: "Library" },
+  { name: "Billiard Room" },
+  { name: "Lounge" },
+  { name: "Ballroom" },
+  { name: "Hall" },
+  { name: "Spa" },
+  { name: "Living Room" },
+  { name: "Observatory" },
+  { name: "Theater" },
+  { name: "Guest House" },
+  { name: "Patio" },
 
 ];
 
