5dd947a9601ce281f3f96597dbb5adfdf376678b
Bruno Mortella <bmortella@gmail.com>
2022-01-20 12:47:43 -0300

    Iteration 3

--- src/clue.js
+++ src/clue.js
@@ -103,7 +103,9 @@ function pickMystery() {
 
 // ITERATION 3
 
-function revealMystery() {}
+function revealMystery(envelope) {
+  return `${envelope.suspect.firstName} ${envelope.suspect.lastName} killed Mr. Boddy using the ${envelope.weapon.name} in the ${envelope.room.name}!`
+}
 
 
 
