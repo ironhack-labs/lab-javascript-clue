ec8ecda88550b3510ec3f9b4fe79d143f5013cd5
Nathalia Maia <nathaliamaiape@gmail.com>
2022-01-20 22:09:28 -0300

    final

--- src/clue.js
+++ src/clue.js
@@ -3,28 +3,137 @@
 // Suspects Array
 
 const suspectsArray = [];
+   
+let mrGreen = {
+  firstName: 'Jacob',
+  lastName: 'Green',
+  occupation: 'Entrepreneur',
+  age: 45,
+  description: 'He has a lot of connections',
+  image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+  color: 'green'
+};
+
+let drOrchid = {
+  firstName: 'Doctor',
+  lastName: 'Orchid',
+  occupation: 'Scientist',
+  age: 26,
+  description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
+  image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
+  color: 'white'
+};
+
+let profPlum = {
+  firstName: 'Victor',
+  lastName: 'Plum',
+  occupation: 'Designer',
+  age: 22,
+  description: 'Billionaire video game designer',
+  image: 'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
+  color: 'purple'
+};
+
+let missScarlet = {
+  firstName: 'Kasandra',
+  lastName: 'Scarlet',
+  occupation: 'Actor',
+  age: 31,
+  description: 'She is an A-list movie star with a dark past',
+  image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
+  color: 'red'
+};
+
+let mrsPeacock = {
+  firstName: 'Eleanor',
+  lastName: 'Peacock',
+  occupation: 'Socialité',
+  age: 36,
+  description: 'She is from a wealthy family and uses her status and money to earn popularity',
+  image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
+  color: 'blue'
+};
+
+let mrMustard = {
+  firstName: 'Jack',
+  lastName: 'Mustard',
+  occupation: 'Retired Football player',
+  age: 62,
+  description: 'He is a former football player who tries to get by on his former glory',
+  image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
+  color: 'yellow'
+};
+
+suspectsArray.push(mrGreen);
+suspectsArray.push(drOrchid);
+suspectsArray.push(profPlum);
+suspectsArray.push(missScarlet);
+suspectsArray.push(mrsPeacock);
+suspectsArray.push(mrMustard);
 
 // Rooms Array
-
-const roomsArray = [];
+ 
+const roomsArray = [
+  {room: 1, name: 'Dining Room'},
+  {room: 2, name: 'Conservatory'},
+  {room: 3, name: 'Kitchen'},
+  {room: 4, name: 'Study'},
+  {room: 5, name: 'Library'},
+  {room: 6, name: 'Billiard Room'},
+  {room: 7, name: 'Lounge'},
+  {room: 8, name: 'Ballroom'},
+  {room: 9 ,name: 'Hall'},
+  {room: 10 ,name: 'Spa'},
+  {room: 11 ,name: 'Living Room'},
+  {room: 12 ,name: 'Observatory'},
+  {room: 13 ,name: 'Theater'},
+  {room: 14 ,name: 'Guest House'},
+  {room: 15 ,name: 'Patio'}
+];
 
 // Weapons Array
 
-const weaponsArray = [];
+const weaponsArray = [
+  {name: 'rope' , weight: 10},
+  {name: 'knife', weight: 8},
+  {name: 'candlestick', weight: 2},
+  {name: 'dumbbell', weight: 30},
+  {name: 'poison', weight: 2},
+  {name: 'axe', weight: 15},
+  {name: 'bat', weight: 13},
+  {name: 'trophy', weight: 25},
+  {name: 'pistol', weight: 20}
+];
 
 
 // ITERATION 2
 
-function selectRandom() {}
+function selectRandom(arr) {
+    let randomPosition = Math.floor((Math.random() * arr.length));
+    let randomElement = arr[randomPosition];
+    return randomElement;
+  }
 
-function pickMystery() {}
+function pickMystery() {
+  let mystery = {}
+  let suspectChoose = selectRandom(suspectsArray);
+  let roomChoose = selectRandom(roomsArray);
+  let weaponChoose = selectRandom(weaponsArray);
+  
+  mystery.suspect = suspectChoose;
+  mystery.room = roomChoose;
+  mystery.weapon = weaponChoose;
+
+  return mystery;
+  
+}
 
 
 // ITERATION 3
 
-function revealMystery() {}
-
-
+function revealMystery(obj) {
+  return `${obj.suspect.firstName} ${obj.suspect.lastName} killed Mr. Boddy using the ${obj.weapon.name} in the ${obj.room.name}!`;
+}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
