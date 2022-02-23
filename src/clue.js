004294959f0e0287070f4798a40b4c3ccbf18ae1
Laurie Bruneau <lauriebr@hotmail.fr>
2022-02-24 00:06:53 +0100

    Clue exercice done

--- src/clue.js
+++ src/clue.js
@@ -1,28 +1,129 @@
 // ITERATION 1
-
 // Suspects Array
 
-const suspectsArray = [];
+const suspectsArray = [
+    {
+    firstName: 'Kasandra',
+    lastName: 'Scarlet',
+    occupation: 'Actor',
+    age: 31,
+    description: 'She is an A-list movie star with a dark past',
+    image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
+    color: 'red'
+    },
+    
+    {
+    firstName: 'Eleanor',
+    lastName: 'Peacock',
+    occupation: 'Socialité',
+    age: 36,
+    description: 'She is from a wealthy family and uses her status and money to earn popularity',
+    image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
+    color: 'blue'
+    },
+
+   {
+      firstName: 'Victor',
+      lastName: 'Plum',
+      occupation: 'Designer',
+      age: 22,
+      description: 'Billionaire video game designer',
+      image: 'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
+      color: 'purple'
+    },
+    
+    {
+    firstName: 'Jack',
+    lastName: 'Mustard',
+    occupation: 'Retired Football player',
+    age: 62,
+    description: 'He is a former football player who tries to get by on his former glory',
+    image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
+    color: 'yellow'
+    },
+    
+    {
+    firstName: 'Kasandra',
+    lastName: 'Scarlet',
+    occupation: 'Actor',
+    age: 31,
+    description: 'She is an A-list movie star with a dark past',
+    image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
+    color: 'red'
+    },
+
+   {
+    firstName: 'Eleanor',
+    lastName: 'Peacock',
+    occupation: 'Socialité',
+    age: 36,
+    description: 'She is from a wealthy family and uses her status and money to earn popularity',
+    image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
+    color: 'blue',
+    }
+];
 
 // Rooms Array
 
-const roomsArray = [];
+let roomsArray = [
+  {name: 'Dining Room'},
+  {name: 'Conservatory'},
+  {name: 'Kitchen'},
+  {name: 'Study'},
+  {name: 'Library'},
+  {name: 'Billiard Room'},
+  {name: 'Lounge'},
+  {name: 'Ballroom'},
+  {name: 'Hall'},
+  {name: 'Spa'},
+  {name: 'Living Room'},
+  {name: 'Observatory'},
+  {name: 'Theater'},
+  {name: 'Guest House'},
+  {name: 'Patio'},
+]
 
 // Weapons Array
 
-const weaponsArray = [];
+let weaponsArray = [
+  {name: 'rope', weight: 10},
+  {name: 'knife', weight: 8},
+  {name: 'candlestick', weight: 2},
+  {name: 'dumbbell', weight: 30},
+  {name: 'poison', weight: 2},
+  {name: 'axe', weight: 15},
+  {name: 'bat', weight: 13},
+  {name: 'trophy', weight: 25},
+  {name: 'pistol', weight: 20}
+]; 
 
 
 // ITERATION 2
 
-function selectRandom() {}
+function selectRandom(arr) {
+  let randomNb = Math.floor(Math.random() * arr.length) 
+  return arr[randomNb]
+} 
 
-function pickMystery() {}
-
+function pickMystery() {
+  const randomSuspect = selectRandom(suspectsArray) 
+  const randomWeapons = selectRandom(weaponsArray)
+  const randomRoom = selectRandom(roomsArray)
+  const randomCards = {suspect: randomSuspect, weapon: randomWeapons, room: randomRoom}
+  return randomCards
+}
 
 // ITERATION 3
 
-function revealMystery() {}
+function revealMystery(envelope) {
+  const finalName = randomSuspect.firstName
+  const finalLastName = randomSuspect.lastName
+  const finalWeapon = randomWeapons
+  const finalRoom = randomRoom
+  const message = `${finalName} ${finalLastName} killed Mr. Bobby using the ${finalWeapon} in the ${finalRoom}!`
+  return message
+} 
+
 
 
 
@@ -38,3 +139,5 @@ if (typeof module !== 'undefined') {
     selectRandom
   };
 }
+
+
--- tests/clue.spec.js
+++ tests/clue.spec.js
@@ -205,3 +205,6 @@ describe('Function "revealMystery" - Reveal the mystery', () => {
     );
   });
 });
+
+
+
