3186e2aa3b3790369b95b4cbd3206b6721b855e5
Matt K <matthedud@hotmail.com>
2022-02-23 16:47:30 +0100

    finish

--- src/clue.js
+++ src/clue.js
@@ -69,18 +69,6 @@ const suspectsArray = [
 
 // Rooms Array
 
-[
-  name: rope --- weight: 10
-  name: knife --- weight: 8
-  name: candlestick --- weight: 2
-  name: dumbbell --- weight: 30
-  name: poison --- weight: 2
-  name: axe --- weight: 15
-  name: bat --- weight: 13
-  name: trophy --- weight: 25
-  name: pistol --- weight: 20
-]
-
 const roomsArray = [
   {name: "Dining Room"},
   {name: "Conservatory"},
