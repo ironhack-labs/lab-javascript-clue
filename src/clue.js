1ba58616c71d4c677b962feb508766ed0f5c90b9
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-02-28 15:56:28 -0400

    iteration 1: weaponsArray

--- src/clue.js
+++ src/clue.js
@@ -65,7 +65,17 @@ const roomsArray = [];
 
 // Weapons Array
 
-const weaponsArray = [];
+const weaponsArray = [
+  {name: "rope", weight: 10},
+  {name: "knife", weight: 8},
+  {name: "candlestick", weight: 2},
+  {name: "dumbbell", weight: 30},
+  {name: "poison", weight: 2},
+  {name: "axe", weight: 15},
+  {name: "bat", weight: 13},
+  {name: "trophy", weight: 25},
+  {name: "pistol", weight: 20}
+];
 
 
 // ITERATION 2
