08335d9a072d8570930c28f20fcdc3fbec71e580
arneeder <arne.eder@gmail.com>
2022-03-23 13:58:06 +0100

    buggs fixed

--- src/clue.js
+++ src/clue.js
@@ -82,7 +82,7 @@ const roomsArray = [
 // Weapons Array
 
 const weaponsArray = [
-  {name: reportError, weight: 10 },
+  {name: 'rope', weight: 10 },
   {name: 'knife', weight: 8},
   {name: 'candlestick' , weight: 2},
   {name: 'dumbbell', weight: 30},
@@ -97,7 +97,7 @@ const weaponsArray = [
 // ITERATION 2
 
 function selectRandom(arr) {
-  let randomNumber = Math.floor(Math.random()*6)
+  let randomNumber = Math.floor(Math.random()*arr.length)
   let element = arr[randomNumber]
   return element
 }
