351d83b64c6fc1346765e213a30253626e9b7824
Amanda Walker <amandajnewalker@gmail.com>
2022-03-24 21:11:12 -0400

    lab 4

--- src/clue.js
+++ src/clue.js
@@ -1,29 +1,86 @@
 // ITERATION 1
 
 // Suspects Array
+ 
+const mrGreen = {firstName: 'Jacob', lastName: 'Green', occupation: 'Entrepreneur', age: '45', description: 'He has a lot of connections',image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg', color: 'green',}
 
-const suspectsArray = [];
+const drOrchid = {firstName: 'Doctor', lastName: 'Orchid', occupation: 'Scientist', age: '26', description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy', image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg', color: 'white',}
+
+const profPlum = {firstName: 'Victor', lastName: 'Plum', occupation: 'Designer', age: '22', description: 'Billionaire video game designer', image:'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg', color: 'purple',}
+
+const missScarlet = {firstName: 'Kasandra', lastName: 'Scarlet', occupation: 'Actor', age: '31', description: 'She is an A-list movie star with a dark past', image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg', color: 'red',}
+
+const mrsPeacock = {firstName: 'Eleanor', lastName: 'Peacock', occupation: 'Socialité', age: '36', description: 'She is from a wealthy family and uses her status and money to earn popularity', image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg', color: 'blue',}
+
+const mrMustard = {firstName: 'Jack', lastName: 'Mustard', occupation: 'Retired Football player', age: '62', description: 'He is a former football player who tries to get by on his former glory', image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg', color: 'yellow',}
+
+const suspectsArray = [mrGreen,drOrchid,profPlum,missScarlet,mrsPeacock,mrMustard];
+
+
 
 // Rooms Array
 
-const roomsArray = [];
+const roomsArray = [
+  {name: 'Dining Room'},
+  {name: 'Conseravtory'},
+  {name: 'Kitchen'},
+  {name: 'Study'},
+  {name: 'Library'},
+  {name:'Billiard Room'},
+  {name:'Lounge'},
+  {name: 'Ballroom'},
+  {name: 'Hall'},
+  {name: 'Spa'},
+  {name: 'Living Room'},
+  {name: 'Obseravatory'},
+  {name: 'Theater'},
+  {name: 'Guest House'},
+  {name: 'Patio'},
+  ];
+  
 
 // Weapons Array
 
-const weaponsArray = [];
+const rope = {name: 'rope', weight: 8}
+const knife = {name: 'knife',weight: 8}
+const candlestick = {name: 'candlestick', weight: 2}
+const dumbell = {name: 'dumbbell', weight: 30}
+const poison = {name: 'poison', weight: 2}
+const axe = {name: 'axe', weight: 15}
+const bat = {name: 'trophy', weight: 13}
+const trophy = {name: 'trophy', weight: 25}
+const pistol = {name: 'pistol', weight: 20}
+
+const weaponsArray = [rope, knife, candlestick,dumbell, poison, axe, bat, trophy, pistol];
 
 
 // ITERATION 2
 
-function selectRandom() {}
+function selectRandom(arrayToShuffle) {
+let randomIndex = Math.floor(Math.random() * arrayToShuffle.length)
 
-function pickMystery() {}
-
+  return arrayToShuffle[randomIndex]; 
+}
+function pickMystery() {
+  const selectRandom(suspectsArray);
+  const selectRandom(weaponsArray);
+  const selectRandom(roomsArray);
+}
+const mysteryObject = {
+return {
+  suspect: selectRandom(suspectsArray),
+  weapon: selectRandom(weaponsArray),
+  room: selectRandom(roomsArray)
+}
+return mysteryObject;
+  
+}
 
 // ITERATION 3
 
-function revealMystery() {}
-
+function revealMystery(envelope){
+    return `${envelope.suspect.firstName} ${envelope.suspect.lastName} killed Mr. Boddy using the ${envelope.weapon.name} in the ${envelope.room.name}!`;
+}
 
 
 // The following is required to make unit tests work.
