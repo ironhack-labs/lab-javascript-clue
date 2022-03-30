35e8482059123f18922926f16ff00f205e5fc172
Mafalda Oliveira <mafalda.oliveira.21@icloud.com>
2022-03-30 17:54:55 +0100

    solve all but last iteration

--- src/clue.js
+++ src/clue.js
@@ -3,12 +3,12 @@
 // Suspects Array
 
 const suspectsArray = [
-  {firstName: 'Jacob', lastName: 'Green', occupation: 'Entrepreneur', age: 45, description: 'He has a lot of connections', color: 'green'},
-  {firstName: 'Doctor', lastName: 'Orchid', occupation: 'Scientist', age: 26, description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy', color: 'white'},
-  {firstName: 'Victor', lastName:'Plum', occupation: 'Designer', age: 22, description:'Billionaire video game designer', color: 'purple'},
-  {firstName: 'Kasandra', lastName: 'Scarlet', occupation: 'Actor',age: 31, description: 'She is an A-list movie star with a dark past', color: 'red'},
-  {firstName: 'Eleanor', lastName: 'Peacock', occupation: 'Socialité', age: 36, description: 'She is from a wealthy family and uses her status and money to earn popularity', color: 'blue'},
-  {firstName: 'Jack', lastName: 'Mustard', occupation: 'Retired Football player', age: 62, description: 'He is a former football player who tries to get by on his former glory', color: 'yellow'},
+  {firstName: 'Jacob', lastName: 'Green', occupation: 'Entrepreneur', age: 45, description: 'He has a lot of connections', color: 'green', image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg'},
+  {firstName: 'Doctor', lastName: 'Orchid', occupation: 'Scientist', age: 26, description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy', color: 'white', image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg'},
+  {firstName: 'Victor', lastName:'Plum', occupation: 'Designer', age: 22, description:'Billionaire video game designer', color: 'purple', image: 'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg'},
+  {firstName: 'Kasandra', lastName: 'Scarlet', occupation: 'Actor',age: 31, description: 'She is an A-list movie star with a dark past', color: 'red', image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg'},
+  {firstName: 'Eleanor', lastName: 'Peacock', occupation: 'Socialité', age: 36, description: 'She is from a wealthy family and uses her status and money to earn popularity', color: 'blue', image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg'},
+  {firstName: 'Jack', lastName: 'Mustard', occupation: 'Retired Football player', age: 62, description: 'He is a former football player who tries to get by on his former glory', color: 'yellow', image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg'},
 ];
 
 // Rooms Array
@@ -64,13 +64,15 @@ function pickMystery() {
   
   
 }
-
+let envelope = pickMystery()
 
 // ITERATION 3
-let mystery = pickMystery()
 
-function revealMystery() {
-  return '${mystery.suspect.firstName} ${mystery.suspect.lastName} killed Mr. Boddy using the ${mystery.weapon} in the ${mystery.room}!'
+
+
+function revealMystery(envelope) {
+  
+  return '${envelope.suspect.firstName} ${envelope.suspect.lastName} killed Mr. Boddy using the ${envelope.weapon} in the ${envelope.room}!'
 }
 
 
