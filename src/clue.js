0f90c4c40978faeaa0eff2c22dda9b0c90eb49a2
Mafalda Oliveira <mafalda.oliveira.21@icloud.com>
2022-03-30 20:30:07 +0100

    finish lab

--- src/clue.js
+++ src/clue.js
@@ -72,7 +72,7 @@ let envelope = pickMystery()
 
 function revealMystery(envelope) {
   
-  return '${envelope.suspect.firstName} ${envelope.suspect.lastName} killed Mr. Boddy using the ${envelope.weapon} in the ${envelope.room}!'
+  return `${envelope.suspect.firstName} ${envelope.suspect.lastName} killed Mr. Boddy using the ${envelope.weapon.name} in the ${envelope.room.name}!`
 }
 
 
