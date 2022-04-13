e91591df75566b5124baba891cbb351660d71af0
Tanvi Gurjar <tanvi.monitors@gmail.com>
2022-04-13 17:41:39 +0200

    Testing

--- src/clue.js
+++ src/clue.js
@@ -25,7 +25,6 @@ function pickMystery() {}
 function revealMystery() {}
 
 
-
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
