92154f53ef43755a199d3004cc4a2d85a9dd4935
begoczb <begocz.31@gmail.com>
2022-05-11 18:08:15 +0200

    add weapon array

--- src/clue.js
+++ src/clue.js
@@ -3,7 +3,7 @@
 // Suspects Array
 
 const suspectsArray = [
-  (mrGreen = {
+  {
     firstName: 'Jacob',
     lastName: 'Green',
     occupation: 'Entrepreneur',
@@ -11,18 +11,18 @@ const suspectsArray = [
     description: 'He has a lot of connections',
     image:
       'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
-    color: green
-  }),
-  (drOrchid = {
+    color: 'green'
+  },
+  {
     firstName: 'Doctor',
     lastName: 'Orchid',
     occupation: 'Scientist',
     age: 26,
     description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
     image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
-    color: white
-  }),
-  (profPlum = {
+    color: 'white'
+  },
+  {
     firstName: 'Victor',
     lastName: 'Plum',
     occupation: 'Designer',
@@ -30,9 +30,9 @@ const suspectsArray = [
     description: 'Billionaire video game designer',
     image:
       'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
-    color: purple
-  }),
-  (missScarlet = {
+    color: 'purple'
+  },
+  {
     firstName: 'Kasandra',
     lastName: 'Scarlet',
     occupation: 'Actor',
@@ -40,8 +40,8 @@ const suspectsArray = [
     description: 'She is an A-list movie star with a dark past',
     image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
     color: 'red'
-  }),
-  (mrsPeacock = {
+  },
+  {
     firstName: 'Eleanor',
     lastName: 'Peacock',
     occupation: 'Socialité',
@@ -50,8 +50,8 @@ const suspectsArray = [
       'She is from a wealthy family and uses her status and money to earn popularity',
     image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
     color: 'blue'
-  }),
-  (mrMustard = {
+  },
+  {
     firstName: 'Jack',
     lastName: 'Mustard',
     occupation: 'Retired Football player',
@@ -61,7 +61,7 @@ const suspectsArray = [
     image:
       'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
     color: 'yellow'
-  })
+  }
 ];
 
 // Rooms Array
@@ -70,7 +70,44 @@ const roomsArray = [];
 
 // Weapons Array
 
-const weaponsArray = [];
+const weaponsArray = [
+  {
+    name: 'rope',
+    weight: 10
+  },
+  {
+    name: 'knife',
+    weight: 8
+  },
+  {
+    name: 'candlestick',
+    weight: 2
+  },
+  {
+    name: 'dumbbell',
+    weight: 30
+  },
+  {
+    name: 'poison',
+    weight: 2
+  },
+  {
+    name: 'axe',
+    weight: 15
+  },
+  {
+    name: 'bat',
+    weight: 13
+  },
+  {
+    name: 'trophy',
+    weight: 25
+  },
+  {
+    name: 'pistol',
+    weight: 20
+  }
+];
 
 // ITERATION 2
 
