959b6381f883f17a11975340d1ac914a835be250
PelayoPAR <pelayopar@gmail.com>
2022-05-26 17:41:32 +0200

    done

--- src/clue.js
+++ src/clue.js
@@ -4,33 +4,35 @@
 
 const suspectsArray = [
   {
-  firstName: 'Jacob',
-  lastName: 'Green',
-  occupation: 'Entrepreneur',
-  age: 45,
-  description: 'He has a lot of connections',
-  image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
-  color: 'green',
-  }, 
+    firstName: 'Jacob',
+    lastName: 'Green',
+    occupation: 'Entrepreneur',
+    age: 45,
+    description: 'He has a lot of connections',
+    image:
+      'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+    color: 'green'
+  },
 
-  { 
+  {
     firstName: 'Doctor',
     lastName: 'Orchid',
     occupation: 'Scientist',
     age: 26,
     description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
     image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
-    color: 'white',
+    color: 'white'
   },
 
   {
-  firstName: 'Victor',
-  lastName: 'Plum',
-  occupation: 'Designer',
-  age: 22,
-  description: 'Billionaire video game designer',
-  image: 'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
-  color: 'purple',
+    firstName: 'Victor',
+    lastName: 'Plum',
+    occupation: 'Designer',
+    age: 22,
+    description: 'Billionaire video game designer',
+    image:
+      'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
+    color: 'purple'
   },
 
   {
@@ -38,87 +40,86 @@ const suspectsArray = [
     lastName: 'Scarlet',
     occupation: 'Actor',
     age: 31,
+    description: 'She is an A-list movie star with a dark past',
     image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
-    color: 'red',
+    color: 'red'
   },
 
   {
-  firstName: 'Eleanor',
-  lastName: 'Peacock',
-  occupation: 'Socialité',
-  age: 36,
-  description: 'She is from a wealthy family and uses her status and money to earn popularity',
-  image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
-  color: 'blue',
+    firstName: 'Eleanor',
+    lastName: 'Peacock',
+    occupation: 'Socialité',
+    age: 36,
+    description: 'She is from a wealthy family and uses her status and money to earn popularity',
+    image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
+    color: 'blue'
   },
 
   {
-  firstName: 'Jack',
-  lastName: 'Mustard',
-  occupation: 'Retired Football player',
-  age: 62,
-  description: 'He is a former football player who tries to get by on his former glory',
-  image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
-  color: 'yellow',
-  },
-  
+    firstName: 'Jack',
+    lastName: 'Mustard',
+    occupation: 'Retired Football player',
+    age: 62,
+    description: 'He is a former football player who tries to get by on his former glory',
+    image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
+    color: 'yellow'
+  }
 ];
 
 // Rooms Array
 
 const roomsArray = [
-  {name: 'Dining Room'},
-  {name: 'Conservatory'},
-  {name: 'Kitchen'},
-  {name: 'Study'},
-  {name: 'Library'},
-  {name: 'Billiard Room'},
-  {name: 'Lounge'},
-  {name: 'Ballroom'},
-  {name: 'Hall'},
-  {name: 'Spa'},
-  {name: 'Living Room'},
-  {name: 'Observatory'},
-  {name: 'Theater'},
-  {name: 'Guest House'},
-  {name: 'Patio'},
-  
-
+  { name: 'Dining Room' },
+  { name: 'Conservatory' },
+  { name: 'Kitchen' },
+  { name: 'Study' },
+  { name: 'Library' },
+  { name: 'Billiard Room' },
+  { name: 'Lounge' },
+  { name: 'Ballroom' },
+  { name: 'Hall' },
+  { name: 'Spa' },
+  { name: 'Living Room' },
+  { name: 'Observatory' },
+  { name: 'Theater' },
+  { name: 'Guest House' },
+  { name: 'Patio' }
 ];
 
 // Weapons Array
 
 const weaponsArray = [
-  {name: 'rope', weight: 10},
-  {name: 'knife', weight: 8},
-  {name: 'candlestick', weight: 2},
-  {name: 'dumbbell', weight: 30},
-  {name: 'poison', weight: 2},
-  {name: 'axe', weight: 15},
-  {name: 'bat', weight: 13},
-  {name: 'trophy', weight: 25},
-  {name: 'pistol', weight: 20},
+  { name: 'rope', weight: 10 },
+  { name: 'knife', weight: 8 },
+  { name: 'candlestick', weight: 2 },
+  { name: 'dumbbell', weight: 30 },
+  { name: 'poison', weight: 2 },
+  { name: 'axe', weight: 15 },
+  { name: 'bat', weight: 13 },
+  { name: 'trophy', weight: 25 },
+  { name: 'pistol', weight: 20 }
 ];
-
 
 // ITERATION 2
 
-function selectRandom() {
-  return (anArray[Math.round(Math.random()*(anArray.length-1))])
- }
-
-function pickMystery() {
-  return {_suspect_: selectRandom(suspectsArray), _weapon_: selectRandom(weaponsArray), _room_: selectRandom(roomsArray)} 
+function selectRandom(anArray) {
+  const randomNum = Math.random()
+  return anArray[Math.floor(randomNum * (anArray.length))];
 }
 
+function pickMystery() {
+  return {
+    suspect: selectRandom(suspectsArray),
+    weapon: selectRandom(weaponsArray),
+    room: selectRandom(roomsArray)
+  };
+}
 
 // ITERATION 3
 
-function revealMystery() {
-  return `**${_envelope_._suspect_.firstName} ${_envelope_._suspect_.lastName} killed Mr. Boddy using the ${_envelope_._weapon_.name} in the ${_envelope_._room_.name}**`
+function revealMystery(envelope) {
+  return `${envelope.suspect.firstName} ${envelope.suspect.lastName} killed Mr. Boddy using the ${envelope.weapon.name} in the ${envelope.room.name}!`;
 }
-
-
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
