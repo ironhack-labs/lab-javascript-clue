20deac061cc9ef88ef27648a701c5a006973e1af
aarestigue <108528013+aarestigue@users.noreply.github.com>
2022-07-08 09:21:04 +0100

    final

--- src/clue.js
+++ src/clue.js
@@ -4,6 +4,16 @@
 
 const suspectsArray = [];
 
+firstName: "Jacob"
+lastName: "Green"
+occupation: "Entrepreneur"
+age: 45
+description: He has a lot of connections
+image: https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg
+color: green
+
+ffff
+
 // Rooms Array
 
 const roomsArray = [];
