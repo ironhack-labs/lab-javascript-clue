f4c6c68b2072b1ddbef2aeb15f0396f3347c5116
João Serafim <46942255+JoaoSerafim3001@users.noreply.github.com>
2022-07-27 16:13:40 +0100

    Update clue.js

--- src/clue.js
+++ src/clue.js
@@ -2,15 +2,28 @@
 
 // Suspects Array
 
-const suspectsArray = [];
+const suspectsArray = [
+  {
+    firstName: "John",
+    lastName: "Doe"
+  }
+];
 
 // Rooms Array
 
-const roomsArray = [];
+const roomsArray = [
+  {
+    roomName: ["living room", "office", "kitchen"]
+  }
+];
 
 // Weapons Array
 
-const weaponsArray = [];
+const weaponsArray = [
+  {
+    weaponName: ["knife", "pistol"]
+  }
+];
 
 
 // ITERATION 2
@@ -22,5 +35,8 @@ function pickMystery() {}
 
 // ITERATION 3
 
-function revealMystery() {}
+function revealMystery() {
+  return `${suspectsArray["firstName"] ${suspectsArray["firstName"] killed Mr. Boddy using the ${weaponsArray[roomName[]]`
+  <FIRST NAME> <LAST NAME> killed Mr. Boddy using the <WEAPON> in the <ROOM>!
+}
 
