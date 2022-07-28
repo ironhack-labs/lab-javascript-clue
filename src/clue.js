fdffd7b11df04d4a06d91185b13294cacac24f83
Guillaume <guillaume.chatriot@gmail.com>
2022-07-28 16:56:46 +0200

    cluedo not finished

--- src/clue.js
+++ src/clue.js
@@ -2,15 +2,154 @@
 
 // Suspects Array
 
-const suspectsArray = [];
+const suspectsArray = [
+    {
+      firstName: 'Colonel', 
+      lastName: 'Moutarde',
+      occupation: 'Army',
+      age: 45,
+      description: "He is a Colonel",
+      image: "https://static.wikia.nocookie.net/cluedo/images/f/f7/05-22---Colonel-Mustard.jpg/revision/latest?cb=20171015125500&path-prefix=fr",
+      color: "Yellow",
+    },
+    {    
+      firstName: 'Madame',
+      lastName: 'Pervenche',
+      occupation: 'Politician',
+      age: 46,
+      description: "She is a Politician",
+      image: "https://static.wikia.nocookie.net/cluedo/images/7/75/C8QfsioWsAAv_L5.jpg/revision/latest?cb=20171016164322&path-prefix=fr",
+      color: "Blue",
+    },
+    {
+      firstName: 'Madame',
+      lastName: 'Pervenche',
+      occupation: 'Politician',
+      age: 46,
+      description: "She is a Politician",
+      image: "https://static.wikia.nocookie.net/cluedo/images/7/75/C8QfsioWsAAv_L5.jpg/revision/latest?cb=20171016164322&path-prefix=fr",
+      color: "Blue",
+    },
+    {
+      firstName: 'Mademoiselle',
+      lastName: 'Rose',
+      occupation: 'Actress',
+      age: 47,
+      description: "She is an actress",
+      image: "https://static.wikia.nocookie.net/cluedo/images/e/e1/05-21---Miss-Scarlett.jpg/revision/latest?cb=20171015125800&path-prefix=fr",
+      color: "Pink",
+    },
+    {
+      firstName: 'Professeur',
+      lastName: 'Violet',
+      occupation: 'Professor',
+      age: 48,
+      description: "he is a Professor",
+      image: "https://static.wikia.nocookie.net/cluedo/images/0/06/05-26---Professor-Plum.jpg/revision/latest?cb=20171015130240&path-prefix=fr",
+      color: "Purple",
+    },
+    {
+      firstName: 'Docteur',
+      lastName: 'Olive',
+      occupation: 'Doctor',
+      age: 49,
+      description: "he is a Doctor",
+      image: "https://pbs.twimg.com/media/EwPH3VRWQAAKC35.jpg",
+      color: "Green",
+    },
+];
 
 // Rooms Array
 
-const roomsArray = [];
+const roomsArray = [
+    {
+      name: "Dining Room",
+    },
+    {
+      name: "Conservatory",
+    },
+    {
+      name: "Kitchen",
+    },
+    {
+      name: "Study",
+    },
+    {
+      name: "Library",
+    },
+    {
+      name: "Billiard Room",
+    },
+    {
+      name: "Lounge",
+    },
+    {
+      name: "Ballroom",
+    },
+    {
+      name: "Hall",
+    },
+    {
+      name: "Spa",
+    },
+    {
+      name: "Living Room",
+    },
+    {
+      name: "Observatory",
+    },
+    {
+      name: "Theater",
+    },
+    {
+      name: "Guest House",
+    },
+    {
+      name: "Patio",
+    },
+  ];
 
 // Weapons Array
 
-const weaponsArray = [];
+const weaponsArray = [
+    {
+      name: "rope",
+      weight: 10,
+    },
+    {
+      name: "knife",
+      weight: 8,
+    },
+    {
+      name: "candlestick",
+      weight: 2,
+    },
+    {
+      name: "dumbbell",
+      weight: 30,
+    },
+    {
+      name: "poison",
+      weight: 2,
+    },
+    {
+      name: "axe",
+      weight: 15,
+    },
+    {
+      name: "bat",
+      weight: 13,
+    },
+    {
+      name: "trophy",
+      weight: 25,
+    },
+    {
+      name: "pistol",
+      weight: 20,
+    }
+    
+  ];
 
 
 // ITERATION 2
