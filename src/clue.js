b05325700247d955a5df5b1a350aa7777e4227e5
efosdan <efosdan@yahoo.com>
2022-08-10 23:33:31 +0200

    Conpleted Itiration

--- src/clue.js
+++ src/clue.js
@@ -6,21 +6,45 @@ const suspectsArray = [];
 
 // Rooms Array
 
-const roomsArray = [];
+const roomsArray = [
+  { name: "Dining Room" },
+  { name: "Conservatory" },
+  { name: "Kitchen" },
+  { name: "Study" },
+  { name: "Library" },
+  { name: "Billiard Room" },
+  { name: "Lounge" },
+  { name: "Ballroom" },
+  { name: "Hall" },
+  { name: "Spa" },
+  { name: "Living Room" },
+  { name: "Observatory" },
+  { name: "Theater" },
+  { name: "Guest House" },
+  { name: "Patio" },
+];
 
 // Weapons Array
 
 const weaponsArray = [];
 
-
 // ITERATION 2
 
-function selectRandom() {}
+function selectRandom(Arr) {
+  let randomNumber = Arr[Math.floor(Math.random() * Arr.length)];
+}
+return randomNumber;
 
-function pickMystery() {}
+function pickMystery() {
+  const pickMystery = {
+    suspect: selectRandom(suspectsArray),
+    weapon: selectRandom(weaponsArray),
+    room: selectRandom(roomsArray),
+  };
 
+  return pickMystery;
+}
 
 // ITERATION 3
 
 function revealMystery() {}
-
