7ebf524adb93ec4266ba0e7b5c2f09e56736f244
Carlos Vallejo <carlosvallejo.webmaster@gmail.com>
2022-08-12 20:01:11 +0200

    last save

--- src/clue.js
+++ src/clue.js
@@ -171,3 +171,4 @@ function revealMystery(mystery) {
     return `${mystery.suspect.firstName} ${mystery.suspect.lastName} killed Mr. Boddy using the ${mystery.weapon.name} in the ${mystery.room.name}!`
 }
 
+console.log(revealMystery(objectRandom))
