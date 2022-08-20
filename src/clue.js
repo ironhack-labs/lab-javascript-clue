a8e4722525d451ca8c953c9a6ad70b2bf741644d
Chris Ziomek <cziomek1989@gmail.com>
2022-08-20 15:31:40 -0400

    still working

--- src/clue.js
+++ src/clue.js
@@ -2,15 +2,144 @@
 
 // Suspects Array
 
-const suspectsArray = [];
+const suspectsArray = [
+{
+    firstName: 'Jacob',
+    lastName: 'Green',
+    occupation: 'Entrepreneur',
+    age: 45, 
+    description: 'He has a lot of connections',
+    image: src="https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+    color: 'green'
+},
+{
+    firstName: 'Doctor',
+    lastName: 'Orchid',
+    occupation: 'Scientist',
+    age: 26,
+    description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
+    image: src="http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+    color: 'white'
+},
+{
+    firstName: 'Kasandra',
+    lastName: 'Scarlet',
+    occupation: 'Actor',
+    age: 31,
+    description: 'She is an A-list movie star with a dark past',
+    image: src="https://www.radiotimes.com/uploads/images/Original/111967.jpg",
+    color: 'red'
+},
+{
+    firstName: 'Eleanor',
+    lastName: 'Peacock',
+    occupation: 'Socialite',
+    age: 36,
+    description: 'She is from a wealthy family and uses her status and money to earn popularity',
+    image: src="https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+    color: 'blue'
+},
+{
+    firstName: 'Jack',
+    lastName: 'Mustard',
+    occupation: 'Retired Football player',
+    age: 62,
+    description: "He is a former football player who tries to get by on his former glory",
+    image: src="https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg",
+    color: 'yellow'
+}
+];
 
 // Rooms Array
 
-const roomsArray = [];
+const roomsArray = [
+{
+    name: 'Dining Room'
+},
+{
+    name: 'Conservatory'
+},
+{
+    name: 'Kitchen'
+},
+{
+    name: 'Study'
+},
+{
+    name: 'Library'
+},
+{
+    name: 'Billiard'
+},
+{
+    name: 'Lounge'
+},
+{
+    name: 'Ballroom'
+},
+{
+    name: 'Hall'
+},
+{
+    name: 'Spa'
+},
+{
+    name: 'Living Room'
+},
+{
+    name: 'Observatory'
+},
+{
+    name: 'Theater'
+},
+{
+    name: 'Guest House'
+},
+{
+    name: 'Patio'
+}    
+];
 
 // Weapons Array
 
-const weaponsArray = [];
+const weaponsArray = [
+{
+    name: 'rope',
+    weight: 10
+},
+{
+    name: 'knife',
+    weight: 8
+},
+{
+    name: 'candlestick',
+    weight: 2
+},
+{
+    name: 'dumbbell',
+    weight: 30
+},
+{
+    name: 'posion',
+    weight: 2
+},
+{
+    name: 'axe',
+    weigth: 15
+},
+{
+    name: 'bat',
+    weight: 13
+},
+{
+    name: 'trophy',
+    weight: 25
+},
+{
+    name: 'pistol',
+    weight: 20
+}    
+];
 
 
 // ITERATION 2
