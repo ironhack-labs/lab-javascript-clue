ec24f518042db85f846f2d971be8eeac933e4e54
humbertovacu <humbertovacu@gmail.com>
2022-09-21 19:35:29 +0200

    Updated arrays woth passed tests

--- src/clue.js
+++ src/clue.js
@@ -9,7 +9,7 @@ const suspectsArray = [
         occupation: "Entrepreneur",
         age: 45,
         description: "He has a lot of connections",
-        imgUrl: https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg,
+        image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
         color: "green"
     },
     {
@@ -17,9 +17,9 @@ const suspectsArray = [
         lastName: "Orchid",
         occupation: "Scientist",
         age: 26,
-        description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy"
-        imgUrl: http://www.radiotimes.com/uploads/images/Original/111967.jpg,
-        color: white
+        description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+        image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+        color: "white"
     },
     {
         firstName: "Victor",
@@ -27,7 +27,7 @@ const suspectsArray = [
         occupation: "Designer",
         age: 22,
         description: "Billionaire video game designer",
-        imgUrl: https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg,
+        image: "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg",
         color: "purple"
     },
     {
@@ -36,7 +36,7 @@ const suspectsArray = [
         occupation: "Actor",
         age: 31,
         description: "She is an A-list movie star with a dark past",
-        imgUrl: https://www.radiotimes.com/uploads/images/Original/111967.jpg,
+        image: "https://www.radiotimes.com/uploads/images/Original/111967.jpg",
         color: "red"
     },
     {
@@ -45,7 +45,7 @@ const suspectsArray = [
         occupation: "Socialité",
         age: 36,
         description: "She is from a wealthy family and uses her status and money to earn popularity",
-        imgUrl: https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg,
+        image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
         color: "blue"
     },
     {
@@ -54,7 +54,7 @@ const suspectsArray = [
         occupation: "Retired Football player",
         age: 62,
         description: "He is a former football player who tries to get by on his former glory",
-        imgUrl: https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg,
+        image: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg",
         color: "yellow"
     }
 ];
@@ -63,89 +63,89 @@ const suspectsArray = [
 
 const roomsArray = [
     {
-        name: Dining Room
+        name: "Dining Room"
     },
     {
-        name: Conservatory
+        name: "Conservatory"
     },
     {
-        name: Kitchen
+        name: "Kitchen"
     },
     {
-        name: Study
+        name: "Study"
     },
     {
-        name: Library
+        name: "Library"
     },
     {
-        name: Billiard Room
+        name: "Billiard Room"
     },
     {
-        name: Lounge
+        name: "Lounge"
     },
     {
-        name: Ballroom
+        name: "Ballroom"
     },
     {
-        name: Hall
+        name: "Hall"
     },
     {
-        name: Spa
+        name: "Spa"
     },
     {
-        name: Living Room
+        name: "Living Room"
     },
     {
-        name: Observatory
+        name: "Observatory"
     },
     {
-        name: Theater
+        name: "Theater"
     },
     {
-        name: Guest House
+        name: "Guest House"
     },
     {
-        name: Patio
-    };
+        name: "Patio"
+    }
 ];
 
 // Weapons Array
 
 const weaponsArray = [
     {
-        name: rope,
+        name: "rope",
         weight: 10
     },
     {
-        name: knife,
+        name: "knife",
         weight: 8
     },
     {
-        name: candlestick,
+        name: "candlestick",
         weight: 2
     },
     {
-        name: dumbbell,
+        name: "dumbbell",
         weight: 30
     },
     {
-        name: poison,
+        name: "poison",
         weight: 2
     },
     {
-        name: axe,
+        name: "axe",
         weight: 15
     },
     {
-        name: bat,
+        name: "bat",
         weight: 13
     },
     {
-        name: trophy,
+        name: "trophy",
         weight: 25
     },
     {
-        name: pistol,
+        name: "pistol",
         weight: 20
     }
 ];
