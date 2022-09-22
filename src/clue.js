cbf3cb5953be82726dc7b40b58a79a7873193eae
MoniIgnacio <moniignacio02@gmail.com>
2022-09-22 19:53:07 +0400

    add roomsArray

--- src/clue.js
+++ src/clue.js
@@ -62,8 +62,60 @@ const suspectsArray = [
 
 // Rooms Array
 
-const roomsArray = [];
+const roomsArray = [
+    {
+        name: 'Conservatory'
+    },
+    {
+        name: 'Kitchen'
+    },
+    {
+        name: 'Study'
+    },
+    {
+        name: 'Library'
+        
+    },
+    {
+        name: 'Billiard Room'
 
+    },
+    {
+        name: 'Lounge'
+
+    },
+    {
+        name: 'Ballroom'
+    },
+    {
+        name: 'Hall'
+
+    },
+    {
+        name: 'Spa'
+    },
+    {
+        name: 'Living Room'
+
+    },
+    {
+        name: 'Observatory'
+
+    },
+    {
+        name: 'Theater'
+
+    },
+    {
+        name:'Guest House'
+    },
+    {
+        name: 'Patio' 
+    }
+];
+/*
+
+*/
 // Weapons Array
 
 const weaponsArray = [
