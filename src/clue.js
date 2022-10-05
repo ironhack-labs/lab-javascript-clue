f20c680a916630630f145be055dd4ebb10fa39ef
Camila <agarcamila@gmail.com>
2022-10-05 17:55:01 +0100

    New update

--- src/clue.js
+++ src/clue.js
@@ -10,13 +10,13 @@ const suspectsArray = [
         age: 45,
         description: 'He has a lot of connections',
         image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
-        color: 'green',
+        color: 'green'
     }, 
         {
             firstName: 'Doctor',
             lastName: 'Orchid',
             occupation: 'Scientist',
-            age: 26
+            age: 26,
             description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
             image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
             color: 'white'
@@ -25,7 +25,7 @@ const suspectsArray = [
             firstName: 'Victor',
             lastName: 'Plum',
             occupation: 'Designer',
-            age: 22
+            age: 22,
             description: 'Billionaire video game designer',
             image: 'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
             color: 'purple'
@@ -34,7 +34,7 @@ const suspectsArray = [
             firstName: 'Kasandra',
             lastName: 'Scarlet',
             occupation: 'Actor',
-            age: 31
+            age: 31,
             description: 'She is an A-list movie star with a dark past',
             image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
             color: 'red',
@@ -43,8 +43,8 @@ const suspectsArray = [
             firstName: 'Eleanor',
             lastName: 'Peacock',
             occupation: 'Socialité',
-            age: 36
-            description: 'She is from a wealthy family and uses her status and money to earn popularity'
+            age: 36,
+            description: 'She is from a wealthy family and uses her status and money to earn popularity',
             image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
             color: 'blue'
         },
@@ -61,18 +61,71 @@ const suspectsArray = [
 
 // Rooms Array
 
-const roomsArray = [];
+const roomsArray = [{name: 'Dining Room' },
+{name: 'Conservatory'},
+{name: 'Kitchen'},
+{
+name: 'Study'
+},
+{
+name: 'Library'
+},
+{
+name: 'Billiard Room'
+},
+{
+name: 'Lounge'
+},
+{
+name: 'Ballroom'
+},
+{
+name: 'Hall'
+},
+{
+name: 'Spa'
+},
+{
+name: 'Living Room'
+},
+{
+name: 'Observatory'
+},
+{
+name: 'Theater'
+},
+{
+name: 'Guest House'
+},
+{
+name: 'Patio'
+}];
+
 
 // Weapons Array
 
-const weaponsArray = [];
+const weaponsArray = [
+{name: 'rope', weight: 10}, 
+{name: 'knife', weight: 8}, 
+{name: 'candlestick', weight: 2}, 
+{name: 'dumbbell', weight: 30}, 
+{name: 'poison', weight: 2},
+{name: 'axe', weight: 15},
+{name: 'bat',  weight: 13},
+{name: 'trophy', weight: 25},
+{name: 'pistol', weight: 20}];
 
 
 // ITERATION 2
 
-function selectRandom() {}
+function selectRandom(arr) {
+    const random = Math.floor(Math.random() * arr.length);
+    return random, arr[random];
+}
 
-function pickMystery() {}
+function pickMystery() {
+return 
+}
 
 
 // ITERATION 3
