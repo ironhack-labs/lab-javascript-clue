eda04eb84c689496adfc847a9daffec2c70d4cd0
David Fdez <dav.fdez.lopez@gmail.com>
2022-10-23 12:36:00 +0200

    I have given a bit of aesthetic consistency to the code

--- src/clue.js
+++ src/clue.js
@@ -150,9 +150,7 @@ function pickMystery() {
 
 
 
-function revealMystery(envelope) {
-
-    
+function revealMystery(envelope) { 
     
     return `${envelope.suspect.firstName} ${envelope.suspect.lastName} killed Mr. Boddy using the ${envelope.weapon.name} in the ${envelope.room.name}!`
 }
