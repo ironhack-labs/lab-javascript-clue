ddb42156b2c19ed8d81b0644d5b844fc4e72af40
IvanLara-hub <116952338+IvanLara-hub@users.noreply.github.com>
2022-11-07 18:07:38 +0100

    second commit

--- src/clue.js
+++ src/clue.js
@@ -2,25 +2,230 @@
 
 // Suspects Array
 
-const suspectsArray = [];
+const suspectsArray = [
+{
+
+    firstName: "Ivan",
+    lastName: "Lara",
+    occupation: "alpinista",
+    age: 33,
+    description: "strongest man",
+    image: "jpeg",
+    color: "black",
+}, {
+
+    firstName: "Ivan",
+    lastName: "Lara",
+    occupation: "alpinista",
+    age: 33,
+    description: "strongest man",
+    image: "jpeg",
+    color: "black",
+
+}, {
+
+    firstName: "Ivan",
+    lastName: "Lara",
+    occupation: "alpinista",
+    age: 33,
+    description: "strongest man",
+    image: "jpeg",
+    color: "black",
+    
+}, {
+
+    firstName: "Ivan",
+    lastName: "Lara",
+    occupation: "alpinista",
+    age: 33,
+    description: "strongest man",
+    image: "jpeg",
+    color: "black",
+
+}, {
+
+    firstName: "Ivan",
+    lastName: "Lara",
+    occupation: "alpinista",
+    age: 33,
+    description: "strongest man",
+    image: "jpeg",
+    color: "black",
+
+}, {
+
+    firstName: "Ivan",
+    lastName: "Lara",
+    occupation: "alpinista",
+    age: 33,
+    description: "strongest man",
+    image: "jpeg",
+    color: "black",
+}
+
+];
+
 
 // Rooms Array
 
-const roomsArray = [];
+const roomsArray = [
+
+{
+
+    name: "living room"
+
+}, {
+
+    name: "living room"
+
+}, {
+
+    name: "living room"
+
+}, {
+    
+    name: "living room"
+
+}, {
+
+    name: "living room"
+
+}, {
+
+    name: "living room"
+
+}, {
+
+    name: "living room"
+
+}, {
+
+    name: "living room"
+
+}, {
+
+    name: "living room"
+
+}, {
+
+    name: "living room"
+
+}, {
+
+    name: "living room"
+
+}, {
+
+    name: "living room"
+
+}, {
+
+    name: "living room"
+
+}, {
+
+    name: "living room"
+
+}, {
+
+    name: "living room"
+
+}
+
+];
+
 
 // Weapons Array
 
-const weaponsArray = [];
+const weaponsArray = [
+
+{
+
+    name: "gun",
+    weight: 0.700 
+}, {
+
+
+    name: "gun",
+    weight: 0.700 
+
+}, {
+
+    name: "gun",
+    weight: 0.700 
+
+}, {
+
+    name: "gun",
+    weight: 0.700 
+
+
+}, {
+
+  
+    name: "gun",
+    weight: 0.700 
+
+
+}, {
+
+    name: "gun",
+    weight: 0.700 
+
+}, {
+
+
+    name: "gun",
+    weight: 0.700 
+
+}, {
+
+    name: "gun",
+    weight: 0.700 
+
+
+}, {
+
+    name: "gun",
+    weight: 0.700 
+
+
+
+}
+
+
+];
 
 
 // ITERATION 2
 
-function selectRandom() {}
+function selectRandom(arr) {
+    const item = arr[Math.floor(Math.random() * arr.length)];
 
-function pickMystery() {}
+    return item;    
+}
+
+
+function pickMystery() {
+    const obj = {
+        suspect: selectRandom(suspectsArray),
+        weapon: selectRandom(weaponsArray),
+        room: selectRandom(roomsArray)
+    }
+    return obj;
+}
+
+
+
+
 
 
 // ITERATION 3
 
-function revealMystery() {}
+function revealMystery(mistery) { 
+   const str = `${mistery.suspect.firstName} ${mistery.suspect.lastName } killed Mr. Boddy using the ${mistery.weapon.name} in the ${mistery.room.name}!`
+   return str
+}
+    
+
 
