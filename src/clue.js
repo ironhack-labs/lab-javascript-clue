eaea5cf430ea7bedfa9caf3dfbe97aec315ee0d6
Ericazach <ericazach@gmail.com>
2022-11-19 13:42:41 +0100

    Add last modifications and done

--- src/clue.js
+++ src/clue.js
@@ -3,7 +3,7 @@
 // Suspects Array
 
 const suspectsArray = [
-    mrGreen = {
+    {
         firstName: "Jacob",
         lastName: "Green",
         occupation: "Entrepreneur",
@@ -11,7 +11,8 @@ const suspectsArray = [
         description: "He has a lot of connections",
         image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
         color: "green"
-    }, drOrchid = {
+    },
+    {
         firstName: "Doctor",
         lastName: "Orchid",
         occupation: "Scientist",
@@ -19,7 +20,8 @@ const suspectsArray = [
         description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
         image: " http://www.radiotimes.com/uploads/images/Original/111967.jpg",
         color: "white"
-    }, profPlum = {
+    },
+    {
         firstName: "Victor",
         lastName: "Plum",
         occupation: "Designer",
@@ -27,7 +29,8 @@ const suspectsArray = [
         description: "Billionaire video game designer",
         image: "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg",
         color: "purple"
-    }, missScarlet = {
+    },
+    {
         firstName: "Kasandra",
         lastName: "Scarlet",
         occupation: "Actor",
@@ -35,7 +38,8 @@ const suspectsArray = [
         description: "She is an A-list movie star with a dark past",
         image: "https://www.radiotimes.com/uploads/images/Original/111967.jpg",
         color: "red"
-    }, mrsPeacock = {
+    },
+    {
         firstName: "Eleanor",
         lastName: "Peacock",
         occupation: "Socialité",
@@ -43,7 +47,8 @@ const suspectsArray = [
         description: "She is from a wealthy family and uses her status and money to earn popularity",
         image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
         color: "blue"
-    }, mrMustard = {
+    },
+    {
         firstName: "Jack",
         lastName: "Mustard",
         occupation: "Retired Football player",
@@ -58,29 +63,50 @@ const suspectsArray = [
 // Rooms Array
 
 const roomsArray = [
-    room1 = { name: "Dining Room" }, room2 = { name: "Conservatory" }, room3 = { name: "Kitchen" }, room4 = { name: "Study" }, room5 = { name: "Library" }, room6 = { name: "Billiard Room" }, room7 = { name: "Lounge" }, room8 = { name: "Ballroom" }, room9 = { name: "Hall" }, room10 = { name: "Spa" }, room11 = { name: "Living Room" }, room12 = { name: "Observatory" }, room13 = { name: "Theater" }, room14 = { name: "Guest House" }, room15 = { name: "Patio" }
+    { name: "Dining Room" },
+    { name: "Conservatory" },
+    { name: "Kitchen" },
+    { name: "Study" },
+    { name: "Library" },
+    { name: "Billiard Room" },
+    { name: "Lounge" },
+    { name: "Ballroom" },
+    { name: "Hall" },
+    { name: "Spa" },
+    { name: "Living Room" },
+    { name: "Observatory" },
+    { name: "Theater" },
+    { name: "Guest House" },
+    { name: "Patio" }
 ];
 
 // Weapons Array
 
 const weaponsArray = [
-    weapon1 = {name: "rope", weight: 10}, weapon2 = {name: "knife", weight: 8}, weapon3 = {name: "candlestick", weight: 2}, weapon4 = {name: "dumbbell", weight: 30}, weapon5 = {name: "poison", weight: 2}, weapon6 = {name: "axe", weight: 15}, weapon7 = {name: "bat", weight: 13},  weapon8 = {name: "trophy", weight: 25}, weapon9 = {name: "pistol", weight: 20}
+    { name: "rope", weight: 10 },
+    { name: "knife", weight: 8 },
+    { name: "candlestick", weight: 2 },
+    { name: "dumbbell", weight: 30 },
+    { name: "poison", weight: 2 },
+    { name: "axe", weight: 15 },
+    { name: "bat", weight: 13 },
+    { name: "trophy", weight: 25 },
+    { name: "pistol", weight: 20 }
 ];
 
 
 // ITERATION 2
 
-function selectRandom(Arr) {
-    let random = Arr[Math.floor(Math.random() * Arr.length)]
-    return random;
+function selectRandom(cards) {
+   return cards[Math.floor(Math.random() * cards.length)]
 }
 
 function pickMystery() {
-    let mystery = {};
-    mystery.suspect = selectRandom(suspectsArray);
-    mystery.weapon = selectRandom(weaponsArray);
-    mystery.room = selectRandom(roomsArray);
-    return mystery;
+    return {
+        suspect: selectRandom(suspectsArray),
+        weapon: selectRandom(weaponsArray),
+        room: selectRandom(roomsArray)
+    };
 }
 
 
