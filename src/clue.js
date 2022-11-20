c7db711b05de643afe19f5cfa86171d62570d110
Victor Lopez <victorlpz2301@gmail.com>
2022-11-20 11:58:07 +0100

    finished

--- src/clue.js
+++ src/clue.js
@@ -65,89 +65,51 @@ const suspectsArray = [
 
 const roomsArray = [
     { name: `Dining Room` },
-    {
-        name: `Conservatory`,
-    },
-    {
-        name: `Kitchen`,
-    },
-    {
-        name: `Study`,
-    },
-    {
-        name: `Library`,
-    },
-    {
-        name: `Billiard Room`,
-    },
-    {
-        name: `Lounge`,
-    },
-    {
-        name: `Ballroom`,
-    },
-    {
-        name: `Hall`,
-    },
-    {
-        name: `Spa`,
-    },
-    {
-        name: `Living Room`,
-    },
-    {
-        name: `Observatory`,
-    },
-    {
-        name: `Theater`,
-    },
-    {
-        name: `Guest House`,
-    },
-    {
-        name: `Patio`,
-    },
+    { name: `Conservatory` },
+    { name: `Kitchen` },
+    { name: `Study` },
+    { name: `Library` },
+    { name: `Billiard Room` },
+    { name: `Lounge` },
+    { name: `Ballroom` },
+    { name: `Hall` },
+    { name: `Spa` },
+    { name: `Living Room` },
+    { name: `Observatory` },
+    { name: `Theater` },
+    { name: `Guest House` },
+    { name: `Patio` },
 ];
 
 // Weapons Array
 
 const weaponsArray = [
-    {
-        name: `rope`,
-        weight: 10,
-    },
-    {
-        name: `knife`, 
-        weight: 8,
-    },
-    {
-        name: `candlestick`, 
-        weight: 2,
-    },
-    {
-        name: `dumbbell`,
-        weight: 30,
-    },
-    {
-        name: `poison`,
-        weight: 2,
-    },
-    {
-        name: `axe`,
-        weight: 15,
-    },
-    {
-        name: `bat`,
-        weight: 13,
-    },
-    {
-        name: `trophy`, 
-        weight: 25,
-    },
-    {
-        name: `pistol`, 
-        weight: 20,
-    },
+    { name: `rope`,
+      weight: 10, },
+
+    { name: `knife`, 
+      weight: 8, },
+
+    { name: `candlestick`, 
+      weight: 2, },
+
+    { name: `dumbbell`,
+      weight: 30,},
+
+    { name: `poison`,
+      weight: 2, },
+
+    { name: `axe`,
+      weight: 15, },
+
+    { name: `bat`,
+      weight: 13, },
+
+    { name: `trophy`, 
+      weight: 25, },
+
+    { name: `pistol`, 
+      weight: 20, },
 ];
 
 
@@ -171,7 +133,6 @@ function pickMystery() {
 
 // ITERATION 3
 
-function revealMystery() {
-    
+function revealMystery(object) {
+    return `${object.suspect.firstName} ${object.suspect.lastName} killed Mr. Boddy using the ${object.weapon.name} in the ${object.room.name}!`
 }
-
