10cb4d0ed6abb3350d5d3b681c08068885b0632c
NabilAslam <nabil.aslam26@gmail.com>
2022-12-15 17:53:58 +0100

    adjustments

--- src/clue.js
+++ src/clue.js
@@ -2,131 +2,90 @@
 
 // Suspects Array
 
-const suspectsArray = [mrGreen, drOrchid, profPlum, missScarlet, mrsPeacock, mrMustard];
+const suspectsArray = [
+{
+firstName: 'Jacob',
+lastName: 'Green',
+occupation: 'Entrepreneur',
+age: '45',
+description: 'He has a lot of connections',
+image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+color: 'green'
+},
 
-let mrGreen = {
-firstName: Jacob
-lastName: Green
-occupation: Entrepreneur
-age: 45
-description: He has a lot of connections
-image: https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg
-color: green
-};
+{
+firstName: 'Doctor',
+lastName: 'Orchid',
+occupation: 'Scientist',
+age: '26',
+description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
+image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
+color: 'white'
+},
 
-let drOrchid = {
-firstName: Doctor
-lastName: Orchid
-occupation: Scientist
-age: 26
-description: PhD in plant toxicology. Adopted daughter of Mr. Boddy
-image: http://www.radiotimes.com/uploads/images/Original/111967.jpg
-color: white
-};
+{
+firstName: 'Victor',
+lastName: 'Plum',
+occupation: 'Designer',
+age: '22',
+description: 'Billionaire video game designer',
+image: 'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
+color: 'purple'
+},
 
-let profPlum = {
-firstName: Victor
-lastName: Plum
-occupation: Designer
-age: 22
-description: Billionaire video game designer
-image: https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg
-color: purple
-};
+{
+firstName: 'Kasandra',
+lastName: 'Scarlet',
+occupation: 'Actor',
+age: '31',
+description: 'She is an A-list movie star with a dark past',
+image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
+color: 'red'
+},
 
-let missScarlet = {
-firstName: Kasandra
-lastName: Scarlet
-occupation: Actor
-age: 31
-description: She is an A-list movie star with a dark past
-image: https://www.radiotimes.com/uploads/images/Original/111967.jpg
-color: red
-};
+{
+firstName: 'Eleanor',
+lastName: 'Peacock',
+occupation: 'Socialité',
+age: '36',
+description: 'She is from a wealthy family and uses her status and money to earn popularity',
+image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
+color: 'blue'
+},
 
-let mrsPeacock = {
-firstName: Eleanor
-lastName: Peacock
-occupation: Socialité
-age: 36
-description: She is from a wealthy family and uses her status and money to earn popularity
-image: https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg
-color: blue
-};
+{
+firstName: 'Jack',
+lastName: 'Mustard',
+occupation: 'Retired Football player',
+age: '62',
+description: 'He is a former football player who tries to get by on his former glory',
+image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
+color: 'yellow'
+},
 
-let mrMustard = {
-firstName: Jack
-lastName: Mustard
-occupation: Retired Football player
-age: 62
-description: He is a former football player who tries to get by on his former glory
-image: https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg
-color: yellow
-};
+];
 
 // Rooms Array
 
-const roomsArray = [];
+const roomsArray = [;
 
-let Dining Room = {
-name: 'Dining Room'
-};
+{name: 'Dining Room'};
+{name: 'Conservatory'};
+{name: 'Kitchen'};
+{name: 'Study'};
+{name: 'Library'};
+{name: 'Billiard Room'};
+{name: 'Lounge'};
+{name: 'Ballroom'};
+{name: 'Hall'};
+{name: 'Spa'};
+{name: 'Living Room'};
+{name: 'Observatory'};
+{name: 'Theater'};
+{name: 'Guest House'};
+{name: 'Patio'};
 
-let Conservatory = {
-name: 'Conservatory'
-};
-
-let Kitchen = {
-name: 'Kitchen'
-};
-
-let Study = {
-name: 'Study'
-};
-
-let Library = {
-name: 'Library'
-};
-
-let Billiard Room = {
-name: 'Billiard Room'
-};
-
-let Lounge = {
-name: 'Lounge'
-};
-
-let Ballroom = {
-name: 'Ballroom'
-};
-
-let Hall = {
-name: 'Hall'
-};
-
-let Spa = {
-name: 'Spa'
-};
-
-let Living Room = {
-name: 'Living Room'
-};
-
-let Observatory = {
-name: 'Observatory'
-};
-
-let Theater = {
-name: 'Theater'
-};
-
-let Guest House = {
-name: 'Guest House'
-};
-
-let Patio = {
-name: 'Patio'
-};
+]
 
 // Weapons Array
 
