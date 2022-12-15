02642f52f34aadcb088676d6de99b54eae377249
NabilAslam <nabil.aslam26@gmail.com>
2022-12-15 17:09:18 +0100

    solved lab

--- src/clue.js
+++ src/clue.js
@@ -2,25 +2,202 @@
 
 // Suspects Array
 
-const suspectsArray = [];
+const suspectsArray = [mrGreen, drOrchid, profPlum, missScarlet, mrsPeacock, mrMustard];
+
+let mrGreen = {
+firstName: Jacob
+lastName: Green
+occupation: Entrepreneur
+age: 45
+description: He has a lot of connections
+image: https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg
+color: green
+};
+
+let drOrchid = {
+firstName: Doctor
+lastName: Orchid
+occupation: Scientist
+age: 26
+description: PhD in plant toxicology. Adopted daughter of Mr. Boddy
+image: http://www.radiotimes.com/uploads/images/Original/111967.jpg
+color: white
+};
+
+let profPlum = {
+firstName: Victor
+lastName: Plum
+occupation: Designer
+age: 22
+description: Billionaire video game designer
+image: https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg
+color: purple
+};
+
+let missScarlet = {
+firstName: Kasandra
+lastName: Scarlet
+occupation: Actor
+age: 31
+description: She is an A-list movie star with a dark past
+image: https://www.radiotimes.com/uploads/images/Original/111967.jpg
+color: red
+};
+
+let mrsPeacock = {
+firstName: Eleanor
+lastName: Peacock
+occupation: Socialité
+age: 36
+description: She is from a wealthy family and uses her status and money to earn popularity
+image: https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg
+color: blue
+};
+
+let mrMustard = {
+firstName: Jack
+lastName: Mustard
+occupation: Retired Football player
+age: 62
+description: He is a former football player who tries to get by on his former glory
+image: https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg
+color: yellow
+};
 
 // Rooms Array
 
 const roomsArray = [];
 
+let Dining Room = {
+name: 'Dining Room'
+};
+
+let Conservatory = {
+name: 'Conservatory'
+};
+
+let Kitchen = {
+name: 'Kitchen'
+};
+
+let Study = {
+name: 'Study'
+};
+
+let Library = {
+name: 'Library'
+};
+
+let Billiard Room = {
+name: 'Billiard Room'
+};
+
+let Lounge = {
+name: 'Lounge'
+};
+
+let Ballroom = {
+name: 'Ballroom'
+};
+
+let Hall = {
+name: 'Hall'
+};
+
+let Spa = {
+name: 'Spa'
+};
+
+let Living Room = {
+name: 'Living Room'
+};
+
+let Observatory = {
+name: 'Observatory'
+};
+
+let Theater = {
+name: 'Theater'
+};
+
+let Guest House = {
+name: 'Guest House'
+};
+
+let Patio = {
+name: 'Patio'
+};
+
 // Weapons Array
 
 const weaponsArray = [];
 
+let rope = {
+name: 'rope',
+weight: 10
+};
+
+let knife = {
+name: 'knife',
+weight: 8
+};
+
+let candlestick = {
+name: 'candlestick',
+weight: 2
+};
+
+let dumbbell = {
+name: 'dumbbell',
+weight: 30
+};
+
+let poison = {
+name: 'poison',
+weight: 2
+};
+
+let axe = {
+name: 'axe',
+weight: 15
+};
+
+let bat = {
+name: 'bat',
+weight: 13
+};
+
+let trophy = {
+name: 'trophy',
+weight: 25
+};
+
+let pistol = {
+name: 'pistol',
+weight: 20
+};
 
 // ITERATION 2
 
-function selectRandom() {}
+function selectRandom(array) {
+    return array(Math.floor(Math.random()*array.lenght))
 
-function pickMystery() {}
+function pickMystery() {
+    let mystery = {
+        suspect: selectRandom(suspectsArray),
+        room: selectRandom(roomsArray),
+        weapon: selectRandom(weaponsArray)
+    };
+
+    return mystery
+}
 
 
 // ITERATION 3
 
-function revealMystery() {}
+function revealMystery(envelope) {
 
+let envelope = pickMystery();
+ 
+return $(envelope.firstName) $(envelope.lastName) killed Mr. Boddy using the $(weapon.Name) in the $(room.name)!;
+};
