80ec15952193974c78aef17967d2b148b8f6ce31
Eduardo Suarez <draco_008@hotmail.com>
2023-01-12 19:16:14 -0800

    Actualizacion-clue.js

--- src/clue.js
+++ src/clue.js
@@ -89,165 +89,183 @@ Patio
 
 // PERSONAS / CHARACTER
 
-let character ={
-  character: [{
-    name: 'mrGreen',
+// ITERATION 1
+
+// Suspects Array
+
+const suspectsArray = [
+  {
     firstName: 'Jacob',
     lastName: 'Green',
-    color: 'Green',
-    description: 'He has a lot of connections',
+    occupation: 'Entrepreneur',
     age: 45,
-    image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
-    occupation: 'Entrepreneur'
+    description: 'He has a lot of connections',
+    image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+    color: 'green'
   },
   {
-    name: 'drOrchid',
     firstName: 'Doctor',
     lastName: 'Orchid',
-    color: 'white',
-    description: 'Phd in plant toxicology. Adopted daughter of Mr. Boddy',
+    occupation: 'Scientist',
     age: 26,
-    image:"http://www.radiotimes.com/uploads/images/Original/111967.jpg" ,
-    occupation: 'Scientist'
+    description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
+    image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
+    color: 'white'
   },
   {
-    name: 'profPlum',
     firstName: 'Victor',
     lastName: 'Plum',
-    color: 'purple',
-    description: 'Billionare videogame designer',
+    occupation: 'Designer',
     age: 22,
-    image: "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
-    occupation: 'Designer'
+    description: 'Billionaire video game designer',
+    image: 'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
+    color: 'purple'
   },
   {
-    name: 'profPlum',
-    firstName: 'Victor',
-    lastName: 'Plum',
-    color: 'purple',
-    description: 'Billionare videogame designer',
-    age: 22,
-    image: "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
-    occupation: 'Designer'
-  },
-  {
-    name: 'msScarlett',
-    firstName: 'Kassandra',
+    firstName: 'Kasandra',
     lastName: 'Scarlet',
-    color: 'red',
-    description: 'She is an A-list movie start with a dark past',
+    occupation: 'Actor',
     age: 31,
-    image: "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",
-    occupation: 'Actor'
+    description: 'She is an A-list movie star with a dark past',
+    image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
+    color: 'red'
   },
   {
-    name: 'mrsPeacock',
     firstName: 'Eleanor',
     lastName: 'Peacock',
-    color: 'Blue',
-    description: 'She is from a wealthy family and user her status and money to earn popularity',
+    occupation: 'Socialité',
     age: 36,
-    image:  "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
-    occupation: 'Socialite'
+    description: 'She is from a wealthy family and uses her status and money to earn popularity',
+    image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
+    color: 'blue'
   },
   {
-    name: 'mrMustard',
     firstName: 'Jack',
     lastName: 'Mustard',
-    color: 'yellow',
-    description: 'He is a former fotball player who tries to get by on his former glory',
+    occupation: 'Retired Football player',
     age: 62,
-    image:  "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg" ,
-    occupation: 'Socialite'
+    description: 'He is a former football player who tries to get by on his former glory',
+    image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
+    color:  'yellow'
+  }
+];
+
+// Rooms Array
+
+const roomsArray = [
+  {
+    name: 'Dining Room'
   },
-]
+  {
+    name: 'Conservatory'
+  },
+  {
+    name: 'Kitchen'
+  },
+  {
+    name: 'Study'
+  },
+  {
+    name: 'Library'
+  },
+  {
+    name: 'Billiard Room'
+  },
+  {
+    name: 'Lounge'
+  },
+  {
+    name: 'Ballroom'
+  },
+  {
+    name: 'Hall'
+  },
+  {
+    name: 'Spa'
+  },
+  {
+    name: 'Living Room'
+  },
+  {
+    name: 'Observatory'
+  },
+  {
+    name: 'Theater'
+  },
+  {
+    name: 'Guest House'
+  },
+  {
+    name: 'Patio'
+  },
+];
+
+// Weapons Array
+
+const weaponsArray = [
+  {
+    name: 'rope',
+    weight: 10
+  },
+  {
+    name: 'knife',
+    weight: 8
+  },
+  {
+    name: 'candlestick',
+    weight: 2
+  },
+  {
+    name: 'dumbbell',
+    weight: 30
+  },
+  {
+    name: 'poison',
+    weight: 2
+  },
+  {
+    name: 'axe',
+    weight: 15
+  },
+  {
+    name: 'bat',
+    weight: 13
+  },
+  {
+    name: 'trophy',
+    weight: 25
+  },
+  {
+    name: 'pistol',
+    weight: 20
+  }
+];
+
+// ITERATION 2
+
+function selectRandom(arr) {
+  return arr[Math.floor(Math.random() * arr.length)]
 }
 
-//Armas
 
-let weapons = {weapon:[
-{
-  name: 'rope',
-  weight: 10
-},
-{
-  name : 'knife',
-  weight: 8
-},
-{
-  name: 'candlestick',
-  weight: 2
-},
-{
-  name: 'dumbbell',
-  weight: 30
-},
-{
-  name: 'poison',
-  weight: 2
-},
-{
-  name: 'axe',
-  weight: 15
-},
-{
-  name: 'bat',
-  weight: 13
-},
-{
-  name: 'trophy',
-  weight: 25
-},
-{
-  name: 'pistol',
-  weight: 20
+function pickMystery() {
+  return {
+    suspect: selectRandom(suspectsArray),
+    weapon: selectRandom(weaponsArray),
+    room: selectRandom(roomsArray)
+  }
 }
 
-]}
 
-// Cuartos
+// ITERATION 3
 
-let Rooms = [
-  'Dinnig Room',
-  'Conservatory',
-  'Kitchen',
-  'Study',
-  'Library',
-  'Billiard Room',
-  'Lounge',
-  'Ballroom',
-  'Hall',
-  'Spa',
-  'Living Room',
-  'Observatory',
-  'Theater',
-  'Ghest House',
-  'Patio'
-]
+function revealMystery(mysterious) {
+  let weapon = mysterious.weapon.name
+  let room = mysterious.room.name
 
-function random_selector(array){
-  let randomNumber = parseInt(Math.random() * array.length)
-  let card = array[randomNumber]
-  return card
-}
+  return `${mysterious.suspect.firstName} ${mysterious.suspect.lastName} killed Mr. Boddy using the ${weapon} in the ${room}!`
+};
 
-function pick_mistery(){
-  let mistery = []
-  mistery.push(random_selector(character.character))
-  mistery.push(random_selector(weapons.weapon))
-  mistery.push(random_selector(rooms))
-  return mistery
-}
-
-function reveal_mistery(mistery){
-
-}
-
-//console.log(pick_mistery())
-
-let mistery_envelope = pick_mistery()
-reveal_mistery(mistery_envelope)
-
-console.log(pick_mistery)
+console.log(revealMystery());
 
 
