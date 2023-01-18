49e5ba34ee7b70b82df602bc6f49ca2e179faac7
ArturoRubin <116040655+ArturoRubin@users.noreply.github.com>
2023-01-18 15:45:50 -0600

    lab casi terminado

--- src/clue.js
+++ src/clue.js
@@ -10,7 +10,7 @@ const suspectsArray = [
         occupation: 'Entrepreneur',
         age: '45',
         description: "He has a lot of connections",
-        image: https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg,
+        image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
         color: 'green',
     },
 
@@ -21,18 +21,18 @@ const suspectsArray = [
         occupation: 'Scientist',
         age: '26',
         description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
-        image: http://www.radiotimes.com/uploads/images/Original/111967.jpg
+        image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
         color: 'white',
     },
 
     {
         name: 'profPlum',
-        firstName: V'ictor',
+        firstName: 'Victor',
         lastName: 'Plum',
         occupation: 'Designer',
         age: '22',
         description: "Billionaire video game designer",
-        image: https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg,
+        image: "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg",
         color: 'purple',
     },
 
@@ -41,9 +41,9 @@ const suspectsArray = [
         firstName: 'Kasandra',
         lastName: 'Scarlet',
         occupation: 'Actor',
-        age: 31',
+        age: '31',
         description: "She is an A-list movie star with a dark past",
-        image: https://www.radiotimes.com/uploads/images/Original/111967.jpg,
+        image: "https://www.radiotimes.com/uploads/images/Original/111967.jpg",
         color: 'red',
     },
 
@@ -54,7 +54,7 @@ const suspectsArray = [
         occupation: 'Socialité',
         age: '36',
         description: "She is from a wealthy family and uses her status and money to earn popularity",
-        image: https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg,
+        image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
         color: 'blue',
     },
 
@@ -65,7 +65,7 @@ const suspectsArray = [
         occupation: "Retired Football player",
         age: '62',
         description: "He is a former football player who tries to get by on his former glory",
-        image: https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg,
+        image: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg",
         color: 'yellow',
     },
 ];
@@ -117,9 +117,12 @@ function selectRandom() {
 }
 
 function pickMystery() {
-
+    return{
+        suspect: selectRandom (suspectsArray),
+        room: selectRandom (roomsArray),
+        weapon: selectRandom(weaponsArray)
+    }
 }
-
 
 // ITERATION 3
 
