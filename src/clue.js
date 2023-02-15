2b9fd1a1ea3e0c909617350b33d71895149b5945
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-02-16 00:41:20 +0100

    Finished Function selectRandom

--- src/clue.js
+++ src/clue.js
@@ -162,7 +162,15 @@ loadWeapons(weaponsData);
 
 // ITERATION 2
 
-function selectRandom() {}
+function selectRandom(array) {
+    if(array.length === 0){
+        return undefined;
+       }else if(array.length === 1){
+        return array[0];
+       }
+       return array[Math.floor(Math.random() * array.length)];
+    
+}
 
 function pickMystery() {}
 
