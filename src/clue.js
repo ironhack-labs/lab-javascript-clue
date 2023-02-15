4054bcde550c6298460525282b623871755893fb
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-02-15 23:22:36 +0100

    Added Function to convert and push the Data from suspectData to suspectsArray

--- src/clue.js
+++ src/clue.js
@@ -4,9 +4,110 @@
 
 const suspectsArray = [];
 
+const suspectData = `
+mrGreen
+firstName: Jacob
+lastName: Green
+occupation: Entrepreneur
+age: 45
+description: He has a lot of connections
+image: https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg
+color: green
+
+drOrchid
+firstName: Doctor
+lastName: Orchid
+occupation: Scientist
+age: 26
+description: PhD in plant toxicology. Adopted daughter of Mr. Boddy
+image: http://www.radiotimes.com/uploads/images/Original/111967.jpg
+color: white
+
+profPlum
+firstName: Victor
+lastName: Plum
+occupation: Designer
+age: 22
+description: Billionaire video game designer
+image: https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg
+color: purple
+
+missScarlet
+firstName: Kasandra
+lastName: Scarlet
+occupation: Actor
+age: 31
+description: She is an A-list movie star with a dark past
+image: https://www.radiotimes.com/uploads/images/Original/111967.jpg
+color: red
+
+mrsPeacock
+firstName: Eleanor
+lastName: Peacock
+occupation: Socialité
+age: 36
+description: She is from a wealthy family and uses her status and money to earn popularity
+image: https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg
+color: blue
+
+mrMustard
+firstName: Jack
+lastName: Mustard
+occupation: Retired Football player
+age: 62
+description: He is a former football player who tries to get by on his former glory
+image: https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg
+color: yellow`
+
+function loadSuspects(data) {
+    let plainSuspectArray = data.split("\n\n");
+    plainSuspectArray.forEach((suspect) => {
+      suspectLines = suspect.split("\n");
+      let suspectObj = {};
+      suspectObj.shortName = suspectLines[0];
+      for (let i = 1; i < suspectLines.length; i++) {
+        suspectObj[suspectLines[i].split(": ")[0]] =
+          suspectLines[i].split(": ")[1];
+      }
+      suspectsArray.push(suspectObj);
+    });
+}
+
+loadSuspects(suspectData);
+console.log(suspectsArray);
+
 // Rooms Array
 
 const roomsArray = [];
+
+const roomData = `
+name: rope --- weight: 10
+name: knife --- weight: 8
+name: candlestick --- weight: 2
+name: dumbbell --- weight: 30
+name: poison --- weight: 2
+name: axe --- weight: 15
+name: bat --- weight: 13
+name: trophy --- weight: 25
+name: pistol --- weight: 20
+`;
+
+function loadRoom(data) {
+    let plainSuspectArray = data.split("\n");
+    plainSuspectArray.forEach((suspect) => {
+      suspectLines = suspect.split("\n");
+      let suspectObj = {};
+      suspectObj.shortName = suspectLines[0];
+      for (let i = 1; i < suspectLines.length; i++) {
+        suspectObj[suspectLines[i].split(": ")[0]] =
+          suspectLines[i].split(": ")[1];
+      }
+      roomsArray.push(suspectObj);
+    });
+}
+
+loadRoom(roomData);
+console.log(roomsArray);
 
 // Weapons Array
 
