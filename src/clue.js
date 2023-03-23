acfc9e631e1ae25a079c61bd678141fa856c3dee
Matt N <mneckelmann@gmail.com>
2023-03-23 16:39:25 +0100

    Solved lab

--- src/clue.js
+++ src/clue.js
@@ -4,67 +4,67 @@
 
 const suspectsArray = [
   {
-    firstName: Jacob,
-    lastName: Green,
+    firstName: "Jacob",
+    lastName: "Green",
     occupation: "Entrepreneur",
     age: 45,
     description: "He has a lot of connections",
     image:
       "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
-    color: green,
+    color: "green",
   },
   {
-    firstName: Doctor,
-    lastName: Orchid,
+    firstName: "Doctor",
+    lastName: "Orchid",
     occupation: "Scientist",
     age: 26,
     description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
     image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
-    color: white,
+    color: "white",
   },
 
   {
-    firstName: Victor,
-    lastName: Plum,
+    firstName: "Victor",
+    lastName: "Plum",
     occupation: "Designer",
     age: 22,
     description: "Billionaire video game designer",
     image:
       "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg",
-    color: purple,
+    color: "purple",
   },
 
   {
-    firstName: Kasandra,
-    lastName: Scarlet,
+    firstName: "Kasandra",
+    lastName: "Scarlet",
     occupation: "Actor",
     age: 31,
     description: "She is an A-list movie star with a dark past",
     image: "https://www.radiotimes.com/uploads/images/Original/111967.jpg",
-    color: red,
+    color: "red",
   },
 
   {
-    firstName: Eleanor,
-    lastName: Peacock,
+    firstName: "Eleanor",
+    lastName: "Peacock",
     occupation: "Socialité",
     age: 36,
     description:
       "She is from a wealthy family and uses her status and money to earn popularity",
     image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
-    color: blue,
+    color: "blue",
   },
 
   {
-    firstName: Jack,
-    lastName: Mustard,
+    firstName: "Jack",
+    lastName: "Mustard",
     occupation: "Retired Football player",
     age: 62,
     description:
       "He is a former football player who tries to get by on his former glory",
     image:
       "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg",
-    color: yellow,
+    color: "yellow",
   },
 ];
 
@@ -90,23 +90,43 @@ const roomsArray = [
 
 // Weapons Array
 const weaponsArray = [
-  { name: rope, weight: 10 },
-  { name: knife, weight: 8 },
-  { name: candlestick, weight: 2 },
-  { name: dumbbell, weight: 30 },
-  { name: poison, weight: 2 },
-  { name: axe, weight: 15 },
-  { name: bat, weight: 13 },
-  { name: trophy, weight: 25 },
-  { name: pistol, weight: 20 },
+  { name: "rope", weight: 10 },
+  { name: "knife", weight: 8 },
+  { name: "candlestick", weight: 2 },
+  { name: "dumbbell", weight: 30 },
+  { name: "poison", weight: 2 },
+  { name: "axe", weight: 15 },
+  { name: "bat", weight: 13 },
+  { name: "trophy", weight: 25 },
+  { name: "pistol", weight: 20 },
 ];
 
 // ITERATION 2
 
-function selectRandom() {}
+function selectRandom(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
 
-function pickMystery() {}
+/*
+Declare a function named pickMystery that takes no arguments and returns an object with three properties: suspect, weapon and room, each holding as a value a card of that specific type. You can get a random card of each type by calling selectRandom on each card stack.
+*/
+
+function pickMystery() {
+  let pickedMystery = {
+    suspect: selectRandom(suspectsArray),
+    weapon: selectRandom(weaponsArray),
+    room: selectRandom(roomsArray),
+  };
+  return pickedMystery;
+}
 
 // ITERATION 3
 
-function revealMystery() {}
+function revealMystery(envelope) {
+  let firstName = pickMystery().suspect.firstName;
+  let lastName = pickMystery().suspect.lastName;
+  let weapon = pickMystery().weapon.name;
+  let room = pickMystery().room.name;
+
+  return `${firstName} ${lastName} killed Mr. Boddy using the ${weapon} in the ${room}!`;
+}
