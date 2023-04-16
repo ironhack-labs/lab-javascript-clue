7dd79f1cbf4b5c28a8b9d1d554ea532324679c5e
Astrid <anaguflo@gmail.com>
2023-04-16 15:20:42 +0200

    cluedo

--- src/clue.js
+++ src/clue.js
@@ -2,25 +2,128 @@
 
 // Suspects Array
 
-const suspectsArray = [];
+const suspectsArray = [
+
+    {
+        firstName: "",
+        lastName: "",
+        occupation: "",
+        age: 0,
+        description: "",
+        image: "",
+        color: ""
+    },
+    {
+        firstName: "",
+        lastName: "",
+        occupation: "",
+        age: 0,
+        description: "",
+        image: "",
+        color: ""
+    },
+    {
+        firstName: "",
+        lastName: "",
+        occupation: "",
+        age: 0,
+        description: "",
+        image: "",
+        color: ""
+    },
+    {
+        firstName: "",
+        lastName: "",
+        occupation: "",
+        age: 0,
+        description: "",
+        image: "",
+        color: ""
+    },
+    {
+        firstName: "",
+        lastName: "",
+        occupation: "",
+        age: 0,
+        description: "",
+        image: "",
+        color: ""
+    },
+    {
+        firstName: "",
+        lastName: "",
+        occupation: "",
+        age: 0,
+        description: "",
+        image: "",
+        color: ""
+    }
+];
 
 // Rooms Array
 
-const roomsArray = [];
+const roomsArray = [
+    { name: "" },
+    { name: "" },
+    { name: "" },
+    { name: "" },
+    { name: "" },
+    { name: "" },
+    { name: "" },
+    { name: "" },
+    { name: "" },
+    { name: "" },
+    { name: "" },
+    { name: "" },
+    { name: "" },
+    { name: "" },
+    { name: "" }
+];
 
 // Weapons Array
 
-const weaponsArray = [];
+const weaponsArray = [
+    { name: "", weight: 0 },
+    { name: "", weight: 0 },
+    { name: "", weight: 0 },
+    { name: "", weight: 0 },
+    { name: "", weight: 0 },
+    { name: "", weight: 0 },
+    { name: "", weight: 0 },
+    { name: "", weight: 0 },
+    { name: "", weight: 0 }
+];
 
 
 // ITERATION 2
 
-function selectRandom() {}
+function selectRandom(cardStack) {
+    const randomCard = Math.floor(Math.random() * cardStack.length);
 
-function pickMystery() {}
+    return cardStack[randomCard];
+}
 
+function pickMystery() {
+
+    const suspect = selectRandom(suspectsArray);
+    const weapon = selectRandom(weaponsArray);
+    const room = selectRandom(roomsArray);
+
+    return {
+        suspect: suspect,
+        weapon: weapon,
+        room: room
+    }
+}
 
 // ITERATION 3
 
-function revealMystery() {}
+function revealMystery(assassin) {
 
+    const firstName = assassin.suspect.firstName;
+    const lastName = assassin.suspect.lastName;
+    const weaponName = assassin.weapon.name;
+    const roomName = assassin.room.name;
+
+    return`${firstName} ${lastName} killed Mr. Boddy using the ${weaponName} in the ${roomName}!`;
+}
