0c8cdbd9f6ce3a538296ba00f97951a5d755baea
churrundo <churrundo@gmail.com>
2023-04-19 18:37:35 -0600

    finish iteration 1

--- src/clue.js
+++ src/clue.js
@@ -11,7 +11,8 @@ const suspectsArray = [
         description: "He has a lot of connections",
         image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
         color: "green",
-    },    
+    },
+
     drOrchid = {
         firstName: "Doctor",
         lastName: "Orchid",
@@ -61,11 +62,46 @@ const suspectsArray = [
 
 // Rooms Array
 
-const roomsArray = [];
+const roomsArray = [
+    {name: "Dining Room"},
+    {name: "Conservatory"},
+    {name: "Kitchen"},
+    {name: "Study"},
+    {name: "Library"},
+    {name: "Billiard Room"},
+    {name: "Lounge"},
+    {name: "Ballroom"},
+    {name: "Hall"},
+    {name: "Spa"},
+    {name: "Living Room"},
+    {name: "Observatory"},
+    {name: "Theater"},
+    {name: "Guest House"},
+    {name: "Patio"}
+    ];
 
 // Weapons Array
 
-const weaponsArray = [];
+const weaponsArray = [
+    {name: "rope",
+     weight: 10},
+    {name: "knife",
+     weight: 8},
+    {name: "candlestick",
+     weight: 2},
+    {name: "dumbbell",
+     weight: 30},
+    {name: "poison",
+     weight: 2},
+    {name: "axe",
+     weight: 15},
+    {name: "bat",
+     weight: 13},
+    {name: "trophy",
+     weight: 25},
+    {name: "pistol",
+     weight: 20}
+];
 
 
 // ITERATION 2
