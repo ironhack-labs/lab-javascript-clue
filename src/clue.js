718e21d99223abaf0209e4b067a5bb3af63dde52
Gabriela García <ggarciaborregon@gmail.com>
2023-05-10 20:56:49 +0200

    changes on iteration 1

--- src/clue.js
+++ src/clue.js
@@ -2,7 +2,36 @@
 
 // Suspects Array
 
-const suspectsArray = [];
+const suspectsArray = [
+  {
+    firstName: "Jacob",
+    lastName: "Green",
+    occupation: "Entrepreneur",
+    age: 45,
+    description: "He has a lot of connections",
+    image:
+      "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+    color: "green",
+  },
+  {
+    firstName: "Doctor",
+    lastName: "Orchid",
+    occupation: "Scientist",
+    age: 26,
+    description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+    image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+    color: "white",
+  },
+  {
+    firstName: "Victor",
+    lastName: "Plum",
+    occupation: "Designer",
+    age: 22,
+    description: "Billionaire video game designer",
+    image: "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg",
+    color: "purple",
+  },
+];
 
 // Rooms Array
 
@@ -12,15 +41,12 @@ const roomsArray = [];
 
 const weaponsArray = [];
 
-
 // ITERATION 2
 
 function selectRandom() {}
 
 function pickMystery() {}
 
-
 // ITERATION 3
 
 function revealMystery() {}
-
