adafa74918fe5a1121fc8d71e33205a96b3885ed
Julietadallapozza <dallapozzajulieta@gmail.com>
2023-08-10 14:33:41 +0200

    first commit

--- src/clue.js
+++ src/clue.js
@@ -2,11 +2,60 @@
 
 // Suspects Array
 
-const suspectsArray = [];
-
+const suspectsArray = [
+    {
+        mrGreen: {
+            firstName: 'Jacob',
+            lastName: 'Green',
+            occupation: 'Entrepreneur',
+            age: 45,
+            description: 'He has a lot of connections',
+            image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+            color: 'green'
+        },
+        drOrchid: {
+            firstName: 'Doctor',
+            lastName: 'Orchid',
+            occupation: 'Scientist',
+            age: 26,
+            description: 'He has a lot of connections',
+            image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
+            color: 'white'
+        },
+        profPlum: {
+            firstName: 'Jacob',
+            lastName: 'Green',
+            occupation: 'Entrepreneur',
+            age: 45,
+            description: 'He has a lot of connections',
+            image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+            color: 'green'
+        },
+        mrsPeacock: {
+            firstName: 'Jacob',
+            lastName: 'Green',
+            occupation: 'Entrepreneur',
+            age: 45,
+            description: 'He has a lot of connections',
+            image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+            color: 'green'
+        },
+        mrMustard: {
+            firstName: 'Jacob',
+            lastName: 'Green',
+            occupation: 'Entrepreneur',
+            age: 45,
+            description: 'He has a lot of connections',
+            image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+            color: 'green'
+        }
+    }
+];
 // Rooms Array
 
-const roomsArray = [];
+const roomsArray = [
+
+];
 
 // Weapons Array
 
@@ -15,12 +64,12 @@ const weaponsArray = [];
 
 // ITERATION 2
 
-function selectRandom() {}
+function selectRandom() { }
 
-function pickMystery() {}
+function pickMystery() { }
 
 
 // ITERATION 3
 
-function revealMystery() {}
+function revealMystery() { }
 
