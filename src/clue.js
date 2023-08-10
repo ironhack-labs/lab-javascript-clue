fdf9e2d89a14f92f40f57b69d67a161b81b84817
Julietadallapozza <dallapozzajulieta@gmail.com>
2023-08-10 17:23:07 +0200

    wip

--- src/clue.js
+++ src/clue.js
@@ -60,27 +60,53 @@ const suspectsArray = [
 
 ];
 // Rooms Array
-
-const roomsArray = [{
-
-    name: 'Dining Room',
-    name: 'Conservatory',
-    name: 'Kitchen',
-    name: 'Study',
-    name: 'Library',
-    name: 'Billiard Room',
-    name: 'Lounge',
-    name: 'Ballroom',
-    name: 'Hall',
-    name: 'Spa',
-    name: 'Living Room',
-    name: 'Observatory',
-    name: 'Theater',
-    name: 'Guest House',
-    name: 'Patio'
-}
+const roomsArray = [
+    {
+        name: 'Dining Room'
+    },
+    {
+        name: 'Conservatory'
+    },
+    {
+        name: 'Kitchen'
+    },
+    {
+        name: 'Study'
+    },
+    {
+        name: 'Library'
+    },
+    {
+        name: 'Billiard Room'
+    },
+    {
+        name: 'Lounge'
+    },
+    {
+        name: 'Ballroom'
+    },
+    {
+        name: 'Hall'
+    },
+    {
+        name: 'Spa'
+    },
+    {
+        name: 'Living Room'
+    },
+    {
+        name: 'Observatory'
+    },
+    {
+        name: 'Theater'
+    },
+    {
+        name: 'Guest House'
+    },
+    {
+        name: 'Patio'
+    }
 ];
-
 
 // Weapons Array
 
