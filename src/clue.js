f58c080cb299f2d83f6d686dfa7521bf14cf58a6
Mohamed khamkham <mohamkham1999@gmail.com>
2023-10-15 19:59:29 +0200

    Lab-Clue

--- src/clue.js
+++ src/clue.js
@@ -1,26 +1,159 @@
 // ITERATION 1
+const suspectsArray = [
 
-// Suspects Array
+    {
+        nickName: "mrGreen",
+        firstName: "Jacob",
+        lastName: "Green",
+        occupation: "Entrepreneur",
+        age: 45,
+        description: "He has a lot of connections",
+        image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+        color: "green"
+    },
 
-const suspectsArray = [];
 
-// Rooms Array
 
-const roomsArray = [];
 
-// Weapons Array
+    {
+        nickName: "drOrchid",
+        firstName: "Doctor",
+        lastName: "Orchid",
+        occupation: "Scientist",
+        age: 26,
+        description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+        image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+        color: "white"
+    },
 
-const weaponsArray = [];
+
+    {
+        nickName: "profPlum",
+        firstName: "Victor",
+        lastName: "Plum",
+        occupation: "Designer",
+        age: 22,
+        description: "Billionaire video game designer",
+        image: "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg",
+        color: "purple"
+    },
+
+
+
+
+
+    {
+        nickeName: "missScarlet",
+        firstName: "Kasandra",
+        lastName: "Scarlet",
+        occupation: "Actor",
+        age: 31,
+        description: "She is an A-list movie star with a dark past",
+        image: "https://www.radiotimes.com/uploads/images/Original/111967.jpg",
+        color: "red"
+    },
+
+
+
+
+    {
+        nickeName: "mrsPeacock",
+        firstName: "Eleanor",
+        lastName: "Peacock",
+        occupation: "Socialité",
+        age: 36,
+        description: "She is from a wealthy family and uses her status and money to earn popularity",
+        image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+        color: "blue"
+    },
+
+
+
+    {
+        nickeName: "mrMustard",
+        firstName: "Jack",
+        lastName: "Mustard",
+        occupation: "Retired Football player",
+        age: 62,
+        description: "He is a former football player who tries to get by on his former glory",
+        image: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg",
+        color: "yellow"
+    },
+
+]
+
+
+
+
+
+
+const roomsArray = [
+
+    { name: 'Dining Room' },
+    { name: 'Conservatory' },
+    { name: 'Kitchen' },
+    { name: 'Study' },
+    { name: 'Library' },
+    { name: 'BilliardRoom' },
+    { name: 'Lounge' },
+    { name: 'Ballroom' },
+    { name: 'Hall' },
+    { name: 'Spa' },
+    { name: 'LivingRoom' },
+    { name: 'Observatory' },
+    { name: 'Theater' },
+    { name: 'GuestHouse' },
+    { name: 'Patio' },
+]
+
+const weaponsArray = [
+
+    { name: "rope", weight: 10 },
+    { name: "knife", weight: 8 },
+    { name: "candlestick", weight: 2 },
+    { name: "dumbbell", weight: 30 },
+    { name: "poison", weight: 2 },
+    { name: "axe", weight: 15 },
+    { name: "bat", weight: 13 },
+    { name: "trophy", weight: 25 },
+    { name: "pistol", weight: 20 },
+
+]
 
 
 // ITERATION 2
 
-function selectRandom() {}
+function selectRandom(aleatorio) {
+    if (aleatorio.length === 0) {
+        return undefined
+    }
+    else if (aleatorio.length === 1) {
+        return aleatorio[0]
+    }
+    else {
+        const random = Math.floor(Math.random() * aleatorio.length)
+        return aleatorio[random]
+    }
+}
 
-function pickMystery() {}
+
+
+function pickMystery() {
+    return {
+        suspect: selectRandom(suspectsArray),
+        weapon: selectRandom(weaponsArray),
+        room: selectRandom(roomsArray)
+    }
+}
 
 
 // ITERATION 3
 
-function revealMystery() {}
+function revealMystery(mystery) {
+
+    console.log(`${mystery.FIRSTNAME} ${mystery.LASTNAME} killed Mr. Boddy using the ${mystery.WEAPON} in the ${mystery.ROOM} !`
+    )
+    return `${mystery.FIRSTNAME} ${mystery.LASTNAME} killed Mr. Boddy using the ${mystery.WEAPON} in the ${mystery.ROOM} !`
+
+}
 
