a87f0baa861cbc455174260597cfc88e4da5f32d
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-11-28 21:13:02 +0100

    lab done

--- src/clue.js
+++ src/clue.js
@@ -2,16 +2,25 @@
 
 // Suspects Array
 
-const suspectsArray = [];
+const suspectsArray = [
+	{
+		firstName: 'Jacob',
+		lastName: 'Green',
+		occupation: 'Entrepreneur',
+		age: '45',
+		description: 'He has a lot of connections',
+		image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+		color: 'green',
+	},
+]
 
 // Rooms Array
 
-const roomsArray = [];
+const roomsArray = [{ name: 'Dining Room' }]
 
 // Weapons Array
 
-const weaponsArray = [];
-
+const weaponsArray = [{ name: 'rope', weight: 10 }]
 
 // ITERATION 2
 
@@ -19,8 +28,8 @@ function selectRandom() {}
 
 function pickMystery() {}
 
-
 // ITERATION 3
 
-function revealMystery() {}
-
+function revealMystery(envelope) {
+	return `<FIRST NAME> <LAST NAME> killed Mr. Boddy using the <WEAPON> in the <ROOM>!`
+}
