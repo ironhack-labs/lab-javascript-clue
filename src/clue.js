0268bf083af3977bbd836da504c66c3a87c1db2a
lucasfajersztejn <96478667+lucasfajersztejn@users.noreply.github.com>
2023-12-16 23:01:47 +0100

    Solved lab with changes

--- src/clue.js
+++ src/clue.js
@@ -67,35 +67,35 @@ const suspectsArray = [
 // Rooms Array
 
 const roomsArray = [
-  {'name': 'Dining Room'},
-  {'name': 'Conservatory'},
-  {'name': 'Kitchen'},
-  {'name': 'Study'},
-  {'name': 'Library'},
-  {'name': 'Billiard Room'},
-  {'name': 'Lounge'},
-  {'name': 'Ballroom'},
-  {'name': 'Hall'},
-  {'name': 'Spa'},
-  {'name': 'Living Room'},
-  {'name': 'Observatory'},
-  {'name': 'Theater'},
-  {'name': 'Guest House'},
-  {'name': 'Patio'}
+  {name: 'Dining Room'},
+  {name: 'Conservatory'},
+  {name: 'Kitchen'},
+  {name: 'Study'},
+  {name: 'Library'},
+  {name: 'Billiard Room'},
+  {name: 'Lounge'},
+  {name: 'Ballroom'},
+  {name: 'Hall'},
+  {name: 'Spa'},
+  {name: 'Living Room'},
+  {name: 'Observatory'},
+  {name: 'Theater'},
+  {name: 'Guest House'},
+  {name: 'Patio'}
   ];
   
 // Weapons Array
 
 const weaponsArray = [
-  {'name': 'rope', 'weight': 10},
-  {'name': 'knife', 'weight': 8},
-  {'name': 'candlestick', 'weight': 2},
-  {'name': 'dumbbell', 'weight': 30},
-  {'name': 'poison', 'weight': 2},
-  {'name': 'axe', 'weight': 15},
-  {'name': 'bat', 'weight': 13},
-  {'name': 'trophy', 'weight': 25},
-  {'name': 'pistol', 'weight': 20}
+  {name: 'rope', 'weight': 10},
+  {name: 'knife', 'weight': 8},
+  {name: 'candlestick', 'weight': 2},
+  {name: 'dumbbell', 'weight': 30},
+  {name: 'poison', 'weight': 2},
+  {name: 'axe', 'weight': 15},
+  {name: 'bat', 'weight': 13},
+  {name: 'trophy', 'weight': 25},
+  {name: 'pistol', 'weight': 20}
 ];
 
 
