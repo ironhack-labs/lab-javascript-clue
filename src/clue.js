be7fde63059db7b94969aec51407acf55782c93e
Alejandro Martínez <alejandromartinezvarel@gmail.comgit config --global user.name Alejandro>
2023-12-23 00:32:25 +0100

    feat: iteration 3 - Solved lab

--- src/clue.js
+++ src/clue.js
@@ -131,7 +131,7 @@ function selectRandom(cards) {
 }
 
 function pickMystery() {
-    return mystery = {
+    return {
         suspect: selectRandom(suspectsArray),
         weapon: selectRandom(weaponsArray),
         room: selectRandom(roomsArray)
@@ -141,5 +141,6 @@ function pickMystery() {
 
 // ITERATION 3
 
-function revealMystery() {}
-
+function revealMystery({suspect, weapon, room}) {
+    return `${suspect.firstName} ${suspect.lastName} killed Mr. Boddy using the ${weapon.name} in the ${room.name}!`;
+}
