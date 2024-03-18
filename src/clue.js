f438ed377557872e700ad37a1bb383c8c08b1881
Ricardo Watson <Rickangelodan31@gmail.com>
2024-03-18 08:50:17 +0100

    Ricardo 03 2024

--- src/clue.js
+++ src/clue.js
@@ -2,25 +2,207 @@
 
 // Suspects Array
 
-const suspectsArray = [];
+const suspectsArray = [{
+        name: "mrGreen",
+        firstName: "Jacob",
+        lastName: "Green",
+        age: 45,
+        occupation : "Entrepreneur",
+        description:"He has a lot of connections", 
+        color: "white",
+        image: `https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg`
+        
+    },
+    {
+        name: "drOrchid",
+        firstName: "Doctor",
+        lastName: "Orchid",
+        occupation: "Scientist",
+        age: 26,
+        description:" PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+        image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+        color: "white"
+        
+    },
+    {
+        name: "profPlum",
+        firstName: "Victor",
+        lastName: "Plum",
+        occupation: "Designer",
+        age: 22,
+        description: "Billionaire video game designer",
+        image: "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg",
+        color: "purple"
+        
+    },
+    {
+        name: "missScarlet",
+        firstName: "Kasandra",
+        lastName: "Scarlet",
+        occupation: "Actor",
+        age: 31,
+        description: "She is an A-list movie star with a dark past",
+        image: "https://www.radiotimes.com/uploads/images/Original/111967.jpg",
+        color: "red"
+        
+    },
+    {
+        name: "mrsPeacock",
+        firstName: "Eleanor",
+        lastName: "Peacock",
+        occupation: "Socialité",
+        age: 36,
+        description:" She is from a wealthy family and uses her status and money to earn popularity",
+        image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+        color: "blue"
+        
+    },
+    {
+        name: "mrMustard",
+        firstName: "Jack",
+        lastName: "Mustard",
+        occupation: "Retired Football player",
+        age: 62,
+        description: "He is a former football player who tries to get by on his former glory",
+        image: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg",
+        color: "yellow"
+    },
+
+];
 
 // Rooms Array
 
-const roomsArray = [];
+const roomsArray = [
+    {
+        name: "Dining Room"
+        },
+    
+        {
+        name: "Conservatory"
+        },
+    
+        {
+        name: "Kitchen"
+        },
+    
+        {
+        name: "Study"
+        },
+    
+        {
+        name: "Library"
+        },
+    
+        {
+        name: "Billiard Room"
+        },
+    
+        {
+        name: "Lounge"
+        },
+    
+        {
+        name: "Ballroom"
+        },
+    
+        {
+        name: "Hall"
+        },
+    
+        {
+        name: "Spa"
+        },
+    
+        {
+        name: "Living Room"
+        },
+    
+        {
+        name: "Observatory"
+        },
+    
+        {
+        name: "Theater"
+        },
+    
+        {
+        name: "Guest House"
+        },
+    
+        {
+        name: "Patio"
+        },
+
+];
 
 // Weapons Array
 
-const weaponsArray = [];
+const weaponsArray = [
+    {name: "rope",
+    weight: 10
+    },
+    {
+    name: "knife", 
+    weight: 8
+    },
+    {
+    name: "candlestick", 
+    weight: 2
+    },
+    {
+    name: "dumbbell", 
+    weight: 30
+    },
+    {
+    name: "poison", 
+    weight: 2
+    },
+    {
+    name: "axe",  
+    weight: 15
+    },
+    {
+    name: "bat",  
+    weight: 13
+    },
+    {
+    name: "trophy",  
+    weight: 25
+    },
+    {
+    name: "pistol", 
+    weight: 20
+    },
+];
 
 
 // ITERATION 2
 
-function selectRandom() {}
+function selectRandom (cardStack) {
+    
+    {
+        return cardStack[Math.floor((Math.random()*cardStack.length))];
+      }
+ 
+    
+ 
+}
+console.log(result);
 
-function pickMystery() {}
 
+function pickMystery() {
+
+    return {
+        suspect: selectRandom(suspectsArray),
+        weapon: selectRandom(weaponsArray),
+        room: selectRandom(roomsArray)
+    };
+}
+    
 
 // ITERATION 3
-
-function revealMystery() {}
+const mystery = pickMystery ();
+function revealMystery(mystery) {
+    return `${mystery.suspect.firstName} ${mystery.suspect.lastName} killed Mr. Boddy using the ${mystery.weapon.name} in the ${mystery.room.name}!`;
+}
 
